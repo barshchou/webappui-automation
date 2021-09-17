5d1255becb061ba02cebafc24b30d149961a9fae
Uladzislau Samykou <vlad.samykou@boweryvaluation.com>
2021-09-17 12:35:34 +0300

    added login by api request

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,15 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("loginByApi", () => {
+    cy.log("Loggin in by api")
+    cy.request({
+        method: "POST",
+        url: "https://bowery-staging.herokuapp.com/user/login",
+        body: {
+            username: Cypress.env("USERNAME"),
+            password: Cypress.env("PASSWORD")
+        },
+    }).then((response) => {
+        const token = response.body.token
+        window.localStorage.setItem("jwToken", token)
+        cy.visit("/")
+    })
+})
