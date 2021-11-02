9ff8f46dd829efe83c4cb90c95ae8c47191f462d
Uladzislau Samykou <vlad.samykou@boweryvaluation.com>
2021-11-02 13:38:19 +0300

    Utilities page done

--- cypress/actions/property/utilities.actions.js
+++ cypress/actions/property/utilities.actions.js
@@ -24,6 +24,94 @@ class UtilitiesActions extends BaseActions{
     checkCoolingSystem() {
         utilitiesPage.coolingSystemCheckbox.check().should("have.value", "true");
     }
+
+    selectCoolingSystemType(typeValue, systemNumber = 0) {
+        utilitiesPage.getCoolingSystemTypeDropdownBySystemNumber(systemNumber).click();
+        utilitiesPage.getDropdownOptionByValue(typeValue).click();
+    }
+
+    selectCoolingSystemLocation(locationValue, systemNumber = 0) {
+        utilitiesPage.getCoolingSystemLocationDropdownBySystemNumber(systemNumber).click();
+        utilitiesPage.getDropdownOptionByValue(locationValue).click();
+    }
+
+    addCoolingSystemParameters(typeValue, locationValue, systemNumber = 0) {
+        this.selectCoolingSystemType(typeValue, systemNumber);
+        this.selectCoolingSystemLocation(locationValue, systemNumber);
+    }
+
+    verifyHeatingCoolingCommentary(commentaryToBe) {
+        utilitiesPage.heatingCoolingCommentary.should("contain.text", commentaryToBe);
+    }
+
+    checkGasMeters() {
+        utilitiesPage.gasMetersCheckbox.check().should("have.value", "true");
+    }
+
+    selectGasMetersType(typeValue) {
+        utilitiesPage.gasMetersTypeDropdown.click();
+        utilitiesPage.getDropdownOptionByValue(typeValue).click();
+    }
+
+    selectGasMetersLocation(locationValue) {
+        utilitiesPage.gasMetersLocationDropdown.click();
+        utilitiesPage.getDropdownOptionByValue(locationValue).click();
+    }
+
+    addGasMetersParameters(typeValue, locationValue) {
+        this.selectGasMetersType(typeValue);
+        this.selectGasMetersLocation(locationValue);
+    }
+
+    verifyGasMetersCommentary(commentaryToBe) {
+        utilitiesPage.gasMetersCommentary.should("contain.text", commentaryToBe);
+    }
+
+    checkElectricMetersCheckbox() {
+        utilitiesPage.electricMetersCheckbox.check().should("have.value", "true");
+    }
+
+    selectElectricMetersType(typeValue) {
+        utilitiesPage.electricMetersTypeDrop.click();
+        utilitiesPage.getDropdownOptionByValue(typeValue).click();
+    }
+
+    selectElectricMetersLocation(locationValue) {
+        utilitiesPage.electricMetersLocationDrop.click();
+        utilitiesPage.getDropdownOptionByValue(locationValue).click();
+    }
+
+    addElectricMetersParameters(typeValue, locationValue) {
+        this.selectElectricMetersType(typeValue);
+        this.selectElectricMetersLocation(locationValue);
+    }
+
+    verifyElectricMetersCommentary(commentaryToBe) {
+        utilitiesPage.electricMetersCommentary.should("contain.text", commentaryToBe);
+    }
+
+    checkHotWaterSystemsCheckbox() {
+        utilitiesPage.hotWaterSystemsCheckbox.check().should("have.value", "true");
+    }
+
+    selectHotWaterSystemTypeBySystemNumber(typeValue, systemNumber = 0) {
+        utilitiesPage.getHotWaterSystemTypeDropBySystemNumber(systemNumber).click();
+        utilitiesPage.getDropdownOptionByValue(typeValue).click();
+    }
+
+    selectHotWaterSystemLocationBySystemNumber(locationValue, systemNumber = 0) {
+        utilitiesPage.getHotWaterSystemLocationDropBySystemNumber(systemNumber).click();
+        utilitiesPage.getDropdownOptionByValue(locationValue).click();
+    }
+
+    addHotWaterSystemParameters(typeValue, locationValue, systemNumber = 0) {
+        this.selectHotWaterSystemTypeBySystemNumber(typeValue, systemNumber);
+        this.selectHotWaterSystemLocationBySystemNumber(locationValue, systemNumber);
+    }
+
+    verifyHotWaterSystemCommentary(commentaryToBe) {
+        utilitiesPage.hotWaterSystemCommentary.should("contain.text", commentaryToBe);
+    }
 }
 
 export default new UtilitiesActions();
--- cypress/pages/property/utilities.page.js
+++ cypress/pages/property/utilities.page.js
@@ -14,6 +14,35 @@ class UtilitiesPage extends BasePage{
     }
 
     get coolingSystemCheckbox() {return cy.get("*[data-qa*='coolingSystems.inspected'] input");}
+
+    getCoolingSystemTypeDropdownBySystemNumber(number) {
+        return cy.get(`*[data-qa='coolingSystems.systems.${number}.type-select-list'] [data-qa='select-value']`);
+    }
+
+    getCoolingSystemLocationDropdownBySystemNumber(number) {
+        return cy.get(`*[data-qa='coolingSystems.systems.${number}.location-select-list'] [data-qa='select-value']`);
+    }
+
+    get heatingCoolingCommentary() {return cy.get("*[data-qa='heatingAndCoolingSystemsDiscussion.commentary-generated-text']");}
+    get gasMetersCheckbox() {return cy.get("*[data-qa*='gasMeters.inspected'] input");}
+    get gasMetersTypeDropdown() {return cy.get("*[data-qa='gasMeters.type-select-list'] [data-qa='select-value']");}
+    get gasMetersLocationDropdown() {return cy.get("*[data-qa='gasMeters.location-select-list'] [data-qa='select-value']");}
+    get gasMetersCommentary() {return cy.get("*[data-qa='gasMetersDiscussion.commentary-generated-text']");}
+    get electricMetersCheckbox() {return cy.get("*[data-qa*='electricMeters.inspected'] input");}
+    get electricMetersTypeDrop() {return cy.get("*[data-qa='electricMeters.type-select-list'] [data-qa='select-value']");}
+    get electricMetersLocationDrop() {return cy.get("*[data-qa='electricMeters.location-select-list'] [data-qa='select-value']");}
+    get electricMetersCommentary() {return cy.get("*[data-qa='electricMetersDiscussion.commentary-generated-text']");}
+    get hotWaterSystemsCheckbox() {return cy.get("*[data-qa='hotWaterSystems.inspected'] input");}
+
+    getHotWaterSystemTypeDropBySystemNumber(number) {
+        return cy.get(`*[data-qa='hotWaterSystems.systems.${number}.type-select-list'] [data-qa='select-value']`);
+    }
+
+    getHotWaterSystemLocationDropBySystemNumber(number) {
+        return cy.get(`*[data-qa='hotWaterSystems.systems.${number}.location-select-list'] [data-qa='select-value']`);
+    }
+
+    get hotWaterSystemCommentary() {return cy.get("*[data-qa='hotWaterSystemsDiscussion.commentary-generated-text']");}
 }
 
 export default new UtilitiesPage();
