552a710689c30dd0ddf0fdbeb06d3d89b8661747
Uladzislau Samykou <vlad.samykou@boweryvaluation.com>
2021-11-24 07:29:53 +0300

    Comparable expenses page started

--- cypress/actions/income/comparableExpenses.actions.js
+++ cypress/actions/income/comparableExpenses.actions.js
@@ -1,5 +1,6 @@
 import BaseActions from "../base/base.actions";
 import compExpensesPage from "../../pages/income/comparableExpenses.page";
+import {numberWithCommas} from "../../../utils/numbers.utils";
 
 class ComparableExpensesActions extends BaseActions {
     clickAddBlankColumnButton() {
@@ -7,11 +8,11 @@ class ComparableExpensesActions extends BaseActions {
     }
 
     enterAddressByColumnIndex(address, index = 0) {
-        compExpensesPage.compAddressCells.eq(index).type(address).should("have.value", address);
+        compExpensesPage.compAddressCells.eq(index).clear().type(address).should("have.value", address);
     }
 
     enterLocationByColumnIndex(location, index = 0) {
-        compExpensesPage.compLocationCells.eq(index).type(location).should("have.value", location);
+        compExpensesPage.compLocationCells.eq(index).clear().type(location).should("have.value", location);
     }
 
     chooseExpensePeriodByColumnIndex(periodValue, index = 0) {
@@ -20,7 +21,34 @@ class ComparableExpensesActions extends BaseActions {
     }
 
     enterExpenseYearByColumnIndex(year, index = 0) {
-        compExpensesPage.expenseYearCells.eq(index).type(year).should("have.value", year);
+        compExpensesPage.expenseYearCells.eq(index).clear().type(year).should("have.value", year);
+    }
+
+    enterExpenseMonthByColumnIndex(month, index = 0) {
+        compExpensesPage.expenseMonthCells.eq(index).clear().type(month).type("{enter}").should("have.value", month);
+    }
+
+    enterSquareFeetByColumnIndex(value, index = 0) {
+        compExpensesPage.squareFeetCells.eq(index).clear().type(value).should("have.value", value);
+    }
+
+    enterResidentialUnitsByColumnIndex(value, index = 0) {
+        compExpensesPage.residentialUnitsCells.eq(index).clear().type(value).should("have.value", value);
+    }
+
+    enterInsuranceByColumnIndex(value, index = 0) {
+        const valueToBe = `$${numberWithCommas(value)}`;
+        compExpensesPage.insuranceCells.eq(index).clear().type(value).should("have.value", valueToBe);
+    }
+
+    enterElectricityByColumnIndex(value, index = 0) {
+        const valueToBe = `$${numberWithCommas(value)}`;
+        compExpensesPage.electricityCells.eq(index).clear().type(value).should("have.value", valueToBe);
+    }
+
+    enterRepairsMaintenanceByColumnIndex(value, index = 0) {
+        const valueToBe = `$${numberWithCommas(value)}`;
+        compExpensesPage.repairsCells.eq(index).clear().type(value).should("have.value", valueToBe);
     }
 
 }
--- cypress/pages/income/comparableExpenses.page.js
+++ cypress/pages/income/comparableExpenses.page.js
@@ -7,7 +7,12 @@ class ComparableExpensesPage extends BasePage {
     get expensePeriodDropdowns() {return cy.get("[data-qa$=expensePeriod-form-control] [data-qa=select-value]");}
     getDropdownOptionByValue(value) {return cy.get(`li[data-value='${value}']`);}
     get expenseYearCells() {return cy.get("[name$=expenseYear]");}
-
+    get expenseMonthCells() {return cy.get("[name$=expenseMonth]");}
+    get squareFeetCells() {return cy.get("[name$=squareFeet]");}
+    get residentialUnitsCells() {return cy.get("[name$=residentialUnits]");}
+    get insuranceCells() {return cy.get("[name$='insurance.total']");}
+    get electricityCells() {return cy.get("[name$='electricity.total']");}
+    get repairsCells() {return cy.get("[name$='repairsAndMaintenance.total']");}
 }
 
 export default new ComparableExpensesPage();
