d82fafd48de8856179d7f20bf771494e817a5cf3
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2021-12-24 12:19:20 +0300

    Changed variable name

--- cypress/integration/not_full_reports/income/residential/in_place_rent_roll/grid.spec.js
+++ cypress/integration/not_full_reports/income/residential/in_place_rent_roll/grid.spec.js
@@ -1,4 +1,4 @@
-import newTestData from "../../../../../fixtures/gridResInPlaceRentRoll.fixtures";
+import testData from "../../../../../fixtures/gridResInPlaceRentRoll.fixtures";
 import homepageActions from "../../../../../actions/base/homepage.actions";
 import navigationSection from "../../../../../actions/base/navigationSection.actions";
 import rentRollActions from "../../../../../actions/income/residential/rentRoll.actions";
@@ -8,7 +8,7 @@ import summaryActions from "../../../../../actions/property/summary.actions";
 describe("In-Place Rent Roll grid tests", () => {
     before("Login and open In-Place Rent Roll", () => {
         cy.login();
-        homepageActions.createReport(newTestData.reportCreationData);
+        homepageActions.createReport(testData.reportCreationData);
         navigationSection.navigateToInPlaceRentRoll();
         cy.saveLocalStorage();
     });
@@ -18,36 +18,36 @@ describe("In-Place Rent Roll grid tests", () => {
     });
 
     it("ID17 and ID18: GRID and #col.", () => {
-        rentRollActions.verifyColumnExist(newTestData.commonData.sharpColumn);
+        rentRollActions.verifyColumnExist(testData.commonData.sharpColumn);
     });
 
     it(`ID36: Save button, Navigate to other page without saving / with saving the page on the ‘You have unsaved changes.
     Would you like to save before continuing?’ modal window`, () => {
-        rentRollActions.checkCheckboxByLabel(newTestData.commonData.forecastLabel)
+        rentRollActions.checkCheckboxByLabel(testData.commonData.forecastLabel)
             .goToPropSummaryWithSaveSaveClickFirst();
         summaryActions.verifyThatPageIsOpened()
             .goBackWithSave();
-        rentRollActions.verifyCheckboxIsChecked(newTestData.commonData.forecastLabel)
-            .uncheckCheckboxByLabel(newTestData.commonData.forecastLabel)
+        rentRollActions.verifyCheckboxIsChecked(testData.commonData.forecastLabel)
+            .uncheckCheckboxByLabel(testData.commonData.forecastLabel)
             .goToPropSummaryWithoutSave();
         summaryActions.verifyThatPageIsOpened()
             .goBackWithSave();
-        rentRollActions.verifyCheckboxIsChecked(newTestData.commonData.forecastLabel)
-            .uncheckCheckboxByLabel(newTestData.commonData.forecastLabel);
+        rentRollActions.verifyCheckboxIsChecked(testData.commonData.forecastLabel)
+            .uncheckCheckboxByLabel(testData.commonData.forecastLabel);
     });
 
     it("ID37: Save & Continue button", () => {
-        rentRollActions.checkCheckboxByLabel(newTestData.commonData.forecastLabel)
+        rentRollActions.checkCheckboxByLabel(testData.commonData.forecastLabel)
             .clickSaveContinueButton();
         unitGroupsActions.verifyThatPageIsOpened()
             .goBack();
-        rentRollActions.uncheckCheckboxByLabel(newTestData.commonData.forecastLabel);
+        rentRollActions.uncheckCheckboxByLabel(testData.commonData.forecastLabel);
     });
 
 
     after("Delete report", () => {
         cy.restoreLocalStorage();
         rentRollActions.returnToHomePage();
-        homepageActions.deleteReport(newTestData.reportCreationData.reportNumber);
+        homepageActions.deleteReport(testData.reportCreationData.reportNumber);
     });
 });
