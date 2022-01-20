87c2af966a6195c1e1c40fbeb6fb4a4b6c0f1d98
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-01-20 18:37:04 +0300

    QA-4409&10 started

--- cypress/fixtures/not_full_reports/income/commercial/in_place_rent_roll/QA-4409&10.fixture.js
+++ cypress/fixtures/not_full_reports/income/commercial/in_place_rent_roll/QA-4409&10.fixture.js
@@ -3,7 +3,7 @@ import Enums from "../../../../../enums/enums";
 const reportCreationFixture = () => {
     return {
         address: "462 1st Avenue, New York, USA",
-        reportNumber: "TestAutoReport-QA4413&14",
+        reportNumber: "TestAutoReport-QA4409&10",
         templateValue: Enums.TEMPLATE_TYPE.FREDDIE_MAC,
         incomeValue: Enums.INCOME_TYPE.BOTH,
         conclusionValue: Enums.VALUE_CONCLUSION_TYPE.AS_IS
@@ -12,5 +12,5 @@ const reportCreationFixture = () => {
 
 export default {
     reportCreationData: Object.freeze(reportCreationFixture()),
-    editedCommentary: "some new commentary to add"
+    newCommentary: "some new commentary to add"
 };
---
+++ cypress/integration/not_full_reports/income/commercial/in_place_rent_roll/QA-4409&10.spec.js
@@ -0,0 +1,18 @@
+import testData from "../../../../../fixtures/not_full_reports/income/commercial/in_place_rent_roll/QA-4409&10.fixture";
+import Homepage from "../../../../../actions/base/homepage.actions";
+import NavigationSection from "../../../../../actions/base/navigationSection.actions";
+import Income from "../../../../../actions/income/income.manager";
+
+
+describe("Verify the functionality of Edit and Cancel commentary buttons", () => {
+
+    before("Login, create report", () => {
+        cy.login();
+        Homepage.createReport(testData.reportCreationData);
+    });
+
+    it("Test body", () => {
+        NavigationSection.navigateToCommercialInPlaceRentRoll();
+        Income.Commercial.InPlaceRentRoll.clickEditDiscussionButton();
+    });
+});
