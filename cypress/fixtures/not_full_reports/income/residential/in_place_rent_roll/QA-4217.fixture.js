8f0925e129d18fb2197ad805773eb7ad0b0db4c1
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-02-03 15:00:05 +0300

    QA-4217 done

---
+++ cypress/fixtures/not_full_reports/income/residential/in_place_rent_roll/QA-4217.fixture.js
@@ -0,0 +1,9 @@
+import ReportDataCreator from "../../../../data_creator/reportData.creator";
+
+export default {
+    reportCreationData: ReportDataCreator.getDefaultReportData("4217"),
+    unitTypeLabelColumn: "Unit Type",
+    bathColumn: "# Bathrooms",
+    bathLabel: "Bathrooms",
+    outdoorLabelAndColumn: "Outdoor Space"
+};
