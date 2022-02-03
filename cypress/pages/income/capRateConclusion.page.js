fc1252b10f70034a349a0b7cd5fab4067d395456
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-02-03 11:17:21 +0300

    Done refactor of some locators

--- cypress/pages/income/capRateConclusion.page.js
+++ cypress/pages/income/capRateConclusion.page.js
@@ -19,7 +19,7 @@ class CapRateConclusionPage extends BasePage {
     get asCompletePeriodCell() {return cy.get("[data-qa=as-complete-period-cell]");}
     get asCompleteAmountCell() {return cy.get("[data-qa=as-complete-amount-cell]");}
     get asCompleteFinalValueCell() {return cy.get("[data-qa=as-complete-final-value-cell]");}
-    get asCompleteLessEntrepreneurialProfit() {return cy.get("[name^=asCompleteLossItems][name$=amount]").last();}
+    get asCompleteLessEntrepreneurialProfit() {return cy.get("[data-qa*='asCompleteLossItems.entrepreneurialProfit'] input[inputmode]");}
     get asIsMarketPeriodCell() {return cy.get("[data-qa=as-is-market-period-cell]");}
     get asIsMarketAmountCell() {return cy.get("[data-qa=as-is-market-amount-cell]");}
     get asIsMarketFinalValueCell() {return cy.get("[data-qa=as-is-market-final-value-cell]");}
@@ -30,7 +30,7 @@ class CapRateConclusionPage extends BasePage {
     get asStabilizedRentLossSwitch() {return cy.get("[data-qa='As Stabilized-switch-button']");}
     get addButton() {return cy.xpath("//button[.='Add']");}
     get asStabResRentLossTimePeriodCells() {return cy.get("[name^=asStabilizedResRentLossItems][name$=months]");}
-    get asCompleteLessBuyoutCost() {return cy.xpath("(//input[starts-with(@name, 'asCompleteLossItems')])[last() - 1]");}
+    get asCompleteLessBuyoutCost() {return cy.get("[data-qa*='asCompleteLossItems.buyoutCost'] input[inputmode]");}
 }
 
 export default new CapRateConclusionPage();
