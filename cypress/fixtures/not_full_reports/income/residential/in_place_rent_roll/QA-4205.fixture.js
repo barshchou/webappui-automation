6c51f0bb9a1992e4eede97b8ed8e14795cde4185
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-02-03 16:22:00 +0300

    QA-4205 done

---
+++ cypress/fixtures/not_full_reports/income/residential/in_place_rent_roll/QA-4205.fixture.js
@@ -0,0 +1,8 @@
+import ReportDataCreator from "../../../../data_creator/reportData.creator";
+
+export default {
+    reportCreationData: ReportDataCreator.getDefaultReportData("4205"),
+    perRoomAnalysis: "Include Per Room Analysis in Report",
+    devForecast: "Developer's Forecast",
+    summarizeRentRoll: "Summarize current rent roll"
+};
