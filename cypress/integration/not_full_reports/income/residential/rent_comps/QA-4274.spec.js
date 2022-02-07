e926bccb451a7a030de229d92b252bf424324603
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-02-07 17:35:56 +0300

    QA-4274 done

---
+++ cypress/integration/not_full_reports/income/residential/rent_comps/QA-4274.spec.js
@@ -0,0 +1,22 @@
+import testData from "../../../../../fixtures/not_full_reports/income/residential/rent_comps/QA-4274.fixture";
+import {createReport, deleteReport} from "../../../../../actions/base/baseTest.actions";
+import NavigationSection from "../../../../../actions/base/navigationSection.actions";
+import Income from "../../../../../actions/income/income.manager";
+
+describe("Verify Amenities drop-down field in the Map filter section on Rent Comps page", () => {
+    before("Login, create report", () => {
+        createReport(testData.reportCreationData);
+    });
+
+    it("Test body", () => {
+        NavigationSection.navigateToRentComps();
+        Income.Residential.RentComps.BaseActions.clickAmenitiesArrow()
+            .checkListOfCheckboxesByQa(testData.amenitiesQaAttr)
+            .clickUnitTypesArrowButton()
+            .clickUnitTypesArrowButton()
+            .clickAmenitiesArrow()
+            .uncheckListOfCheckboxesByQa(testData.amenitiesQaAttr)
+            .clickAmenitiesArrow();
+        deleteReport(testData.reportCreationData.reportNumber);
+    });
+});
