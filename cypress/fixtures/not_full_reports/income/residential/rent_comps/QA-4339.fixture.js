5f18b5f011ca14b759ea7ac50b2f57c3debfaada
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-02-07 11:11:58 +0300

    QA-4339 done

---
+++ cypress/fixtures/not_full_reports/income/residential/rent_comps/QA-4339.fixture.js
@@ -0,0 +1,24 @@
+import ReportDataCreator from "../../../../data_creator/reportData.creator";
+
+const formDataFixture = () => {
+    return {
+        address: "230 Park Avenue, New York, NY 10169",
+        unitNumber: "someTest196Numb",
+        unitType: "Typical",
+        monthly: 3000,
+        squareFootage: 95,
+        infoSource: "bowerySubject",
+        bedrooms: 5,
+        rooms: 7,
+        amenitiesQaAttr: ["none", "backyard", "balcony", "roof", "terrace", "buildingLaundry", "unitLaundry"],
+        sourceInfoCheck: "Bowery Subject"
+    };
+};
+
+export default {
+    reportCreationData: ReportDataCreator.getDefaultReportData("4339"),
+    numberOfUnits: 2,
+    rentType: "Market Rate",
+    numberOfBedrooms: 5,
+    formData: Object.freeze(formDataFixture())
+};
---
+++ cypress/integration/not_full_reports/income/residential/rent_comps/QA-4339.spec.js
@@ -0,0 +1,38 @@
+import testData from "../../../../../fixtures/not_full_reports/income/residential/rent_comps/QA-4339.fixture";
+import {createReport, deleteReport} from "../../../../../actions/base/baseTest.actions";
+import NavigationSection from "../../../../../actions/base/navigationSection.actions";
+import Income from "../../../../../actions/income/income.manager";
+import Property from "../../../../../actions/property/property.manager";
+
+describe("Verify clicking Add Unit button on Add New Rent Comp overlay is adding the unit to the appropriate " +
+    "group of comparable units on the Rent Comps page (Unit type of search is selected)", () => {
+    before("Login, create report", () => {
+        createReport(testData.reportCreationData);
+    });
+
+    it("Test body", () => {
+        NavigationSection.navigateToPropertySummary();
+        Property.Summary.enterNumberOfResUnits(testData.numberOfUnits);
+        NavigationSection.navigateToResInPlaceRentRoll();
+        Income.Residential.InPlaceRentRoll.fillAllRentTypeCellsWithEqualValue(testData.rentType)
+            .enterBedroomsNumberByRowNumber(testData.numberOfBedrooms);
+        NavigationSection.navigateToRentComps();
+        Income.Residential.RentComps.BaseActions.verifyUnitSelected()
+            .openAddNewComparableForm(testData.formData.address);
+        Income.Residential.RentComps.AddForm.enterUnitNumber(testData.formData.unitNumber)
+            .selectUnitTypeAndVerify(testData.formData.unitType)
+            .enterMonthlyRent(testData.formData.monthly)
+            .enterDate()
+            .enterSquareFootage(testData.formData.squareFootage)
+            .selectSourceOfInfoAndVerify(testData.formData.infoSource)
+            .enterNumberOfBedrooms(testData.formData.bedrooms)
+            .enterNumberOfRooms(testData.formData.rooms)
+            .enterNumberOfBathrooms()
+            .clickUnitAmenitiesDropdown()
+            .checkListOfCheckboxesByQa(testData.formData.amenitiesQaAttr)
+            .clickUnitAmenitiesDropdown()
+            .clickSubmitCompButton();
+        Income.Residential.RentComps.BaseActions.verifyComparableBedroomTableByNumber(0, testData.formData);
+        deleteReport(testData.reportCreationData.reportNumber);
+    });
+});
