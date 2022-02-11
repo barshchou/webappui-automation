968183b280d8e752d78640ab227ca5d9fa72a84e
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-02-11 16:58:51 +0300

    QA-4645 done

--- cypress/pages/report/client.page.js
+++ cypress/pages/report/client.page.js
@@ -4,6 +4,12 @@ class ClientPage extends BasePage{
     get clientNameField() {return cy.get("*[name=client]");}
     get addClientButton() {return cy.xpath("//a[.='Add']");}
     get appraiserCommentary() {return cy.get("[name='clientGuidelinesDiscussion.additionalCommentary']");}
+    get guidelinesTooltip() {return cy.get("[title='The following generated commentary will appear in the Letter " +
+        "of Transmittal of your report.'] svg");}
+    get clientGuidelinesCommentary() {return cy.get("[data-qa^='clientGuidelinesDiscussion.commentary']");}
+    get guidelinesCommentaryEditButton() {return cy.get("[data-qa=generated-commentary-edit-btn]");}
+    get guidelinesCommentaryInput() {return cy.get("[name='clientGuidelinesDiscussion.commentary']");}
+    get revertToGeneratedButton() {return cy.get("[data-qa=generated-commentary-revert-btn]");}
 }
 
 export default new ClientPage();
