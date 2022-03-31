627205dd5e1183f6315f4992941128a3eda4b5a4
Ernst-D <ernst.dzerevanko@gmail.com>
2022-03-31 12:56:17 +0400

    Revert "[hotfix] edited env check logic"
    This reverts commit 763e2e91f27de452ddc0d25a990a5b0691a2640c.

--- utils/env.utils.ts
+++ utils/env.utils.ts
@@ -1,7 +1,7 @@
 import {cutLastLetter} from "./string.utils";
 import Enums from "../cypress/enums/enums";
 
-export const getEnvUrl = (): string => {
+export const getEnvUrl = () => {
     let envUrl;
     switch (Cypress.env("url")) {
         case "dev":
@@ -11,15 +11,14 @@ export const getEnvUrl = (): string => {
             envUrl = Enums.ENV_URLS.PROD;
             break;
         case "custom":
-            // if (isCorrectCustomEnv(Cypress.env("customEnv"))) {
-            //     envUrl = Cypress.env("customEnv");
-            //     if (!isCorrectLink(envUrl)) {
-            //         envUrl = cutLastLetter(envUrl);
-            //     }
-            // } else {
-            //     throw new Error("You haven't entered custom environment url!");
-            // }
-            envUrl = Cypress.env("customEnv");
+            if (isCorrectCustomEnv(Cypress.env("customEnv"))) {
+                envUrl = Cypress.env("customEnv");
+                if (!isCorrectLink(envUrl)) {
+                    envUrl = cutLastLetter(envUrl);
+                }
+            } else {
+                throw new Error("You haven't entered custom environment url!");
+            }
             break;
         default:
             envUrl = Enums.ENV_URLS.STAGING;
@@ -31,7 +30,7 @@ const isCorrectLink = (link) => {
     return !link.endsWith("/");
 };
 
-const isCorrectCustomEnv = (customEnvUrl: string) => {
+const isCorrectCustomEnv = (customEnvUrl) => {
     if (customEnvUrl === "" || customEnvUrl === undefined || customEnvUrl === null) {
         return false;
     } else {
