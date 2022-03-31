9482c8c50b56ae02659c802b728f8b29fbbdb993
Ernst-D <ernst.dzerevanko@gmail.com>
2022-03-31 19:48:47 +0400

    [QA-5006] fixed locators in NaviagtionSection and skipping some action

--- cypress/pages/base/navigationSection.page.ts
+++ cypress/pages/base/navigationSection.page.ts
@@ -3,16 +3,16 @@ import BasePage from "./base.page";
 class NavigationSection extends BasePage {
     get reportInfoButton() { return cy.get('[id="report-information"]')};
 
-    get incomeApproachButton() {return cy.get("#income-approach > svg");}
-    get residentialIncomeArrow() {return cy.get("#residentialIncome > svg");}
+    get incomeApproachButton() {return cy.get("#income-approach svg");}
+    get residentialIncomeArrow() {return cy.get("#residentialIncome svg");}
     get inPlaceRentRollButton() {return cy.get("#residential-rent-roll");}
     get rentCompsButton() {return cy.get("#residential-rent-comps");}
-    get commercialIncomeArrow() {return cy.get("#commercialIncome > svg");}
+    get commercialIncomeArrow() {return cy.get("#commercialIncome svg");}
     get commercialRentRollButton() {return cy.get("#commercial-rent-roll");}
     get commercialStabRentRollButton() {return cy.get("#commercial-projected-rent-roll");}
-    get finalButton() {return cy.get("#final > svg");}
+    get finalButton() {return cy.get("#final svg");}
     get unitInspectionButton() {return cy.get("#unit-inspection");}
-    get propertyButton() {return cy.get("#property-information > svg");}
+    get propertyButton() {return cy.get("#property-information svg");}
     get commercialUnitsButton() {return cy.get("#commercial-units");}
     get summaryButton() {return cy.get("#property-summary");}
     get clientButton() {return cy.get("#client");}
