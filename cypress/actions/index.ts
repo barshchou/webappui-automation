4bea6aa5a2a8c29aab6b0d751c05a579ac846d1b
Ernst-D <ernst.dzerevanko@gmail.com>
2022-04-05 17:20:49 +0400

    [QA-4870] added Final actions to actions module

--- cypress/actions/index.ts
+++ cypress/actions/index.ts
@@ -1,5 +1,7 @@
 import * as _BaseActions from "./base";
 import * as _IncomeActions from "./income";
+import * as _FinalActions from "./final";
 
 export const Base = _BaseActions;
 export const Income = _IncomeActions;
+export const Final = _FinalActions;
