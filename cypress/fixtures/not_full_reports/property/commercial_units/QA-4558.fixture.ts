7f5856166b1d3b6682c020cf47e6a17ec86cf096
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-04-14 14:02:29 +0300

    Spec and feature files added

---
+++ cypress/fixtures/not_full_reports/property/commercial_units/QA-4558.fixture.ts
@@ -0,0 +1,11 @@
+import ReportDataCreator from "../../../data_creator/reportData.creator";
+import Enums from "../../../../enums/enums";
+
+export default {
+    reportCreationData: ReportDataCreator.getReportData("4558", {
+        incomeValue: Enums.INCOME_TYPE.BOTH,
+        conclusionValue: Enums.VALUE_CONCLUSION_TYPE.AS_IS
+    }),
+
+
+};
---
+++ cypress/integration/not_full_reports/property/commercial_units/QA-4558.spec.ts
@@ -0,0 +1,5 @@
+import testData from "../../../../fixtures/not_full_reports/property/commercial_units/QA-4558.fixture";
+
+describe("Verify the functionality of the Use* radio button", () => {
+
+});
