ed5bece7eb3775b48567f9a552c72cf03069ce3f
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-04-14 13:32:55 +0300

    Corrected test name

--- cypress/fixtures/not_full_reports/income/commercial/stabilized_rent_roll/QA-4607.fixture.ts
+++ cypress/fixtures/not_full_reports/income/commercial/stabilized_rent_roll/QA-4607.fixture.ts
@@ -2,7 +2,7 @@ import ReportDataCreator from "../../../../data_creator/reportData.creator";
 import Enums from "../../../../../enums/enums";
 
 export default {
-    reportCreationData: ReportDataCreator.getReportSpecificIncomeValue(Enums.INCOME_TYPE.BOTH, "4566"),
+    reportCreationData: ReportDataCreator.getReportSpecificIncomeValue(Enums.INCOME_TYPE.BOTH, "4607"),
     numberOfCommercialUnits: 1,
     groupName: "Frontage",
     useRadios: ["small", "medium", "large", "other"],
