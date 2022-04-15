0f7797cacbb42525167f29aa6c8871716660cadc
Ernst-D <ernst.dzerevanko@gmail.com>
2022-04-15 10:14:07 +0400

    [refactor] linter autofixes

--- cypress/integration/not_full_reports/income/expense_forecast/QA-4924.spec.ts
+++ cypress/integration/not_full_reports/income/expense_forecast/QA-4924.spec.ts
@@ -57,12 +57,12 @@ describe("User selects Per SF radiobutton for Repairs & Maintenance on Expense F
             .verifyForecastItemByExpensePeriodType(testData.ownerProjectionRepairsAndMaintenanceItem, testData.buildingDescription, "Owner's Projection")
             .hideExpenseForecastHeader();
 
-        cy.stepInfo(`QA-4924 =>5.2 Check historical expenses values for Repairs & Maintenance card. They should be correctly displayed on slidebars`)
+        cy.stepInfo(`QA-4924 =>5.2 Check historical expenses values for Repairs & Maintenance card. They should be correctly displayed on slidebars`);
 
         Income.ExpenseForecast.Actions.matchElementSnapshot(
             Income.ExpenseForecast.Page.RepairsAndMaintenanceCard, testData.repairsAndMaintenanceCardSnapshotName);
 
         deleteReport(testData.reportCreationData.reportNumber);
-    })
+    });
 
-})
+});
