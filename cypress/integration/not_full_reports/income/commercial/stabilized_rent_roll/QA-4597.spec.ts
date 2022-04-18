4cc3e96af5c4fe05b140af7a19b6605a5496c2f9
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-04-18 08:52:41 +0300

    Added steps

---
+++ cypress/integration/not_full_reports/income/commercial/stabilized_rent_roll/QA-4597.spec.ts
@@ -0,0 +1,22 @@
+import { Income, Property } from "../../../../../actions";
+import { _NavigationSection } from "../../../../../actions/base";
+import { createReport, deleteReport } from "../../../../../actions/base/baseTest.actions";
+import testData from "../../../../../fixtures/not_full_reports/income/commercial/stabilized_rent_roll/QA-4577.fixture";
+
+
+describe(`Verify the suggested text dropdown in the new narrative component added through "=" for the 'Unchanged Renovation' 
+  option in the Generated Commentary on the Stabilized Rent Roll page.`, () => {
+  before("Login, create report", () => {
+    cy.stepInfo(`Preconditions: The mixed report is created and several commercial units are added.`);
+    createReport(testData.reportCreationData);
+    _NavigationSection.navigateToPropertySummary();
+    Property._Summary.enterNumberOfCommercialUnits(testData.numberOfCommercialUnits);
+    _NavigationSection.navigateToCommercialUnits();
+
+  it("Test body", () => {
+    cy.stepInfo("1. Proceed to the Income > Commercial > Stabilized Rent Roll page.");
+    cy.stepInfo("2. Click on the Edit button in the Stabilized Commercial Income Discussion section.");
+    cy.stepInfo("3. Enter the “=Un“ and select the 'Unchanged Renovation' option.");
+  });
+});
+});
