f63a11840313c2856e331091e668ddcd5561f702
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-04-19 12:49:04 +0300

    Refactored 4602&04&05&10 fixture

--- cypress/fixtures/not_full_reports/income/pro_forma/QA-4602&04&05&10.fixture.ts
+++ cypress/fixtures/not_full_reports/income/pro_forma/QA-4602&04&05&10.fixture.ts
@@ -1,13 +1,18 @@
 import ReportDataCreator from "../../../data_creator/reportData.creator";
 import Enums from "../../../../enums/enums";
 
+const _reportCreationData: BoweryAutomation.ReportCreationData = ReportDataCreator
+    .getReportSpecificIncomeValue(Enums.INCOME_TYPE.BOTH, "4602&04&05&10");
+
+const _leaseStatuses: Array<BoweryReports.LeaseStatus> = ["Occupied", "Occupied"];
+
 export default {
-    reportCreationData: ReportDataCreator.getReportSpecificIncomeValue(Enums.INCOME_TYPE.BOTH, "4602&04&05&10"),
+    reportCreationData: _reportCreationData,
     grossBuildingArea: 3150,
     numberOfResidentialUnits: 2,
     numberOfCommercialUnits: 2,
     listOfUnitsSF: [1000, 2000],
-    leaseStatuses: ["Occupied", "Occupied"],
+    leaseStatuses: _leaseStatuses,
     rentsPsf: [100, 120],
     useValue: "undetermined",
     useText: "Undetermined",
