f7607c2de337b375c13511819ac1ec3f13833385
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-04-20 16:38:16 +0300

    test added

--- cypress/fixtures/not_full_reports/income/commercial/stabilized_rent_roll/QA-4581.fixture.ts
+++ cypress/fixtures/not_full_reports/income/commercial/stabilized_rent_roll/QA-4581.fixture.ts
@@ -4,7 +4,7 @@ import enums from "../../../../../enums/enums";
 
 const _buildingDescription: BoweryReports.BuildingDescription = {
     grossArea: 200,
-    numberOfUnits: 2
+    numberOfUnits: 3
 };
 
 
@@ -14,18 +14,9 @@ export default {
 reportCreationData: ReportDataCreator.getReportSpecificIncomeValue(enums.INCOME_TYPE.BOTH, "4581"),
 buildingDescription: _buildingDescription,
 
-    numberOfCommercialUnits: 2,
-    isInspected: [true, false],
-    listOfUnitsSF: [1000, 2000],
-    groupName: "Use",
-    useRadios: ["retail", "office"],
-    useTexts: ["Retail", "Office"],
-    leaseStatuses: ["Vacant", "Occupied"],
-    tenantNames: ["vacant name", "test name"],
-    rentsPsf: [100, 120],
-    annualRent: "$240,000.00",
-    monthlyRent: "$20,000.00",
-    newTenantName: "new test name",
-    rentToBe: [100, 120],
-    perSFList: [100, 120]
+    numberOfCommercialUnits: 3,
+    isInspected: ["Inspected", "NotInspected", "Inspected"],
+    leaseStatuses: ["Vacant", "Occupied", "Vacant"],
+    rentToBe: [100, 0, 150]
+   
 };
--- cypress/integration/not_full_reports/income/commercial/stabilized_rent_roll/QA-4581.spec.ts
+++ cypress/integration/not_full_reports/income/commercial/stabilized_rent_roll/QA-4581.spec.ts
@@ -1,7 +1,6 @@
 /// <reference types="cypress-grep" />
 
 import testData from "../../../../../fixtures/not_full_reports/income/commercial/stabilized_rent_roll/QA-4581.fixture";
-//import { Income, Property } from "../../../../../actions";
 import NavigationSection from "../../../../../actions/base/navigationSection.actions";
 import { createReport, deleteReport } from "../../../../../actions/base/baseTest.actions";
 import Property from "../../../../../actions/property/property.manager";
@@ -9,65 +8,43 @@ import Income from "../../../../../actions/income/income.manager";
 
 
 
-
-
-
-
-
 describe("Verify the Save & Continue button functionality on the Stabilized Rent Roll page:", () => {
+
+
     before("Login, create report", () => {
         createReport(testData.reportCreationData);
     });
-  
 
-    it("Test body", () => {  
-        cy.stepInfo(` 1. report creation and several commercial units addition `);
+
+    it("Test body", () => {
+        cy.stepInfo(` 1. Report creation and several commercial units addition `);
         NavigationSection.navigateToPropertySummary();
-     Property.Summary.enterNumberOfCommercialUnits(testData.buildingDescription.numberOfUnits);
-    // NavigationSection.navigateToCommercialUnits();
+        Property.Summary.enterNumberOfCommercialUnits(testData.buildingDescription.numberOfUnits);
 
-    NavigationSection.navigateToCommercialInPlaceRentRoll();
-   // Income.Commercial.InPlaceRentRoll.clickIsInspectedCheckboxByOrder(testData.numberOfCommercialUnits, testData.isInspected);
-    Income.Commercial.InPlaceRentRoll.chooseListLeaseStatuses(testData.leaseStatuses, testData.numberOfCommercialUnits);
-        //.enterTenantNames(testData.tenantNames, testData.leaseStatuses)
-       // .verifyUseCells(testData.useTexts);
+        NavigationSection.navigateToCommercialInPlaceRentRoll();
+        Income.Commercial.InPlaceRentRoll.chooseListLeaseStatuses(testData.leaseStatuses, testData.numberOfCommercialUnits);
+        Income.Commercial.InPlaceRentRoll.chooseCheckBoxesIsInspectedFromList(testData.isInspected);
 
 
+        cy.stepInfo(` 2. Verify the Save & Continue button is displayed on the Stabilized Rent Roll page `);
+        NavigationSection.clickIncomeApproachButton()
+            .clickCommercialArrow().openCommercialStabilizedRentRollInCommercial();
+        cy.wait(3000);
+        Income.Commercial.StabilizedRentRoll.verifySaveContinueButtonExist();
 
+        cy.stepInfo(` 3. Fill in the editable fields with values or/and check check-boxes or/and click the radio button and click on the Save & Continue button. `);
+        Income.Commercial.StabilizedRentRoll.verifyIsInspectedCheckedAll(testData.isInspected);
+        Income.Commercial.StabilizedRentRoll.enterListPerSF(testData.leaseStatuses, testData.rentToBe);
+        Income.Commercial.StabilizedRentRoll.clickSaveContinueButton();
 
-       cy.stepInfo(`1. Proceed to the Property > Commercial Units page.`);
-       NavigationSection.clickIncomeApproachButton()
-       .clickCommercialArrow().openCommercialStabilizedRentRollInCommercial()
+        cy.stepInfo(` 4. Verify that the changes are saved and the user is redirected to the next page (Income > Miscellaneous > Laundry). `);
+        Income.Miscellaneous.Laundry.verifyThatPageIsOpened();
+        NavigationSection.clickIncomeApproachButton()
+            .clickCommercialArrow().openCommercialStabilizedRentRollInCommercialWhenThereIsNoSaveNotification();
+        Income.Commercial.StabilizedRentRoll.verifyAnnuallyRentPsfByRowNumber(testData.leaseStatuses, testData.rentToBe);
 
-
-       cy.stepInfo(` 1. Verify the Save & Continue button is displayed on the Stabilized Rent Roll page `);
-      
-       Income.Commercial.StabilizedRentRoll.enterListPerSF(testData.leaseStatuses, testData.perSFList)
-       //.verifyIsInspectedChecked()
-     //  .verifyProgressBarNotExist();
-
-   //    Income.Commercial.StabilizedRentRoll.verifySaveContinueButtonExist();
-      // cy.log();
-  //      Income.Commercial.StabilizedRentRoll.clickSaveContinueButton();
-      
-    //        .goBackWithSave();
-   //     Income.Residential.InPlaceRentRoll.verifyCheckboxIsChecked(testData.forecastLabel);
-  //      deleteReport(testData.reportCreationData.reportNumber);
-   
-       
-
-
-
-
-
-
-       
-       
-       cy.stepInfo(` 1. Fill in the editable fields with values or/and check check-boxes or/and click the radio button and click on the Save & Continue button. `);
-
-
-       cy.stepInfo(` 1. Verify that the changes are saved and the user is redirected to the next page (Income > Miscellaneous > Laundry). `);
-
+        cy.stepInfo(` 4. Delete report`);
+        deleteReport(testData.reportCreationData.reportNumber);
 
 
     });
