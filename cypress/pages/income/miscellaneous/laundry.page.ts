6024b9152c6d07a075af5f89ec4ccc079031e0e0
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-04-20 11:35:14 +0300

    class properties added

--- cypress/pages/income/miscellaneous/laundry.page.ts
+++ cypress/pages/income/miscellaneous/laundry.page.ts
@@ -1,6 +1,7 @@
 import BasePage from "../../base/base.page";
 
 class LaundryPage extends BasePage{
+    get laundryheaderSection() {return cy.get("*[data-qa='laundry']");}
     get noLaundryButton() {return cy.get("[data-qa=no-laundry-room-callout-btn]");}
     get laundryIncomeInput() {return cy.get("[name=laundryIncome]");}
     get laundryVCLossRadio() {return cy.get("[data-qa=laundryVCLossType-radio-group] input");}
