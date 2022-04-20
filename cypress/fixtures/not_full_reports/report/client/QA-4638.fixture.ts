0337233e5c787ac876bc08805d644a227c21479f
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-04-20 14:23:29 +0300

    Added QA-4637.fixture

--- cypress/fixtures/not_full_reports/report/client/QA-4638.fixture.ts
+++
@@ -1,8 +0,0 @@
-import ReportDataCreator from "../../../data_creator/reportData.creator";
-
-export default {
-    reportCreationData: ReportDataCreator.getDefaultReportData("4638"),
-    textToType: "=S",
-    verifyListValue: "Sherrif's Sale",
-    verifyAreaValue: "The above transaction reflects a Sheriff's sale of the property. Typically in a Sheriff’s sale, the buyer assumes all encumbrances on the site, including any outstanding mortgage amount and legal fees (\"upset costs\"), delinquent taxes and water and sewer charges, Sheriff's sale fee, and realty transfer taxes. This information was requested from the owner; however, not provided."
-};
