65817f9f6890a90c1f0baf127e59f176552336e8
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-04-21 23:28:55 +0300

    code was prettified

--- cypress/fixtures/not_full_reports/income/commercial/stabilized_rent_roll/QA-4581.fixture.ts
+++ cypress/fixtures/not_full_reports/income/commercial/stabilized_rent_roll/QA-4581.fixture.ts
@@ -1,22 +1,16 @@
 import ReportDataCreator from "../../../../data_creator/reportData.creator";
 import enums from "../../../../../enums/enums";
 
-
 const _buildingDescription: BoweryReports.BuildingDescription = {
     grossArea: 200,
     numberOfUnits: 3
 };
 
-
-
 export default {
-
-reportCreationData: ReportDataCreator.getReportSpecificIncomeValue(enums.INCOME_TYPE.BOTH, "4581"),
-buildingDescription: _buildingDescription,
-
+    reportCreationData: ReportDataCreator.getReportSpecificIncomeValue(enums.INCOME_TYPE.BOTH, "4581"),
+    buildingDescription: _buildingDescription,
     numberOfCommercialUnits: 3,
     isInspected: ["Inspected", "NotInspected", "Inspected"],
     leaseStatuses: ["Vacant", "Occupied", "Vacant"],
     rentToBe: [100, 0, 150]
-   
 };
