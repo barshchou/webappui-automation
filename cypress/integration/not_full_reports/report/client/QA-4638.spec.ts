1a5b3f44ea48ffbcf33b70dc468bf0b9f3d709a9
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-04-22 14:58:29 +0300

    Changed improts

--- cypress/integration/not_full_reports/report/client/QA-4638.spec.ts
+++ cypress/integration/not_full_reports/report/client/QA-4638.spec.ts
@@ -1,6 +1,6 @@
+import { Report } from "../../../../actions";
 import { _NavigationSection } from "../../../../actions/base";
 import {createReport, deleteReport} from "../../../../actions/base/baseTest.actions";
-import { _Client } from "../../../../actions/report";
 import testData from '../../../../fixtures/not_full_reports/report/client/QA-4638.fixture';
 
 describe(`Verify the suggested text dropdown in the new narrative component added through "=" for the 'Sheriff's sale' option 
@@ -14,18 +14,18 @@ describe(`Verify the suggested text dropdown in the new narrative component adde
         _NavigationSection.navigateToClientPage();
 
         cy.stepInfo('2. Click on the Edit button for Intended User and Identification of the Client sections.');
-        _Client.verifyProgressBarNotExist()
+        Report._Client.verifyProgressBarNotExist()
             .clickTextBoxEditButton()
             .clickTextBoxEditButton();
 
         cy.stepInfo('3. Enter the “=S“ and select the \'Sheriff\'s sale\' option for both sections.');
-        _Client.enterIntendedUserTextBox(testData.textToType)
+        Report._Client.enterIntendedUserTextBox(testData.textToType)
             .clickNarrativeSuggestions(testData.verifyListValue)
             .enterIdentificationOfTheClientTextBox(testData.textToType)
             .clickNarrativeSuggestions(testData.verifyListValue, 1);
 
         cy.stepInfo('4.Verify that the following text appears for both sections.');
-        _Client.verifyIntendedUserTextBox(testData.verifyAreaValue)
+        Report._Client.verifyIntendedUserTextBox(testData.verifyAreaValue)
             .verifyIdentificationOfTheClientTextBox(testData.verifyAreaValue);
 
         deleteReport(testData.reportCreationData.reportNumber);
