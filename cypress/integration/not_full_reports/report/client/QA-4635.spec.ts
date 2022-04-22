20549ad3b4a264d288e8d79e423b99fca0e9c96b
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-04-22 13:36:29 +0300

    Fixed remarks

--- cypress/integration/not_full_reports/report/client/QA-4635.spec.ts
+++ cypress/integration/not_full_reports/report/client/QA-4635.spec.ts
@@ -1,6 +1,6 @@
+import { _NavigationSection } from "../../../../actions/base";
 import {createReport, deleteReport} from "../../../../actions/base/baseTest.actions";
-import NavigationSection from "../../../../actions/base/navigationSection.actions";
-import Report from "../../../../actions/report/report.manager";
+import { _Client } from "../../../../actions/report";
 import testData from '../../../../fixtures/not_full_reports/report/client/QA-4635.fixture';
 
 describe("Verify the Save button functionality for Intended User and Identification of the Client sections.", () => {
@@ -10,14 +10,14 @@ describe("Verify the Save button functionality for Intended User and Identificat
 
     it("Test body", () => {
         cy.stepInfo("1. Click on the Edit button on the Report > Client page for Intended User and Identification of the Client sections.");
-        NavigationSection.navigateToClientPage();
+        _NavigationSection.navigateToClientPage();
 
-        Report.Client.verifyProgressBarNotExist()
+        _Client.verifyProgressBarNotExist()
             .clickTextBoxEditButton()
             .clickTextBoxEditButton();
 
         cy.stepInfo("2. Edit comment and click on the Save button for both sections.");
-        Report.Client.enterIntendedUserTextBox(testData.textToType)
+        _Client.enterIntendedUserTextBox(testData.textToType)
             .clickNarrativeSuggestions(testData.verifyListValue)
             .enterIdentificationOfTheClientTextBox(testData.textToType)
             .clickNarrativeSuggestions(testData.verifyListValue, 1)
@@ -25,7 +25,7 @@ describe("Verify the Save button functionality for Intended User and Identificat
             .clickTextBoxSaveButton();
 
         cy.stepInfo("3. Verify that the changes from step 2 are saved.");
-        Report.Client.verifyIntendedUserTextBox(testData.verifyAreaValue)
+        _Client.verifyIntendedUserTextBox(testData.verifyAreaValue)
             .verifyIdentificationOfTheClientTextBox(testData.verifyAreaValue);
 
         deleteReport(testData.reportCreationData.reportNumber);
