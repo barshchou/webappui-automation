d71f418bde5331b54ae4c78be91a59bb03b44c85
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-04-22 10:10:19 +0300

    Changed imports

--- cypress/integration/not_full_reports/report/client/QA-4638.spec.ts
+++ cypress/integration/not_full_reports/report/client/QA-4638.spec.ts
@@ -1,6 +1,6 @@
+import { _NavigationSection } from "../../../../actions/base";
 import {createReport, deleteReport} from "../../../../actions/base/baseTest.actions";
-import NavigationSection from "../../../../actions/base/navigationSection.actions";
-import Report from "../../../../actions/report/report.manager";
+import { _Client } from "../../../../actions/report";
 import testData from '../../../../fixtures/not_full_reports/report/client/QA-4638.fixture';
 
 describe(`Verify the suggested text dropdown in the new narrative component added through "=" for the 'Sheriff's sale' option 
@@ -11,21 +11,21 @@ describe(`Verify the suggested text dropdown in the new narrative component adde
 
     it("Test body", () => {
         cy.stepInfo('1. Proceed to the Report > Client page.');
-        NavigationSection.navigateToClientPage();
+        _NavigationSection.navigateToClientPage();
 
         cy.stepInfo('2. Click on the Edit button for Intended User and Identification of the Client sections.');
-        Report.Client.verifyProgressBarNotExist()
+        _Client.verifyProgressBarNotExist()
             .clickTextBoxEditButton()
             .clickTextBoxEditButton();
 
         cy.stepInfo('3. Enter the “=S“ and select the \'Sheriff\'s sale\' option for both sections.');
-        Report.Client.enterIntendedUserTextBox(testData.textToType)
+        _Client.enterIntendedUserTextBox(testData.textToType)
             .clickNarrativeSuggestions(testData.verifyListValue)
             .enterIdentificationOfTheClientTextBox(testData.textToType)
             .clickNarrativeSuggestions(testData.verifyListValue, 1);
 
         cy.stepInfo('4.Verify that the following text appears for both sections.');
-        Report.Client.verifyIntendedUserTextBox(testData.verifyAreaValue)
+        _Client.verifyIntendedUserTextBox(testData.verifyAreaValue)
             .verifyIdentificationOfTheClientTextBox(testData.verifyAreaValue);
 
         deleteReport(testData.reportCreationData.reportNumber);
