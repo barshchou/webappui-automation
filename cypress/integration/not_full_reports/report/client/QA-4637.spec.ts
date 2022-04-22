18fabcc4a3bbb1bb16e9621afefef1343429e03a
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-04-22 15:02:53 +0300

    Changed improts

--- cypress/integration/not_full_reports/report/client/QA-4637.spec.ts
+++ cypress/integration/not_full_reports/report/client/QA-4637.spec.ts
@@ -1,6 +1,6 @@
+import { Report } from "../../../../actions";
 import { _NavigationSection } from "../../../../actions/base";
 import {createReport, deleteReport} from "../../../../actions/base/baseTest.actions";
-import { _Client } from "../../../../actions/report";
 import testData from '../../../../fixtures/not_full_reports/report/client/QA-4637.fixture';
 
 describe(`Verify the suggested text dropdown in the new narrative component added through "=" for the 'Foreclosure sale' 
@@ -14,18 +14,18 @@ describe(`Verify the suggested text dropdown in the new narrative component adde
         _NavigationSection.navigateToClientPage();
 
         cy.stepInfo('2. Click on the Edit button for Intended User and Identification of the Client sections.');
-        _Client.verifyProgressBarNotExist()
+        Report._Client.verifyProgressBarNotExist()
             .clickTextBoxEditButton()
             .clickTextBoxEditButton();
 
         cy.stepInfo('3. Enter the “=F“ and select the \'Foreclosure sale\' option for both sections.');
-        _Client.enterIntendedUserTextBox(testData.textToType)
+        Report._Client.enterIntendedUserTextBox(testData.textToType)
             .clickNarrativeSuggestions(testData.verifyListValue)
             .enterIdentificationOfTheClientTextBox(testData.textToType)
             .clickNarrativeSuggestions(testData.verifyListValue, 1);
 
         cy.stepInfo('4. Verify that the following text appears in both sections.');
-        _Client.verifyIntendedUserTextBox(testData.verifyAreaValue)
+        Report._Client.verifyIntendedUserTextBox(testData.verifyAreaValue)
             .verifyIdentificationOfTheClientTextBox(testData.verifyAreaValue);
 
         deleteReport(testData.reportCreationData.reportNumber);
