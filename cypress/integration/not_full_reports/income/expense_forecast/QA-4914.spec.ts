a2ed699f72c6297bb2eac6dc0b2d1530f016b673
AlexeiMo <prestigio458@gmail.com>
2022-04-25 15:03:30 +0300

    change to import from modules

--- cypress/integration/not_full_reports/income/expense_forecast/QA-4914.spec.ts
+++ cypress/integration/not_full_reports/income/expense_forecast/QA-4914.spec.ts
@@ -1,8 +1,8 @@
 import testData from "../../../../fixtures/not_full_reports/income/expense_forecast/QA-4914.fixture";
 import {createReport, deleteReport} from "../../../../actions/base/baseTest.actions";
-import NavigationSection from "../../../../actions/base/navigationSection.actions";
-import Property from "../../../../actions/property/property.manager";
-import Income from "../../../../actions/income/income.manager";
+import { _NavigationSection } from "../../../../actions/base";
+import { Property } from "../../../../actions";
+import { Income } from "../../../../actions";
 
 describe("Per Unit Appraiser's Forecast value for Water & Sewer is correctly converted to Per SF value", () => {
     before("Login, create report", () => {
@@ -11,20 +11,20 @@ describe("Per Unit Appraiser's Forecast value for Water & Sewer is correctly con
 
     it("Test body", () => {
         cy.stepInfo("1. Navigate to Property -> Summary and enter gross building area and number of residential units");
-        NavigationSection.Actions.navigateToPropertySummary();
-        Property.Summary.Actions.enterGrossBuildingArea(testData.buildingDescription.grossArea)
+        _NavigationSection.Actions.navigateToPropertySummary();
+        Property._Summary.Actions.enterGrossBuildingArea(testData.buildingDescription.grossArea)
             .enterNumberOfResUnits(testData.buildingDescription.numberOfUnits);
 
         cy.stepInfo("2. Go to Expense Forecast and make sure that Per Unit radiobutton is selected for Water & Sewer card");
-        NavigationSection.Actions.navigateToExpenseForecast();
-        Income.ExpenseForecast.Actions.chooseForecastItemBasis(testData.forecastItem);
-        Income.ExpenseForecast.Actions.verifyForecastItemBasis(testData.forecastItem);
+        _NavigationSection.Actions.navigateToExpenseForecast();
+        Income._ExpenseForecastActions.Actions.chooseForecastItemBasis(testData.forecastItem);
+        Income._ExpenseForecastActions.Actions.verifyForecastItemBasis(testData.forecastItem);
 
         cy.stepInfo("3. Fill in Appraiser's Forecast field for Water & Sewer card");
-        Income.ExpenseForecast.Actions.enterForecastItemForecast(testData.forecastItem);
+        Income._ExpenseForecastActions.Actions.enterForecastItemForecast(testData.forecastItem);
 
         cy.stepInfo("4. Verify that Per SF value below this field is calculated as: Per Unit Appraiser’s Forecast * # of Resi Units / GBA");
-        Income.ExpenseForecast.Actions.verifyForecastItemBasisMoney(testData.forecastItem, testData.buildingDescription);
+        Income._ExpenseForecastActions.Actions.verifyForecastItemBasisMoney(testData.forecastItem, testData.buildingDescription);
         deleteReport(testData.reportCreationData.reportNumber);
     });
 });
