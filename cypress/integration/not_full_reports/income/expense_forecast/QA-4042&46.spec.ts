4d2b388c20e1dbfab29704deac157ced7cb67ded
Ernst-D <ernst.dzerevanko@gmail.com>
2022-04-25 15:14:16 +0400

    [QA-4042] fixed import from modules

--- cypress/integration/not_full_reports/income/expense_forecast/QA-4042&46.spec.ts
+++ cypress/integration/not_full_reports/income/expense_forecast/QA-4042&46.spec.ts
@@ -1,6 +1,6 @@
 import testData from "../../../../fixtures/not_full_reports/income/expense_forecast/QA-4042.fixture";
 import { _NavigationSection} from "../../../../actions/base";
-import Income from "../../../../actions/income/income.manager";
+import { Income } from "../../../../actions";
 import {createReport, deleteReport} from "../../../../actions/base/baseTest.actions";
 
 describe(`Verify that Generated Commentary are updated on the Expense Forecast page`, () => {
@@ -13,34 +13,34 @@ describe(`Verify that Generated Commentary are updated on the Expense Forecast p
         cy.stepInfo(`[QA-4042] => 1. Go to Expense Forecast`);
         _NavigationSection.navigateToExpenseForecast();
         cy.stepInfo(`[QA-4042] => 2. Fill Forecast value for SF basis and verify commentary`);
-        Income.ExpenseForecast.enterForecastItemForecast(testData.expenseForecastWaterAndSewer)
+        Income._ExpenseForecastActions.enterForecastItemForecast(testData.expenseForecastWaterAndSewer)
             .verifyForecastCommentary(testData.commentariesWaterAndSewer.generatedPerSF, testData.expenseForecastWaterAndSewer)
             .editExpenseForecastCommentary(testData.commentariesWaterAndSewer.edited, testData.expenseForecastWaterAndSewer)
             .verifyForecastCommentary(testData.commentariesWaterAndSewer.edited, testData.expenseForecastWaterAndSewer);
         cy.stepInfo(`[QA-4042] => 3. Revert commentary value, switch to unit basis and fill Forecast value and verify commentary`);
-        Income.ExpenseForecast.revertToOriginalExpenseForecastCommentary(testData.expenseForecastWaterAndSewer);
+        Income._ExpenseForecastActions.revertToOriginalExpenseForecastCommentary(testData.expenseForecastWaterAndSewer);
         testData.expenseForecastWaterAndSewer.basis = "unit";
-        Income.ExpenseForecast.switchExpenseForecastBasis(testData.expenseForecastWaterAndSewer);
-        Income.ExpenseForecast.enterForecastItemForecast(testData.expenseForecastWaterAndSewer)
+        Income._ExpenseForecastActions.switchExpenseForecastBasis(testData.expenseForecastWaterAndSewer);
+        Income._ExpenseForecastActions.enterForecastItemForecast(testData.expenseForecastWaterAndSewer)
             .verifyForecastCommentary(testData.commentariesWaterAndSewer.generatedPerUnit, testData.expenseForecastWaterAndSewer)
             .editExpenseForecastCommentary(testData.commentariesWaterAndSewer.edited, testData.expenseForecastWaterAndSewer)
             .verifyForecastCommentary(testData.commentariesWaterAndSewer.edited, testData.expenseForecastWaterAndSewer);
-        Income.ExpenseForecast.clickSaveButton();
+        Income._ExpenseForecastActions.clickSaveButton();
     });
 
     it("[QA-4046]", () => {
         cy.stepInfo(`[QA-4046] => 1. Go to Expense Forecast`);
         _NavigationSection.navigateToExpenseForecast();
         cy.stepInfo(`[QA-4046] => 2. Fill Forecast value for SF basis and verify commentary`);
-        Income.ExpenseForecast.enterForecastItemForecast(testData.expenseForecastReplacementReserve)
+        Income._ExpenseForecastActions.enterForecastItemForecast(testData.expenseForecastReplacementReserve)
             .verifyForecastCommentary(testData.commentariesReplacementReserve.generatedPerSF, testData.expenseForecastReplacementReserve)
             .editExpenseForecastCommentary(testData.commentariesReplacementReserve.edited, testData.expenseForecastReplacementReserve)
             .verifyForecastCommentary(testData.commentariesReplacementReserve.edited, testData.expenseForecastReplacementReserve);
         cy.stepInfo(`[QA-4046] => 3. Revert commentary value, switch to unit basis and fill Forecast value and verify commentary`);
-        Income.ExpenseForecast.revertToOriginalExpenseForecastCommentary(testData.expenseForecastReplacementReserve);
+        Income._ExpenseForecastActions.revertToOriginalExpenseForecastCommentary(testData.expenseForecastReplacementReserve);
         testData.expenseForecastReplacementReserve.basis = "unit";
-        Income.ExpenseForecast.switchExpenseForecastBasis(testData.expenseForecastReplacementReserve);
-        Income.ExpenseForecast.enterForecastItemForecast(testData.expenseForecastReplacementReserve)
+        Income._ExpenseForecastActions.switchExpenseForecastBasis(testData.expenseForecastReplacementReserve);
+        Income._ExpenseForecastActions.enterForecastItemForecast(testData.expenseForecastReplacementReserve)
             .verifyForecastCommentary(testData.commentariesReplacementReserve.generatedPerUnit, testData.expenseForecastReplacementReserve)
             .editExpenseForecastCommentary(testData.commentariesReplacementReserve.edited, testData.expenseForecastReplacementReserve)
             .verifyForecastCommentary(testData.commentariesReplacementReserve.edited, testData.expenseForecastReplacementReserve);
