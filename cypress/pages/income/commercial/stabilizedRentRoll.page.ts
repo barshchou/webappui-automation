3bd1ad47829dd1cf6dc2bb292ab24fc02d36e509
Ernst-D <ernst.dzerevanko@gmail.com>
2022-04-25 13:32:13 +0400

    [QA-5117] fixed pr comment locator

--- cypress/pages/income/commercial/stabilizedRentRoll.page.ts
+++ cypress/pages/income/commercial/stabilizedRentRoll.page.ts
@@ -23,7 +23,7 @@ class StabilizedRentRollPage extends BasePage{
 
     get annualRentPsfCells() {return cy.xpath("//*[contains(@class, 'htNumeric')]|(//*[contains(@data-qa, 'annualRentPsf')])[position() < last()]");}
    
-    get annualRentPsfCellsInputField() {return cy.get('textarea.handsontableInput');}
+    get annualRentPsfCellsInputField() {return cy.get('[id="incomeApproach.commercialIncome.commercialProjectedRentRoll-final-form"] textarea.handsontableInput');}
 
     get monthlyRentPsfCells() {return cy.xpath("//*[contains(@class, 'htNumeric')]|(//*[contains(@data-qa, 'monthlyRentPsf')])[position() < last()]");}
 
