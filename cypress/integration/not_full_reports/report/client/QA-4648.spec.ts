2c92ba50d789b55a30076c647bd823ae967489f3
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-04-26 15:46:52 +0300

    Done QA-4648.spec

--- cypress/integration/not_full_reports/report/client/QA-4648.spec.ts
+++ cypress/integration/not_full_reports/report/client/QA-4648.spec.ts
@@ -1,7 +1,7 @@
 import { Report } from "../../../../actions";
 import { _NavigationSection } from "../../../../actions/base";
 import {createReport, deleteReport} from "../../../../actions/base/baseTest.actions";
-import testData from '../../../../fixtures/not_full_reports/report/client/QA-4635.fixture';
+import testData from '../../../../fixtures/not_full_reports/report/client/QA-4648.fixture';
 
 describe("Verify the Save button functionality on the Report > Client page", () => {
     before("Login, create report", () => {
@@ -19,18 +19,23 @@ describe("Verify the Save button functionality on the Report > Client page", () 
         Report._Client.Page.formSaveBtn(0).should('be.visible');
         Report._Client.Page.formSaveBtn(1).should('be.visible');
 
-        // cy.stepInfo("2. Edit comment and click on the Save button for both sections.");
-        // Report._Client.enterIntendedUserTextBox(testData.textToType)
-        //     .clickNarrativeSuggestions(testData.verifyListValue)
-        //     .enterIdentificationOfTheClientTextBox(testData.textToType)
-        //     .clickNarrativeSuggestions(testData.verifyListValue, 1)
-        //     .clickTextBoxSaveButton()
-        //     .clickTextBoxSaveButton();
+        cy.stepInfo("2. Fill in the editable fields with values and click on the Save button.");
+        Report._Client.enterIntendedUserTextBox(testData.textToType)
+            .clickNarrativeSuggestions(testData.verifyListValue)
+            .enterIdentificationOfTheClientTextBox(testData.textToType)
+            .clickNarrativeSuggestions(testData.verifyListValue, 1)
+            .Page.formSaveBtn(0).click();
+            Report._Client.Page.formSaveBtn(0).click();
+            Report._Client.verifyIntendedUserTextBox(testData.verifyAreaValue)
+                .verifyIdentificationOfTheClientTextBox(testData.verifyAreaValue);
 
-        // cy.stepInfo("3. Verify that the changes from step 2 are saved.");
-        // Report._Client.verifyIntendedUserTextBox(testData.verifyAreaValue)
-        //     .verifyIdentificationOfTheClientTextBox(testData.verifyAreaValue);
+        cy.stepInfo("3. Refresh the page / or re-enter the page and verify that the changes from step 2 are still applied.");
+        Report._Client.clickSaveButton()
+            .verifyProgressBarNotExist();
+        cy.reload();
+        Report._Client.verifyIntendedUserTextBox(testData.verifyAreaValue)
+            .verifyIdentificationOfTheClientTextBox(testData.verifyAreaValue);
 
-        // deleteReport(testData.reportCreationData.reportNumber);
+        deleteReport(testData.reportCreationData.reportNumber);
     });
 });
