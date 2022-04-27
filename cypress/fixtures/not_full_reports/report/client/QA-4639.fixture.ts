d29ef64291f5753cf95e05c7df224987ca239739
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-04-27 15:35:38 +0300

    Changed type of report

--- cypress/fixtures/not_full_reports/report/client/QA-4639.fixture.ts
+++ cypress/fixtures/not_full_reports/report/client/QA-4639.fixture.ts
@@ -1,8 +1,7 @@
-import Enums from "../../../../enums/enums";
 import ReportDataCreator from "../../../data_creator/reportData.creator";
 
 export default {
-    reportCreationData: ReportDataCreator.getReportSpecificConclusionValue(Enums.VALUE_CONCLUSION_TYPE.AS_STABILIZED, "4639"),
+    reportCreationData: ReportDataCreator.getDefaultReportData("4639"),
     textToType: "=Un",
     verifySuggestion: "Unchanged Renovation",
     verifyTextArea: "Upon renovation, the subject unit count and gross building area will remain unchanged."
