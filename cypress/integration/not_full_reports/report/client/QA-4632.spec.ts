8fe66ac1d622ca4c8fa7f3e86a1db5be675b359c
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-04-28 09:49:15 +0300

    Done QA-4632.spec

--- cypress/integration/not_full_reports/report/client/QA-4632.spec.ts
+++ cypress/integration/not_full_reports/report/client/QA-4632.spec.ts
@@ -18,23 +18,10 @@ describe("Verify the Cancel button functionality for Intended User and Identific
         cy.stepInfo("2. Verify that the Cancel button is displayed instead of the Edit button for both sections.");
         Report._Client.Page.formCancelButton(0).should("be.visible");
         Report._Client.Page.formCancelButton(1).should("be.visible");
-        Report._Client.Page.formRevertToOriginalBtn(0).should("be.visible");
-        Report._Client.Page.formRevertToOriginalBtn(1).should("be.visible");
 
         cy.stepInfo("3. Click on the Cancel button and verify that the form closes form without saving changes with no warning message for both sections.");
-        Report._Client.enterIntendedUserTextBox(testData.textToType)
-            .clickNarrativeSuggestions(testData.verifySuggestion)
-            .enterIdentificationOfTheClientTextBox(testData.textToType)
-            .clickNarrativeSuggestions(testData.verifySuggestion, 1);
-
-        Report._Client.Page.formRevertToOriginalBtn(0).should("be.enabled");
-        Report._Client.Page.formRevertToOriginalBtn(1).should("be.enabled");
-
-        cy.stepInfo("4. Click on the Revert to Original button and verify the ‘Changes will be lost modal’ is displayed for both sections.");
-        Report._Client.Page.formRevertToOriginalBtn(0).click();
-        Report._Client.Page.formYesRevertBtn.click();
-        Report._Client.Page.formRevertToOriginalBtn(1).click();
-        Report._Client.Page.formYesRevertBtn.click();
+        Report._Client.Page.formCancelButton(0).click();
+        Report._Client.Page.formCancelButton(0).click();
         Report._Client.verifyIntendedUserTextBox(testData.verifyIntendedUserTextArea)
             .verifyIdentificationOfTheClientTextBox(testData.verifyIdentificationOfTheClientTextArea);
 
