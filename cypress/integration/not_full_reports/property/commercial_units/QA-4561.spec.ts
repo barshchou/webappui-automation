7ad6062aa724b492229c0a4c368fb1b6dae7104d
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-04-28 15:47:05 +0300

    complete test added

---
+++ cypress/integration/not_full_reports/property/commercial_units/QA-4561.spec.ts
@@ -0,0 +1,43 @@
+/// <reference types="cypress-grep" />
+
+import testData from "../../../../fixtures/not_full_reports/property/commercial_units/QA-4561.fixture";
+import { Base, Property } from "../../../../actions";
+import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
+import { _NavigationSection } from "../../../../actions/base";
+
+
+describe("Verify the functionality of the Street Type radio button", () => {
+
+
+    before("Login, create report", () => {
+
+        cy.stepInfo(` 1. Report creation and several commercial units addition`);
+        createReport(testData.reportCreationData);
+        Base._NavigationSection.navigateToPropertySummary();
+        Property._Summary.enterNumberOfCommercialUnits(testData.numberOfCommercialUnits);
+    });
+
+
+    it("Test body", () => {
+
+        cy.stepInfo("2.  Proceed to the Property > Commercial Units page.");
+        _NavigationSection.navigateToCommercialUnits();
+
+        cy.stepInfo("3. Verify the Street Type contains the following radio buttons: Side Street, Avenue, and no value is selected by default");
+        testData.streetTypeValues.forEach(value => {
+            Property._CommercialUnits.Page.getRadioButtonByValueAndUnitIndex(testData.groupName, value).should('exist');
+            Property._CommercialUnits.verifyRadioIsNotChecked(testData.groupName, value);
+        });
+
+        cy.stepInfo("4. Verify that each radio button can be selected and saved and verify text field appears with other value");
+        testData.streetTypeValues.forEach(value => {
+            Property._CommercialUnits.clickRadioButtonByValueAndUnitIndex(testData.groupName, value);
+            Property._CommercialUnits.clickSaveButton()
+                .verifyProgressBarNotExist();
+            cy.reload();
+            Property._CommercialUnits.verifyRadioIsChecked(testData.groupName, value);
+        });
+
+        deleteReport(testData.reportCreationData.reportNumber);
+    });
+});
