d25ab155c4e44378eb55dd5d1b3776fafe92c3d2
Ernst-D <ernst.dzerevanko@gmail.com>
2022-04-29 19:44:14 +0400

    [QA-5170] added jsdoc to plugins tasks

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,16 +1,11 @@
-/// <reference types="cypress" />
 // ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
 // You can change the location of this file or turn off loading
 // the plugins file with the 'pluginsFile' configuration option.
 //
 // You can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+/// <reference types="cypress" />
 const { existsSync, writeFileSync, renameSync } = require("fs");
 const mammoth = require("mammoth");
 const {
@@ -20,14 +15,38 @@ const {
 const grepFilterPlugin = require("cypress-grep/src/plugin");
 
 /**
- * NOTE: Sometimes we need call functions recursively (function calls itself).
+ * NOTE: (ernst) Sometimes we need call functions recursively (function calls itself).
  * If you try to make INLINED function call itself (how it often described in cy.task examples) - 
  * you will get "`function_name` is not defined" error.  
  * So we better first define function for tasks, and then return it from task 
  * (see `waitForFileExists` task as an example) 
  */
+
+/**
+ * WARN: (ernst) functions, called in tasks, 
+ * must to return a value, null, or a promise that resolves to a value or null.
+ * Otherwise you get the error:
+ * `
+ * You must return a value, null, or a promise 
+ * that resolves to a value or null 
+ * to indicate that the task was handled.
+ * `
+ */
+
 //#region Functions to be used in Cypress' tasks
 
+/**
+ * Waiting recursevly file to exist. If file exists - return Promise<true>.
+ * If not - resolve Promise with timeout for 1 second (this is our interval for checks),
+ * and return function itself.
+ * if currentTime == timeout 
+ * (notice, that we call function itself with currentTime+1second,
+ * so we will make recursion until timeout exceed) - return Promise<false>
+ * @param {string} filePath 
+ * @param {number} currentTime 
+ * @param {number} timeout 
+ * @returns {Promise<boolean>} recursive call or Promise<boolean>
+ */
 const _waitForFileExists = async (filePath, currentTime = 0, timeout = 60000) => {
   if (existsSync(filePath)) {
     return true;  
@@ -42,27 +61,24 @@ const _waitForFileExists = async (filePath, currentTime = 0, timeout = 60000) =>
 }
 
 /**
- * @typedef { BoweryReports.ReportFile } ReportFile
- */
-/**
+ * ernst: for type - check `cypress/types`, I couldn't import it via JSDoc explicitly
  * 
+ * Converts docx file into html via mammoth lib and writes it into cypress/downloads
  * @param {ReportFile} report 
- * @returns 
+ * @returns {null} for tasks' return type - please check notes above
  */
 const _convertDocxToHtml = async (report) => {
   let result = await mammoth.convertToHtml({path: `${report.path}/${report.name}.${report.extension}`});
   writeFileSync(`${report.path}/${report.name}.html`,result.value);
-  /**
-   * ernst: returning null in order to avoid error:
-   * `
-   * You must return a value, null, or a promise 
-   * that resolves to a value or null 
-   * to indicate that the task was handled.
-   * `
-   */
   return null;
 }
 
+/**
+ * Renames file via fs.renameSync
+ * @param {string} oldPath 
+ * @param {string} newPath 
+ * @returns {null} - for tasks' return type - please check notes above
+ */
 const _renameFile = (oldPath,newPath) =>{
   renameSync(`./${oldPath}`,`./${newPath}`)
   return null;
@@ -72,13 +88,17 @@ const _renameFile = (oldPath,newPath) =>{
 
 /**
  * @type {Cypress.PluginConfig}
+ * This function is called when a project is opened or re-opened (e.g. due to 
+ * the project's config changing).
+ * @param {Cypress.PluginEvents}`on` is used to hook into various events Cypress emits
+ * @param {Cypress.PluginConfigOptions} `config` is the resolved Cypress config 
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
+ 
   addMatchImageSnapshotPlugin(on, config);
   grepFilterPlugin(config);
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+
 
   on("before:browser:launch", (browser, launchOptions) => {
       if (browser.isHeadless === true) {
@@ -88,14 +108,16 @@ module.exports = (on, config) => {
   });
 
   //#region Cypress Tasks (more about tasks: https://docs.cypress.io/api/commands/task)
+  
   on("task",{
-    async waitForFileExists(filePath, currentTime = 0, timeout = 5000){
-      return await _waitForFileExists(filePath, currentTime = 0, timeout = 5000);
+    async waitForFileExists(filePath, currentTime = 0, timeout = 60000){
+      return await _waitForFileExists(filePath, currentTime, timeout);
     }
   });
 
   on("task",{
     async convertDocxToHtml(report){
+      // ernst: it's async because it has call of async function from mammoth lib 
       return await _convertDocxToHtml(report);
     }
   });
@@ -107,5 +129,6 @@ module.exports = (on, config) => {
   });
 
   //#endregion
+
   return config;
 };
