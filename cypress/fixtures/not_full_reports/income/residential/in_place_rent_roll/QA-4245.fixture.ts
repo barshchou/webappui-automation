8565885866b1728a5332ff3d18350b5f867fe052
AlexeiMo <prestigio458@gmail.com>
2022-05-02 12:01:50 +0300

    Add ResUnitDataItem type in fixture file

--- cypress/fixtures/not_full_reports/income/residential/in_place_rent_roll/QA-4245.fixture.ts
+++ cypress/fixtures/not_full_reports/income/residential/in_place_rent_roll/QA-4245.fixture.ts
@@ -1,29 +1,44 @@
 import ReportDataCreator from "../../../../data_creator/reportData.creator";
 
+type ResUnitDataItem = {
+    leaseStatus: BoweryReports.LeaseStatus,
+    rent: number
+};
+
+const _firstItem: ResUnitDataItem = {
+    leaseStatus: "Occupied",
+    rent: 20
+};
+
+const _secondItem: ResUnitDataItem = {
+    leaseStatus: "Occupied",
+    rent: 60
+};
+
+const _thirdItem: ResUnitDataItem = {
+    leaseStatus: "Vacant",
+    rent: 100
+};
+
+const _fourthItem: ResUnitDataItem = {
+    leaseStatus: "Occupied",
+    rent: 50
+};
+
+const _fifthItem: ResUnitDataItem = {
+    leaseStatus: "Vacant",
+    rent: 70
+};
+
 export default {
     reportCreationData: ReportDataCreator.getDefaultReportData("4245"),
     numberOfUnits: 5,
     columnName: "Monthly Total",
-    resUnitsData: [
-        {
-            leaseStatus: "Occupied",
-            rent: 20
-        },
-        {
-            leaseStatus: "Occupied",
-            rent: 60
-        },
-        {
-            leaseStatus: "Vacant",
-            rent: 100,
-        },
-        {
-            leaseStatus: "Occupied",
-            rent: 50
-        },
-        {
-            leaseStatus: "Vacant",
-            rent: 70
-        }
+    resUnitData: [
+        _firstItem,
+        _secondItem,
+        _thirdItem,
+        _fourthItem,
+        _fifthItem
     ]
 };
