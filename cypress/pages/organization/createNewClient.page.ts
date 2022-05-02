2395c06fc826ea526c1699e99c59c5b699e57f21
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-05-02 15:49:48 +0300

    Added selctor fields

--- cypress/pages/organization/createNewClient.page.ts
+++ cypress/pages/organization/createNewClient.page.ts
@@ -1,27 +1,27 @@
 import BasePage from "../base/base.page";
 
 class OrganizationCreateNewClientPage extends BasePage{
-    get addresseePrefixField() {return cy.get("[data-qa=prefix-form-control]");}
+    get addresseePrefixField() {return cy.xpath("//label[contains(text(), 'Addressee Prefix')]//following::*[@data-qa='select-value'][1]");}
 
-    get addresseeTitleField() {return cy.get("");}
+    get addresseeTitleField() {return cy.xpath("//label[contains(text(), 'Addressee Title')]//following::*[@type='text'][1]");}
 
-    get addresseeFirstNameField() {return cy.get("");}
+    get addresseeFirstNameField() {return cy.xpath("//label[contains(text(), 'Addressee First Name')]//following::*[@type='text'][1]");}
 
-    get addresseeMiddleInitialField() {return cy.get("");}
+    get addresseeMiddleInitialField() {return cy.xpath("//label[contains(text(), 'Addressee Middle Initial')]//following::*[@type='text'][1]");}
 
-    get addresseeLastNameField() {return cy.get("");}
+    get addresseeLastNameField() {return cy.xpath("//label[contains(text(), 'Addressee Last Name')]//following::*[@type='text'][1]");}
 
-    get addresseeClientSuffixField() {return cy.get("");}
+    get clientSuffixField() {return cy.xpath("//label[contains(text(), 'Client Suffix')]//following::*[@type='text'][1]");}
 
-    get addresseeClientCompanyNameField() {return cy.get("");}
+    get clientCompanyNameField() {return cy.xpath("//label[contains(text(), 'Client/Company Name')]//following::*[@type='text'][1]");}
 
-    get addresseeStreetAddressField() {return cy.get("");}
+    get streetAddressField() {return cy.xpath("//label[contains(text(), 'Street Address')]//following::*[@type='text'][1]");}
 
-    get addresseeCityField() {return cy.get("");}
+    get cityField() {return cy.xpath("//label[contains(text(), 'City')]//following::*[@type='text'][1]");}
 
-    get addresseeStateField() {return cy.get("");}
+    get stateField() {return cy.xpath("//label[contains(text(), 'State')]//following::*[@data-qa='select-value'][1]");}
 
-    get addresseeZipCodeField() {return cy.get("");}
+    get zipCodeField() {return cy.xpath("//label[contains(text(), 'Zip Code')]//following::*[@type='text'][1]");}
 }
 
 export default new OrganizationCreateNewClientPage();
