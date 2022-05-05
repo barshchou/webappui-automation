03dc1b785583d2c1d4d8ea2319e4b4e88c553479
AlexeiMo <prestigio458@gmail.com>
2022-05-05 11:39:13 +0300

    [remove-odd-report-creation-methods] Resolve conflicts

--- cypress/fixtures/not_full_reports/income/commercial/in_place_rent_roll/QA-4413&14.fixture.ts
+++ cypress/fixtures/not_full_reports/income/commercial/in_place_rent_roll/QA-4413&14.fixture.ts
@@ -2,7 +2,7 @@ import Enums from "../../../../../enums/enums";
 import ReportDataCreator from "../../../../data_creator/reportData.creator";
 
 const reportCreationFixture = () => {
-    return ReportDataCreator.getReportData("413&144", {
+    return ReportDataCreator.getReportData("4413&14", {
         incomeValue: Enums.INCOME_TYPE.BOTH,
     });
 };
--- cypress/fixtures/not_full_reports/income/commercial/stabilized_rent_roll/QA-4015.fixture.ts
+++ cypress/fixtures/not_full_reports/income/commercial/stabilized_rent_roll/QA-4015.fixture.ts
@@ -2,7 +2,7 @@ import ReportDataCreator from "../../../../data_creator/reportData.creator";
 import Enums from "../../../../../enums/enums";
 
 export default {
-    reportCreationData: ReportDataCreator.getReportData("4827", {
+    reportCreationData: ReportDataCreator.getReportData("4015", {
         incomeValue: Enums.INCOME_TYPE.BOTH,
     }),
     numberOfCommercialUnits: 2,
