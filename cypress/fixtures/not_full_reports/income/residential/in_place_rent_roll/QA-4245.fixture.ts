64398dc8cb5edb9bd23c7878d65b603fb2e0c25c
AlexeiMo <prestigio458@gmail.com>
2022-05-05 12:17:20 +0300

    [QA-5155] Resolve conflicts

--- cypress/fixtures/not_full_reports/income/residential/in_place_rent_roll/QA-4245.fixture.ts
+++ cypress/fixtures/not_full_reports/income/residential/in_place_rent_roll/QA-4245.fixture.ts
@@ -1,47 +1,42 @@
 import ReportDataCreator from "../../../../data_creator/reportData.creator";
 
-type ResUnitDataItem = {
-    leaseStatus: BoweryReports.LeaseStatus,
-    rent: number
-};
 
 const occupied: BoweryReports.LeaseStatus = "Occupied";
 const vacant: BoweryReports.LeaseStatus = "Vacant";
 
-const _firstItem: ResUnitDataItem = {
-    leaseStatus: occupied,
-    rent: 20
-};
-
-const _secondItem: ResUnitDataItem = {
-    leaseStatus: occupied,
-    rent: 60
-};
-
-const _thirdItem: ResUnitDataItem = {
-    leaseStatus: vacant,
-    rent: 100
-};
-
-const _fourthItem: ResUnitDataItem = {
-    leaseStatus: occupied,
-    rent: 50
-};
-
-const _fifthItem: ResUnitDataItem = {
-    leaseStatus: vacant,
-    rent: 70
+const rentRollResidentialUnitsFixture = () : BoweryReports.ResidentialUnit[] => {
+    return [
+        {
+            footage: 0,
+            monthlyRent: 20,
+            leaseStatus: occupied
+        },
+        {
+            footage: 0,
+            monthlyRent: 60,
+            leaseStatus: occupied
+        },
+        {
+            footage: 0,
+            monthlyRent: 100,
+            leaseStatus: vacant,
+        },
+        {
+            footage: 0,
+            monthlyRent: 50,
+            leaseStatus: occupied
+        },
+        {
+            footage: 0,
+            monthlyRent: 70,
+            leaseStatus: vacant
+        }
+    ];
 };
 
 export default {
     reportCreationData: ReportDataCreator.getDefaultReportData("4245"),
     numberOfUnits: 5,
     columnName: "Monthly Total",
-    resUnitsData: [
-        _firstItem,
-        _secondItem,
-        _thirdItem,
-        _fourthItem,
-        _fifthItem
-    ]
+    residentialUnits: rentRollResidentialUnitsFixture()
 };
