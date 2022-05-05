8f84ae60c2633208f2ac9a31fb78bf25d1e8c078
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-05-05 17:07:26 +0300

    doc with array added

---
+++ cypress/enums/expenseHistoryTableRows.enum.ts
@@ -0,0 +1,9 @@
+const x = {
+    
+    cell1: ["grossRevenue", "realEstateTaxes", "insurance", "electricity", "fuel", "waterAndSewer", "repairsAndMaintenance", "payrollAndBenefits",
+     "generalAndAdministrative", "legalAndProfessionalFees", "miscellaneous", "management", "reserves"],
+
+     cell2: ["total", "totalExcludingTaxes", "noi"]
+};
+
+export default Object.freeze(x);
