bc82c0e2b1c029ae503a6fb675566fdb5d97a1cb
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-05-05 16:18:48 +0300

    [QA-5278] Done QA-4568.fixture

--- cypress/fixtures/not_full_reports/property/commercial_units/QA-4568.fixture.ts
+++ cypress/fixtures/not_full_reports/property/commercial_units/QA-4568.fixture.ts
@@ -1,18 +1,57 @@
 import ReportDataCreator from "../../../data_creator/reportData.creator";
 import Enums from "../../../../enums/enums";
 
-const _reportCreationData: BoweryAutomation.ReportCreationData = ReportDataCreator.getReportData("4559", {
+const _reportCreationData: BoweryAutomation.ReportCreationData = ReportDataCreator.getReportData("4568", {
     incomeValue: Enums.INCOME_TYPE.BOTH,
     conclusionValue: Enums.VALUE_CONCLUSION_TYPE.AS_COMPLETE
 });
 
-const _stateValues: Array<BoweryReports.CommercialUnitsStateValues> = ["finished", "unfinished", "vanilla box", "other"];
-const _groupName: BoweryReports.CommercialUnitsGroups = "State";
+type groupValue = {
+    group:BoweryReports.CommercialUnitsGroups, 
+    value: BoweryReports.CommercialUnits.GroupsValues,
+}
+
+const _arrayValuesAndGroup: Array<groupValue> = [
+    {
+        group: "Use",
+        value: "retail"
+    },
+    {
+        group: "State",
+        value: "finished"
+    },
+    {
+        group: "Location",
+        value: "corner"
+    },
+    {
+        group: "Street Type",
+        value: "side street"
+    },
+    {
+        group: "Floor",
+        value: "belowGrade"
+    },
+    {
+        group: "Grade",
+        value: "atGrade"
+    },
+    {
+        group: "Facade",
+        value: "plate glass"
+    },
+    {
+        group: "Ceiling Height",
+        value: "low"
+    },
+    {
+        group: "Frontage",
+        value: "small"
+    },
+];
 
 export default {
     reportCreationData: _reportCreationData,
-    numberOfCommercialUnits: 5,
-    stateValues: _stateValues,
-    groupName: _groupName,
-    otherValue: "Other choice"
+    numberOfCommercialUnits: 1,
+    arrayValuesAndGroup: _arrayValuesAndGroup
 };
