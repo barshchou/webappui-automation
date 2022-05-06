f7eb02e2b82ddc57ce1fd0ef3c856102db5a7d00
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-05-06 08:33:47 +0300

    [QA-5300] Done QA-4569

---
+++ cypress/fixtures/not_full_reports/property/commercial_units/QA-4569.fixture.ts
@@ -0,0 +1,58 @@
+import ReportDataCreator from "../../../data_creator/reportData.creator";
+import Enums from "../../../../enums/enums";
+
+const _reportCreationData: BoweryAutomation.ReportCreationData = ReportDataCreator.getReportData("4569", {
+    incomeValue: Enums.INCOME_TYPE.BOTH,
+    conclusionValue: Enums.VALUE_CONCLUSION_TYPE.AS_COMPLETE
+});
+
+type groupValue = {
+    group:BoweryReports.CommercialUnitsGroups, 
+    value: BoweryReports.CommercialUnits.GroupsValues,
+}
+
+const _arrayValuesAndGroup: Array<groupValue> = [
+    {
+        group: "Use",
+        value: "retail"
+    },
+    {
+        group: "State",
+        value: "finished"
+    },
+    {
+        group: "Location",
+        value: "corner"
+    },
+    {
+        group: "Street Type",
+        value: "side street"
+    },
+    {
+        group: "Floor",
+        value: "belowGrade"
+    },
+    {
+        group: "Grade",
+        value: "atGrade"
+    },
+    {
+        group: "Facade",
+        value: "plate glass"
+    },
+    {
+        group: "Ceiling Height",
+        value: "low"
+    },
+    {
+        group: "Frontage",
+        value: "small"
+    },
+];
+
+export default {
+    reportCreationData: _reportCreationData,
+    titleValue: "Utilities Description",
+    numberOfCommercialUnits: 1,
+    arrayValuesAndGroup: _arrayValuesAndGroup
+};
---
+++ cypress/integration/not_full_reports/property/commercial_units/QA-4569.spec.ts
@@ -0,0 +1,56 @@
+import testData from "../../../../fixtures/not_full_reports/property/commercial_units/QA-4569.fixture";
+import {createReport, deleteReport} from "../../../../actions/base/baseTest.actions";
+import {_NavigationSection} from "../../../../actions/base";
+import {Property} from "../../../../actions";
+
+describe("Verify the Unsaved changes modal functionality on the Commercial Units page", () => {
+    before("Login, create report", () => {
+        createReport(testData.reportCreationData);
+    });
+
+    it("Test body", () => {
+        cy.stepInfo(`1. Fill in the editable fields with values or/and check check-boxes 
+            or/and click the radio button and do NOT click on the Save button.`);
+        _NavigationSection.navigateToCommercialUnits();
+
+        testData.arrayValuesAndGroup.forEach(item => {
+            Property._CommercialUnits.clickRadioButtonByValueAndUnitIndex(item.group, item.value);
+            Property._CommercialUnits.verifyProgressBarNotExist();
+            Property._CommercialUnits.verifyRadioIsChecked(item.group, item.value);
+        });
+        
+        cy.stepInfo("2. Try to proceed on any other page and verify that the Unsaved changes modal is displayed.");
+        _NavigationSection.navigateToPropertySummary();
+
+        cy.stepInfo("3. Click on the Yes button and verify that the changes are saved on the Commercial Units page.");
+        _NavigationSection.navigateToCommercialUnits();
+
+        testData.arrayValuesAndGroup.forEach(item => {
+            Property._CommercialUnits.verifyRadioIsChecked(item.group, item.value);
+        });
+
+        deleteReport(testData.reportCreationData.reportNumber);
+
+        cy.stepInfo(`4. Repeat step 1, try to proceed on any other page from the Commercial Units 
+            page and verify that the Unsaved changes modal is displayed.`);
+        createReport(testData.reportCreationData);
+        _NavigationSection.navigateToCommercialUnits();
+
+        testData.arrayValuesAndGroup.forEach(item => {
+            Property._CommercialUnits.clickRadioButtonByValueAndUnitIndex(item.group, item.value);
+            Property._CommercialUnits.verifyRadioIsChecked(item.group, item.value);
+        });
+
+        _NavigationSection.clickAmenitiesButton()
+            .clickNoButton();
+
+        cy.stepInfo("5. Click on the No button and verify that the changes are NOT saved on the Commercial Units page.");
+        _NavigationSection.clickCommercialUnits();
+
+        testData.arrayValuesAndGroup.forEach(item => {
+            Property._CommercialUnits.verifyRadioIsNotChecked(item.group, item.value);
+        });
+        
+        deleteReport(testData.reportCreationData.reportNumber);
+    });
+});
