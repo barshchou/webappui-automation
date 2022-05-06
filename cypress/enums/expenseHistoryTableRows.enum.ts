9c79237037744513d2a1f82e213e48aa9d1dd6fc
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-05-06 10:44:48 +0300

    tableExpenseHistory edited

--- cypress/enums/expenseHistoryTableRows.enum.ts
+++ cypress/enums/expenseHistoryTableRows.enum.ts
@@ -1,7 +1,25 @@
 const tableExpenseHistory = {
 
-    operatingExpensesCell: ["grossRevenue", "realEstateTaxes", "insurance", "electricity", "fuel", "waterAndSewer", "repairsAndMaintenance", "payrollAndBenefits",
-        "generalAndAdministrative", "legalAndProfessionalFees", "miscellaneous", "management", "reserves"],
+ /*   operatingExpensesCell: ["grossRevenue", "realEstateTaxes", "insurance", "electricity", "fuel", "waterAndSewer", "repairsAndMaintenance", "payrollAndBenefits",
+        "generalAndAdministrative", "legalAndProfessionalFees", "miscellaneous", "management", "reserves"],*/
+     //   operatingExpensesCell: [
+            grossRevenue: "grossRevenue", 
+            realEstateTaxes: "realEstateTaxes", 
+            insurance:  "insurance", 
+            electricity: "electricity", 
+            fuel:  "fuel", 
+            waterAndSewer: "waterAndSewer", 
+            repairsAndMaintenance:  "repairsAndMaintenance", 
+            payrollAndBenefits:  "payrollAndBenefits",
+            generalAndAdministrative:  "generalAndAdministrative", 
+            legalAndProfessionalFees : "legalAndProfessionalFees", 
+            miscellaneous :   "miscellaneous", 
+            management : "management", 
+            reserves:   "reserves",
+ //   ]
+
+
+
 
     totalOperatingExpensesCell: ["total", "totalExcludingTaxes", "noi"]
 };
