670d2bf9cb42d4b5299c4f9270f8f2ede6c1a4b0
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-05-09 23:59:18 +0300

    unnecessary  locators was deleted

--- cypress/pages/income/expenseHistory.page.ts
+++ cypress/pages/income/expenseHistory.page.ts
@@ -11,30 +11,10 @@ class ExpenseHistoryPage extends BasePage {
     get addExpenseYearButton() { return cy.get("[data-qa=expense-history-new-expense-year-btn]"); }
 
     get grossRevenueCheckboxes() { return cy.get("[data-qa*='grossRevenue.reported'] input"); }
-
-    get grossRevenueInputs() { return cy.get("[row-id='grossRevenue'] div.right-aligned-cell:not([col-id=average])"); }
-
-    get realEstateTaxesInputs() { return cy.get("[row-id='realEstateTaxes'] div.right-aligned-cell:not([col-id=average])"); }
-
-    get insuranceInputs() { return cy.get("[row-id='insurance'] div.right-aligned-cell:not([col-id=average])"); }
-
-    get electricityInputs() { return cy.get("[row-id='electricity'] div.right-aligned-cell:not([col-id=average])"); }
-
-    get fuelInputs() { return cy.get("[row-id='fuel'] div.right-aligned-cell:not([col-id=average])"); }
-
-    get repairsAndMaintenanceInputs() { return cy.get("[row-id='repairsAndMaintenance'] div.right-aligned-cell"); }           //!!!!!
-
+ 
     get fuelCheckboxes() { return cy.get("[data-qa*='expenses.fuel.reported'] input"); }
 
     get waterSewerCheckboxes() { return cy.get("[data-qa*='expenses.waterAndSewer.reported'] input"); }
-
-    get payrollBenefitsInputs() { return cy.get("[row-id='payrollAndBenefits'] div.right-aligned-cell:not([col-id=average])"); }
-
-    get totalOpExpenseCells() { return cy.get("[row-id=total] div.right-aligned-cell:not([col-id=average])"); }
-
-    get toeExclRealEstTaxesCells() { return cy.get("[row-id=totalExcludingTaxes] div.right-aligned-cell:not([col-id=average])"); }
-
-    get netOpIncomeCells() { return cy.get("[row-id=noi] div.right-aligned-cell:not([col-id=average])"); }
 
     get expenseHistoryCommentary() { return cy.get("[data-qa^='expenseHistoryDiscussion.commentary']"); }
 
