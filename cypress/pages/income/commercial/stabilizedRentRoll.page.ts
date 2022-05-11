05c4fb4555cbb102b5f7de2f61990c5e175cae9a
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-05-11 14:53:08 +0300

    [QA-5148] Moved monthlyRentPerSFCells from Commercial StabilizedRentRoll page to SharedComponent and updated methods in actions

--- cypress/pages/income/commercial/stabilizedRentRoll.page.ts
+++ cypress/pages/income/commercial/stabilizedRentRoll.page.ts
@@ -7,8 +7,6 @@ class StabilizedRentRollPage extends BasePage{
 
     get stabilizedRentRollHeaderSection() {return cy.get("*[data-qa='stabilizedRentRoll']");}
 
-    get monthlyRentPsfCells() {return cy.xpath("//*[contains(@class, 'htNumeric')]|(//*[contains(@data-qa, 'monthlyRentPsf')])[position() < last()]");}
-
     get stabilizedCommercialIncomeTextArea() {return cy.xpath("//*[.='Stabilized Commercial Income Discussion']//following::*[@data-slate-editor][1]");}
 
     get narrativeSuggestionsList() {return cy.get("[data-qa='narrative-suggestions-list'] > ul");}
