fee6c3e23f6f32914267da31de3b0be00d22bca1
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-05-12 08:48:09 +0300

    [QA-5354] Done QA-4104

--- cypress/integration/not_full_reports/report/key_info/QA-4104.spec.ts
+++ cypress/integration/not_full_reports/report/key_info/QA-4104.spec.ts
@@ -4,15 +4,13 @@ import {_NavigationSection} from "../../../../actions/base";
 import { Report, ReviewExport } from "../../../../actions";
 
 
-describe("[QA-4104] Verify the Market Value generated commentary", () => {
+describe("[QA-4104] Verify the Market Value generated commentary", { tags: '@to_check_export' }, () => {
 
-    before("Login, create report", () => {
+    it("Test body", () => {
         cy.stepInfo(`1. Create report while creating set the same Job number as report from SalesForce has (e.g. JOB-1764459005) 
             Make sure that there is no Inspection Date in the Salesforce job`);
         createReport(testData.reportCreationData);
-    });
 
-    it("Test body", () => {
         cy.stepInfo("2. Go to Report > Key Info > Definition of Market Value");
         _NavigationSection.navigateToReportInformation();
         Report._KeyInfo.clickYesButton();
@@ -31,48 +29,13 @@ describe("[QA-4104] Verify the Market Value generated commentary", () => {
         deleteReport(testData.reportCreationData.reportNumber);
     });
 
-    // it("Check export", () => {
-    //     cy.task("getFilePath", {_reportName: testData.reportCreationData.reportNumber, _docx_html: "html"}).then(file => {
-    //         cy.log(<string>file);
-    //         cy.stepInfo("5. Check that this sentence exports in the Introduction, replacing the boilerplate sentence currently exported there");
-    //         cy.visit(<string>file);
-
-    //         cy.contains("Definition of Market Value").next().scrollIntoView();
-    //     });
-    // });
-
-    it("Check export",() => {
-        cy.task("getFilePath",
-        {_reportName: testData.reportCreationData.reportNumber, _docx_html: "html"}
-        ).then(file => {
+    it("Check export", () => {
+        cy.task("getFilePath", {_reportName: testData.reportCreationData.reportNumber, _docx_html: "html"}).then(file => {
             cy.log(<string>file);
-            cy.stepInfo(`
-            2. Go to the Commercial Stabilized Rent Roll table in the export and check:
-                removed the leading # column; 
-                removed the decimal place for Annual Rent, represent as a whole number;
-                removed the decimal place for Monthly Rent, represent as a whole number.
-            `);
+            cy.stepInfo("5. Check that this sentence exports in the Introduction, replacing the boilerplate sentence currently exported there");
             cy.visit(<string>file);
-            
-            cy.contains("Lease Structure").prev().scrollIntoView().within(() => {
-                cy.get("tr").eq(0).find("p").eq(0).should("not.have.text","#");
-                
-                cy.get("tr").eq(1).find("p").eq(0).invoke("attr","text").then(value => {
-                    expect(Number.isInteger(Number.parseInt(value)),
-                    "The value in cell is not Number"
-                    ).to.be.equal(false);
-                });
 
-                cy.get("tr").eq(1).find("p").filter(':contains("$0")').then(value => {
-                    value.toArray().slice(0,1).forEach(elem => {
-                        expect(
-                            (elem.textContent.endsWith(".00")),
-                            "Not ends with decimal part"
-                        ).to.be.equal(false);
-                    });
-                });
-            });
+            cy.contains("Definition of Market Value").next().next().scrollIntoView().should("have.text", testData.verifyText);
         });
-    });
-    
+    }); 
 });
