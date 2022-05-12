23007185a38865bac7400a4c34086982569e7ad9
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-05-12 08:34:41 +0300

    [QA-5354] Need to refactor verify export

--- cypress/integration/not_full_reports/report/key_info/QA-4104.spec.ts
+++ cypress/integration/not_full_reports/report/key_info/QA-4104.spec.ts
@@ -1,7 +1,7 @@
 import testData from "../../../../fixtures/not_full_reports/report/key_info/QA-4104.fixture";
 import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
 import {_NavigationSection} from "../../../../actions/base";
-import { Report } from "../../../../actions";
+import { Report, ReviewExport } from "../../../../actions";
 
 
 describe("[QA-4104] Verify the Market Value generated commentary", () => {
@@ -24,8 +24,55 @@ describe("[QA-4104] Verify the Market Value generated commentary", () => {
         Report._KeyInfo.Page.iconDefinitionOfMarketValue.trigger("mouseover");
         Report._KeyInfo.Page.tooltipDefinitionOfMarketValue.should("include.text", testData.tooltipText);
 
-        // Need add logic to verify export
+        _NavigationSection.openReviewAndExport();
+        ReviewExport.generateDocxReport().waitForReportGenerated()
+        .downloadAndConvertDocxReport(testData.reportCreationData.reportNumber);
 
         deleteReport(testData.reportCreationData.reportNumber);
     });
+
+    // it("Check export", () => {
+    //     cy.task("getFilePath", {_reportName: testData.reportCreationData.reportNumber, _docx_html: "html"}).then(file => {
+    //         cy.log(<string>file);
+    //         cy.stepInfo("5. Check that this sentence exports in the Introduction, replacing the boilerplate sentence currently exported there");
+    //         cy.visit(<string>file);
+
+    //         cy.contains("Definition of Market Value").next().scrollIntoView();
+    //     });
+    // });
+
+    it("Check export",() => {
+        cy.task("getFilePath",
+        {_reportName: testData.reportCreationData.reportNumber, _docx_html: "html"}
+        ).then(file => {
+            cy.log(<string>file);
+            cy.stepInfo(`
+            2. Go to the Commercial Stabilized Rent Roll table in the export and check:
+                removed the leading # column; 
+                removed the decimal place for Annual Rent, represent as a whole number;
+                removed the decimal place for Monthly Rent, represent as a whole number.
+            `);
+            cy.visit(<string>file);
+            
+            cy.contains("Lease Structure").prev().scrollIntoView().within(() => {
+                cy.get("tr").eq(0).find("p").eq(0).should("not.have.text","#");
+                
+                cy.get("tr").eq(1).find("p").eq(0).invoke("attr","text").then(value => {
+                    expect(Number.isInteger(Number.parseInt(value)),
+                    "The value in cell is not Number"
+                    ).to.be.equal(false);
+                });
+
+                cy.get("tr").eq(1).find("p").filter(':contains("$0")').then(value => {
+                    value.toArray().slice(0,1).forEach(elem => {
+                        expect(
+                            (elem.textContent.endsWith(".00")),
+                            "Not ends with decimal part"
+                        ).to.be.equal(false);
+                    });
+                });
+            });
+        });
+    });
+    
 });
