2a1567d14a39e66c10e05ff3b96fbe6176e87add
Ernst-D <ernst.dzerevanko@gmail.com>
2022-05-13 17:49:29 +0400

    [QA-5383] removed const utils, added alias utils and refactored QA-4248 spec

--- cypress/integration/not_full_reports/sales/find_comps/QA-4248.spec.ts
+++ cypress/integration/not_full_reports/sales/find_comps/QA-4248.spec.ts
@@ -2,10 +2,12 @@ import Homepage from "../../../../actions/base/homepage.actions";
 import testData from "../../../../fixtures/not_full_reports/sales/find_comps/QA-4248.fixture";
 import NavigationSection from "../../../../actions/base/navigationSection.actions";
 import Sales from "../../../../actions/sales/sales.manager";
-import { getEnvUrl } from "../../../../../utils/env.utils";
 import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
+import { Tag } from "../../../../utils/tags.utils";
+import { Alias } from "../../../../utils/alias.utils";
 
-describe("Verify the Comps can be added by entering the existing Report ID in the modal", () => {
+describe("Verify the Comps can be added by entering the existing Report ID in the modal", 
+{ tags:[ Tag.comp_plex, Tag.sales, Tag.find_comps ] }, () => {
     before("Login, create report", () => {
         createReport(testData.reportCreationData);
     });
@@ -15,21 +17,24 @@ describe("Verify the Comps can be added by entering the existing Report ID in th
         Sales.FindComps.selectCompFromMapByAddress(testData.comparable.address)
             .clickSaveContinueButton();
         Sales.CreateCompMap.verifyPageOpened();
-        cy.url().then(url => {
-            const reportID = url.replace("/sales-comps-map", "").replace(`${getEnvUrl()}/report/`, "");
-            cy.log(`Current report ID is ${reportID}`);
+        cy.get(`@${Alias.reportId}`).then(reportId => {
+            cy.log(`Current report ID is ${reportId}`);
             Sales.CreateCompMap.returnToHomePage();
             Homepage.verifyThatPageIsOpened()
                 .verifyProgressBarNotExist();
             Homepage.createReport(testData.reportCreationData);
             NavigationSection.navigateToFindComps();
             Sales.FindComps.clickImportComparableButton()
-                .enterReportToSearchComp(reportID);
+                .enterReportToSearchComp(<any>reportId);
         });
         Sales.FindComps.clickSearchButton()
             .selectAllCompsForImport()
             .clickImportCompsFromReportButton();
         Sales.FindComps.verifyAddedCompAddress(testData.comparable.address);
+        // cy.get("@salesEventId").then(val => {
+        //     cy.log(<any>val);
+        // });
+        cy.pause();
         deleteReport(testData.reportCreationData.reportNumber);
         cy.reload();
         Homepage.deleteReport(testData.reportCreationData.reportNumber);
---
+++ cypress/utils/alias.utils.ts
@@ -0,0 +1,5 @@
+const _reportId = `reportId_${Cypress._.random(0, 1e6)}`;
+export const Alias = {
+    reportId: _reportId,
+    
+};
