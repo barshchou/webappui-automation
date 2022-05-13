9abcac20f005e4511cd1d5e1d02c9825c4ede35b
Ernst-D <ernst.dzerevanko@gmail.com>
2022-05-13 20:41:42 +0400

    [misc] refactored aliases

--- cypress/utils/alias.utils.ts
+++ cypress/utils/alias.utils.ts
@@ -1,5 +1,8 @@
-const _reportId = `reportId_${Cypress._.random(0, 1e6)}`;
+const _alias = (aliasName:string) => `${aliasName}_${Cypress._.random(0, 1e6)}`;
+
 export const Alias = {
-    reportId: _reportId,
-    
+    reportId: _alias("reportId"),
+    gqlRequest:_alias("gqlRequest"),
+    salesEventId: _alias("salesEventId"),
+    salesComps_eventIds: _alias("salesComps_eventIds")
 };
