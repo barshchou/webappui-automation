9505bb72bcf36847052e110001bc0d2833e5f625
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-05-17 08:43:02 +0300

    [QA-5390] Created QA-4168.fixture

---
+++ cypress/fixtures/not_full_reports/sales/find_comps/QA-4168.fixture.ts
@@ -0,0 +1,47 @@
+import ReportDataCreator from "../../../data_creator/reportData.creator";
+import Enums from "../../../../enums/enums";
+
+export default {
+    reportCreationData: ReportDataCreator.getReportData("4139&43&44&4482&83", {
+            incomeValue: Enums.INCOME_TYPE.BOTH,
+            conclusionValue: Enums.VALUE_CONCLUSION_TYPE.AS_IS
+        }),
+    compAddress: "140 East 13 Street",
+    condition: "Shell",
+    comparableType: "Mixed-Use",
+    spec4139: {
+        numberOfUnitsDefault: 4,
+        regularNum: 10000,
+        decimalNum: -38.54,
+        nonNumberValue: "Some not number",
+        longValue: 55555555555555528
+    },
+    spec4143: {
+        regularNumber: 8,
+        regularNumOverThousand: 10000,
+        decimalNum: -38.54,
+        nonNumberValue: "Some not number",
+        longValue: 55555555555555528
+    },
+    spec4144: {
+        regularNumber: 850,
+        regularNumOverThousand: 10000,
+        decimalNum: -38.54,
+        nonNumberValue: "Some not number",
+        longValue: 55555555555555528
+    },
+    spec4482: {
+        regularNumber: 850,
+        regularNumOverThousand: 10000,
+        decimalNum: -38.54,
+        nonNumberValue: "Some not number",
+        longValue: 55555555555555528
+    },
+    spec4483: {
+        regularNumber: 999,
+        regularNumOverThousand: 9999,
+        decimalNum: -1258.54,
+        nonNumberValue: "Some not number",
+        longValue: 55555555555555528
+    }
+};
