7c37f3f9a74a2fa63101f5079e06a463bed52d55
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-05-18 12:16:49 +0300

    [QA-5417] Done QA-4623.fixture

---
+++ cypress/fixtures/not_full_reports/report/key_info/QA-4623.fixture.ts
@@ -0,0 +1,14 @@
+import Enums from "../../../../enums/enums";
+import ReportDataCreator from "../../../data_creator/reportData.creator";
+
+const reportCreationFixture = () => {
+    return ReportDataCreator.setAddress()
+        .setReportNumber("1764459100", true)
+        .setTemplateValue(Enums.TEMPLATE_TYPE.NOT_FREDDIE_MAC)
+        .setIncomeValue(Enums.INCOME_TYPE.BOTH)
+        .setConclusionValue(Enums.VALUE_CONCLUSION_TYPE.AS_COMPLETE).build();
+};
+
+export default {
+    reportCreationData: reportCreationFixture(),
+};
