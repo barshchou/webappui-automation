6fc855fc3e450c218749e573e0e4880502b02b94
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-05-18 09:31:28 +0300

    [QA-5391] Added new locator

--- cypress/pages/sales/findComps.page.ts
+++ cypress/pages/sales/findComps.page.ts
@@ -85,6 +85,8 @@ class FindCompsPage extends BasePage {
     get newCompSaveAndCloseButton() {return cy.xpath("//button[.='Save & Close']");}
 
     get internalNotesTextArea() {return cy.xpath("//*[text()='Internal Notes']//following::*[@class='ant-input'][1]");}
+
+    get appraiserCommentaryTextArea() {return cy.xpath("//*[text()='Appraiser Commentary']//following::*[contains(@class, 'ant-input')][1]");}
 }
 
 export default new FindCompsPage();
