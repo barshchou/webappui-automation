597d32f9564206ce5a0ecf715098c721aa0a590f
Viachaslau.Barshchou <Viachaslau.Barshchou@boweryvaluation.com>
2022-05-18 16:40:49 +0300

    [QA-5409] added 4522 test

---
+++ cypress/integration/not_full_reports/income/expense_forecast/QA-4522.spec.ts
@@ -0,0 +1,51 @@
+import testData from "../../../../fixtures/not_full_reports/income/expense_forecast/QA-4522.fixture";
+import { _NavigationSection } from "../../../../actions/base";
+import { Income } from "../../../../actions";
+import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
+import { Tag } from "../../../../utils/tags.utils";
+
+describe(`Comparable Min, Max, Avg values for Water & Sewer Per Unit are correctly calculated and displayed`,
+    { tags:[ Tag.income, Tag.expense_forecast, Tag.snapshot_tests ] }, () => {
+
+    before("Login, create report", () => {
+        createReport(testData.reportCreationData);
+    });
+
+    it("Test body", () => {
+        cy.stepInfo("1. Navigate to Find Comps and add few comparables");
+        _NavigationSection.navigateToComparableExpenses();
+        
+        cy.stepInfo(`2. Add several comps (via Search, Filter or Add blank column) and make sure that Insurance  
+        and Residential Units fields are filled in for all added columns and save changes`);
+        testData.comparables.forEach((comp) => {
+            Income._ComparableExpenses.clickAddBlankColumnButton()
+                .enterAddressByColumnIndex(comp.address)
+                .enterCellDollarValueByColumnIndex(Income._ComparableExpenses.Page.getUnifiedEditableAndTotalCells(testData.insuranceItem.name),
+                    comp.insurance)
+                .enterResidentialUnitsByColumnIndex(comp.resUnits);
+        });
+
+        cy.stepInfo("3. Go to Expense Forecast and make sure that Per Unit radiobutton is selected for Insurance card");
+        _NavigationSection.Actions.navigateToExpenseForecast();
+        Income._ExpenseForecastActions.chooseForecastItemBasis(testData.insuranceItem)
+            .verifyForecastItemBasis(testData.insuranceItem);
+
+        cy.stepInfo(`4.1 Check Comp Min, Comp Max and Comp Avg values for Insurance card. They should be calculated as: 
+        Min, Max and Avg of range of values [Comp_Insurance / Square Feet]`);
+        Income._ExpenseForecastActions.verifyForecastItemCompMin(testData.insuranceItem, testData.comparables)
+            .verifyForecastItemCompAverage(testData.insuranceItem, testData.comparables)
+            .verifyForecastItemCompMax(testData.insuranceItem, testData.comparables)
+            .hideExpenseForecastHeader();
+
+        cy.stepInfo("4.2 Check Comp Min, Comp Max and Comp Avg values for Water & Sewer card. They should be correctly displayed on a slidebar");
+        Income._ExpenseForecastActions.matchElementSnapshot(
+            Income._ExpenseForecastActions.Page.forecastItemCard(
+                Income._ExpenseForecastActions.getItemNameForAverage(
+                    testData.insuranceItem.name)), 
+                    testData.insuranceCardSnapshotName, { padding: [ 10, 100 ] }
+        );
+
+        cy.stepInfo("5. Delete report");
+        deleteReport(testData.reportCreationData.reportNumber);
+    });
+});
