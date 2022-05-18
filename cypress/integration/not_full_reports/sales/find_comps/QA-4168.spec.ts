d955052c1bf91e29be46ec91a2bb9ac73ef21be5
Ernst-D <ernst.dzerevanko@gmail.com>
2022-05-18 14:36:13 +0400

    [QA-5391] small changes to QA-4168

--- cypress/integration/not_full_reports/sales/find_comps/QA-4168.spec.ts
+++ cypress/integration/not_full_reports/sales/find_comps/QA-4168.spec.ts
@@ -5,31 +5,28 @@ import testData from "../../../../fixtures/not_full_reports/sales/find_comps/QA-
 import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
 
 describe("[QA-4168] Verify the Internal Notes field", { tags: [ Tag.sales, Tag.find_comps, Tag.comp_plex ] }, () => {
-    before("Login, create report", () => {
+    before("Login, create report, precondition", () => {
         createReport(testData.reportCreationData);
-    });
-
-    it("Test body", () => {
         cy.stepInfo(`Preconditions: 
             -Click on the Create Comp button > Enter New Comparable Address > Select the Address from the Search Results and click on the Create 
             New button to proceed to the Enter Property Information form. 
             -Fill in all required fields and click on the Continue button to proceed to the Enter Sale Information form.
             -Fill in all required fields and click on the Continue button to proceed to the Enter Property Description form.`);
-
         _NavigationSection.navigateToFindComps();
-
         Sales._FindComps.openAddNewComparableFormSearchResult(testData.compAddress)
             .selectDropdownOptionNewComp(Sales._FindComps.Page.conditionDropdown, testData.selectItems.condition);
         Sales._FindComps.Page.createCompNumberCommercialUnits.type(`${testData.units.numberOfUnits}`);
         Sales._FindComps.Page.commercialAreaNewComp.type(`${testData.units.numberOfUnits}`);
         Sales._FindComps.Page.newCompContinueButton.click();
-        Sales._FindComps.Page.SaleDateCalendarNewComp.click().type(`${testData.saleInfo.saleDate}{enter}`);
+        Sales._FindComps.Actions.SaleInfo.selectSaleDate();
         Sales._FindComps.Page.BuyerGranteeNewComp.type(testData.saleInfo.buyer);
         Sales._FindComps.Page.SellerGrantor.type(testData.saleInfo.seller);
         Sales._FindComps.selectDropdownOptionNewComp(
             Sales._FindComps.Page.SourceInput, testData.selectItems.source);
         Sales._FindComps.Page.newCompContinueButton.click();
+    });
 
+    it("Test body", () => {
         cy.stepInfo(` 1.Verify the Internal Notes is free text input type;
             -Try to enter any numerical / non-integer / text value;
             -Try to copy-paste;
@@ -40,7 +37,6 @@ describe("[QA-4168] Verify the Internal Notes field", { tags: [ Tag.sales, Tag.f
         Sales._FindComps.Actions.PropertyDescription.enterInternalNotes(testData.verifyTextValue);
         Sales._FindComps.Page.internalNotesTextArea.clear().invoke("val", testData.verifyTextValue);
         cy.contains(testData.verifyTextUnderTextArea).should("be.visible");
-
         deleteReport(testData.reportCreationData.reportNumber);
     });
 });
