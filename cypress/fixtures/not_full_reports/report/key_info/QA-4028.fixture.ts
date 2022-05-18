8a8c76d67726ed719c505ee9d5ee82fed79a6647
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-05-18 14:01:08 +0300

    [QA-5418] Removed comments

--- cypress/fixtures/not_full_reports/report/key_info/QA-4028.fixture.ts
+++ cypress/fixtures/not_full_reports/report/key_info/QA-4028.fixture.ts
@@ -1,16 +1,6 @@
 import Enums from "../../../../enums/enums";
-import ReportDataCreator from "../../../data_creator/reportData.creator";
-
-// const reportCreationFixture = conclusionValue => {
-//     return ReportDataCreator.setAddress()
-//         .setReportNumber("1764459100", true)
-//         .setTemplateValue(Enums.TEMPLATE_TYPE.NOT_FREDDIE_MAC)
-//         .setIncomeValue(Enums.INCOME_TYPE.BOTH)
-//         .setConclusionValue(conclusionValue).build();
-// };
 
 export default {
-    // reportCreationData: reportCreationFixture(),
     dateType: "dueDate",
     verifyValue: "03-16-2021",
     conclusionValues: [
