b69fe8c2d4fa663464687802dd04aa35225b2804
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-05-20 17:34:17 +0300

    complete test added

---
+++ cypress/fixtures/not_full_reports/income/expense_forecast/QA-4520.fixture.ts
@@ -0,0 +1,29 @@
+import ReportDataCreator from "../../../data_creator/reportData.creator";
+
+const _insuranceItem: BoweryReports.ForecastItem = {
+    name: "insurance",
+    basis: "sf"
+};
+
+export default {
+    reportCreationData: ReportDataCreator.getReportData("4520"),
+    comparables: [
+        {
+            address: "6001 S Sacramento Ave",
+            squareFeet: 6608,
+            insurance: 10600
+        },
+        {
+            address: "7955 S Emerald Ave",
+            squareFeet: 9000,
+            insurance: 7777
+        },
+        {
+            address: "7613 S Kingston Ave",
+            squareFeet: 9750,
+            insurance: 17777
+        }
+    ],
+    insuranceItem: _insuranceItem,
+    insuranceCardSnapshotName: "Insurance_Forecast_Item_Component"
+};
