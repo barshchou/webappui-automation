ae430a5a87de25bcaf4f4141414f70dfc04dc004
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-05-23 15:41:11 +0300

    tests combained

--- cypress/fixtures/not_full_reports/property/commercial_units/QA-4543&44&47.fixture.ts
+++ cypress/fixtures/not_full_reports/property/commercial_units/QA-4543&44&47.fixture.ts
@@ -1,7 +1,7 @@
 import ReportDataCreator from "../../../data_creator/reportData.creator";
 import Enums from "../../../../enums/enums";
 
-const _reportCreationData: BoweryAutomation.ReportCreationData = ReportDataCreator.getReportData("4544", {
+const _reportCreationData: BoweryAutomation.ReportCreationData = ReportDataCreator.getReportData('4543&44&47', {
     incomeValue: Enums.INCOME_TYPE.BOTH,
     conclusionValue: Enums.VALUE_CONCLUSION_TYPE.AS_COMPLETE
 });
@@ -9,5 +9,5 @@ const _reportCreationData: BoweryAutomation.ReportCreationData = ReportDataCreat
 export default {
     reportCreationData: _reportCreationData,
     numberOfCommercialUnits: 2,
-    text: "Text for test",
+    text: "Text for test"
 };
--- cypress/integration/not_full_reports/property/commercial_units/QA-4543&44&47.spec.ts
+++ cypress/integration/not_full_reports/property/commercial_units/QA-4543&44&47.spec.ts
@@ -1,10 +1,10 @@
-import testData from "../../../../fixtures/not_full_reports/property/commercial_units/QA-4543.fixture";
+import testData from "../../../../fixtures/not_full_reports/property/commercial_units/QA-4543&44&47.fixture";
 import { Property } from "../../../../actions";
 import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
 import { _NavigationSection } from "../../../../actions/base";
 import { Tag } from "../../../../utils/tags.utils";
 
-describe("Verify the Edit button functionality",
+describe("[QA-4543], [QA-4544], [QA-4547] Verify the buttons functionality on Property > Commercial Units page.",
     { tags: [ Tag.property, Tag.commercial_units ] }, () => {
 
         before("Report creation and several commercial units addition", () => {
@@ -14,8 +14,7 @@ describe("Verify the Edit button functionality",
         });
 
         it("Test body", () => {
-
-            cy.stepInfo("1. Verify that the Edit button is displayed on the Property > Commercial Units page.");
+            cy.stepInfo("1.[QA-4543] Verify that the Edit button is displayed on the Property > Commercial Units page.");
             _NavigationSection.navigateToCommercialUnits();
             Property._CommercialUnits.verifyThatPageIsOpened();
             Property._CommercialUnits.Page.formEditBtn(0).should('exist');
@@ -28,7 +27,21 @@ describe("Verify the Edit button functionality",
             Property._CommercialUnits.Page.formCancelButton(0).should('exist');
             Property._CommercialUnits.Page.formRevertToOriginalBtn(0).should('exist');
             Property._CommercialUnits.Page.formSaveBtn(0).should('exist');
+        
+            cy.stepInfo("4.[QA-4544] Click on the Cancel button and verify that the form closes form without saving changes with no warning message.");
+            Property._CommercialUnits.Page.commercialUnitSFDiscussionTextArea.clear().type(testData.text);
+            Property._CommercialUnits.Page.formCancelButton(0).scrollIntoView().click();
+            Property._CommercialUnits.Page.commercialUnitSFDiscussionText(testData.text).should('not.exist');
+    
+            cy.stepInfo("5.[QA-4547] Edit comment and click on the Save button.");
+            Property._CommercialUnits.Page.formEditBtn(0).scrollIntoView().click();  
+            Property._CommercialUnits.Page.commercialUnitSFDiscussionTextArea.clear().type(testData.text);
+            Property._CommercialUnits.Page.formSaveBtn(0).first().click();
 
+            cy.stepInfo("6. Verify that the changes from step 5 are saved.");
+            Property._CommercialUnits.Page.commercialUnitSFDiscussionText(testData.text).should('exist');
+           
             deleteReport(testData.reportCreationData.reportNumber);
+            
         });
     });
