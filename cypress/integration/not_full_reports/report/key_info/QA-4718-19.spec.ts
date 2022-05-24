a1406a153bb95a61ed1e00a16a7bde2a71bbd071
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-05-24 18:15:19 +0300

    [QA-5504] Tried to use cycle for

--- cypress/integration/not_full_reports/report/key_info/QA-4718-19.spec.ts
+++ cypress/integration/not_full_reports/report/key_info/QA-4718-19.spec.ts
@@ -9,9 +9,11 @@ import testData from '../../../../fixtures/not_full_reports/report/key_info/QA-4
 describe(`[QA-4718-19] Verify the "Linked" chips dropdown in the new narrative component for Property Rights Appraised and Definition of Market Value sections`,
     { tags:[ Tag.report, Tag.key_info, Tag.check_export ] }, () => {
     it("Test body", () => {
-        testData.typesReport.forEach((report, index) => {
+        let i = 0;
+        let j = 0;
+        for (; i < testData.typesReport.length; i++) {
             cy.stepInfo("Login, create report");
-            createReport(reportCreationFixture(report, `_${index + 1}`));
+            createReport(reportCreationFixture(testData.typesReport[i], `_${i + 1}`));
     
             cy.stepInfo("1. Proceed to the Report > Key Info page.");
             _NavigationSection.navigateToReportInformation()
@@ -22,22 +24,60 @@ describe(`[QA-4718-19] Verify the "Linked" chips dropdown in the new narrative c
                 'Condition'.`);
             Report._KeyInfo.Page.formEditBtn(0).click();
             Report._KeyInfo.Page.formEditBtn(0).click();
-            testData.chips.forEach(chip => {
-                Report._KeyInfo.enterPropertyRightsAppraisedComment(`=${chip.typeSuggestValue}`, false, false, false);
-                Report._KeyInfo.clickNarrativeSuggestions(chip.suggestionName);
-                Report._KeyInfo.Page.textBoxPropertyRightsAppraised.should("include.text", chip.verifySuggest);
-                Report._KeyInfo.enterDefinitionMarketValue(`=${chip.typeSuggestValue}`, false, false, false);
-                Report._KeyInfo.clickNarrativeSuggestions(chip.suggestionName, 1);
-                Report._KeyInfo.Page.textBoxPropertyRightsAppraised.should("include.text", chip.verifySuggest);
-            });
+            for(; j < testData.chips.length; j++) {
+                Report._KeyInfo.enterPropertyRightsAppraisedComment(`=${testData.chips[j].typeSuggestValue}`, false, false, false);
+                Report._KeyInfo.clickNarrativeSuggestions(testData.chips[j].suggestionName);
+                Report._KeyInfo.Page.textBoxPropertyRightsAppraised.should("include.text", testData.chips[j].verifySuggest);
+                Report._KeyInfo.enterDefinitionMarketValue(`=${testData.chips[j].typeSuggestValue}`, false, false, false);
+                Report._KeyInfo.clickNarrativeSuggestions(testData.chips[j].suggestionName, 1);
+                Report._KeyInfo.Page.textBoxPropertyRightsAppraised.should("include.text", testData.chips[j].verifySuggest);
+            }
+            // testData.chips.forEach(chip => {
+            //     Report._KeyInfo.enterPropertyRightsAppraisedComment(`=${chip.typeSuggestValue}`, false, false, false);
+            //     Report._KeyInfo.clickNarrativeSuggestions(chip.suggestionName);
+            //     Report._KeyInfo.Page.textBoxPropertyRightsAppraised.should("include.text", chip.verifySuggest);
+            //     Report._KeyInfo.enterDefinitionMarketValue(`=${chip.typeSuggestValue}`, false, false, false);
+            //     Report._KeyInfo.clickNarrativeSuggestions(chip.suggestionName, 1);
+            //     Report._KeyInfo.Page.textBoxPropertyRightsAppraised.should("include.text", chip.verifySuggest);
+            // });
             Report._KeyInfo.Page.formSaveBtn(0).click();
             Report._KeyInfo.Page.formSaveBtn(1).click();
     
             _NavigationSection.openReviewAndExport();
             ReviewExport.generateDocxReport().waitForReportGenerated()
-            .downloadAndConvertDocxReport(reportCreationFixture(report, `_${index + 1}`).reportNumber);
-            deleteReport(reportCreationFixture(report, `_${index + 1}`).reportNumber);
-        });
+                .downloadAndConvertDocxReport(reportCreationFixture(testData.typesReport[i], `_${i + 1}`).reportNumber);
+            deleteReport(reportCreationFixture(testData.typesReport[i], `_${i + 1}`).reportNumber);
+            j = 0;
+        }
+        // testData.typesReport.forEach((report, index) => {
+        //     cy.stepInfo("Login, create report");
+        //     createReport(reportCreationFixture(report, `_${index + 1}`));
+    
+        //     cy.stepInfo("1. Proceed to the Report > Key Info page.");
+        //     _NavigationSection.navigateToReportInformation()
+        //         .clickYesButton();
+    
+        //     cy.stepInfo(`2.Enter the “=“ and verify the "Linked" chips dropdown for both sections: options 'Gross Building Area', 'Building Name', 'Property Type', 
+        //         'Residential Unit Count', 'Commercial Unit Count', 'Street Address', 'Street Name', 'Site Area', 'Year Built', 'Block', 'Lot', 'Concluded Cap Rate', 'Zones', 
+        //         'Condition'.`);
+        //     Report._KeyInfo.Page.formEditBtn(0).click();
+        //     Report._KeyInfo.Page.formEditBtn(0).click();
+        //     testData.chips.forEach(chip => {
+        //         Report._KeyInfo.enterPropertyRightsAppraisedComment(`=${chip.typeSuggestValue}`, false, false, false);
+        //         Report._KeyInfo.clickNarrativeSuggestions(chip.suggestionName);
+        //         Report._KeyInfo.Page.textBoxPropertyRightsAppraised.should("include.text", chip.verifySuggest);
+        //         Report._KeyInfo.enterDefinitionMarketValue(`=${chip.typeSuggestValue}`, false, false, false);
+        //         Report._KeyInfo.clickNarrativeSuggestions(chip.suggestionName, 1);
+        //         Report._KeyInfo.Page.textBoxPropertyRightsAppraised.should("include.text", chip.verifySuggest);
+        //     });
+        //     Report._KeyInfo.Page.formSaveBtn(0).click();
+        //     Report._KeyInfo.Page.formSaveBtn(1).click();
+    
+        //     _NavigationSection.openReviewAndExport();
+        //     ReviewExport.generateDocxReport().waitForReportGenerated()
+        //         .downloadAndConvertDocxReport(reportCreationFixture(report, `_${index + 1}`).reportNumber);
+        //     deleteReport(reportCreationFixture(report, `_${index + 1}`).reportNumber);
+        // });
     });
 
     it("Check export", () => {
@@ -46,7 +86,6 @@ describe(`[QA-4718-19] Verify the "Linked" chips dropdown in the new narrative c
                 cy.log(<string>file);
                 cy.stepInfo("3. Verify the linked chips on export for both sections");
                 cy.visit(<string>file);
-    
                 testData.chips.forEach(chip => {
                     cy.contains("Property Rights Appraised").next().scrollIntoView().should("include.text", chip.verifyExport);
                     cy.contains("Definition of Market Value").next().next().scrollIntoView().should("include.text", chip.verifySuggest);
