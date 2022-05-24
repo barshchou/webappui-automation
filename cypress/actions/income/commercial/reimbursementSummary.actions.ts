20b1fed55b393ba2a168c8be1cc35f237d46d95a
AlexeiMo <prestigio458@gmail.com>
2022-05-24 12:09:33 +0300

    [QA-5150] Create Commercial Reimbursement Summary page and actions files

---
+++ cypress/actions/income/commercial/reimbursementSummary.actions.ts
@@ -0,0 +1,9 @@
+import BaseActionsExt from "../../base/base.actions.ext";
+import commercialReimbursementSummaryPage from "../../../pages/income/commercial/reimbursementSummary.page";
+
+
+class CommercialReimbursementSummaryActions extends BaseActionsExt<typeof commercialReimbursementSummaryPage> {
+    
+}
+
+export default new CommercialReimbursementSummaryActions(commercialReimbursementSummaryPage);
