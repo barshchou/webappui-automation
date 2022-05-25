6ed6e95fd7fa5a67233c72eafb3000df9314b0cb
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-05-25 10:40:29 +0300

    locators added

--- cypress/pages/income/proForma.page.ts
+++ cypress/pages/income/proForma.page.ts
@@ -3,6 +3,14 @@ import BasePage from "../base/base.page";
 
 class ProFormaPage extends BasePage {
 
+    get columnHeaderIncome() {return cy.xpath('(//div[@role="columnheader"][@col-id="label"])');}
+
+    get columnHeaderTotal() {return cy.xpath('(//div[@role="columnheader"][@col-id="total"])');}
+
+    get columnHeaderPSF() {return cy.xpath('(//div[@role="columnheader"][@col-id="psf"])');}
+
+    get columnHeaderPerUnit() {return cy.xpath('(//div[@role="columnheader"][@col-id="perUnit"])');}
+
     get residentialVCLossLabelCell() {return cy.xpath('(//div[@row-id="Potential Gross Income_2"])[2]//following-sibling::div[@role="row"]').eq(0);}
 
     get residentialVCLossTotal() {return this.residentialVCLossLabelCell.children('[col-id="total"]');}
