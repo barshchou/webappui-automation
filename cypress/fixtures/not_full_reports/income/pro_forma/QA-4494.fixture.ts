994332eeb0981d8dd0ae2da90d5df02c0b76478b
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-05-25 23:59:41 +0300

    tests added

--- cypress/fixtures/not_full_reports/income/pro_forma/QA-4494.fixture.ts
+++
@@ -1,9 +0,0 @@
-import ReportDataCreator from "../../../data_creator/reportData.creator";
-
-const _reportCreationData: BoweryAutomation.ReportCreationData = ReportDataCreator.getReportData("4494");
-
-export default {
-    reportCreationData: _reportCreationData,
-    resUnit: 3,
-    monthlyRent: [ 60, 500, 450 ]
-};
---
+++ cypress/integration/not_full_reports/income/pro_forma/QA-4494&95&96&97.spec.ts
@@ -0,0 +1,72 @@
+import { _PotentialGrossIncome } from '../../../../actions/income/index';
+import testData from "../../../../fixtures/not_full_reports/income/pro_forma/QA-4494&95&96&97.fixture";
+import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
+import { Income, Property } from "../../../../actions";
+import { _NavigationSection } from "../../../../actions/base";
+import enums from "../../../../enums/enums";
+import proFormaTypes from "../../../../enums/proFormaTypes.enum";
+import { numberWithCommas } from '../../../../../utils/numbers.utils';
+
+describe("[QA-4494] [QA-4495] [QA-4496] [QA-4497]",
+    { tags: [ '@income', '@pro_forma' ] }, () => {
+        
+        before("Preconditions", () => {
+            cy.stepInfo("1. Login, create report.");
+            createReport(testData.reportCreationData);
+
+            cy.stepInfo("2. Go to Property → Summary and add residential units");
+            _NavigationSection.navigateToPropertySummary();
+            Property._Summary.enterNumberOfResUnits(testData.resUnits)
+                .enterGrossBuildingArea(testData.grossBuildingArea);
+
+            cy.stepInfo("3. Go to Income → Residential → In-Place Rent Role and fill in all necessary values to the table");
+            _NavigationSection.navigateToResInPlaceRentRoll();
+            Income._Residential.InPlaceRentRoll.enterListMonthlyRents(testData.monthlyRent);
+
+           cy.saveLocalStorage(); 
+        });
+
+        beforeEach("Restore local storeage", () => {
+            cy.restoreLocalStorage();
+        });
+
+        it("[QA-4494] Verify table with the following columns: Income, Total, PSF, Per Unit", () => {
+            cy.stepInfo("1. Go to Income -> Pro Forma page and verify that the table contains Income, Total, PSF, Per Unit columns");
+            _NavigationSection.navigateToProForma();
+            Income._ProFormaActions.Page.columnHeaderIncome.should('exist');
+            Income._ProFormaActions.Page.columnHeaderTotal.should('exist');
+            Income._ProFormaActions.Page.columnHeaderPSF.should('exist');
+            Income._ProFormaActions.Page.columnHeaderPerUnit.should('exist');
+              });
+
+            // it("[QA-4495] Potential Residential Income >Total [taken from Income>Potential Gross Income -> calculated on Stabilized Rent Roll Summary page] (is rounded)", () => {
+
+            //     cy.stepInfo("3. Go to Income → Pro Forma page. Verify that value in the Potential Residential Income ->Total is taken from Income → Potential Gross Income → table → Potential Residential Income → $");
+            //       _NavigationSection.navigateToPotentialGrossIncome();
+
+            // not this but Potential Gross Income
+            //       Income._ProFormaActions.verifyCategoryTotal(
+            //         `$${numberWithCommas(testData.annualTotalRent)}`,
+            //         proFormaTypes.potentialResIncome
+            //       )
+
+             it("[QA-4496] [Income -> Pro Forma] Potential Residential Income -> PSF", () => {
+            cy.stepInfo(`1. Go to Income → Pro Forma page. 
+    Verify that value in the Potential Residential Income → PSF is calculated by the formula: Total / GBA`);
+            Income._ProFormaActions.verifyCategoryPSFTotal(
+                `$${numberWithCommas(testData.totalPerSF.toFixed(2))}`,
+                proFormaTypes.potentialResIncome);
+            });
+
+             it("[QA-4497] [Income -> Pro Forma] Potential Residential Income -> Per Unit", () => {
+            cy.stepInfo(`1. Go to Income → Pro Forma page.
+        Verify that value in the Potential Residential Income → Per Unit is calculated by the formula: Total / # of Residential Units`);
+            Income._ProFormaActions.verifyCategoryPerUnitTotal(
+                `$${numberWithCommas(testData.totalPerUnit)}`,
+                proFormaTypes.potentialResIncome);
+
+                deleteReport(testData.reportCreationData.reportNumber);
+        });
+
+
+    });
