9b9103f02daa079b9540908d41e17b34b828814e
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-05-27 09:39:24 +0300

    [QA-5432] Resolved suggestions

--- cypress/fixtures/not_full_reports/report/key_info/QA-4621-24.fixture.ts
+++ cypress/fixtures/not_full_reports/report/key_info/QA-4621-24.fixture.ts
@@ -1,42 +1,17 @@
 import Enums from "../../../../enums/enums";
 import ReportDataCreator from "../../../data_creator/reportData.creator";
 
-const firstReportCreationFixture = () => {
-    return ReportDataCreator.setAddress()
-        .setReportNumber("2200016363", true)
-        .setTemplateValue(Enums.TEMPLATE_TYPE.NOT_FREDDIE_MAC)
-        .setIncomeValue(Enums.INCOME_TYPE.BOTH)
-        .setConclusionValue(Enums.VALUE_CONCLUSION_TYPE.AS_COMPLETE).build();
-};
+const _numbersJobId = [ "2200016363", "2200018586", "1764459100", "1764459005" ];
 
-const secondReportCreationFixture = () => {
+export const reportCreationData = (numbersJobId: string) => {
     return ReportDataCreator.setAddress()
-        .setReportNumber("2200018586", true)
-        .setTemplateValue(Enums.TEMPLATE_TYPE.NOT_FREDDIE_MAC)
-        .setIncomeValue(Enums.INCOME_TYPE.BOTH)
-        .setConclusionValue(Enums.VALUE_CONCLUSION_TYPE.AS_COMPLETE).build();
-};
-
-const thirdReportCreationFixture = () => {
-    return ReportDataCreator.setAddress()
-        .setReportNumber("1764459100", true)
-        .setTemplateValue(Enums.TEMPLATE_TYPE.NOT_FREDDIE_MAC)
-        .setIncomeValue(Enums.INCOME_TYPE.BOTH)
-        .setConclusionValue(Enums.VALUE_CONCLUSION_TYPE.AS_COMPLETE).build();
-};
-
-const fourthReportCreationFixture = () => {
-    return ReportDataCreator.setAddress()
-        .setReportNumber("1764459005", true)
+        .setReportNumber(numbersJobId, true)
         .setTemplateValue(Enums.TEMPLATE_TYPE.NOT_FREDDIE_MAC)
         .setIncomeValue(Enums.INCOME_TYPE.BOTH)
         .setConclusionValue(Enums.VALUE_CONCLUSION_TYPE.AS_COMPLETE).build();
 };
 
 export default {
-    firstReportCreationData: firstReportCreationFixture(),
-    secondReportCreationData: secondReportCreationFixture(),
-    thirdReportCreationData: thirdReportCreationFixture(),
-    fourthReportCreationData: fourthReportCreationFixture(),
+    numbersJobId: _numbersJobId,
     verifyValue: "EngagementLetter984234_SIGNED.pdf"
 };
--- cypress/integration/not_full_reports/report/key_info/QA-4621-24.spec.ts
+++ cypress/integration/not_full_reports/report/key_info/QA-4621-24.spec.ts
@@ -1,59 +1,51 @@
 import testData from "../../../../fixtures/not_full_reports/report/key_info/QA-4621-24.fixture";
+import { reportCreationData } from "../../../../fixtures/not_full_reports/report/key_info/QA-4621-24.fixture";
 import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
-import { _NavigationSection } from "../../../../actions/base";
 import { Report } from "../../../../actions";
 
 describe("The Letter of Engagement prefill from Dropbox to LoE filed in WebApp - 1 LoE PDF file",
     { tags: [ "@report", "@key_info", "@salesforce" ] }, () => {
     it("[QA-4621]", () => {
         cy.stepInfo(" 1. Create a new report on the WebApp (Note: the JOB # of that report corresponds with the JOB # of an open job on SalesForce)");
-        createReport(testData.firstReportCreationData);
-
-        cy.stepInfo("2. Navigate to the Report > Key Info > Engagement");
-        _NavigationSection.navigateToReportInformation();
+        createReport(reportCreationData(testData.numbersJobId[0]));
         
-        cy.stepInfo("3. Verify that the Letter Of Engagement PDF from Dropbox is auto-filled.");
+        cy.stepInfo(`2. Navigate to the Report > Key Info > Engagement and Verify that the Letter Of Engagement field is empty and nothing 
+        is auto-filled from Dropbox`);
         Report._KeyInfo.Page.inputToCheckUpload.should("have.value", testData.verifyValue);
 
-        deleteReport(testData.firstReportCreationData.reportNumber);
+        deleteReport(reportCreationData(testData.numbersJobId[0]).reportNumber);
     });
 
     it("[QA-4622]", () => {
         cy.stepInfo(" 1. Create a new report on the WebApp (Note: the JOB # of that report corresponds with the JOB # of an open job on SalesForce)");
-        createReport(testData.secondReportCreationData);
-
-        cy.stepInfo("2. Navigate to the Report > Key Info > Engagement");
-        _NavigationSection.navigateToReportInformation();
+        createReport(reportCreationData(testData.numbersJobId[1]));
         
-        cy.stepInfo("3. Verify that the Letter Of Engagement field is empty and nothing is auto-filled from Dropbox");
+        cy.stepInfo(`2. Navigate to the Report > Key Info > Engagement and Verify that the Letter Of Engagement field is empty and nothing 
+        is auto-filled from Dropbox`);
         Report._KeyInfo.Page.inputToCheckUpload.should("be.empty");
 
-        deleteReport(testData.secondReportCreationData.reportNumber);
+        deleteReport(reportCreationData(testData.numbersJobId[1]).reportNumber);
     });
 
     it("[QA-4623]", () => {
         cy.stepInfo(" 1. Create a new report on the WebApp (Note: the JOB # of that report corresponds with the JOB # of an open job on SalesForce)");
-        createReport(testData.thirdReportCreationData);
-
-        cy.stepInfo("2. Navigate to the Report > Key Info > Engagement");
-        _NavigationSection.navigateToReportInformation();
+        createReport(reportCreationData(testData.numbersJobId[2]));
         
-        cy.stepInfo("3. Verify that the Letter Of Engagement field is empty and nothing is auto-filled from Dropbox");
+        cy.stepInfo(`2. Navigate to the Report > Key Info > Engagement and Verify that the Letter Of Engagement field is empty and nothing 
+            is auto-filled from Dropbox`);
         Report._KeyInfo.Page.inputToCheckUpload.should("be.empty");
 
-        deleteReport(testData.thirdReportCreationData.reportNumber);
+        deleteReport(reportCreationData(testData.numbersJobId[2]).reportNumber);
     });
 
     it("[QA-4624]", () => {
         cy.stepInfo(" 1. Create a new report on the WebApp (Note: the JOB # of that report corresponds with the JOB # of an open job on SalesForce)");
-        createReport(testData.fourthReportCreationData);
-
-        cy.stepInfo("2. Navigate to the Report > Key Info > Engagement");
-        _NavigationSection.navigateToReportInformation();
+        createReport(reportCreationData(testData.numbersJobId[3]));
         
-        cy.stepInfo("3. Verify that the Letter Of Engagement field is empty and nothing is auto-filled from Dropbox");
+        cy.stepInfo(`2. Navigate to the Report > Key Info > Engagement and Verify that the Letter Of Engagement field is empty and nothing 
+        is auto-filled from Dropbox`);
         Report._KeyInfo.Page.inputToCheckUpload.should("be.empty");
 
-        deleteReport(testData.fourthReportCreationData.reportNumber);
+        deleteReport(reportCreationData(testData.numbersJobId[3]).reportNumber);
     });
 });
