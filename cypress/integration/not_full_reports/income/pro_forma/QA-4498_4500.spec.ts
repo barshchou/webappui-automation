9240e0d6ba657be50c72ecc1b00093bb35529cde
Viachaslau.Barshchou <>
2022-05-30 00:25:30 +0300

    removed temp comments; reordered scripts

--- cypress/integration/not_full_reports/income/pro_forma/QA-4498_4500.spec.ts
+++ cypress/integration/not_full_reports/income/pro_forma/QA-4498_4500.spec.ts
@@ -8,10 +8,8 @@ import proFormaTypes from "../../../../enums/proFormaTypes.enum";
 import Enums from "../../../../enums/incomeTypesCellNames.enum";
 
 describe("Potential [USE(Property -> Commercial Units] Income -> PSF / Per Unit", 
-    { tags:["@income", "@pro_forma"] }, () => {
+    { tags:[ "@income", "@pro_forma" ] }, () => {
     
-
-    // test pre-commit lint script test 2 test 3
     before("Login, create report, prepare data", () => {
         cy.stepInfo(`1. Create new report or open the report which is already created. 
                     Make sure that there is at least two commercial units.`);
