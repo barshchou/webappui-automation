b9fb71cdc8215487bd3f1eef1375bb5324ff74d7
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-05-30 12:22:35 +0300

    [QA-5570] Created QA-4606

---
+++ cypress/fixtures/not_full_reports/sales/adjust_comps/QA-4606.fixture.ts
@@ -0,0 +1,19 @@
+import Enums from "../../../../enums/enums";
+import ReportDataCreator from "../../../data_creator/reportData.creator";
+
+const comparableFixture = () => {
+    return {
+        address: "200 West 78 Street",
+        sizeAdjustment: 20,
+        conditionAdjustment: -30,
+        otherAdjustment: 40,
+        propertyRights: -60
+    };
+};
+
+export default {
+    reportMixedCreationData: ReportDataCreator.getReportData("4107", { incomeValue: Enums.INCOME_TYPE.BOTH }),
+    reportCreationData: ReportDataCreator.getReportData("4114-15"),
+    calculationUnits: [ "PSF", "Per Residential Units" ],
+    comparable: Object.freeze(comparableFixture())
+};
