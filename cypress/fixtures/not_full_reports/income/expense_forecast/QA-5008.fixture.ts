0bf136dd039a2d08c7befe913e6fa54acaa1d792
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-05-30 23:44:12 +0300

    complete tests added

--- cypress/fixtures/not_full_reports/income/expense_forecast/QA-5008.fixture.ts
+++ cypress/fixtures/not_full_reports/income/expense_forecast/QA-5008.fixture.ts
@@ -2,11 +2,9 @@ import ReportDataCreator from "../../../data_creator/reportData.creator";
 
 const _customCategory: BoweryReports.ForecastItem = {
     name: "customExpence"
-
 };
 
 export default {
     reportCreationData: ReportDataCreator.getReportData("5008&09&10"),
-    customCategory: _customCategory,
-   // checkBoxLabel: "Include Expense on Pro Forma"
+    customCategory: _customCategory
 };
--- cypress/integration/not_full_reports/income/expense_forecast/QA-5008.spec.ts
+++ cypress/integration/not_full_reports/income/expense_forecast/QA-5008.spec.ts
@@ -4,74 +4,47 @@ import { Income } from "../../../../actions";
 import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
 import expensesCardsNames from " ../../../cypress/enums/expenseForecast.enum";
 
-describe(`[] [Income>Expense forecast] “Include Expense on Pro Forma” checkbox is displayed under existing expense card`,
-    { tags:[ "@income", "@expense_forecast" ] }, () => {
+describe(`[QA-5008] [QA-5009] [QA-5010] [Income>Expense forecast] “Include Expense on Pro Forma” checkbox and tooltip functionality`,
+    { tags: [ "@income", "@expense_forecast" ] }, () => {
 
-    before("Login, create report", () => {
-        createReport(testData.reportCreationData);
- //  cy.saveLocalStorage();
-    });
-    
-    // beforeEach(() => {
-    //     cy.restoreLocalStorage();
-    // });
+        before("Login, create report", () => {
+            createReport(testData.reportCreationData);
+            cy.saveLocalStorage();
+        });
 
-    it("[QA-5008]", () => {
-        cy.stepInfo(`1. Go to Income > Expense Forecast`);
-        _NavigationSection.navigateToExpenseForecast();
+        beforeEach(() => {
+            cy.restoreLocalStorage();
+        });
 
-        cy.stepInfo(`2. Verify  “Include Expense on Pro Forma”  checkbox is displayed under existing expense card`);
-        expensesCardsNames.expensesCheckboxIncludeInProFormaNamesArray.forEach(element => {
-          Income._ExpenseForecastActions.verifyIncludeInProFormaCheckboxExists(element);
-});
+        it("[QA-5008]", () => {
+            cy.stepInfo(`1. Go to Income > Expense Forecast`);
+            _NavigationSection.navigateToExpenseForecast();
 
-cy.stepInfo(`3. Verify  “Include Expense on Pro Forma” checkbox is NOT displayed under custom expense card`);
-Income._ExpenseForecastActions.addCustomExpenseCategory(testData.customCategory.name);
-   Income._ExpenseForecastActions.Page.forecastItemCardFull(testData.customCategory.name).children()
-   .contains("Include Expense on Pro Forma").should('not.exist')
+            cy.stepInfo(`2. Verify  “Include Expense on Pro Forma”  checkbox is displayed under existing expense card`);
+            expensesCardsNames.expenseCardsIDArray.forEach(element => {
+                Income._ExpenseForecastActions.verifyIncludeInProFormaCheckboxExists(element);
+            });
 
-});
+            cy.stepInfo(`3. Verify  “Include Expense on Pro Forma” checkbox is NOT displayed under custom expense card`);
+            Income._ExpenseForecastActions.addCustomExpenseCategory(testData.customCategory.name);
+            Income._ExpenseForecastActions.Page.forecastItemCardFull(testData.customCategory.name).children()
+                .contains("Include Expense on Pro Forma").should('not.exist');
+        });
 
-it("[QA-5009]", () => {
-    cy.stepInfo(` Verify “Include Expense on Pro Forma” checkbox is selected by default for each existing expense card`);
-    expensesCardsNames.expensesCheckboxIncludeInProFormaNamesArray.forEach(element => {
-        Income._ExpenseForecastActions.verifyIncludeInProFormaCheckboxIsChecked(element);
-});
-});
+        it("[QA-5009]", () => {
+            cy.stepInfo(`1. Verify “Include Expense on Pro Forma” checkbox is selected by default for each existing expense card`);
+            expensesCardsNames.expenseCardsIDArray.forEach(element => {
+                Income._ExpenseForecastActions.verifyIncludeInProFormaCheckboxIsChecked(element);
+            });
+        });
 
-it("[QA-5010]", () => {
-    cy.stepInfo(` Verify user hover on icon right to the "Include Expense on Pro Forma" checkbox - tooltip
+        it("[QA-5010]", () => {
+            cy.stepInfo(`1. Verify user hover on icon right to the "Include Expense on Pro Forma" checkbox - tooltip
      with the following text "Unchecking this box will hide the expense from showing up on the Pro Forma." appears `);
-     expensesCardsNames.expensesCheckboxIncludeInProFormaNamesArray.forEach(element => {
-      //  Income._ExpenseForecastActions.(element);
+            expensesCardsNames.expenseCardsIDArray.forEach(element => {
+                Income._ExpenseForecastActions.verifyProFormaTooltip(element);
+            });
 
-     //   verifyGuidelineTooltip() {
-        Income._ExpenseForecastActions.Page.forecastItemTooltip(element).should("exist");
-            // clientPage.toCheckTooltipExist.should("not.exist");
-            // clientPage.guidelinesTooltip.trigger("mouseover");
-            // clientPage.toCheckTooltipExist.should("exist");
-       //     return this;
-     //   }
-
-
-});
-
-
-// label="Include Expense on Pro Forma"span
-// cy.get(`[data-qa="${forecastItem}"].filter('.users')
-// cy.get('td').filter('.users')
-        // cy.stepInfo(`[] => 3. Fill expenses forecast for custom and regular categories`);
-        // testData.expensesItems.forEach(foreCastItem => {
-        //     Income._ExpenseForecastActions.enterForecastItemForecast(foreCastItem);
-        // });
-        // Income._ExpenseForecastActions.enterForecastItemForecast(testData.firstCustomCategory, true, 0);
-        // Income._ExpenseForecastActions.enterForecastItemForecast(testData.secondCustomCategory, true, 1);
-        
-        // cy.stepInfo(`[] => 4. Verify total expenses calculation and commentary`);
-        // Income._ExpenseForecastActions.verifyTOECommentary(testData.commentaryTotalExpenses.generatedPerSF)
-        //     .verifyTotalForecast();
-
-
-        // deleteReport(testData.reportCreationData.reportNumber);
+            deleteReport(testData.reportCreationData.reportNumber);
+        });
     });
-});
