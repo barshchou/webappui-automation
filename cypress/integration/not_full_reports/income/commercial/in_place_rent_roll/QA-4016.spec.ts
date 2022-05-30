cb48847560cafdcafd422c093fe66103e56917e4
AlexeiMo <47743960+RuslanGitelman@users.noreply.github.com>
2022-05-30 08:24:41 +0300

    [QA-4828] Create spec and fixture files

---
+++ cypress/integration/not_full_reports/income/commercial/in_place_rent_roll/QA-4016.spec.ts
@@ -0,0 +1,18 @@
+import testData from "../../../../../fixtures/not_full_reports/income/commercial/in_place_rent_roll/QA-4016.fixture";
+import NavigationSection from "../../../../../actions/base/navigationSection.actions";
+import Income from "../../../../../actions/income/income.manager";
+import { createReport, deleteReport } from "../../../../../actions/base/baseTest.actions";
+
+describe("Verify the Basis of Rent tooltip", 
+    { tags:[ "@income", "@commercial", "@in_place_rent_roll" ] }, () => {
+        
+    before("Login, create report", () => {
+        createReport(testData.reportCreationData);
+    });
+
+    it("Test body", () => {
+        NavigationSection.navigateToCommercialInPlaceRentRoll();
+        Income.Commercial.InPlaceRentRoll.verifyBasisOfRentTooltip();
+        deleteReport(testData.reportCreationData.reportNumber);
+    });
+});
