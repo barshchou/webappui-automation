818382bf01dc2ad20eb72f1fded65a1cf258927c
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-06-01 13:54:42 +0300

    test divided into 3 it

--- cypress/integration/not_full_reports/income/residential/in_place_rent_roll/QA-4088-90.spec.ts
+++ cypress/integration/not_full_reports/income/residential/in_place_rent_roll/QA-4088-90.spec.ts
@@ -8,13 +8,18 @@ describe("[QA-4088], [QA-4089], [QA-4090] 'What time period should rent PSF be b
 
         before("Login, create report", () => {
             createReport(testData.reportCreationData);
+            cy.saveLocalStorage();
         });
 
-        it("Test body", () => {
+        beforeEach(() => {
+            cy.restoreLocalStorage();
+        });
+
+        it("[QA-4088]", () => {
             cy.stepInfo("1. Go to Income > Residential > In-Place Rent Roll.");
             _NavigationSection.navigateToResInPlaceRentRoll();
 
-            cy.stepInfo(`2. [QA-4088] Verify if "What time period should rent PSF be based on?" question with 2 radiobuttons ( Per year and Per Month) 
+            cy.stepInfo(`2. Verify if "What time period should rent PSF be based on?" question with 2 radiobuttons ( Per year and Per Month) 
             appears under the question "Do you know per unit square footage?
             when checkbox "Yes" is selected under question field "Do you know per unit square footage?"`);
             Income._Residential.InPlaceRentRoll.Page.getPerUnitSFRadio('false').should("be.checked");
@@ -22,13 +27,17 @@ describe("[QA-4088], [QA-4089], [QA-4090] 'What time period should rent PSF be b
             Income._Residential.InPlaceRentRoll.Page.getPSFRadio(testData.psfRadioValuePerAnnually).should("exist");
             Income._Residential.InPlaceRentRoll.Page.getPSFRadio(testData.psfRadioValuePerMonthly).should("exist");
             Income._Residential.InPlaceRentRoll.Page.radioButtonLabelByText(testData.labelText).should("exist");
+        });
 
-            cy.stepInfo(`3. [QA-4089] Verify if "What time period should PSF rent be based on?" question is not displayed 
+        it("[QA-4089]", () => {
+            cy.stepInfo(`1. Verify if "What time period should PSF rent be based on?" question is not displayed 
             if "No" radiobutton is selected in "Do you know per unit square footage?"`);
             Income._Residential.InPlaceRentRoll.checkPerUnitSquareFootage('false');
             Income._Residential.InPlaceRentRoll.Page.radioButtonLabelByText(testData.labelText).should("not.exist");
+        });
 
-            cy.stepInfo(`4. [QA-4090] "Verify if  'Per Year' radiobutton is selected by default" `);
+        it("[QA-4090]", () => {
+            cy.stepInfo(`1. "Verify if  'Per Year' radiobutton is selected by default" `);
             Income._Residential.InPlaceRentRoll.checkPerUnitSquareFootage('true');
             Income._Residential.InPlaceRentRoll.Page.getPSFRadio(testData.psfRadioValuePerAnnually).should("be.checked");
 
