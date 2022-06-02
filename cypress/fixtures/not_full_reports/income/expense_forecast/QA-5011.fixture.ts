992d933b713c2664f71727f9a7a1f0be10fe533e
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-06-02 18:15:23 +0300

    test 5011 added

--- cypress/fixtures/not_full_reports/income/expense_forecast/QA-5011.fixture.ts
+++ cypress/fixtures/not_full_reports/income/expense_forecast/QA-5011.fixture.ts
@@ -1,13 +1,21 @@
 import ReportDataCreator from "../../../data_creator/reportData.creator";
 import proFormaTypes from "../../../../../cypress/enums/proFormaTypes.enum";
+import Enums from "../../../../enums/enums";
 
-
+const _reportCreationData: BoweryAutomation.ReportCreationData = ReportDataCreator.getReportData("5011", {
+    incomeValue: Enums.INCOME_TYPE.BOTH,
+    conclusionValue: Enums.VALUE_CONCLUSION_TYPE.AS_COMPLETE
+});
 
 const _customCategory: BoweryReports.ForecastItem = {
     name: "customExpence"
 };
 
-const _expensesByDefaultArray = [
+const _numberOfCommercialUnits = 2;
+const _numberOfResidentialUnits = 2;
+
+
+const _expensesInProFormaByDefaultArray = [
     proFormaTypes.insurace,
     proFormaTypes.electricity,
     proFormaTypes.fuel,
@@ -21,94 +29,154 @@ const _expensesByDefaultArray = [
     proFormaTypes.replacementsAndReserves,
 ];
 
-
-const expenseForecastInsuranceFixture: BoweryReports.ForecastItem = {
-    name: "insurance",
-    basis: "sf",
-    forecast: 5
+const expenseForecastInsuranceFixture = (_basis: "unit" | "sf" ): BoweryReports.ForecastItem => {
+    return {
+        name: "insurance",
+        basis: _basis as BoweryReports.UnitSF,
+        forecast: 5
+    };
 };
 
-const expenseForecastElectricityFixture: BoweryReports.ForecastItem = {
-    name: "electricity",
-    basis: "sf",
-    forecast: 5
+const expenseForecastElectricityFixture = (_basis: "unit" | "sf"): BoweryReports.ForecastItem => {
+    return {
+        name: "electricity",
+        basis: _basis as BoweryReports.UnitSF,
+        forecast: 5
+    };
 };
 
-const expenseForecastFuelFixture: BoweryReports.ForecastItem = {
-    name: "fuel",
-    basis: "sf",
-    forecast: 5
+const expenseForecastFuelFixture = (_basis: "unit" | "room" | "sf"): BoweryReports.ForecastItem => {
+    return {
+        name: "fuel",
+        basis: _basis as BoweryReports.UnitSF,
+        forecast: 5
+    };
 };
 
-const expenseForecastWaterAndSewerFixture: BoweryReports.ForecastItem = {
-    name: "waterAndSewer",
-    basis: "sf",
-    forecast: 5
+const expenseForecastWaterAndSewerFixture = (_basis: "unit" | "sf"): BoweryReports.ForecastItem => {
+    return {
+        name: "waterAndSewer",
+        basis: _basis as BoweryReports.UnitSF,
+        forecast: 5
+    };
 };
 
-const expenseForecastRepairsAndMaintenanceFixture: BoweryReports.ForecastItem = {
-    name: "repairsAndMaintenance",
-    basis: "sf",
-    forecast: 5
+const expenseForecastRepairsAndMaintenanceFixture = (_basis: "unit" | "sf"): BoweryReports.ForecastItem => {
+    return {
+        name: "repairsAndMaintenance",
+        basis: _basis as BoweryReports.UnitSF,
+        forecast: 5
+    };
 };
 
-const expenseForecastPayrollAndBenefitsFixture: BoweryReports.ForecastItem = {
-    name: "payrollAndBenefits",
-    basis: "sf",
-    forecast: 5
+const expenseForecastPayrollAndBenefitsFixture = (_basis: "unit" | "sf"): BoweryReports.ForecastItem => {
+    return {
+        name: "payrollAndBenefits",
+        basis: _basis as BoweryReports.UnitSF,
+        forecast: 5
+    };
 };
 
-const expenseForecastGeneralAndAdministrativeFixture: BoweryReports.ForecastItem = {
-    name: "generalAndAdministrative",
-    basis: "sf",
-    forecast: 5
+const expenseForecastGeneralAndAdministrativeFixture = (_basis: "unit" | "sf"): BoweryReports.ForecastItem => {
+    return {
+        name: "generalAndAdministrative",
+        basis: _basis as BoweryReports.UnitSF,
+        forecast: 5
+    };
 };
 
-const expenseForecastLegalAndProfessionalFeesFixture: BoweryReports.ForecastItem = {
-    name: "legalAndProfessionalFees",
-    basis: "sf",
-    forecast: 5
+const expenseForecastLegalAndProfessionalFeesFixture = (_basis: "unit" | "sf"): BoweryReports.ForecastItem => {
+    return {
+        name: "legalAndProfessionalFees",
+        basis: _basis as BoweryReports.UnitSF,
+        forecast: 5
+    };
 };
 
-const expenseForecastMiscellaneousFixture: BoweryReports.ForecastItem = {
-    name: "miscellaneous",
-    basis: "sf",
-    forecast: 5
+const expenseForecastMiscellaneousFixture = (_basis: "unit" | "sf"): BoweryReports.ForecastItem => {
+    return {
+        name: "miscellaneous",
+        basis: _basis as BoweryReports.UnitSF,
+        forecast: 5
+    };
 };
 
-const expenseForecastManagementFixture: BoweryReports.ForecastItem = {
-    name: "management",
-    basis: "sf",
-    forecast: 5
+const expenseForecastManagementFixture = (_basis: "unit" | "sf"): BoweryReports.ForecastItem => {
+    return {
+        name: "management",
+        basis: _basis as BoweryReports.UnitSF,
+        forecast: 5
+    };
 };
 
-const expenseForecastReservesFixture: BoweryReports.ForecastItem = {
-    name: "reserves",
-    basis: "sf",
-    forecast: 5
+const expenseForecastReservesFixture = (_basis: "unit" | "sf"): BoweryReports.ForecastItem => {
+    return {
+        name: "reserves",
+        basis: _basis as BoweryReports.UnitSF,
+        forecast: 5
+    };
 };
 
-const array1 = [expenseForecastInsuranceFixture,
-    expenseForecastElectricityFixture,
-    expenseForecastFuelFixture,
-    expenseForecastWaterAndSewerFixture,
-    expenseForecastRepairsAndMaintenanceFixture, 
-    expenseForecastPayrollAndBenefitsFixture,
-    expenseForecastGeneralAndAdministrativeFixture,
-    expenseForecastLegalAndProfessionalFeesFixture,
-    expenseForecastMiscellaneousFixture,
-    expenseForecastManagementFixture,
-    expenseForecastReservesFixture] 
+const expenseForecastTotalFixture = (_basis: "unit" | "sf"): BoweryReports.ForecastItem => {
+    return {
+        name: "total",
+        basis: _basis as BoweryReports.UnitSF,
+    };
+};
 
 
+
+
+const expenseForecastFixtureWithUnitArray = [
+    expenseForecastInsuranceFixture("unit"),
+    expenseForecastElectricityFixture("unit"),
+    expenseForecastFuelFixture("unit"),
+    expenseForecastWaterAndSewerFixture("unit"),
+    expenseForecastRepairsAndMaintenanceFixture("unit"), 
+    expenseForecastPayrollAndBenefitsFixture("unit"),
+    expenseForecastGeneralAndAdministrativeFixture("unit"),
+    expenseForecastLegalAndProfessionalFeesFixture("unit"),
+    expenseForecastMiscellaneousFixture("unit"),
+    expenseForecastManagementFixture("unit"),
+    expenseForecastReservesFixture("unit")
+];
+
+const expenseForecastFixtureWithPSFArray = [
+    expenseForecastInsuranceFixture("sf"),
+    expenseForecastElectricityFixture("sf"),
+    expenseForecastFuelFixture("sf"),
+    expenseForecastWaterAndSewerFixture("sf"),
+    expenseForecastRepairsAndMaintenanceFixture("sf"), 
+    expenseForecastPayrollAndBenefitsFixture("sf"),
+    expenseForecastGeneralAndAdministrativeFixture("sf"),
+    expenseForecastLegalAndProfessionalFeesFixture("sf"),
+    expenseForecastMiscellaneousFixture("sf"),
+    expenseForecastManagementFixture("sf"),
+    expenseForecastReservesFixture("sf")
+]; 
+
+
+const commentariesFixture = () => {
+    return {
+        generated: "Operating expenses, exclusive of real estate taxes, were forecasted at $0.00 per square foot and $0 per unit. " +
+        "Excluding real estate taxes, the comparables ranged from Comp totals map psf min to Comp totals map psf max per square foot and Comp totals map unit min to " +
+        "Comp totals map unit max per unit. Our forecast is near the comparable range on both a per square foot and per unit basis, " +
+        "as well as logically placed in relation to the historical performance of the asset. Thus, this forecast is " +
+        "reasonable and will be applied in our valuation analysis.",
+    };
+};
+
+const _forecastPSFnotIncluded = '$0.00';
+const _forecastPerUnitnotIncluded = '$0';
 
 export default {
-    reportCreationData: ReportDataCreator.getReportData("5011"),
+    reportCreationData: _reportCreationData,
     customCategory: _customCategory,
-    expensesByDefaultArray: _expensesByDefaultArray,
+    expensesInProFormaByDefaultArray: _expensesInProFormaByDefaultArray,
     // expenseForecastInsuranceFixture,
     // expenseForecastElectricityFixture,
-    // expenseForecastFuelFixture,
+     expenseForecastFuelFixture,
+     expenseForecastTotalFixture,
     // expenseForecastWaterAndSewerFixture,
     // expenseForecastRepairsAndMaintenanceFixture, 
     // expenseForecastPayrollAndBenefitsFixture,
@@ -117,6 +185,11 @@ export default {
     // expenseForecastMiscellaneousFixture,
     // expenseForecastManagementFixture,
     // expenseForecastReservesFixture
-
-    array1
+    numberOfResidentialUnits: _numberOfResidentialUnits,
+    numberOfCommercialUnits: _numberOfCommercialUnits,
+    expenseForecastFixtureWithUnitArray,
+    expenseForecastFixtureWithPSFArray,
+    commentaries: Object.freeze(commentariesFixture()),
+    forecastPSFnotIncluded: _forecastPSFnotIncluded,
+    forecastPerUnitnotIncluded: _forecastPerUnitnotIncluded
 };
--- cypress/integration/not_full_reports/income/expense_forecast/QA-5011.spec.ts
+++ cypress/integration/not_full_reports/income/expense_forecast/QA-5011.spec.ts
@@ -1,6 +1,6 @@
 import testData from "../../../../fixtures/not_full_reports/income/expense_forecast/QA-5011.fixture";
 import { _NavigationSection } from "../../../../actions/base";
-import { Income } from "../../../../actions";
+import { Income, Property } from "../../../../actions";
 import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
 import expensesCardsNames from "../../../../../cypress/enums/expenseForecast.enum";
 
@@ -18,28 +18,139 @@ describe(`[QA-5011] [Income>Expense forecast] “Include Expense on Pro Forma”
         });
 
         it("[QA-5011]", () => {
-            cy.stepInfo(`1. Go to Income > Expense Forecast`);
-            _NavigationSection.navigateToExpenseForecast();
+            cy.stepInfo(`1. Go to Property > Summary and add residential and commertial units`);
+            _NavigationSection.navigateToPropertySummary();
+        Property._Summary.enterNumberOfResUnits(testData.numberOfResidentialUnits)
+            .enterNumberOfCommercialUnits(testData.numberOfCommercialUnits);
+            
 
             cy.stepInfo(`2. Verify if “Include Expense on Pro Forma” is unselected -> this  expense category 
-            is NOT displayed in the Pro Forma page> Operating Expenses section`);
-            Income._ExpenseForecastActions
+            is NOT displayed in the Pro Forma page> Operating Expenses section and
+            is NOT displayed in the Expense Forecast page>Total operating expenses card and generated comment (PSF measure + Empty Appraiser’s forecasts)`);
+            _NavigationSection.navigateToExpenseForecast();
             expensesCardsNames.expenseCardsIDArray.forEach(element => {
                 Income._ExpenseForecastActions.uncheckIncludeInProFormaCheckbox(element);
             });
             _NavigationSection.navigateToProForma();
-            testData.expensesByDefaultArray.forEach(element => {
+            testData.expensesInProFormaByDefaultArray.forEach(element => {
                 Income._ProFormaActions.Page.categoryCellTotal(element).should('not.exist');
             });
+            _NavigationSection.navigateToExpenseForecast();
+            Income._ExpenseForecastActions.verifyTOECommentary(testData.commentaries.generated);
+            Income._ExpenseForecastActions.Page.toeAppraisersForecastValueLine.should('contain', testData.forecastPSFnotIncluded);
+            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastTotalFixture('unit'))
+            .verifyTOECommentary(testData.commentaries.generated);
+            Income._ExpenseForecastActions.Page.toeAppraisersForecastValueLine.should('contain', testData.forecastPerUnitnotIncluded);
+            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastTotalFixture('sf'));
 
             cy.stepInfo(`3. Verify if “Include Expense on Pro Forma” is unselected -> this  expense category 
-            is NOT displayed in the Expense Forecast page>Total operating expenses  card and generated comment`);
+            is NOT displayed in the Pro Forma page> Operating Expenses section and
+            is NOT displayed in the Expense Forecast page>Total operating expenses card and generated comment (Per Unit measure + Empty Appraiser’s forecasts)`);
+           
+            testData.expenseForecastFixtureWithUnitArray.forEach(element => {
+                Income._ExpenseForecastActions.chooseForecastItemBasis(element);
+            });
+            _NavigationSection.navigateToProForma();
+            testData.expensesInProFormaByDefaultArray.forEach(element => {
+                Income._ProFormaActions.Page.categoryCellTotal(element).should('not.exist');
+            });
             _NavigationSection.navigateToExpenseForecast();
+            Income._ExpenseForecastActions.verifyTOECommentary(testData.commentaries.generated);
+            Income._ExpenseForecastActions.Page.toeAppraisersForecastValueLine.should('contain', testData.forecastPSFnotIncluded);
+            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastTotalFixture('unit'))
+            .verifyTOECommentary(testData.commentaries.generated);
+            Income._ExpenseForecastActions.Page.toeAppraisersForecastValueLine.should('contain', testData.forecastPerUnitnotIncluded);
+            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastTotalFixture('sf'));
 
-            
-            testData.array1.forEach(element => {
+            cy.stepInfo(`4. Verify if “Include Expense on Pro Forma” is unselected -> this  expense category 
+            is NOT displayed in the Pro Forma page> Operating Expenses section and
+            is NOT displayed in the Expense Forecast page>Total operating expenses card and generated comment (Per Room measure for Fuel + Empty Appraiser’s forecasts)`);
+                Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastFuelFixture('room'));
+            _NavigationSection.navigateToProForma();
+            testData.expensesInProFormaByDefaultArray.forEach(element => {
+                Income._ProFormaActions.Page.categoryCellTotal(element).should('not.exist');
+            });
+            _NavigationSection.navigateToExpenseForecast();
+            Income._ExpenseForecastActions.verifyTOECommentary(testData.commentaries.generated);
+            Income._ExpenseForecastActions.Page.toeAppraisersForecastValueLine.should('contain', testData.forecastPSFnotIncluded);
+            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastTotalFixture('unit'))
+            .verifyTOECommentary(testData.commentaries.generated);
+            Income._ExpenseForecastActions.Page.toeAppraisersForecastValueLine.should('contain', testData.forecastPerUnitnotIncluded);
+            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastTotalFixture('sf'));
+
+            cy.stepInfo(`5. Verify if “Include Expense on Pro Forma” is unselected -> this  expense category 
+            is NOT displayed in the Pro Forma page> Operating Expenses section and
+            is NOT displayed in the Expense Forecast page>Total operating expenses card and generated comment (Per Unit measure + Full Appraiser’s forecasts)`);
+            testData.expenseForecastFixtureWithUnitArray.forEach(element => {
                 Income._ExpenseForecastActions.enterForecastItemForecast(element);
             });
+            _NavigationSection.navigateToProForma();
+            testData.expensesInProFormaByDefaultArray.forEach(element => {
+                Income._ProFormaActions.Page.categoryCellTotal(element).should('not.exist');
+            });
+            _NavigationSection.navigateToExpenseForecast();
+            Income._ExpenseForecastActions.verifyTOECommentary(testData.commentaries.generated);
+            Income._ExpenseForecastActions.Page.toeAppraisersForecastValueLine.should('contain', testData.forecastPSFnotIncluded);
+            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastTotalFixture('unit'))
+            .verifyTOECommentary(testData.commentaries.generated);
+            Income._ExpenseForecastActions.Page.toeAppraisersForecastValueLine.should('contain', testData.forecastPerUnitnotIncluded);
+            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastTotalFixture('sf'));
+
+            cy.stepInfo(`6. Verify if “Include Expense on Pro Forma” is unselected -> this  expense category 
+            is NOT displayed in the Pro Forma page> Operating Expenses section and
+            is NOT displayed in the Expense Forecast page>Total operating expenses card and generated comment (PSF measure + Full Appraiser’s forecasts)`);
+            testData.expenseForecastFixtureWithPSFArray.forEach(element => {
+                Income._ExpenseForecastActions.chooseForecastItemBasis(element);
+            });
+            _NavigationSection.navigateToProForma();
+            testData.expensesInProFormaByDefaultArray.forEach(element => {
+                Income._ProFormaActions.Page.categoryCellTotal(element).should('not.exist');
+            });
+            _NavigationSection.navigateToExpenseForecast();
+            Income._ExpenseForecastActions.verifyTOECommentary(testData.commentaries.generated);
+            Income._ExpenseForecastActions.Page.toeAppraisersForecastValueLine.should('contain', testData.forecastPSFnotIncluded);
+            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastTotalFixture('unit'))
+            .verifyTOECommentary(testData.commentaries.generated);
+            Income._ExpenseForecastActions.Page.toeAppraisersForecastValueLine.should('contain', testData.forecastPerUnitnotIncluded);
+            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastTotalFixture('sf'));
+
+            cy.stepInfo(`7. Verify if “Include Expense on Pro Forma” is unselected -> this  expense category 
+            is NOT displayed in the Pro Forma page> Operating Expenses section and
+            is NOT displayed in the Expense Forecast page>Total operating expenses card and generated comment (Per Room measure for Fuel + Full Appraiser’s forecasts)`);
+            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastFuelFixture('room'));
+            _NavigationSection.navigateToProForma();
+            testData.expensesInProFormaByDefaultArray.forEach(element => {
+                Income._ProFormaActions.Page.categoryCellTotal(element).should('not.exist');
+            });
+            _NavigationSection.navigateToExpenseForecast();
+            Income._ExpenseForecastActions.verifyTOECommentary(testData.commentaries.generated);
+            Income._ExpenseForecastActions.Page.toeAppraisersForecastValueLine.should('contain', testData.forecastPSFnotIncluded);
+            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastTotalFixture('unit'))
+            .verifyTOECommentary(testData.commentaries.generated);
+            Income._ExpenseForecastActions.Page.toeAppraisersForecastValueLine.should('contain', testData.forecastPerUnitnotIncluded);
+            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastTotalFixture('sf'));
+        });
+
+
+
+
+
+        
+        it("[QA-5012]", () => {
+            cy.stepInfo(`1. Verify If “Include Expense on Pro Forma”  checkbox is unselected but there is 
+            data left in the forecast, this data is not included in calculations on Pro forma and Expense forecast page (PSF measure)`);
+
+
+
+            //  cy.stepInfo(`8. Verify if “Include Expense on Pro Forma” is unselected -> this  expense category 
+            // // is NOT displayed in the Expense Forecast page>Total operating expenses card and generated comment (PSF measure + Full Appraiser’s forecasts)`);
+            //  _NavigationSection.navigateToExpenseForecast();
+            //  Income._ExpenseForecastActions.chooseForecastItemBasis(testData.expenseForecastFuelFixture('sf'));
+            //  Income._ExpenseForecastActions.verifyTOECommentary(testData.commentaries.generated);
+            //  Income._ExpenseForecastActions.Page.toeAppraisersForecastValueLine.should('contain', testData.forecastPSFnotIncluded)
+            // testData.array1.forEach(element => {
+            //     Income._ExpenseForecastActions.enterForecastItemForecast(element);
+            // });
 
             // cy.stepInfo(`3. Verify  “Include Expense on Pro Forma” checkbox is NOT displayed under custom expense card`);
             // Income._ExpenseForecastActions.addCustomExpenseCategory(testData.customCategory.name);
