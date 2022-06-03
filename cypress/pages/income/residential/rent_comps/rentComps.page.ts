93af91483571f32361d48b7a8bb948b43a80ce04
AlexeiMo <47743960+RuslanGitelman@users.noreply.github.com>
2022-06-03 18:21:12 +0300

    [refactor] Refactor rentComps.page.ts file

--- cypress/pages/income/residential/rent_comps/rentComps.page.ts
+++ cypress/pages/income/residential/rent_comps/rentComps.page.ts
@@ -1,42 +1,44 @@
 import BasePage from "../../../base/base.page";
 
 class RentCompsPage extends BasePage{
-    get generatedCommentary() {return cy.get("*[data-qa='rentCompsIntroduction.commentary-generated-text']");}
+    get generatedCommentary() { return cy.get("*[data-qa='rentCompsIntroduction.commentary-generated-text']"); }
 
-    get unitSwitchButton() {return cy.get("*[data-qa='unit-switch-button']");}
+    get unitSwitchButton() { return cy.get("*[data-qa='unit-switch-button']"); }
 
-    get buildingSwitchButton() {return cy.get("*[data-qa='building-switch-button']");}
+    get buildingSwitchButton() { return cy.get("*[data-qa='building-switch-button']"); }
 
-    get switchSearchConfirmButton() {return cy.get("*[data-qa='switch-search-confirm']");}
+    get switchSearchConfirmButton() { return cy.get("*[data-qa='switch-search-confirm']"); }
 
-    get unitTypesArrowButton() {return cy.xpath("//*[text()='Unit Type']//following-sibling::button");}
+    get unitTypesArrowButton() { return cy.xpath("//*[text()='Unit Type']//following-sibling::button"); }
 
-    getCheckboxByDataQaAttr(attribute: string | number) {return cy.get(`*[data-qa='${attribute}'] input`);}
+    getCheckboxByDataQaAttr(attribute: string | number) { return cy.get(`*[data-qa='${attribute}'] input`); }
 
-    get changeCompTypePopUpMessage() {return cy.xpath("//*[text()='Changing your comp type will cause you " +
-        "to lose all currently selected comparables.']");}
+    get changeCompTypePopUpMessage() {
+        return cy.xpath("//*[text()='Changing your comp type will cause you " +
+                        "to lose all currently selected comparables.']");
+    }
 
-    get minRentInput() {return cy.get("*[name='filters.minRent']");}
+    get minRentInput() { return cy.get("*[name='filters.minRent']"); }
 
-    get maxRentInput() {return cy.get("*[name='filters.maxRent']");}
+    get maxRentInput() { return cy.get("*[name='filters.maxRent']"); }
 
-    get minSquareFeet() {return cy.get("*[name='filters.minSqft']");}
+    get minSquareFeet() { return cy.get("*[name='filters.minSqft']"); }
 
-    get maxSquareFeet() {return cy.get("*[name='filters.maxSqft']");}
+    get maxSquareFeet() { return cy.get("*[name='filters.maxSqft']"); }
 
-    get numberOfBedroomsArrowButton() {return cy.xpath("//*[text()='Bedrooms']//following-sibling::button");}
+    get numberOfBedroomsArrowButton() { return cy.xpath("//*[text()='Bedrooms']//following-sibling::button"); }
 
-    get sourceOfInfoArrow() {return cy.xpath("//*[text()='Source of Information']//following-sibling::button");}
+    get sourceOfInfoArrow() { return cy.xpath("//*[text()='Source of Information']//following-sibling::button"); }
 
-    get minDateValueInput() {return cy.get("*[data-qa='filters.minDateOfValue-date-picker'] input[placeholder='MM-DD-YYYY']");}
+    get minDateValueInput() { return cy.get("*[data-qa='filters.minDateOfValue-date-picker'] input[placeholder='MM-DD-YYYY']"); }
 
-    get maxDateValueInput() {return cy.get("*[data-qa='filters.maxDateOfValue-date-picker'] input[placeholder='MM-DD-YYYY']");}
+    get maxDateValueInput() { return cy.get("*[data-qa='filters.maxDateOfValue-date-picker'] input[placeholder='MM-DD-YYYY']"); }
 
-    get dateMinInputToCheckValue() {return cy.get("*[data-qa='filters.minDateOfValue-date-picker'] > input");}
+    get dateMinInputToCheckValue() { return cy.get("*[data-qa='filters.minDateOfValue-date-picker'] > input"); }
 
-    get dateMaxInputToCheckValue() {return cy.get("*[data-qa='filters.maxDateOfValue-date-picker'] > input");}
+    get dateMaxInputToCheckValue() { return cy.get("*[data-qa='filters.maxDateOfValue-date-picker'] > input"); }
 
-    get errorMessage() {return cy.get("#component-error-text");}
+    get errorMessage() { return cy.get("#component-error-text"); }
 
     get dateMinPickerButton() {
         return cy.xpath("//*[@data-qa='filters.minDateOfValue-date-picker']//child::button");
@@ -46,131 +48,131 @@ class RentCompsPage extends BasePage{
         return cy.xpath("//*[@data-qa='filters.maxDateOfValue-date-picker']//child::button");
     }
 
-    get pickerCalendar() {return cy.get("*[role='document']");}
+    get pickerCalendar() { return cy.get("*[role='document']"); }
 
-    getDayInCurrentMonthPicker(day: string | number) {return cy.xpath(`//*[text()='${day}']//parent::button[@tabindex='0']`);}
+    getDayInCurrentMonthPicker(day: string | number) { return cy.xpath(`//*[text()='${day}']//parent::button[@tabindex='0']`); }
 
-    get amenitiesArrowButton() {return cy.xpath("//*[text()='Amenities']//following-sibling::button");}
+    get amenitiesArrowButton() { return cy.xpath("//*[text()='Amenities']//following-sibling::button"); }
 
-    get numberOfFoundResults() {return cy.get("*[data-qa='rentComps-count']");}
+    get numberOfFoundResults() { return cy.get("*[data-qa='rentComps-count']"); }
 
-    get resetFiltersButton() {return cy.get("*[data-qa='reset-filters-button']");}
+    get resetFiltersButton() { return cy.get("*[data-qa='reset-filters-button']"); }
 
-    get sortByDropdown() {return cy.get("*[data-qa='select-value']");}
+    get sortByDropdown() { return cy.get("*[data-qa='select-value']"); }
 
-    getSortDropdownOptionByValue(value: string) {return cy.get(`*[data-value='${value}']`);}
+    getSortDropdownOptionByValue(value: string) { return cy.get(`*[data-value='${value}']`); }
 
-    get loadingModal() {return cy.get("*[data-qa='loading-modal']");}
+    get loadingModal() { return cy.get("*[data-qa='loading-modal']"); }
 
-    get photoElementLocator() {return "*[data-qa='photo']";}
+    get photoElementLocator() { return "*[data-qa='photo']"; }
 
-    get photoElements() {return cy.get(this.photoElementLocator);}
+    get photoElements() { return cy.get(this.photoElementLocator); }
 
-    get nextPhotoButtons() {return cy.get("button.slick-next");}
+    get nextPhotoButtons() { return cy.get("button.slick-next"); }
 
-    get prevPhotoButtons() {return cy.get("button.slick-prev");}
+    get prevPhotoButtons() { return cy.get("button.slick-prev"); }
 
-    get comparableItems() {return cy.get("*[data-qa='rentComp-item']");}
+    get comparableItems() { return cy.get("*[data-qa='rentComp-item']"); }
 
-    get comparableAddressesTexts() {return cy.get("*[data-qa='rentComp-address-text']");}
+    get comparableAddressesTexts() { return cy.get("*[data-qa='rentComp-address-text']"); }
 
-    get rentElementsTexts() {return cy.get("*[data-qa='rentComp-address-text'] + p");}
+    get rentElementsTexts() { return cy.get("*[data-qa='rentComp-address-text'] + p"); }
 
-    get comparablesAmenitiesTexts() {return cy.get("*[data-qa='rentComp-contitions']");}
+    get comparablesAmenitiesTexts() { return cy.get("*[data-qa='rentComp-contitions']"); }
 
-    get comparablePropertyTexts() {return cy.get("*[data-qa='rentComp-property']");}
+    get comparablePropertyTexts() { return cy.get("*[data-qa='rentComp-property']"); }
 
-    get selectComparableButtons() {return cy.get("*[data-qa='rentComp-select-btn']");}
+    get selectComparableButtons() { return cy.get("*[data-qa='rentComp-select-btn']"); }
 
-    get selectedMapComparableRemoveButtons() {return cy.get("*[data-qa='rentComp-remove-btn']");}
+    get selectedMapComparableRemoveButtons() { return cy.get("*[data-qa='rentComp-remove-btn']"); }
 
-    get zoomInButton() {return cy.get("*[title='Zoom in']");}
+    get zoomInButton() { return cy.get("*[title='Zoom in']"); }
 
-    get zoomOutButton() {return cy.get("*[title='Zoom out']");}
+    get zoomOutButton() { return cy.get("*[title='Zoom out']"); }
 
-    get uncategorizedTable() {return cy.get("*[data-qa='uncategorized']");}
+    get uncategorizedTable() { return cy.get("*[data-qa='uncategorized']"); }
 
-    get indexColumnCellsSelector() {return "*[data-qa='col-index']";}
+    get indexColumnCellsSelector() { return "*[data-qa='col-index']"; }
 
-    get indexColumnCells() {return cy.get(this.indexColumnCellsSelector);}
+    get indexColumnCells() { return cy.get(this.indexColumnCellsSelector); }
 
-    get addNewRentCompButton() {return cy.get("*[data-qa='add-new-rent-comp-btn']");}
+    get addNewRentCompButton() { return cy.get("*[data-qa='add-new-rent-comp-btn']"); }
 
-    get findRentCompSection() {return cy.contains("Find Rent Comparable");}
+    get findRentCompSection() { return cy.contains("Find Rent Comparable"); }
 
-    get searchAddressField() {return cy.get("*[placeholder='Search Address']");}
+    get searchAddressField() { return cy.get("*[placeholder='Search Address']"); }
 
-    get submitButton() {return cy.get("*[data-qa='submit-button']");}
+    get submitButton() { return cy.get("*[data-qa='submit-button']"); }
 
-    get searchResultsRows() {return cy.get("tr[data-qa^='search-results']");}
+    get searchResultsRows() { return cy.get("tr[data-qa^='search-results']"); }
 
-    get newUnitForm() {return cy.get("*[data-qa='new-unit-modal']");}
+    get newUnitForm() { return cy.get("*[data-qa='new-unit-modal']"); }
 
-    getCategoryRowByIndexLocator(index: number) {return `*[data-qa='row-${index}']`;}
+    getCategoryRowByIndexLocator(index: number) { return `*[data-qa='row-${index}']`; }
 
-    getCategoryRowByIndex(index: number) {return cy.get(this.getCategoryRowByIndexLocator(index));}
+    getCategoryRowByIndex(index: number) { return cy.get(this.getCategoryRowByIndexLocator(index)); }
 
-    get categoryRoomsCellsLocator() {return "*[data-qa='col-rooms']";}
+    get categoryRoomsCellsLocator() { return "*[data-qa='col-rooms']"; }
 
-    get categoryBedroomsCellsLocator() {return "*[data-qa='col-bedrooms']";}
+    get categoryBedroomsCellsLocator() { return "*[data-qa='col-bedrooms']"; }
 
-    get categoryRentsCellsLocator() {return "*[data-qa='col-rent']";}
+    get categoryRentsCellsLocator() { return "*[data-qa='col-rent']"; }
 
-    get categoryRentPerRoomLocator() {return "*[data-qa='col-rentPerRoom']";}
+    get categoryRentPerRoomLocator() { return "*[data-qa='col-rentPerRoom']"; }
 
-    get categorySourceOfInfoLocator() {return "*[data-qa='col-source-of-info']";}
+    get categorySourceOfInfoLocator() { return "*[data-qa='col-source-of-info']"; }
 
-    get advancedSearchButton() {return cy.get("[data-qa=advanced-search-link]");}
+    get advancedSearchButton() { return cy.get("[data-qa=advanced-search-link]"); }
 
-    get selectStateButton() {return cy.get("[data-qa='search.locationIdentifier-select-list'] [role='button']");}
+    get selectStateButton() { return cy.get("[data-qa='search.locationIdentifier-select-list'] [role='button']"); }
 
-    getStateByName(name: string) {return cy.get(`[data-qa='search.locationIdentifier-${name}-select-option']`);}
+    getStateByName(name: string) { return cy.get(`[data-qa='search.locationIdentifier-${name}-select-option']`); }
 
-    get propertyIdentifierInput() {return cy.get("*[name='search.propertyIdentifier']");}
+    get propertyIdentifierInput() { return cy.get("*[name='search.propertyIdentifier']"); }
 
-    getBedroomTableByNumber(bedroomsNumber: number | string) {return cy.get(`[data-qa='bedrooms_${bedroomsNumber}']`);}
+    getBedroomTableByNumber(bedroomsNumber: number | string) { return cy.get(`[data-qa='bedrooms_${bedroomsNumber}']`); }
 
-    getSelectButtonByAddress(address: string) {return cy.xpath(`//*[text()='${address}']//following-sibling::button`);}
+    getSelectButtonByAddress(address: string) { return cy.xpath(`//*[text()='${address}']//following-sibling::button`); }
 
-    getRemoveButtonByAddress(address: string) {return cy.xpath(`//*[text()='${address}']//following-sibling::button[contains(@data-qa, 'remove-btn')]`);}
+    getRemoveButtonByAddress(address: string) { return cy.xpath(`//*[text()='${address}']//following-sibling::button[contains(@data-qa, 'remove-btn')]`); }
 
-    get editButtonLocator() {return "[data-qa=col-edit] button";}
+    get editButtonLocator() { return "[data-qa=col-edit] button"; }
 
-    get removeButtonLocator() {return "[data-qa=col-remove] button";}
+    get removeButtonLocator() { return "[data-qa=col-remove] button"; }
 
-    get moveCellLocator() {return "[data-qa=drag-cell] svg";}
+    get moveCellLocator() { return "[data-qa=drag-cell] svg"; }
 
-    get indexCellLocator() {return "[data-qa=col-index]";}
+    get indexCellLocator() { return "[data-qa=col-index]"; }
 
-    get unitAddressLocator() {return "[data-qa=col-unitAddress]";}
+    get unitAddressLocator() { return "[data-qa=col-unitAddress]"; }
 
-    get uncategorizedMinCell() {return cy.xpath("//*[@data-qa='uncategorized']//descendant::td[.='Min']//following-sibling::td[1]");}
+    get uncategorizedMinCell() { return cy.xpath("//*[@data-qa='uncategorized']//descendant::td[.='Min']//following-sibling::td[1]"); }
 
-    get uncategorizedAverageCell() {return cy.xpath("//*[@data-qa='uncategorized']//descendant::td[.='Average']//following-sibling::td[1]");}
+    get uncategorizedAverageCell() { return cy.xpath("//*[@data-qa='uncategorized']//descendant::td[.='Average']//following-sibling::td[1]"); }
 
-    get uncategorizedMaxCell() {return cy.xpath("//*[@data-qa='uncategorized']//descendant::td[.='Max']//following-sibling::td[1]");}
+    get uncategorizedMaxCell() { return cy.xpath("//*[@data-qa='uncategorized']//descendant::td[.='Max']//following-sibling::td[1]"); }
 
-    get rentRollSummary() {return cy.contains("Rent Roll Summary");}
+    get rentRollSummary() { return cy.contains("Rent Roll Summary"); }
 
-    get uncategorizedSubjectMin() {return cy.get("[data-qa=rent-comps-type-uncategorized] [data-qa='Min:-cell']");}
+    get uncategorizedSubjectMin() { return cy.get("[data-qa=rent-comps-type-uncategorized] [data-qa='Min:-cell']"); }
 
-    get uncategorizedSubjectAverage() {return cy.get("[data-qa=rent-comps-type-uncategorized] [data-qa='Avg:-cell']");}
+    get uncategorizedSubjectAverage() { return cy.get("[data-qa=rent-comps-type-uncategorized] [data-qa='Avg:-cell']"); }
 
-    get uncategorizedSubjectMax() {return cy.get("[data-qa=rent-comps-type-uncategorized] [data-qa='Max:-cell']");}
+    get uncategorizedSubjectMax() { return cy.get("[data-qa=rent-comps-type-uncategorized] [data-qa='Max:-cell']"); }
 
-    get uncategorizedSubjectColumn() {return cy.get("[data-qa=rent-comps-type-uncategorized] [data-qa=computed-panel] [data-qa='0-column']");}
+    get uncategorizedSubjectColumn() { return cy.get("[data-qa=rent-comps-type-uncategorized] [data-qa=computed-panel] [data-qa='0-column']"); }
 
-    get displaySquareFootageForCompsCheckbox() {return cy.get("[data-qa=showSFForComps] input");}
+    get displaySquareFootageForCompsCheckbox() { return cy.get("[data-qa=showSFForComps] input"); }
 
-    get squareFootageCellsLocator() {return "[data-qa=col-sqft]";}
+    get squareFootageCellsLocator() { return "[data-qa=col-sqft]"; }
 
-    get rentPerSfCellsLocator() {return "[data-qa=col-rentPerSf]";}
+    get rentPerSfCellsLocator() { return "[data-qa=col-rentPerSf]"; }
 
-    get uncategorizedDevForecast() {return cy.get("[data-qa=rent-comps-type-uncategorized] [data-qa=computed-panel] [data-qa='1-column']");}
+    get uncategorizedDevForecast() { return cy.get("[data-qa=rent-comps-type-uncategorized] [data-qa=computed-panel] [data-qa='1-column']"); }
 
-    get bathroomsCellsLocator() {return "[data-qa=col-bathrooms]";}
+    get bathroomsCellsLocator() { return "[data-qa=col-bathrooms]"; }
 
-    getBedroomsTableHeader(bedroomsNumber: number) {return cy.get(`[data-qa='rent-comps-type-bedrooms_${bedroomsNumber}'] > p`);}
+    getBedroomsTableHeader(bedroomsNumber: number) { return cy.get(`[data-qa='rent-comps-type-bedrooms_${bedroomsNumber}'] > p`); }
 
     get uncategorizedTableHeader() { return cy.get("[data-qa=rent-comps-type-uncategorized] > p"); }
 
