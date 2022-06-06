71e8729315ea5429088aeed30335f77152312a8f
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-06-06 12:26:19 +0300

    tags edited

--- cypress/integration/not_full_reports/income/commercial/stabilized_rent_roll/QA-4582.spec.ts
+++ cypress/integration/not_full_reports/income/commercial/stabilized_rent_roll/QA-4582.spec.ts
@@ -2,10 +2,9 @@ import { Income, Property } from "../../../../../actions";
 import { _NavigationSection } from "../../../../../actions/base";
 import { createReport, deleteReport } from "../../../../../actions/base/baseTest.actions";
 import testData from "../../../../../fixtures/not_full_reports/income/commercial/stabilized_rent_roll/QA-4582.fixture";
-import { Tag } from "../../../../../utils/tags.utils";
 
 describe(`Verify the Unsaved changes modal functionality on the Income > Commercial > Stabilized Rent Roll page:`,
-    { tags: [ Tag.income, Tag.commercial, Tag.stabilized_rent_roll ] }, () => {
+    { tags: [ '@income', '@commercial', '@stabilized_rent_roll' ] }, () => {
 
         before("Login, create report", () => {
             createReport(testData.reportCreationData);
