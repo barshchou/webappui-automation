2ba9271df09086e177cb6bb2008d4a33e5d7fec9
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-06-07 10:43:55 +0300

    [openPageSmoke] Added pages titles enum, replaced it's usages

--- cypress/integration/smoke/criticalPagesOpen.spec.ts
+++ cypress/integration/smoke/criticalPagesOpen.spec.ts
@@ -5,6 +5,7 @@ import { getEnvUrl } from "../../../utils/env.utils";
 import { _NavigationSection } from "../../actions/base";
 import { Income, Sales } from "../../actions";
 import Enums from "../../enums/enums";
+import { _IncomeTitles, _SalesTitles } from "../../enums/pages_titles";
 
 const reportCreationData: BoweryAutomation.ReportCreationData = ReportDataCreator.getReportData("criticalPages", {
     incomeValue: Enums.INCOME_TYPE.BOTH,
@@ -29,51 +30,51 @@ describe("Verify that critical pages are opening", { tags: [ "@smoke" ] }, () =>
 
     it("Residential In-Place Rent Roll", () => {
         _NavigationSection.navigateToResInPlaceRentRoll();
-        Income._Residential.InPlaceRentRoll.Page.pageTitle.should("have.text", "In-Place Rent Roll");
+        Income._Residential.InPlaceRentRoll.Page.pageTitle.should("have.text", _IncomeTitles._Residential.IN_PLACE_RENT_ROLL);
     });
 
     it("Residential Stabilized Rent Roll", () => {
         _NavigationSection.navigateToResidentialStabilizedRentRoll();
-        Income._Residential.StabilizedRentRoll.Page.pageTitle.should("have.text", "Stabilized Rent Roll");
+        Income._Residential.StabilizedRentRoll.Page.pageTitle.should("have.text", _IncomeTitles._Residential.STABILIZED_RENT_ROLL);
     });
 
     it("Commercial In-Place Rent Roll", () => {
         _NavigationSection.navigateToCommercialInPlaceRentRoll();
-        Income._CommercialManager.InPlaceRentRoll.Page.pageTitle.should("have.text", "In-Place Rent Roll");
+        Income._CommercialManager.InPlaceRentRoll.Page.pageTitle.should("have.text", _IncomeTitles._Commercial.IN_PLACE_RENT_ROLL);
     });
 
     it("Commercial Stabilized Rent Roll", () => {
         _NavigationSection.navigateToCommercialStabilizedRentRoll();
-        Income._CommercialManager.StabilizedRentRoll.Page.pageTitle.should("have.text", "Stabilized Rent Roll");
+        Income._CommercialManager.StabilizedRentRoll.Page.pageTitle.should("have.text", _IncomeTitles._Commercial.STABILIZED_RENT_ROLL);
     });
 
     it("Expense Forecast Page", () => {
         _NavigationSection.navigateToExpenseForecast();
-        Income._ExpenseForecastActions.Page.pageTitle.should("have.text", "Expense Forecast");
+        Income._ExpenseForecastActions.Page.pageTitle.should("have.text", _IncomeTitles.EXPENSE_FORECAST);
     });
 
     it("Residential Rent Comps", () => {
         _NavigationSection.navigateToRentComps();
-        Income._Residential.RentComps.BaseActions.Page.pageTitle.should("have.text", "Rent Comps");
+        Income._Residential.RentComps.BaseActions.Page.pageTitle.should("have.text", _IncomeTitles._Residential.RENT_COMPS);
     });
 
     it("Commercial Rent Comps", () => {
         _NavigationSection.navigateToCommercialRentComps();
-        Income._CommercialManager.RentComps.Page.pageTitle.should("have.text", "Rent Comps");
+        Income._CommercialManager.RentComps.Page.pageTitle.should("have.text", _IncomeTitles._Commercial.RENT_COMPS);
     });
 
     it("Adjust Comps page", () => {
         _NavigationSection.navigateToAdjustComps();
-        Sales._AdjustComps.Page.pageTitle.should("contain.text", "Adjust Comps");
+        Sales._AdjustComps.Page.pageTitle.should("contain.text", _SalesTitles.ADJUST_COMPS);
     });
 
     it("Sales Value Conclusion page", () => {
         _NavigationSection.navigateToSalesValueConclusion();
-        Sales._ValueConclusion.Page.pageTitle.should("have.text", "Value Conclusion");
+        Sales._ValueConclusion.Page.pageTitle.should("have.text", _SalesTitles.VALUE_CONCLUSION);
     });
 
     it("Pro Forma page", () => {
         _NavigationSection.navigateToProForma();
-        Income._ProFormaActions.Page.pageTitle.should("have.text", "Pro Forma");
+        Income._ProFormaActions.Page.pageTitle.should("have.text", _IncomeTitles.PRO_FORMA);
     });
 });
