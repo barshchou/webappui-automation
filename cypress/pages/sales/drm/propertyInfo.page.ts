483557dbc4191e90961cf8b2cd2562a700b1215c
Ernst-D <ernst.dzerevanko@gmail.com>
2022-06-07 12:17:47 +0400

    [QA-5673] refactored PropertyInfo Form locators to data-qa

--- cypress/pages/sales/drm/propertyInfo.page.ts
+++ cypress/pages/sales/drm/propertyInfo.page.ts
@@ -1,29 +1,23 @@
 export default class PropertyInformationForm {
-    // get createCompNumberResidentialUnits() {return cy.xpath("//*[.='# Residential Units*']//child::input");}
-
-    get createCompNumberResidentialUnits() {return cy.get("comp-plex").shadow().contains("# Residential Units").next().find("input");}
-
-    // get conditionDropdown() {return cy.xpath("//*[text()='Condition*']//following::*[@class='ant-select-selector'][1]");}
-
-    get conditionDropdown() {return cy.get("comp-plex").shadow().contains("Condition").next();}
+    get conditionDropdown() {return cy.get('[data-qa="Condition"]', { includeShadowDom: true });}
 
     getDropdownOption(title: string) {return cy.get(`.ant-select-item-option[title='${title}']`, { includeShadowDom: true });}
 
-    get errorMessageNewComp() {return cy.xpath("//*[text()='Required'][contains(@class, 'errorText')]");}
+    get errorMessageNewComp() {return cy.get('[data-qa="error-text"]', { includeShadowDom: true });}
 
-    // get errorMessageNewComp() {return cy.get("comp-plex").shadow().contains("Required").find()}
+    get newCompContinueButton() {return cy.get('[data-qa="Continue"]', { includeShadowDom: true });}
 
-    get newCompContinueButton() {return cy.xpath("//button[.='Continue']");}
+    get comparableTypeDropdown() {return cy.get('[data-qa="Comparable Type"]', { includeShadowDom: true });}
 
-    get comparableTypeDropdown() {return cy.xpath("//*[text()='Comparable Type*']//following::*[@class='ant-select-selector'][1]");}
+    get createCompNumberCommercialUnits() {return cy.get('[data-qa="# Commercial Units"]', { includeShadowDom:true }).as("createCompNumberCommercialUnits");}
 
-    get createCompNumberCommercialUnits() {return cy.xpath("//*[text()='# Commercial Units*']//following::input[1]");}
+    get createCompNumberResidentialUnits() {return cy.get('[data-qa="# Residential Units"]', { includeShadowDom: true });}
 
-    get commercialAreaNewComp() {return cy.xpath("//*[text()='Commercial Area*']//following::input[1]");}
+    get commercialAreaNewComp() {return cy.get('[data-qa="Commercial Area"]', { includeShadowDom:true });}
 
-    get netRentableAreaNewComp() {return cy.xpath("//*[.='Net Rentable Area']//child::input");}
+    get netRentableAreaNewComp() {return cy.get('[data-qa="Net Rentable Area"]', { includeShadowDom: true });}
 
-    get averageUnitSizeNewComp() {return cy.xpath("//*[.='Average Unit Size']//child::input");}
+    get averageUnitSizeNewComp() {return cy.get('[data-qa="Average Unit Size"]', { includeShadowDom: true });}
 
     get internalNotesTextArea() {return cy.xpath("//*[text()='Internal Notes']//following::*[@class='ant-input'][1]");}
 
--- cypress/pages/sales/findComps.page.ts
+++ cypress/pages/sales/findComps.page.ts
@@ -60,47 +60,13 @@ class FindCompsPage extends BasePage {
     get importReportCompsButton() {return this.importCompModal.find("span").contains("Import")
         .parent("button");}
 
-    // get searchButton() {return cy.xpath("//button[.='Search']");}
-
     get searchButton() {return cy.get("button[.='Search']", { includeShadowDom: true });}
-
-    // get importCompsSelectButtons() {return cy.xpath("//*[.='Selected for report']//following::button[.='Select']");}
 
     get importCompsSelectButtons() {return cy.contains("Selected for report", { includeShadowDom: true }).siblings("button[.='Select']");}
 
     get createCompSearchResults() {return cy.get("[data-qa=search-result-form] tbody tr", { includeShadowDom: true });}
 
-    // get createNewCompButton() {return cy.xpath("//button[.='Create New']");}
-
     get createNewCompButton() {return cy.contains("Create New", { includeShadowDom: true });}
-
-    // get createCompNumberResidentialUnits() {return cy.xpath("//*[.='# Residential Units*']//child::input");}
-
-    get createCompNumberResidentialUnits() {return cy.get("comp-plex").shadow().contains("Residential Units").children("input");}
-
-    // get conditionDropdown() {return cy.xpath("//*[text()='Condition']//following::*[@class='ant-select-selector'][1]");}
-
-    get conditionDropdown() {return cy.contains("Condition", { includeShadowDom: true }).next(".ant-select");}
-
-    getDropdownOption(title: string) {return cy.get(`.ant-select-item-option[title='${title}']`, { includeShadowDom: true });}
-
-    // get errorMessageNewComp() {return cy.xpath("//*[text()='Required'][contains(@class, 'errorText')]");}
-
-    get errorMessageNewComp() {return cy.contains("Required", { includeShadowDom: true });}
-
-    // get newCompContinueButton() {return cy.xpath("//button[.='Continue']");}
-
-    get newCompContinueButton() {return cy.get("button.Continue", { includeShadowDom: true });}
-
-    // get netRentableAreaNewComp() {return cy.xpath("//*[.='Net Rentable Area']//child::input");}
-
-    get netRentableAreaNewComp() {return cy.contains("Net Rentable Area", { includeShadowDom: true }).children("input");}
-
-    // get averageUnitSizeNewComp() {return cy.xpath("//*[.='Average Unit Size']//child::input");}
-
-    get averageUnitSizeNewComp() {return cy.contains("Average Unit Size", { includeShadowDom: true }).children("input");}
-
-    // get newCompSaveAndCloseButton() {return cy.xpath("//button[.='Save & Close']");}
 
     get newCompSaveAndCloseButton() {return cy.contains("Save & Close", { includeShadowDom: true });}
 }
