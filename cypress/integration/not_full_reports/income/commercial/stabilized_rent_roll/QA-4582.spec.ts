e73fa8b885fda091303c553a519af9d2212e8d04
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-06-08 11:00:13 +0300

    indentation fixed

--- cypress/integration/not_full_reports/income/commercial/stabilized_rent_roll/QA-4582.spec.ts
+++ cypress/integration/not_full_reports/income/commercial/stabilized_rent_roll/QA-4582.spec.ts
@@ -17,8 +17,8 @@ describe(`Verify the Unsaved changes modal functionality on the Income > Commerc
             _NavigationSection.navigateToCommercialInPlaceRentRoll();
             Income._CommercialManager.InPlaceRentRoll.chooseListLeaseStatuses(testData.leaseStatuses, testData.numberOfCommercialUnits);
 
-            cy.stepInfo(`1. Fill in the editable fields with values or/and check check-boxes or/and click the radio button 
-        and do NOT click on the Save button.`);
+            cy.stepInfo(`1. Fill in the editable fields with values or/and check check-boxes or/and click 
+            the radio button and do NOT click on the Save button.`);
             _NavigationSection.navigateToStabilizedRentRollInCommercial();
             Income._CommercialManager.StabilizedRentRoll.verifyThatPageIsOpened()
                 .Page.rentPerSFAnnuallyTotal.scrollIntoView();
@@ -43,7 +43,7 @@ describe(`Verify the Unsaved changes modal functionality on the Income > Commerc
                 .verifyCommentaryContainsText(testData.commentText);
 
             cy.stepInfo(`4. Repeat step 1, try to proceed on any other page from the Stabilized Rent Roll page 
-       and verify that the Unsaved changes modal is displayed.`);
+            and verify that the Unsaved changes modal is displayed.`);
             Income._CommercialManager.StabilizedRentRoll
                 .Page.rentPerSFAnnuallyTotal.scrollIntoView();
             Income._CommercialManager.StabilizedRentRoll.enterListPerSFAnnually(testData.leaseStatuses, testData.rentToBe2)
