ad54a3d76f23821282b1d6cef2aacee620247558
Viachaslau Barshchou <viachaslau.barshchou@coxautoinc.com>
2022-06-08 10:14:14 +0300

    [QA-5674] fixes after review

--- cypress/integration/not_full_reports/income/pro_forma/QA-4995.spec.ts
+++ cypress/integration/not_full_reports/income/pro_forma/QA-4995.spec.ts
@@ -16,7 +16,8 @@ describe("[QA-4995] Verify that combined utilities expenses is enabled on the Pr
         Property._Summary.enterNumberOfResUnits(testData.buildingDescription.numberOfUnits)
             .enterGrossBuildingArea(testData.buildingDescription.grossArea);
 
-        cy.stepInfo(`Navigate to Expense Forecast page and set expenses for Fuel, Electricity and Water&Sewer.`);
+        cy.stepInfo(`2. Navigate to Expense Forecast page and set expenses for Fuel, 
+                    Electricity and Water&Sewer.`);
         _NavigationSection.navigateToExpenseForecast();
         testData.forecastItems.forEach(expense => {
             Income._ExpenseForecastActions.enterForecastItemForecast(expense);   
@@ -24,50 +25,54 @@ describe("[QA-4995] Verify that combined utilities expenses is enabled on the Pr
     });
 
     it("Test body", () => {
-        cy.stepInfo(`Navigate to Expense History and Set Expense mode: Electricity and Fuel`);
+        cy.stepInfo(`3. Navigate to Expense History and Set Expense mode: Electricity and Fuel`);
         _NavigationSection.navigateToExpenseHistory();
         Income._ExpenseHistory.checkUtilitiesExpensesOption(testData.expenseModeElectricityFuel);
 
-        cy.stepInfo(`Navigate to Expense Forecast and set Utilities expenses`);
+        cy.stepInfo(`4. Navigate to Expense Forecast and set Utilities expenses`);
         _NavigationSection.navigateToExpenseForecast();
         Income._ExpenseForecastActions.enterForecastItemForecast(testData.utilitiesFuelElectricityItem);
 
-        cy.stepInfo(`4.1 Navigate to Pro Forma page and validate: 
+        cy.stepInfo(`5. Navigate to Pro Forma page and validate: 
                     If Combined Electricity and Fuel on Income > Expense History: the Utilities line item instead
                     of Electricity and Fuel which reflects the Total, PSF, and per unit forecasted values for the Utilities expense. `);
         _NavigationSection.navigateToProForma();
         Income._ProFormaActions.verifyExpensesCombined(testData.expenseModeElectricityFuel);
-        Income._ProFormaActions.verifyCategoryTotal(`$${numberWithCommas(Math.round(testData.totalElectricityAndFuel))}`, proFormaTypesEnum.utilities)
-            .verifyCategoryTotal(`$${numberWithCommas(Math.round(testData.totalWater))}`, proFormaTypesEnum.waterAndSewer);
+        Income._ProFormaActions.verifyCategoryTotal(
+            `$${numberWithCommas(Math.round(testData.totalElectricityAndFuel))}`, proFormaTypesEnum.utilities)
+                .verifyCategoryTotal(
+                    `$${numberWithCommas(Math.round(testData.totalWater))}`, proFormaTypesEnum.waterAndSewer);
 
-        cy.stepInfo(`Navigate to Expense History and Set Expense mode: Electricity, Fuel and Water `);
+        cy.stepInfo(`6. Navigate to Expense History and Set Expense mode: Electricity, Fuel and Water `);
         _NavigationSection.navigateToExpenseHistory();
         Income._ExpenseHistory.checkUtilitiesExpensesOption(testData.expenseModeElectricityFuelWater);
     
-        cy.stepInfo(`Navigate to Expense Forecast and set Utilities expenses`);
+        cy.stepInfo(`7. Navigate to Expense Forecast and set Utilities expenses`);
         _NavigationSection.navigateToExpenseForecast();
         Income._ExpenseForecastActions.enterForecastItemForecast(testData.utilitiesFuelElectricityWaterItem);
 
-        cy.stepInfo(`4.2 Navigate to Pro Forma page and validate: 
+        cy.stepInfo(`8. Navigate to Pro Forma page and validate: 
                     If Combined Electricity, Fuel, Water & Sewer on Income > Expense History: 
                     the Utilities line item instead of Electricity, Fuel, and Water & Sewer line items which 
                     reflects the Total, PSF, and per unit forecasted values for the Utilities expense.`);
         _NavigationSection.navigateToProForma();
         Income._ProFormaActions.verifyExpensesCombined(testData.expenseModeElectricityFuelWater);
-        Income._ProFormaActions.verifyCategoryTotal(`$${numberWithCommas(Math.round(testData.totalElectricityFuelWater))}`, proFormaTypesEnum.utilities);
+        Income._ProFormaActions.verifyCategoryTotal(
+            `$${numberWithCommas(Math.round(testData.totalElectricityFuelWater))}`, proFormaTypesEnum.utilities);
 
-        cy.stepInfo(`Navigate to Expense History and Set Expense mode: Broken Out`);
+        cy.stepInfo(`9. Navigate to Expense History and Set Expense mode: Broken Out`);
         _NavigationSection.navigateToExpenseHistory();
         Income._ExpenseHistory.checkUtilitiesExpensesOption(testData.expenseModeBrokenOut);
 
-        cy.stepInfo(`4.3 Navigate to Pro Forma page and validate: 
+        cy.stepInfo(`10. Navigate to Pro Forma page and validate: 
                     If Broken Out on Income > Expense History: 
                     no change from current behavior, Electricity, Fuel, and Water & Sewer line items all appear`);
         _NavigationSection.navigateToProForma();
         Income._ProFormaActions.verifyExpensesCombined(testData.expenseModeBrokenOut);
-        Income._ProFormaActions.verifyCategoryTotal(`$${numberWithCommas(Math.round(testData.totalElectricity))}`, proFormaTypesEnum.electricity)
-            .verifyCategoryTotal(`$${numberWithCommas(Math.round(testData.totalFuel))}`, proFormaTypesEnum.fuel)
-            .verifyCategoryTotal(`$${numberWithCommas(Math.round(testData.totalWater))}`, proFormaTypesEnum.waterAndSewer);
+        Income._ProFormaActions.verifyCategoryTotal(
+            `$${numberWithCommas(Math.round(testData.totalElectricity))}`, proFormaTypesEnum.electricity)
+                .verifyCategoryTotal(`$${numberWithCommas(Math.round(testData.totalFuel))}`, proFormaTypesEnum.fuel)
+                .verifyCategoryTotal(`$${numberWithCommas(Math.round(testData.totalWater))}`, proFormaTypesEnum.waterAndSewer);
 
 
         deleteReport(testData.reportCreationData.reportNumber);
