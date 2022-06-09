556d432c170df31b680a14c5071d03a338e029c6
Ernst-D <ernst.dzerevanko@gmail.com>
2022-06-09 13:22:00 +0400

    [QA-5673] fixed QA-4332 with realClick command

--- cypress/actions/income/residential/rent_comps/rentComps.actions.ts
+++ cypress/actions/income/residential/rent_comps/rentComps.actions.ts
@@ -334,14 +334,10 @@ class RentCompsActions extends BaseActionsExt<typeof rentCompsPage> {
             currentPhoto.should("exist");
             let currentStyle = jQueryPhotoElements.eq(i).attr("style");
             currentPhoto.invoke("attr", "style").should("not.equal", style);
-            /**
-             * ernst: very unstable part in QA-4332, simetimes this spec fails here with
-             * probably gonna need add {force: true}
-             */
             if (direction === "forward") {
-                rentCompsPage.nextPhotoButtons.eq(comparableIndex).click();
+                rentCompsPage.nextPhotoButtons.eq(comparableIndex).realClick();
             } else {
-                rentCompsPage.prevPhotoButtons.eq(comparableIndex).click();
+                rentCompsPage.prevPhotoButtons.eq(comparableIndex).realClick();
             }
             this.verifyLoadingDoesntExist();
             style = currentStyle;
