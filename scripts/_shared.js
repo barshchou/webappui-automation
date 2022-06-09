7b70e34a8a64365762d24242b018df352805a9b8
Ernst-D <ernst.dzerevanko@gmail.com>
2022-06-09 17:11:35 +0400

    [hotfix] added support for dev/staging/prod env selection

--- scripts/_shared.js
+++ scripts/_shared.js
@@ -1,4 +1,5 @@
 const { exec } = require("child_process");
+const envUrls = require("./env.urls");
 
 function _exec(command,cb) {   
     exec(command, function(err,stdout,stderr){
@@ -14,13 +15,36 @@ function _mutateCommand(command, args){
     if(args != undefined || args == ""){
       command = command.concat(` ${args}`)
       let customEnv = "";
-      customEnv =  args.split(",").find(elem => elem.startsWith("customEnv="))
+      let url = "";
+
+      customEnv = args.split(",").find(elem => elem.startsWith("customEnv="));
+      url = args.split(",").find(elem => elem.includes("url="))
+      .split(" ").find(elem => elem.startsWith("url="));
+
+      console.log("\n"+"Env is: "+url);
+      console.log("Custom url is:"+customEnv+"\n");
+      
       if(customEnv != undefined){
         customEnv = customEnv.replace("customEnv=","").replace(/["']/g, "");
         command = command.concat(` --config baseUrl=${customEnv}`)
+        return command;
+      }
+      else if(url != undefined){
+        switch(true){
+          case (url == "url=dev"):
+            command = command.concat(` --config baseUrl=${envUrls.DEV}`);
+            break;
+          case (url == "url=staging"):
+            command = command.concat(` --config baseUrl=${envUrls.STAGING}`);
+            break;
+          case (url == "url=prod"):
+            command = command.concat(` --config baseUrl=${envUrls.PROD}`);
+            break;
+        }        
+        return command;
       }
     }
-    return command
+    throw new Error("Invalid args!");
 }
 
 module.exports = {_exec, _mutateCommand};
