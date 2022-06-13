35c1c45b0693ff158d8d636d97235960e576482b
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-06-13 14:38:31 +0300

    action renamed

--- cypress/integration/not_full_reports/report/key_info/QA-4426.spec.ts
+++ cypress/integration/not_full_reports/report/key_info/QA-4426.spec.ts
@@ -21,7 +21,7 @@ describe("[QA-4426] Check the generated commentary for Property Rights Appraised
 
             cy.stepInfo("3. Verify that the interest appraised elements of the generated commentary (highlighted in red) are chips");
             Report._KeyInfo.Page.textBoxPropertyRightsAppraised.contains(item.check).trigger("mouseover");
-            Report._KeyInfo.verifyTooltipExist();
+            Report._KeyInfo.verifyTooltipNotExist();
 
             cy.stepInfo(`4. Verify that the Property Rights Appraised  discussion appears below the h2 Introduction > Property Rights 
                 Appraised section in the exported report.`);
