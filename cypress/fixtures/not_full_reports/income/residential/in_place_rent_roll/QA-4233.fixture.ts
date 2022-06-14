efca2a15183a614ad0b0646c8ba4ba04c96120fc
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-06-14 16:01:02 +0300

    [QA-5697] Done QA-4233

--- cypress/fixtures/not_full_reports/income/residential/in_place_rent_roll/QA-4233.fixture.ts
+++ cypress/fixtures/not_full_reports/income/residential/in_place_rent_roll/QA-4233.fixture.ts
@@ -5,38 +5,29 @@ import ReportDataCreator from "../../../../data_creator/reportData.creator";
 const rentRollResidentialUnitsFixture = () : BoweryReports.ResidentialUnit[] => {
     return [
         {
-            rooms: 5,
             monthlyRent: 500.01,
-            leaseStatus: "Vacant",
-            rentType: "Market Rate",
+            bedRooms: 2
         },
         {
-            rooms: 10,
             monthlyRent: 701,
-            leaseStatus: "Occupied",
-            rentType: "Rent Stabilized",
+            bedRooms: 5.5
         },
         {
-            rooms: 5,
             monthlyRent: 12345,
-            leaseStatus: "Employee",
-            rentType: "Rent Controlled",
+            bedRooms: "fdsf"
         },
         {
-            rooms: 4,
             monthlyRent: 125,
-            leaseStatus: "Employee",
-            rentType: "Section 8",
+            bedRooms: 0
         },
     ];
 };
 
 export default {
-    reportCreationData: ReportDataCreator.getReportData("4238-39_41-42", {
+    reportCreationData: ReportDataCreator.getReportData("4233", {
         conclusionValue: enums.VALUE_CONCLUSION_TYPE.AS_IS,
         incomeValue: enums.INCOME_TYPE.residential
     }),
-    perRoomAnalysis: "Include Per Room Analysis in Report",
     columnName: "Rent/Room",
     residentialUnits: rentRollResidentialUnitsFixture()
 };
