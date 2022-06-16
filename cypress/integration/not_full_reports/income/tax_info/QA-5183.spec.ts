ef46c2fc1339c475b985c88f46a60a27ebbdb40d
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-06-16 14:16:05 +0300

    [QA-5562] WIP

--- cypress/integration/not_full_reports/income/tax_info/QA-5183.spec.ts
+++ cypress/integration/not_full_reports/income/tax_info/QA-5183.spec.ts
@@ -1,42 +1,44 @@
-// import testData from "../../../../fixtures/not_full_reports/income/tax_info/QA-4295.fixture";
-// import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
-// import { _NavigationSection } from "../../../../actions/base";
-// import { _TaxInfo } from "../../../../actions/income";
-// import { ReviewExport } from './../../../../actions/index';
+import testData from "../../../../fixtures/not_full_reports/income/tax_info/QA-5183.fixture";
+import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
+import { _NavigationSection } from "../../../../actions/base";
+import { _TaxInfo } from "../../../../actions/income";
+import { ReviewExport } from './../../../../actions/index';
 
-// describe("Export column order both assessment psf and assessment per unit", () => {
 
-//     it("Test body", { tags: [ "@check_export", "@income", "@tax_info" ] }, () => {
-//         createReport(testData.reportCreationData);
+// Wait until functionality is added to stage 
+describe.skip("[QA-5183] Export column order both assessment psf and assessment per unit", () => {
 
-//         cy.stepInfo("1. Navigate to Income -> Tax Info");
-//         _NavigationSection.navigateToTaxInfo();
+    it("Test body", { tags: [ "@check_export", "@income", "@tax_info" ] }, () => {
+        createReport(testData.reportCreationData);
 
-//         cy.stepInfo("2. Verify  Tax Calculation discussion title and commentary is diplayed");
-//         _TaxInfo.verifyTaxSummaryDiscussionTitle(testData.title).verifyTaxSummaryCommentary(testData.commentary);
+        cy.stepInfo("1. Navigate to Income -> Tax Info");
+        _NavigationSection.navigateToTaxInfo();
 
-//         cy.stepInfo("3. Verify Tax Calculation discussion's tooltip content");
-//         _TaxInfo.verifyTaxSummaryTooltip(testData.tooltip);
+        cy.stepInfo("2. Verify  Tax Calculation discussion title and commentary is displayed");
+        _TaxInfo.verifyTaxSummaryDiscussionTitle(testData.title).verifyTaxSummaryCommentary(testData.commentary);
 
-//         cy.stepInfo("4. Export the report");
-//         _NavigationSection.Actions.openReviewAndExport();
-//         ReviewExport.generateDocxReport().waitForReportGenerated()
-//             .downloadAndConvertDocxReport(testData.reportCreationData.reportNumber);
+        cy.stepInfo("3. Verify Tax Calculation discussion's tooltip content");
+        _TaxInfo.verifyTaxSummaryTooltip(testData.tooltip);
 
-//         deleteReport(testData.reportCreationData.reportNumber);
-//     });
+        cy.stepInfo("4. Export the report");
+        _NavigationSection.Actions.openReviewAndExport();
+        ReviewExport.generateDocxReport().waitForReportGenerated()
+            .downloadAndConvertDocxReport(testData.reportCreationData.reportNumber);
 
-//     it("Check export", () => {
-//         cy.task("getFilePath",
-//         { _reportName: testData.reportCreationData.reportNumber, _docx_html: "html" }
-//         ).then(file => {
-//             cy.log(<string>file);
+        deleteReport(testData.reportCreationData.reportNumber);
+    });
+
+    it("Check export", () => {
+        cy.task("getFilePath",
+        { _reportName: testData.reportCreationData.reportNumber, _docx_html: "html" }
+        ).then(file => {
+            cy.log(<string>file);
             
-//             cy.stepInfo("Check that Tax Calculation Discussion commentary exports in the 'Assessed Value & RE Taxes' section of the report.");
+            cy.stepInfo("Check that Tax Calculation Discussion commentary exports in the 'Assessed Value & RE Taxes' section of the report.");
             
-//             cy.visit(<string>file);
-//             cy.contains("Assessed Value & Real Estate Taxes").scrollIntoView().next().next()
-//                 .next().next().next().next().next().should("have.text", testData.commentary);
-//         });
-//     });
-// });
+            cy.visit(<string>file);
+            cy.contains("Assessed Value & Real Estate Taxes").scrollIntoView().next().next()
+                .next().next().next().next().next().should("have.text", testData.commentary);
+        });
+    });
+});
