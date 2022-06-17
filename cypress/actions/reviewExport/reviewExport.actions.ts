179c1c38d06d76a39d815cbb26a2b5fc15adff84
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-06-17 16:57:21 +0300

    verify XML actions added

--- cypress/actions/reviewExport/reviewExport.actions.ts
+++ cypress/actions/reviewExport/reviewExport.actions.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import reviewExportPage from "../../pages/reviewExport/reviewExport.page";
 import BaseActionsExt from "../base/base.actions.ext";
+import { snakeCase } from 'lodash';
 class ReviewExportActions extends BaseActionsExt<typeof reviewExportPage> {
 
     verifyPageIsOpened() {
         reviewExportPage.headerTitle.should("exist");
         return this;
     }
-    
+
     waitForReportGenerated(): this {
         reviewExportPage.statusBar.should("contain.text", "Pending");
         reviewExportPage.statusBar.should("contain.text", "Complete");
@@ -20,16 +21,51 @@ class ReviewExportActions extends BaseActionsExt<typeof reviewExportPage> {
         return this;
     }
 
+    generateXMLReport(): this {
+        reviewExportPage.generateXmlBtn.click();
+        return this;
+    }
+
+    verifyXMLReportID(reportName: string): this {
+        let aliasXMLGeneration = "aliasXMLGeneration";
+        cy.intercept({
+            method: 'GET',
+            url: '**api/xmlGeneration/?*'
+        }).as(aliasXMLGeneration);
+        this.generateXMLReport();
+        cy.wait(`@${aliasXMLGeneration}`, { timeout: 20000 })
+            .then(({ response }) => {
+                const fileNamePart = `${snakeCase(reportName)}`;
+                expect(response.statusCode).equal(200);
+                expect(response.body.downloadUrl).contain(`/downloadXML/${fileNamePart}`);
+            });
+        return this;
+    }
+
+    verifyXMLReportOpens(reportName: string): this {
+        let aliasOpenXML = "aliasOpenXML";
+        const urlNamePart = `${snakeCase(reportName)}`;
+        cy.intercept({
+            method: 'GET',
+            url: `**/downloadXML/${urlNamePart}*`
+        }).as(aliasOpenXML);
+        cy.wait(`@${aliasOpenXML}`, { timeout: 20000 })
+            .then(({ response }) => {
+                expect(response.statusCode).equal(200);
+            });
+        return this;
+    }
+
     /**
      * Downloads and converts *.docx report into html
      * and renames it to *current_spec_name*.html
      */
-    downloadAndConvertDocxReport(reportName:string): this {
+    downloadAndConvertDocxReport(reportName: string): this {
         reviewExportPage.downloadBtn.click();
         cy.task("getFilePath", { _reportName: reportName, _docx_html: "docx" }).then(file => {
             cy.log(<string>file);
             cy.task("waitForFileExists", file);
-            cy.task("convertDocxToHtml", file); 
+            cy.task("convertDocxToHtml", file);
         });
         return this;
     }
