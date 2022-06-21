55b2ae6b7a5f01d40236487f21f565b2b4018231
Viachaslau.Barshchou <Viachaslau.Barshchou@boweryvaluation.com>
2022-06-21 15:12:52 +0300

    [QA-5881] update test to use chip creator class

--- cypress/fixtures/not_full_reports/income/commercial/stabilized_rent_roll/QA-4598-00.fixture.ts
+++ cypress/fixtures/not_full_reports/income/commercial/stabilized_rent_roll/QA-4598-00.fixture.ts
@@ -1,5 +1,8 @@
 import enums from "../../../../../enums/enums";
+import { BoweryReports } from "../../../../../types";
+import chipsDataCreator from "../../../../data_creator/chipsData.creator";
 import reportDataCreator from "../../../../data_creator/reportData.creator";
+
 
 const _numberOfCommercialUnits = 3;
 const _numberOfResidentialUnits = 2;
@@ -9,149 +12,15 @@ const _siteArea = 735000;
 const _currentNumberOfCommercialUnits = 5;
 const _currentNumberOfResidentialUnits = 4;
 
-const _baseChip = () => {
-    return [
-        {
-            suggestionName: 'Block',
-            typeSuggestValue: 'Bloc',
-            verifySuggest: '962',
-            verifyExport: '962'
-        }, 
-        {
-            suggestionName: 'Building Name',
-            typeSuggestValue: 'Buildin',
-            verifySuggest: _buildingName,
-            verifyExport: _buildingName
-        }, 
-        {
-            suggestionName: 'Concluded Cap Rate',
-            typeSuggestValue: 'Conclude',
-            verifySuggest: '0%',
-            verifyExport: 'Conclude'
-        }, 
-        {
-            suggestionName: 'Foreclosure Sale',
-            typeSuggestValue: 'Foreclosur',
-            verifySuggest: 'The above transaction reflects a foreclosure sale of the property. Typically in a foreclosure sale, the buyer assumes all encumbrances on the site, including any outstanding mortgage amount and legal fees ("upset costs"), delinquent taxes and water and sewer charges, foreclosure sale fee, and realty transfer taxes. This information was requested from the owner; however, not provided.',
-            verifyExport: 'foreclosure sale'
-        }, 
-        {
-            suggestionName: 'Sherrif\'s Sale',
-            typeSuggestValue: 'Sherri',
-            verifySuggest: "The above transaction reflects a Sheriff's sale of the property. Typically in a Sheriff’s sale, the buyer assumes all encumbrances on the site, including any outstanding mortgage amount and legal fees (\"upset costs\"), delinquent taxes and water and sewer charges, Sheriff's sale fee, and realty transfer taxes. This information was requested from the owner; however, not provided.",
-            verifyExport: "The above transaction reflects a Sheriff's sale of the property. Typically in a Sheriff’s sale, the buyer assumes all encumbrances on the site, including any outstanding mortgage amount and legal fees (\"upset costs\"), delinquent taxes and water and sewer charges, Sheriff's sale fee, and realty transfer taxes. This information was requested from the owner; however, not provided."
-        },
-        {
-            suggestionName: 'Gross Building Area',
-            typeSuggestValue: 'Gros',
-            verifySuggest: _grossBuildingArea,
-            verifyExport: _grossBuildingArea
-        }, 
-        {
-            suggestionName: 'Site Area',
-            typeSuggestValue: 'Sit',
-            verifySuggest: _siteArea,
-            verifyExport: _siteArea
-        }, 
-        {
-            suggestionName: 'Street Address',
-            typeSuggestValue: 'Street',
-            verifySuggest: '462 1st Avenue',
-            verifyExport: '462 1st Avenue'
-        },
-        {
-            suggestionName: 'Lot',
-            typeSuggestValue: 'Lo',
-            verifySuggest: '100',
-            verifyExport: '100'
-        },  
-        {
-            suggestionName: 'Street Name',
-            typeSuggestValue: 'Stree',
-            verifySuggest: '1st Avenue',
-            verifyExport: '1st Avenue'
-        }, 
-        {
-            suggestionName: 'Unchanged Renovations',
-            typeSuggestValue: 'Unchange',
-            verifySuggest: 'Upon renovation, the subject unit count and gross building area will remain unchanged.',
-            verifyExport: 'Upon renovation, the subject unit count and gross building area will remain unchanged.'
-        },
-        {
-            suggestionName: 'Zone(s)',
-            typeSuggestValue: 'Zone',
-            verifySuggest: 'R8',
-            verifyExport: 'R8'
-        }, 
-    ];
-}; 
-
-const _baseChipsAsCompleted = () => {
-    return [
-        {
-            suggestionName: 'Current Residential Unit Count',
-            typeSuggestValue: 'Residential',
-            verifySuggest: _numberOfCommercialUnits,
-            verifyExport: _numberOfCommercialUnits
-        },
-        {
-            suggestionName: 'Current Commercial Unit Count',
-            typeSuggestValue: 'Commercial',
-            verifySuggest: _numberOfResidentialUnits,
-            verifyExport: _numberOfResidentialUnits
-        },
-        {
-            suggestionName: 'Current Condition',
-            typeSuggestValue: 'Condition',
-            verifySuggest: 'in  condition',
-            verifyExport: 'in  condition'
-        },
-        {
-            suggestionName: 'As Complete Residential Unit Count',
-            typeSuggestValue: 'Residential',
-            verifySuggest: _currentNumberOfResidentialUnits,
-            verifyExport: _currentNumberOfResidentialUnits
-        },
-        {
-            suggestionName: 'As Complete Commercial Unit Count',
-            typeSuggestValue: 'Commercial',
-            verifySuggest: _currentNumberOfCommercialUnits,
-            verifyExport: _currentNumberOfCommercialUnits
-        },
-        {
-            suggestionName: 'As Stabilized Condition',
-            typeSuggestValue: 'Conditio',
-            verifySuggest: 'in  condition',
-            verifyExport: 'condition'
-        }
-    ];
+const _chipsOptions: BoweryReports.ChipsCreationOptions = {
+    buildingName: _buildingName,
+    grossBuildingArea: _grossBuildingArea,
+    numberOfCommercialUnits: _numberOfCommercialUnits,
+    numberOfResidentialUnits: _numberOfResidentialUnits,
+    siteArea: _siteArea,
+    currentCommercialUnits: _currentNumberOfCommercialUnits,
+    currentResidentialUnits: _currentNumberOfResidentialUnits
 };
-
-const _baseChipsAsIsAsStabilized = () => {
-    return [
-        {
-            suggestionName: 'Residential Unit Count',
-            typeSuggestValue: 'Residentia',
-            verifySuggest: _numberOfResidentialUnits,
-            verifyExport: _numberOfResidentialUnits
-        },
-        {
-            suggestionName: 'Commercial Unit Count',
-            typeSuggestValue: 'Commerci',
-            verifySuggest: _numberOfCommercialUnits,
-            verifyExport: _numberOfCommercialUnits
-        },
-        {
-            suggestionName: 'Condition',
-            typeSuggestValue: 'Conditio',
-            verifySuggest: 'in  condition',
-            verifyExport: 'condition'
-        }
-    ];
-};
-
-const _asIsAsStabilizedChips = _baseChip().concat(_baseChipsAsIsAsStabilized());
-const _asCompletedChips = _baseChip().concat(_baseChipsAsCompleted());
 
 export default {
     reportCreationDataAsIs: reportDataCreator.getReportData("4598-00", {
@@ -168,9 +37,9 @@ export default {
     }),
     numberOfCommercialUnits: _numberOfCommercialUnits,
     numberOfResidentialUnits: _numberOfResidentialUnits,
-    asIschips: _asIsAsStabilizedChips,
-    asStabilizedChips: _asIsAsStabilizedChips,
-    asCompletedChips: _asCompletedChips,
+    asIschips: chipsDataCreator.getChipsData(_chipsOptions),
+    asStabilizedChips: chipsDataCreator.getChipsData(_chipsOptions, enums.VALUE_CONCLUSION_TYPE.AS_STABILIZED),
+    asCompletedChips: chipsDataCreator.getChipsData(_chipsOptions, enums.VALUE_CONCLUSION_TYPE.AS_COMPLETE),
     currentNumberOfCommercialUnits: _currentNumberOfCommercialUnits,
     currentNumberOfResidentialUnits: _currentNumberOfResidentialUnits,
     grossBuildingArea: _grossBuildingArea,
--- cypress/fixtures/not_full_reports/property/commercial_units/QA-4549-54.fixture.ts
+++ cypress/fixtures/not_full_reports/property/commercial_units/QA-4549-54.fixture.ts
@@ -1,165 +1,25 @@
 import enums from "../../../../enums/enums";
+import { BoweryReports } from "../../../../types";
+import chipsDataCreator from "../../../data_creator/chipsData.creator";
 import reportDataCreator from "../../../data_creator/reportData.creator";
-import { Chips } from "../../data_chips/chips.fixture";
 
 const _numberOfCommercialUnits = 3;
 const _numberOfResidentialUnits = 2;
-const _buildingName = 'Test Building Name QA-4549-54';
+const _buildingName = 'Test Building Name QA-4598-00';
 const _grossBuildingArea = 5999;
 const _siteArea = 735000;
 const _currentNumberOfCommercialUnits = 5;
 const _currentNumberOfResidentialUnits = 4;
-const _block = '962';
-const _streetAddress = '462 1st Avenue';
-const _lotValue = '100';
-const _streetName = '1st Avenue';
 
-let chips = new Chips(_block, _buildingName, _grossBuildingArea, _lotValue, _numberOfCommercialUnits, _numberOfResidentialUnits,
-                    _siteArea, _streetAddress, _streetName, _currentNumberOfCommercialUnits, _currentNumberOfResidentialUnits);
-
-// const _baseChip = () => {
-//     return [
-//         {
-//             suggestionName: 'Block',
-//             typeSuggestValue: 'Bloc',
-//             verifySuggest: _block,
-//             verifyExport: _block
-//         }, 
-//         {
-//             suggestionName: 'Building Name',
-//             typeSuggestValue: 'Buildin',
-//             verifySuggest: _buildingName,
-//             verifyExport: _buildingName
-//         }, 
-//         {
-//             suggestionName: 'Concluded Cap Rate',
-//             typeSuggestValue: 'Conclude',
-//             verifySuggest: '0%',
-//             verifyExport: 'Conclude'
-//         }, 
-//         {
-//             suggestionName: 'Foreclosure Sale',
-//             typeSuggestValue: 'Foreclosur',
-//             verifySuggest: 'The above transaction reflects a foreclosure sale of the property. Typically in a foreclosure sale, the buyer assumes all encumbrances on the site, including any outstanding mortgage amount and legal fees ("upset costs"), delinquent taxes and water and sewer charges, foreclosure sale fee, and realty transfer taxes. This information was requested from the owner; however, not provided.',
-//             verifyExport: 'foreclosure sale'
-//         }, 
-//         {
-//             suggestionName: 'Sherrif\'s Sale',
-//             typeSuggestValue: 'Sherri',
-//             verifySuggest: "The above transaction reflects a Sheriff's sale of the property. Typically in a Sheriff’s sale, the buyer assumes all encumbrances on the site, including any outstanding mortgage amount and legal fees (\"upset costs\"), delinquent taxes and water and sewer charges, Sheriff's sale fee, and realty transfer taxes. This information was requested from the owner; however, not provided.",
-//             verifyExport: "The above transaction reflects a Sheriff's sale of the property. Typically in a Sheriff’s sale, the buyer assumes all encumbrances on the site, including any outstanding mortgage amount and legal fees (\"upset costs\"), delinquent taxes and water and sewer charges, Sheriff's sale fee, and realty transfer taxes. This information was requested from the owner; however, not provided."
-//         },
-//         {
-//             suggestionName: 'Gross Building Area',
-//             typeSuggestValue: 'Gros',
-//             verifySuggest: _grossBuildingArea,
-//             verifyExport: _grossBuildingArea
-//         }, 
-//         {
-//             suggestionName: 'Site Area',
-//             typeSuggestValue: 'Sit',
-//             verifySuggest: _siteArea,
-//             verifyExport: _siteArea
-//         }, 
-//         {
-//             suggestionName: 'Street Address',
-//             typeSuggestValue: 'Street',
-//             verifySuggest: _streetAddress,
-//             verifyExport: _streetAddress
-//         },
-//         {
-//             suggestionName: 'Lot',
-//             typeSuggestValue: 'Lo',
-//             verifySuggest: _lotValue,
-//             verifyExport: _lotValue
-//         },  
-//         {
-//             suggestionName: 'Street Name',
-//             typeSuggestValue: 'Stree',
-//             verifySuggest: _streetName,
-//             verifyExport: _streetName
-//         }, 
-//         {
-//             suggestionName: 'Unchanged Renovations',
-//             typeSuggestValue: 'Unchange',
-//             verifySuggest: 'Upon renovation, the subject unit count and gross building area will remain unchanged.',
-//             verifyExport: 'Upon renovation, the subject unit count and gross building area will remain unchanged.'
-//         },
-//         {
-//             suggestionName: 'Zone(s)',
-//             typeSuggestValue: 'Zone',
-//             verifySuggest: 'R8',
-//             verifyExport: 'R8'
-//         }, 
-//     ];
-// }; 
-
-// const _baseChipsAsCompleted = () => {
-//     return [
-//         {
-//             suggestionName: 'Current Residential Unit Count',
-//             typeSuggestValue: 'Residential',
-//             verifySuggest: _numberOfCommercialUnits,
-//             verifyExport: _numberOfCommercialUnits
-//         },
-//         {
-//             suggestionName: 'Current Commercial Unit Count',
-//             typeSuggestValue: 'Commercial',
-//             verifySuggest: _numberOfResidentialUnits,
-//             verifyExport: _numberOfResidentialUnits
-//         },
-//         {
-//             suggestionName: 'Current Condition',
-//             typeSuggestValue: 'Condition',
-//             verifySuggest: 'in  condition',
-//             verifyExport: 'in  condition'
-//         },
-//         {
-//             suggestionName: 'As Complete Residential Unit Count',
-//             typeSuggestValue: 'Residential',
-//             verifySuggest: _currentNumberOfResidentialUnits,
-//             verifyExport: _currentNumberOfResidentialUnits
-//         },
-//         {
-//             suggestionName: 'As Complete Commercial Unit Count',
-//             typeSuggestValue: 'Commercial',
-//             verifySuggest: _currentNumberOfCommercialUnits,
-//             verifyExport: _currentNumberOfCommercialUnits
-//         },
-//         {
-//             suggestionName: 'As Stabilized Condition',
-//             typeSuggestValue: 'Conditio',
-//             verifySuggest: 'in  condition',
-//             verifyExport: 'condition'
-//         }
-//     ];
-// };
-
-// const _baseChipsAsIsAsStabilized = () => {
-//     return [
-//         {
-//             suggestionName: 'Residential Unit Count',
-//             typeSuggestValue: 'Residentia',
-//             verifySuggest: _numberOfResidentialUnits,
-//             verifyExport: _numberOfResidentialUnits
-//         },
-//         {
-//             suggestionName: 'Commercial Unit Count',
-//             typeSuggestValue: 'Commerci',
-//             verifySuggest: _numberOfCommercialUnits,
-//             verifyExport: _numberOfCommercialUnits
-//         },
-//         {
-//             suggestionName: 'Condition',
-//             typeSuggestValue: 'Conditio',
-//             verifySuggest: 'in  condition',
-//             verifyExport: 'condition'
-//         }
-//     ];
-// };
-
-// const _asIsAsStabilizedChips = _baseChip().concat(_baseChipsAsIsAsStabilized());
-// const _asCompletedChips = _baseChip().concat(_baseChipsAsCompleted());
+const _chipsOptions: BoweryReports.ChipsCreationOptions = {
+    buildingName: _buildingName,
+    grossBuildingArea: _grossBuildingArea,
+    numberOfCommercialUnits: _numberOfCommercialUnits,
+    numberOfResidentialUnits: _numberOfResidentialUnits,
+    siteArea: _siteArea,
+    currentCommercialUnits: _currentNumberOfCommercialUnits,
+    currentResidentialUnits: _currentNumberOfResidentialUnits
+};
 
 export default {
     reportCreationDataAsIs: reportDataCreator.getReportData("4549-54", {
@@ -176,9 +36,9 @@ export default {
     }),
     numberOfCommercialUnits: _numberOfCommercialUnits,
     numberOfResidentialUnits: _numberOfResidentialUnits,
-    // asIsChips: _asIsAsStabilizedChips,
-    // asStabilizedChips: _asIsAsStabilizedChips,
-    // asCompletedChips: _asCompletedChips,
+    asIsChips: chipsDataCreator.getChipsData(_chipsOptions),
+    asStabilizedChips: chipsDataCreator.getChipsData(_chipsOptions, enums.VALUE_CONCLUSION_TYPE.AS_STABILIZED),
+    asCompletedChips: chipsDataCreator.getChipsData(_chipsOptions, enums.VALUE_CONCLUSION_TYPE.AS_COMPLETE),
     currentNumberOfCommercialUnits: _currentNumberOfCommercialUnits,
     currentNumberOfResidentialUnits: _currentNumberOfResidentialUnits,
     grossBuildingArea: _grossBuildingArea,
