5f7c7f1d14e59d8d3c0d6f9e26861663a7c261ec
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-06-21 08:35:40 +0300

    [QA-5139] Refactor capRateComps.actions, removed JS Doc

--- cypress/actions/final/capRateComps.actions.ts
+++ cypress/actions/final/capRateComps.actions.ts
@@ -1,25 +1,17 @@
-import BaseActions from "../base/base.actions";
 import capRateCompsPage from "../../pages/final/capRateComps.page";
 import { isDateHasCorrectFormat } from "../../../utils/date.utils";
+import BaseActionsExt from "../base/base.actions.ext";
 
-class CapRateCompsActions extends BaseActions {
+class CapRateCompsActions extends BaseActionsExt<typeof capRateCompsPage> {
 
-    /**
-     *
-     * @returns {CapRateCompsActions}
-     */
-    verifyPageIsOpened() {
+    verifyPageIsOpened(): CapRateCompsActions {
         capRateCompsPage.headerSection.should("exist");
         return this;
     }
 
-    /**
-     *
-     * @param {Readonly<{stateValue: string, address: string, id: string, source: string, sourceName: string,
-     * sourceUrl: string}>} comparable
-     * @returns {CapRateCompsActions}
-     */
-    addComparable(comparable) {
+    addComparable(comparable: Readonly<{stateValue: string, 
+        address: string, id: string, source: string, 
+        sourceName: string, sourceUrl: string}>): CapRateCompsActions  {
         capRateCompsPage.addCompButton.click();
         capRateCompsPage.advancedSearchButton.click();
         capRateCompsPage.selectStateDropdown.click();
@@ -36,106 +28,56 @@ class CapRateCompsActions extends BaseActions {
         return this;
     }
 
-    /**
-     *
-     * @param {string} address
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    verifyCompAddressByRow(address, index = 0) {
+    verifyCompAddressByRow(address: string, index = 0): CapRateCompsActions {
         capRateCompsPage.comparablesAddresses.eq(index).as("address");
         cy.get("@address").should("contain.text", address);
         return this;
     }
 
-    /**
-     *
-     * @param {number | string} gba
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    enterCompGBAByRow(gba, index = 0) {
+    enterCompGBAByRow(gba: number | string, index = 0): CapRateCompsActions {
         capRateCompsPage.comparablesGBAs.eq(index).as("gba");
-        cy.get("@gba").clear().type(gba).should("have.value", gba);
+        cy.get("@gba").clear().type(`${gba}`).should("have.value", gba);
         return this;
     }
 
-    /**
-     *
-     * @param {string} typeValue
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    chooseCompTypeByRow(typeValue, index = 0) {
+    chooseCompTypeByRow(typeValue: string, index = 0): CapRateCompsActions {
         capRateCompsPage.comparablesTypesDrops.eq(index).as("type").click();
         capRateCompsPage.getDropdownOptionByValue(typeValue).click();
         cy.get("@type").should("have.text", typeValue);
         return this;
     }
 
-    /**
-     *
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    checkElevatoredCheckboxByRow(index = 0) {
+    checkElevatoredCheckboxByRow(index = 0): CapRateCompsActions {
         capRateCompsPage.compsElevatoredCheckboxes.eq(index).as("elevatored").check();
         cy.get("@elevatored").should("have.value", "true");
         return this;
     }
 
-    /**
-     *
-     * @param {number | string} number
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    enterNumberOfResUnitsByRow(number, index = 0) {
+    enterNumberOfResUnitsByRow(number: number | string, index = 0): CapRateCompsActions {
         capRateCompsPage.compsResUnits.eq(index).as("unitsNumber");
-        cy.get("@unitsNumber").clear().type(number).should("have.value", `${number}`);
+        cy.get("@unitsNumber").clear().type(`${number}`).should("have.value", `${number}`);
         return this;
     }
 
-    /**
-     *
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    checkListingCheckboxByRow(index = 0) {
+    checkListingCheckboxByRow(index = 0): CapRateCompsActions {
         capRateCompsPage.compsListingCheckboxes.eq(index).as("listing").check();
         cy.get("@listing").should("have.value", "true");
         return this;
     }
 
-    /**
-     *
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    checkInContractCheckboxByRow(index = 0) {
+    checkInContractCheckboxByRow(index = 0): CapRateCompsActions {
         capRateCompsPage.compsInContractCheckboxes.eq(index).as("inContract").check();
         cy.get("@inContract").should("have.value", "true");
         return this;
     }
 
-    /**
-     *
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    verifyInContractDisabledByRow(index = 0) {
+    verifyInContractDisabledByRow(index = 0): CapRateCompsActions {
         capRateCompsPage.compsInContractCheckboxes.eq(index).as("inContract");
         cy.get("@inContract").should("be.disabled").should("have.value", "false");
         return this;
     }
 
-    /**
-     *
-     * @param {string} date
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    enterSaleDateByRow(date, index = 0) {
+    enterSaleDateByRow(date: string, index = 0): CapRateCompsActions {
         capRateCompsPage.compsSaleDates.eq(index).clear().type(date);
         if (isDateHasCorrectFormat(date)) {
             capRateCompsPage.inputsToCheckDate.eq(index).should("have.value", date);
@@ -145,79 +87,43 @@ class CapRateCompsActions extends BaseActions {
         return this;
     }
 
-    /**
-     *
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    verifySaleDateDisabledByRow(index = 0) {
+    verifySaleDateDisabledByRow(index = 0): CapRateCompsActions {
         capRateCompsPage.compsSaleDates.eq(index).should("be.disabled");
         return this;
     }
 
-    /**
-     *
-     * @param {number | string} year
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    enterYearBuiltByRow(year, index = 0) {
+    enterYearBuiltByRow(year: number | string, index = 0): CapRateCompsActions {
         capRateCompsPage.compsYearsBuilt.eq(index).as("year");
         if (year === 0) {
             cy.get("@year").clear();
         } else {
-            cy.get("@year").clear().type(year).should("have.value", year);
+            cy.get("@year").clear().type(`${year}`).should("have.value", year);
         }
         return this;
     }
 
-    /**
-     *
-     * @param {number | string} price
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    verifyPricePerSFByRow(price, index = 0) {
+    verifyPricePerSFByRow(price: number | string, index = 0): CapRateCompsActions {
         capRateCompsPage.compsPricesPerSF.eq(index).should("have.text", price);
         return this;
     }
 
-    /**
-     *
-     * @param {string | number} rate
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    enterCapRateByRow(rate, index = 0) {
+    enterCapRateByRow(rate: string | number, index = 0): CapRateCompsActions {
         capRateCompsPage.compsCapRates.eq(index).as("rate");
-        cy.get("@rate").clear().type(rate);
+        cy.get("@rate").clear().type(`${rate}`);
         cy.get("@rate").should("have.value", `${rate}`);
         return this;
     }
 
-    /**
-     *
-     * @param {string} sourceName
-     * @param {string} sourceUrl
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    verifySourceOfInfoCellByRow(sourceName, sourceUrl, index = 0) {
+    verifySourceOfInfoCellByRow(sourceName: string, sourceUrl: string, index = 0): CapRateCompsActions {
         capRateCompsPage.compsSourceOfInfoCells.eq(index).as("source");
         cy.get("@source").should("have.text", sourceName);
         cy.get("@source").should("have.attr", "href", sourceUrl);
         return this;
     }
 
-    /**
-     *
-     * @param {Readonly<{address: string, gba: number, type: string, isElevatored: boolean, numberOfUnits: number,
-     * isListing: boolean, isInContract: boolean, saleDate: string, yearBuilt: number, pricePerSF: string, capRate: string,
-     * sourceName: string, sourceUrl: string}>} comparable
-     * @param {number} index
-     * @returns {CapRateCompsActions}
-     */
-    fillAddedCompWithInfo(comparable, index) {
+    fillAddedCompWithInfo(comparable: Readonly<{address: string, gba: number, type: string, isElevatored: boolean, numberOfUnits: number,
+        isListing: boolean, isInContract: boolean, saleDate: string, yearBuilt: number, pricePerSF: string, capRate: string,
+        sourceName: string, sourceUrl: string}>, index: number): CapRateCompsActions {
         this.verifyCompAddressByRow(comparable.address, index)
             .enterCompGBAByRow(comparable.gba, index)
             .chooseCompTypeByRow(comparable.type, index);
@@ -245,26 +151,12 @@ class CapRateCompsActions extends BaseActions {
         return this;
     }
 
-    /**
-     *
-     * @param {number} minRate
-     * @param {number} maxRate
-     * @param {number | string} avgRate
-     * @returns {CapRateCompsActions}
-     */
-    verifyCapRateCommentary(minRate, maxRate, avgRate) {
+    verifyCapRateCommentary(minRate: number, maxRate: number, avgRate: number | string): CapRateCompsActions {
         capRateCompsPage.capRateCommentary.should("have.text", this.getCapRateCommentary(minRate, maxRate, avgRate));
         return this;
     }
 
-    /**
-     * @private
-     * @param {number} minRate
-     * @param {number} maxRate
-     * @param {number} avgRate
-     * @returns {string}
-     */
-    getCapRateCommentary(minRate, maxRate, avgRate) {
+    private getCapRateCommentary(minRate: number, maxRate: number, avgRate: number): string {
         return "We analyzed sales of comparable multifamily " +
             `assets within the subject’s periphery and they exhibit overall capitalization rates from ${minRate}% to ${maxRate}%` +
             ` with an average of ${avgRate}%. We have supplemented our capitalization rate comparables with additional ` +
@@ -273,38 +165,23 @@ class CapRateCompsActions extends BaseActions {
             "justify utilizing the comparable in the sales comparison approach (i.e. location, size, etc.).";
     }
 
-    /**
-     *
-     * @param {string} value
-     * @returns {CapRateCompsActions}
-     */
-    chooseCompIncomePotential(value) {
+    chooseCompIncomePotential(value: string): CapRateCompsActions {
         capRateCompsPage.comparableIncomePotentialRadio.check(value);
         capRateCompsPage.getElementToCheckRadio(value).should("exist");
         return this;
     }
 
-    /**
-     *
-     * @param {string} value
-     * @returns {CapRateCompsActions}
-     */
-    chooseCompPropertyConditions(value) {
+    chooseCompPropertyConditions(value: string): CapRateCompsActions {
         capRateCompsPage.propertyConditionsRadio.check(value);
         capRateCompsPage.getElementToCheckRadio(value).should("exist");
         return this;
     }
 
-    /**
-     *
-     * @param {string} value
-     * @returns {CapRateCompsActions}
-     */
-    chooseCompPropertyLocations(value) {
+    chooseCompPropertyLocations(value: string): CapRateCompsActions {
         capRateCompsPage.propertyLocationsRadio.check(value);
         capRateCompsPage.getElementToCheckRadio(value).should("exist");
         return this;
     }
 }
 
-export default new CapRateCompsActions();
+export default new CapRateCompsActions(capRateCompsPage);
