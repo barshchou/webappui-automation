3e345ee49b5cb0ed0e73f8671d6e458000cbec36
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-06-25 00:34:51 +0300

    5048 added

--- cypress/integration/not_full_reports/income/expense_forecast/QA-5042.spec.ts
+++ cypress/integration/not_full_reports/income/expense_forecast/QA-5042.spec.ts
@@ -4,7 +4,7 @@ import { Income } from "../../../../actions";
 import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
 
 describe(`[QA-5008] [QA-5009] [QA-5010] [Income>Expense forecast] “Include Expense on Pro Forma” checkbox and tooltip functionality`,
-    { tags: [ "@income", "@expense_forecast" ] }, () => {
+    { tags: ["@income", "@expense_forecast"] }, () => {
 
         before("Login, create report", () => {
             createReport(testData.reportCreationData);
@@ -43,27 +43,27 @@ describe(`[QA-5008] [QA-5009] [QA-5010] [Income>Expense forecast] “Include Exp
         });
 
 
-       
-            // it("should handle the alerts automatically", () => {
-            //   Cypress.on("uncaught:exception", (err, runnable) => {
-            //     // returning false here prevents Cypress from failing the test
-            //     return false;
-            //   });
-            //   Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.trigger('mouseover', 'center', { bubbles: true }).invoke('show') //trigger('mouseover', { bubbles: true }  )    //focused({timeout:10000})
-            //   cy.wait(10000)
-              
-            //   cy.on("window:alert", (str) => {
-                
-            //   //window:alert is the event which get fired on alert open
-            //     expect(str).to.equal("I am alert");
-            //   cy.get('[name="alert"]').click();
-            //   });
-            // });
-          
+
+        // it("should handle the alerts automatically", () => {
+        //   Cypress.on("uncaught:exception", (err, runnable) => {
+        //     // returning false here prevents Cypress from failing the test
+        //     return false;
+        //   });
+        //   Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.trigger('mouseover', 'center', { bubbles: true }).invoke('show') //trigger('mouseover', { bubbles: true }  )    //focused({timeout:10000})
+        //   cy.wait(10000)
+
+        //   cy.on("window:alert", (str) => {
+
+        //   //window:alert is the event which get fired on alert open
+        //     expect(str).to.equal("I am alert");
+        //   cy.get('[name="alert"]').click();
+        //   });
+        // });
+
 
         it("[QA-5045]", () => {
             cy.stepInfo(`1. Verify Category Name field is required on "Add New Expense Category" modal `);
-         
+
             // cy.on("window:alert", (str) => {
 
             //     Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.trigger('mouseover')
@@ -75,7 +75,7 @@ describe(`[QA-5008] [QA-5009] [QA-5010] [Income>Expense forecast] “Include Exp
 
 
             //pointerover???
-           
+
             cy.stepInfo(`2. Verify "Save" button is disabled if Category Name field is empty and user can not new expense with the empty field`);
             Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.should('be.empty');
             Income._ExpenseForecastActions.Page.addCustomExpenseCategorySaveButton.should('be.disabled').click({ force: true });    //maybe from base page?
@@ -86,7 +86,7 @@ describe(`[QA-5008] [QA-5009] [QA-5010] [Income>Expense forecast] “Include Exp
             Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.click().blur();
             Income._ExpenseForecastActions.Page.addCustomExpenseCategoryWarning.should('exist');
             Income._ExpenseForecastActions.Page.addCustomExpenseCategoryFieldset.should('have.css', 'border-color', 'rgb(244, 67, 54)');
-          
+
 
             cy.stepInfo(`4. Verify "Save" button is enabled if Category Name field is filled with valid data (the field is free-text. `);
             Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.click().type(testData.customCategoryNameValidation.name);
@@ -94,32 +94,32 @@ describe(`[QA-5008] [QA-5009] [QA-5010] [Income>Expense forecast] “Include Exp
         });
 
         it("[QA-5047]", () => {
+
             cy.stepInfo(`1. Verify after clicking Save button  on the “Add New Expense Category” modal with the filled "Category Name" field ->  
             a new card is added to the bottom of the page underneath “Replacement Reserves” and below Total Operating Expenses card`);
             Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.clear().type(testData.firstCustomCategory.name);
             Income._ExpenseForecastActions.Page.addCustomExpenseCategorySaveButton.click();
-           
 
-         Income._ExpenseForecastActions.Page.forecastItemCardFull('plantsGrowing').should('exist');
-        // .then($el => $el[0].offsetTop)
+            Income._ExpenseForecastActions.Page.forecastItemCardFull(testData.firstCustomCategory.name, true).should('exist');
+            // .then($el => $el[0].offsetTop)
 
-           //.offsetTop
-            
-           // assert.isAbove(valueToCheck, valueToBeAbove, [message])
+            //.offsetTop
 
-           cy.stepInfo(`2. Verify new custom expense forecast is added on “Save” button on “Adding New Expense Category” after “Editing Expense Category” modal was closed`);
-           Income._ExpenseForecastActions.Page.editCustomExpenseCategoryButton('plantsGrowing').click();
-           Income._ExpenseForecastActions.Page.formCancelButton().click();
-           Income._ExpenseForecastActions.Page.createNewCategoryButton.click();
-           Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.invoke('val').should('be.empty');
-           Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.type(testData.secondCustomCategory.name);
-           Income._ExpenseForecastActions.Page.addCustomExpenseCategorySaveButton.click();
-           Income._ExpenseForecastActions.Page.forecastItemCardFull('poolCleaning').should('exist');
-           Income._ExpenseForecastActions.Page.forecastItemCardFull('plantsGrowing').should('exist');
+            // assert.isAbove(valueToCheck, valueToBeAbove, [message])
 
-           cy.stepInfo(`3. Verify each subsequent custom expense card is added underneath  previously added custom expense card and below  Total Operating Expenses `);
-  //  ???
-         
+            cy.stepInfo(`2. Verify new custom expense forecast is added on “Save” button on “Adding New Expense Category” after “Editing Expense Category” modal was closed`);
+            Income._ExpenseForecastActions.Page.editCustomExpenseCategoryButton(testData.firstCustomCategory.name, true).click();
+            Income._ExpenseForecastActions.Page.formCancelButton().click();
+            Income._ExpenseForecastActions.Page.createNewCategoryButton.click();
+            Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.invoke('val').should('be.empty');
+            Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.type(testData.secondCustomCategory.name);
+            Income._ExpenseForecastActions.Page.addCustomExpenseCategorySaveButton.click();
+            Income._ExpenseForecastActions.Page.forecastItemCardFull(testData.secondCustomCategory.name, true).should('exist');
+            Income._ExpenseForecastActions.Page.forecastItemCardFull(testData.firstCustomCategory.name, true).should('exist');
+
+            cy.stepInfo(`3. Verify each subsequent custom expense card is added underneath  previously added custom expense card and below  Total Operating Expenses `);
+            //  ???
+
         });
 
         it("[QA-5048]", () => {
@@ -127,17 +127,26 @@ describe(`[QA-5008] [QA-5009] [QA-5010] [Income>Expense forecast] “Include Exp
             no expense card is added`);
             Income._ExpenseForecastActions.Page.createNewCategoryButton.click();
             Income._ExpenseForecastActions.Page.formCancelButton().click();
-// .should('not.exist');
+            // .should('not.exist');
 
-Income._ExpenseForecastActions.Page.createNewCategoryButton.click();
-Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.type(testData.thirdCustomCategory.name);
+            Income._ExpenseForecastActions.Page.createNewCategoryButton.click();
+            Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.type(testData.thirdCustomCategory.name);
             Income._ExpenseForecastActions.Page.formCancelButton().click();
+            Income._ExpenseForecastActions.Page.forecastItemCardFull(testData.thirdCustomCategory.name, true).should('not.exist');
 
             cy.stepInfo(`2. Verify  “Add New Expense Category” modal closes after clicking on Cancel button`);
+            Income._ExpenseForecastActions.Page.createNewCategoryButton.click();
+            Income._ExpenseForecastActions.Page.formCancelButton().click();
+            Income._ExpenseForecastActions.Page.addCustomExpenseCategoryModal.should('not.exist');
 
-            cy.stepInfo(`2. Verify  if user clicks on Cancel button with filled Category Name and opens “Add New Expense Category” modal again-> 
+
+            cy.stepInfo(`3. Verify  if user clicks on Cancel button with filled Category Name and opens “Add New Expense Category” modal again-> 
             Category Name field is empty. no previous data is displayed there`);
-            
+            Income._ExpenseForecastActions.Page.createNewCategoryButton.click();
+            Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.type(testData.thirdCustomCategory.name);
+            Income._ExpenseForecastActions.Page.formCancelButton().click();
+            Income._ExpenseForecastActions.Page.createNewCategoryButton.click();
+            Income._ExpenseForecastActions.Page.addCustomExpenseCategoryInput.invoke('val').should('be.empty');
         });
 
 
--- cypress/pages/income/expenseForecast.page.ts
+++ cypress/pages/income/expenseForecast.page.ts
@@ -3,11 +3,11 @@ import BasePage from "../base/base.page";
 
 class ExpenseForecastPage extends BasePage {
 
-    get expenseForecastHeader(){ return cy.get('[data-qa="expenseForecast"]'); }
+    get expenseForecastHeader() { return cy.get('[data-qa="expenseForecast"]'); }
 
-    get electricityCard(){ return cy.get("[data-qa='electricity-forecast-item'] > div").last(); }
+    get electricityCard() { return cy.get("[data-qa='electricity-forecast-item'] > div").last(); }
 
-    get insuranceCard(){ return cy.get('[data-qa="insurance-forecast-item"] > div').last(); }
+    get insuranceCard() { return cy.get('[data-qa="insurance-forecast-item"] > div').last(); }
 
     get fuelCard() { return cy.get("[data-qa=fuel-forecast-item] > div").last(); }
 
@@ -15,91 +15,97 @@ class ExpenseForecastPage extends BasePage {
 
     forecastItemCard(forecastItem: string) { return cy.get(`[data-qa=${forecastItem}-forecast-item] > div`).last(); }
 
-    forecastItemCardFull(forecastItem: string) { return cy.get(`[data-qa=${forecastItem}-forecast-item]`); }       
-    
+    forecastItemCardFull(forecastItem: string, custom = false) {
+        return !custom ? cy.get(`[data-qa=${forecastItem}-forecast-item]`) :
+            cy.get(`[data-qa=${Cypress._.camelCase(Cypress._.toLower(Cypress._.replace(forecastItem, "&", "And")))}-forecast-item]`);
+    }
+
     forecastItemTooltipButton(forecastItem: string) { return cy.get(`[data-qa=${forecastItem}-forecast-item] svg[aria-label="Unchecking this box will hide the expense from showing up on the Pro Forma."]`); }
 
     get toeCard() { return cy.xpath("//*[.='TOTAL OPERATING EXPENSES ($/SF)']/parent::div").first(); }
 
-    getForecastItemBasisRadio(item) {return cy.get(`[name='${item}.basis']`);}
+    getForecastItemBasisRadio(item) { return cy.get(`[name='${item}.basis']`); }
 
-    getElementToCheckRadio(forecastItem: string, radioValue: BoweryReports.UnitSF) {return cy.get(`[data-qa=checked] [name='${forecastItem}.basis'][value='${radioValue}']`);}
+    getElementToCheckRadio(forecastItem: string, radioValue: BoweryReports.UnitSF) { return cy.get(`[data-qa=checked] [name='${forecastItem}.basis'][value='${radioValue}']`); }
 
-    getElementBasisToSwitch(forecastItem: string, radioValue: BoweryReports.UnitSF) {return cy.get(`[name='${forecastItem}.basis'][value='${radioValue}']`);}
+    getElementBasisToSwitch(forecastItem: string, radioValue: BoweryReports.UnitSF) { return cy.get(`[name='${forecastItem}.basis'][value='${radioValue}']`); }
 
-    getForecastItemForecastInput(item: string, custom = false, index = 0) {return !custom ? cy.get(`[name='${item}.concludedValue']`) : cy.get(`[name='customExpenses[${index}].concludedValue']`);}
+    getForecastItemForecastInput(item: string, custom = false, index = 0) { return !custom ? cy.get(`[name='${item}.concludedValue']`) : cy.get(`[name='customExpenses[${index}].concludedValue']`); }
 
-    getForecastItemCompMin(item) {return cy.get(`[data-qa=${item}-forecast-item] [data-qa=comp-min]`);}
+    getForecastItemCompMin(item) { return cy.get(`[data-qa=${item}-forecast-item] [data-qa=comp-min]`); }
 
-    getForecastItemCompAvg(item) {return cy.get(`[data-qa=${item}-forecast-item] [data-qa=comp-avg]`);}
+    getForecastItemCompAvg(item) { return cy.get(`[data-qa=${item}-forecast-item] [data-qa=comp-avg]`); }
 
-    getForecastItemCompMax(item) {return cy.get(`[data-qa=${item}-forecast-item] [data-qa=comp-max]`);}
+    getForecastItemCompMax(item) { return cy.get(`[data-qa=${item}-forecast-item] [data-qa=comp-max]`); }
 
-    getForecastItemBasisMoneyValue(item) {return cy.get(`[data-qa=${item}-forecast-item] [data-qa=basis]`);}
+    getForecastItemBasisMoneyValue(item) { return cy.get(`[data-qa=${item}-forecast-item] [data-qa=basis]`); }
 
-    getForecastItemProjectionByType(item, type) {return cy.contains(`[data-qa=${item}-forecast-item] [data-qa$=historical]`, type);}
+    getForecastItemProjectionByType(item, type) { return cy.contains(`[data-qa=${item}-forecast-item] [data-qa$=historical]`, type); }
 
-    getExpenseCommentary(forecastItem: string, index = 1) {return cy.xpath(`//*[@data-qa="${forecastItem}-forecast-item"]//following::div[@data-slate-editor][${index}]`);}
+    getExpenseCommentary(forecastItem: string, index = 1) { return cy.xpath(`//*[@data-qa="${forecastItem}-forecast-item"]//following::div[@data-slate-editor][${index}]`); }
 
-    getExpenseCommentaryEditButton(forecastItem: string, index = 1) {return cy.xpath(`//*[@data-qa="${forecastItem}-forecast-item"]//following::button[.='Edit'][${index}]`);}
+    getExpenseCommentaryEditButton(forecastItem: string, index = 1) { return cy.xpath(`//*[@data-qa="${forecastItem}-forecast-item"]//following::button[.='Edit'][${index}]`); }
 
-    getExpenseCommentarySaveButton(forecastItem: string, index = 1) {return cy.xpath(`//*[@data-qa="${forecastItem}-forecast-item"]//following::button[.='Save'][${index}]`);}
+    getExpenseCommentarySaveButton(forecastItem: string, index = 1) { return cy.xpath(`//*[@data-qa="${forecastItem}-forecast-item"]//following::button[.='Save'][${index}]`); }
 
-    getExpenseCommentaryModified(forecastItem: string) {return cy.xpath(`//*[@data-qa="${forecastItem}-forecast-item"]//following::*[.='Modified'][2]`);}
+    getExpenseCommentaryModified(forecastItem: string) { return cy.xpath(`//*[@data-qa="${forecastItem}-forecast-item"]//following::*[.='Modified'][2]`); }
 
-    getExpenseCommentaryRevertToOriginal(forecastItem: string) {return cy.xpath(`//*[@data-qa="${forecastItem}-forecast-item"]//following::button[.='Revert to Original'][1]`);}
+    getExpenseCommentaryRevertToOriginal(forecastItem: string) { return cy.xpath(`//*[@data-qa="${forecastItem}-forecast-item"]//following::button[.='Revert to Original'][1]`); }
 
-    getCheckboxIncludeInProForma(forecastItem: string) {return cy.get(`[data-qa=${forecastItem}-forecast-item] input[type="checkbox"]`);}
+    getCheckboxIncludeInProForma(forecastItem: string) { return cy.get(`[data-qa=${forecastItem}-forecast-item] input[type="checkbox"]`); }
 
-    get inputPercentOfEGICheckbox() {return cy.get("[label='Input % of EGI'] input");}
+    get inputPercentOfEGICheckbox() { return cy.get("[label='Input % of EGI'] input"); }
 
-    get percentOfEgiInput() {return cy.get("[name='management.percentOfEgi']");}
+    get percentOfEgiInput() { return cy.get("[name='management.percentOfEgi']"); }
 
-    get toeCompMin() {return cy.get("[data-qa=comp-min]").last();}
+    get toeCompMin() { return cy.get("[data-qa=comp-min]").last(); }
 
-    get toeCompAvg() {return cy.get("[data-qa=comp-avg]").last();}
+    get toeCompAvg() { return cy.get("[data-qa=comp-avg]").last(); }
 
-    get toeCompMax() {return cy.get("[data-qa=comp-max]").last();}
+    get toeCompMax() { return cy.get("[data-qa=comp-max]").last(); }
 
-    get allProjections() {return cy.get("[data-qa$=historical]");}
+    get allProjections() { return cy.get("[data-qa$=historical]"); }
 
-    get allForecastsInputs() {return cy.get("[name$=concludedValue]");}
+    get allForecastsInputs() { return cy.get("[name$=concludedValue]"); }
 
-    get toeOwnerProjection() {return cy.get("[data-qa=owners-projection]");}
+    get toeOwnerProjection() { return cy.get("[data-qa=owners-projection]"); }
 
-    get appraisersTotalForecast() {return cy.get("[data-qa=appraisers-total-conclusion]");}
+    get appraisersTotalForecast() { return cy.get("[data-qa=appraisers-total-conclusion]"); }
 
-    get toeCommentary() {return cy.xpath("//*[.='TOTAL OPERATING EXPENSES']//following::div[@data-slate-editor]");}
+    get toeCommentary() { return cy.xpath("//*[.='TOTAL OPERATING EXPENSES']//following::div[@data-slate-editor]"); }
 
-    get toeCommentaryEditButton() {return cy.xpath("//*[.='TOTAL OPERATING EXPENSES']//following::button[.='Edit']");}
+    get toeCommentaryEditButton() { return cy.xpath("//*[.='TOTAL OPERATING EXPENSES']//following::button[.='Edit']"); }
 
-    get toeCommentarySaveButton() {return cy.xpath("//*[.='TOTAL OPERATING EXPENSES']//following::button[.='Save'][1]");}
+    get toeCommentarySaveButton() { return cy.xpath("//*[.='TOTAL OPERATING EXPENSES']//following::button[.='Save'][1]"); }
 
-    get toeCommentaryModified() {return cy.xpath("//*[.='TOTAL OPERATING EXPENSES']//following::*[.='Modified']");}
+    get toeCommentaryModified() { return cy.xpath("//*[.='TOTAL OPERATING EXPENSES']//following::*[.='Modified']"); }
 
-    get expenseConfirmRevertButton() {return cy.xpath("//*[.='Yes, revert']");}
+    get expenseConfirmRevertButton() { return cy.xpath("//*[.='Yes, revert']"); }
 
-    get createNewCategoryButton() {return cy.contains('Add Expense Category +');}
+    get createNewCategoryButton() { return cy.contains('Add Expense Category +'); }
 
-    get addCustomExpenseCategoryModal () {return cy.contains('Add Custom Expense Category').parent('[role=dialog] ');} 
+    get addCustomExpenseCategoryModal() { return cy.xpath("//*[.='Add Custom Expense Category']//parent::div[@role='dialog']"); }
 
-    get titleOfaddCustomExpenseCategoryModal() {return cy.get('[role=dialog]').contains('Add Custom Expense Category');} 
+    get titleOfaddCustomExpenseCategoryModal() { return cy.get('[role=dialog]').contains('Add Custom Expense Category'); }
 
-    get addCustomExpenseCategoryInput() {return cy.get('input[placeholder="Enter Custom Expense..."]');} 
+    get addCustomExpenseCategoryInput() { return cy.get('input[placeholder="Enter Custom Expense..."]'); }
 
-    get addCustomExpenseCategoryFieldset() {return cy.get('[data-qa="autosuggest-text-input-field"]').find('fieldset');} 
-    
-   // get addCustomExpenseCategoryCancelButton() {return cy.contains('Add Custom Expense Category').parent('[role=dialog]').contains('Cancel');}
+    get addCustomExpenseCategoryFieldset() { return cy.get('[data-qa="autosuggest-text-input-field"]').find('fieldset'); }
 
-    get addCustomExpenseCategorySaveButton() {return cy.contains('Add Custom Expense Category').parent('[role=dialog]').contains('Save');}
+    // get addCustomExpenseCategoryCancelButton() {return cy.contains('Add Custom Expense Category').parent('[role=dialog]').contains('Cancel');}
 
-    get addCustomExpenseCategoryWarning() {return cy.contains('Category name is required');}
-    
-    editCustomExpenseCategoryButton(forecastItem: string) {return cy.get(`[data-qa=${forecastItem}-forecast-item]`).find('[data-testid="EditIcon"]');} 
+    get addCustomExpenseCategorySaveButton() { return cy.xpath("//*[.='Add Custom Expense Category']//following::button[.='Save']"); }
 
-    get newCategoryExpenseName() { return cy.get('[data-qa="expenseName-form-control"] input');}
+    get addCustomExpenseCategoryWarning() { return cy.contains('Category name is required'); }
 
-    
+    editCustomExpenseCategoryButton(forecastItem: string, custom = false) {
+        return !custom ? cy.get(`[data-qa=${forecastItem}-forecast-item]`).find('[data-testid="EditIcon"]') :
+            cy.get(`[data-qa=${Cypress._.camelCase(Cypress._.toLower(Cypress._.replace(forecastItem, "&", "And")))}-forecast-item]`).find('[data-testid="EditIcon"]');
+    }
+
+    get newCategoryExpenseName() { return cy.get('[data-qa="expenseName-form-control"] input'); }
+
+
 
 }
 
