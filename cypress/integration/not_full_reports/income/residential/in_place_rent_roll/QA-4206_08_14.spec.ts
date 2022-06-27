4a9f64cfb26b8c760ce611b764faa5832c2fc95d
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-06-27 09:39:13 +0300

    [QA-5692] Fixed QA-4206_08_14 changes

--- cypress/integration/not_full_reports/income/residential/in_place_rent_roll/QA-4206_08_14.spec.ts
+++ cypress/integration/not_full_reports/income/residential/in_place_rent_roll/QA-4206_08_14.spec.ts
@@ -4,69 +4,28 @@ import NavigationSection from "../../../../../actions/base/navigationSection.act
 import Income from "../../../../../actions/income/income.manager";
 import { reportCreationData } from "../../../../../fixtures/not_full_reports/income/residential/in_place_rent_roll/QA-4206_08_14.fixture";
 
-describe("Verify the Developer's Forecast checkbox on the In-Place Rent Roll page", 
+describe("[QA-4206_08_14] Verify the Developer's Forecast checkbox on the In-Place Rent Roll page", 
     { tags:[ "@income", "@residential", "@in_place_rent_roll" ] }, () => {
-
-    it("[QA-4206_08_14] Test body, AS_IS Report", () => {
-        cy.stepInfo("1. Login, create report");
-        createReport(reportCreationData(testData.conclusionValues[0]));
-
-        cy.stepInfo("2. Navigate to Income < Residential < In-Place Rent Roll");
-        NavigationSection.navigateToResInPlaceRentRoll();
-
-        cy.stepInfo("3. Verify Element Exist and Check / Uncheck them");
-        Income.Residential.InPlaceRentRoll.verifyColumnNotExist(testData.column.devForecast)
-            .verifyColumnNotExist(testData.column.perRoom)
-            .verifyColumnNotExist(testData.column.bathrooms)
-            .verifyColumnNotExist(testData.column.outSpace)
-            .verifyColumnNotExist(testData.column.unitType)
-            .checkUncheckCheckboxForColumn(testData.column.devForecast, testData.label.devForecast)
-            .checkUncheckCheckboxForColumn(testData.column.perRoom, testData.label.perRoom)
-            .checkUncheckCheckboxForColumn(testData.column.bathrooms, testData.label.bathrooms)
-            .checkUncheckCheckboxForColumn(testData.column.outSpace, testData.label.outSpace)
-            .checkUncheckCheckboxForColumn(testData.column.unitType, testData.label.unitType);
-        deleteReport(reportCreationData(testData.conclusionValues[0]).reportNumber);
-    });
-
-    it("[QA-4206_08_14] Test body, AS_STABILIZED Report", () => {
-        cy.stepInfo("1. Login, create report");
-        createReport(reportCreationData(testData.conclusionValues[0]));
-
-        cy.stepInfo("2. Navigate to Income < Residential < In-Place Rent Roll");
-        NavigationSection.navigateToResInPlaceRentRoll();
-
-        cy.stepInfo("3. Verify Element Exist and Check / Uncheck them");
-        Income.Residential.InPlaceRentRoll.verifyColumnNotExist(testData.column.devForecast)
-            .verifyColumnNotExist(testData.column.perRoom)
-            .verifyColumnNotExist(testData.column.bathrooms)
-            .verifyColumnNotExist(testData.column.outSpace)
-            .verifyColumnNotExist(testData.column.unitType)
-            .checkUncheckCheckboxForColumn(testData.column.devForecast, testData.label.devForecast)
-            .checkUncheckCheckboxForColumn(testData.column.perRoom, testData.label.perRoom)
-            .checkUncheckCheckboxForColumn(testData.column.bathrooms, testData.label.bathrooms)
-            .checkUncheckCheckboxForColumn(testData.column.outSpace, testData.label.outSpace)
-            .checkUncheckCheckboxForColumn(testData.column.unitType, testData.label.unitType);
-        deleteReport(reportCreationData(testData.conclusionValues[0]).reportNumber);
-    });
-
-    it("[QA-4206_08_14] Test body, AS_COMPLETE Report", () => {
-        cy.stepInfo("1. Login, create report");
-        createReport(reportCreationData(testData.conclusionValues[0]));
-
-        cy.stepInfo("2. Navigate to Income < Residential < In-Place Rent Roll");
-        NavigationSection.navigateToResInPlaceRentRoll();
-
-        cy.stepInfo("3. Verify Element Exist and Check / Uncheck them");
-        Income.Residential.InPlaceRentRoll.verifyColumnNotExist(testData.column.devForecast)
-            .verifyColumnNotExist(testData.column.perRoom)
-            .verifyColumnNotExist(testData.column.bathrooms)
-            .verifyColumnNotExist(testData.column.outSpace)
-            .verifyColumnNotExist(testData.column.unitType)
-            .checkUncheckCheckboxForColumn(testData.column.devForecast, testData.label.devForecast)
-            .checkUncheckCheckboxForColumn(testData.column.perRoom, testData.label.perRoom)
-            .checkUncheckCheckboxForColumn(testData.column.bathrooms, testData.label.bathrooms)
-            .checkUncheckCheckboxForColumn(testData.column.outSpace, testData.label.outSpace)
-            .checkUncheckCheckboxForColumn(testData.column.unitType, testData.label.unitType);
-        deleteReport(reportCreationData(testData.conclusionValues[0]).reportNumber);
+    testData.conclusionValues.forEach(value => {
+        it(`${value} test`, () => {
+            cy.stepInfo("1. Login, create report");
+            createReport(reportCreationData(value));
+        
+            cy.stepInfo("2. Navigate to Income < Residential < In-Place Rent Roll");
+            NavigationSection.navigateToResInPlaceRentRoll();
+        
+            cy.stepInfo("3. Verify Element Exist and Check / Uncheck them");
+            Income.Residential.InPlaceRentRoll.verifyColumnNotExist(testData.column.devForecast)
+                .verifyColumnNotExist(testData.column.perRoom)
+                .verifyColumnNotExist(testData.column.bathrooms)
+                .verifyColumnNotExist(testData.column.outSpace)
+                .verifyColumnNotExist(testData.column.unitType)
+                .checkUncheckCheckboxForColumn(testData.column.devForecast, testData.label.devForecast)
+                .checkUncheckCheckboxForColumn(testData.column.perRoom, testData.label.perRoom)
+                .checkUncheckCheckboxForColumn(testData.column.bathrooms, testData.label.bathrooms)
+                .checkUncheckCheckboxForColumn(testData.column.outSpace, testData.label.outSpace)
+                .checkUncheckCheckboxForColumn(testData.column.unitType, testData.label.unitType);
+            deleteReport(reportCreationData(value).reportNumber);
+        });
     });
 });
