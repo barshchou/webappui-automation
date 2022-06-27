468e60fd5059f49e5962b85f4a1ae5f321b89a5c
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-06-27 13:48:06 +0300

    [QA-5884] QA-4185 changed describe title, added tags and stepInfos

--- cypress/integration/not_full_reports/income/commercial/rent_comps/QA-4185.spec.ts
+++ cypress/integration/not_full_reports/income/commercial/rent_comps/QA-4185.spec.ts
@@ -4,14 +4,17 @@ import { _NavigationSection } from "../../../../../actions/base";
 import { Income } from "../../../../../actions";
 
 
-describe("Test", () => {
+describe("Per Square Foot Per Month radiobutton is displayed on Commercial Unit Details modal",
+    { tags: [ "@income", "@commercial", "@rent_comps" ] }, () => {
 
     before("Create report", () => {
         createReport(testData.reportCreationData);
     });
 
     it("Test body", () => {
+        cy.stepInfo("1. Navigate to commercial rent Comps");
         _NavigationSection.navigateToCommercialRentComps();
+        cy.stepInfo("2. Add any comp, open CommercialUnitDetails modal, verify button is displayed");
         Income._CommercialManager.RentComps.clickAddCompButtonByIndex()
             .clickEditButtonByRowNumber().Page.getUnitMeasureRadioByValue(testData.unitMeasure).should("exist");
         deleteReport(testData.reportCreationData.reportNumber);
