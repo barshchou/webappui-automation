65f9a8d86906ebd0fc56c330148b6ce9bd6af56f
Viachaslau.Barshchou <Viachaslau.Barshchou@boweryvaluation.com>
2022-06-28 14:43:30 +0300

    [QA-6028] removed unused reportCreation data

--- cypress/fixtures/not_full_reports/organizations/QA-5355-58.fixture.ts
+++ cypress/fixtures/not_full_reports/organizations/QA-5355-58.fixture.ts
@@ -1,14 +1,7 @@
-import ReportDataCreator from "../../data_creator/reportData.creator";
 import Enums from "../../../enums/enums";
 import { BoweryAutomation } from "../../../types/boweryAutomation.type";
 import userDataCreator from "../../data_creator/userData.creator";
 import enums from "../../../enums/enums";
-
-const reportCreationFixture = () => {
-    return ReportDataCreator.getReportData("5848_57-59", {
-        incomeValue: Enums.INCOME_TYPE.both
-    });
-};
 
 const _leadAppraiserUsername = Cypress.env(`${Enums.USERS.webapp_lead_appraiser_username}`);
 const _leadAppraiserPassword = Cypress.env(`${Enums.USERS.webapp_lead_appraiser_password}`);
@@ -36,7 +29,6 @@ const _userUpdatedLastName = "UpdatedUserFirstName-" + `${Date.now()}`;
 const _updatedRoleName = [ enums.USER_ROLES.leadAppraiser, enums.USER_ROLES.inspector ];
 
 export default {
-    reportCreationData: reportCreationFixture(),
     clientCreationData: userDataCreator.getUserData(_userOptions),
     userFirstName: _userFirstName,
     userLastName: _userLastName,
