f3c334d0ccc4714190f38e2fc073e9bd3b657f2d
Ernst Derevanko <ernst.dzeravianka@boweryvaluation.com>
2022-06-28 17:06:59 +0400

    [WEB-5733] init QA-5272

---
+++ cypress/integration/comp_plex/sales/find_comps/QA-5272.spec.ts
@@ -0,0 +1,17 @@
+import { salesInterceptions } from "../../../../actions/base/baseTest.actions";
+import * as _Sales from "../../../../actions/sales";
+
+describe("[QA-5272]", () => {
+    it("Enter Sale Information form] Verify the Buyer(Grantee) field", () => {
+        salesInterceptions();
+        cy.visit("/");
+        _Sales._FindComps.selectCompFromMapByAddress("443 East 88 Street");
+        cy.get('[id="selectedComps"]').find("tr").contains("443 East 88 Street")
+        .parent()
+        .find("a").click();
+        
+        cy.get('[data-qa="property-info-edit-btn"]', { includeShadowDom:true }).should("be.visible").click();
+        cy.get('[data-qa="city"] input', { includeShadowDom:true }).should("be.disabled");
+        
+    });
+});
