7f4e44941486ec05078d626e867f744b8ae24d8b
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-06-28 09:29:01 +0300

    [QA-4826] Fixed QA-4642, wait when fix bug with chip buildingName

--- cypress/integration/not_full_reports/report/client/QA-4642.spec.ts
+++ cypress/integration/not_full_reports/report/client/QA-4642.spec.ts
@@ -1,23 +1,22 @@
-import { Tag } from './../../../../utils/tags.utils';
 import { Report, ReviewExport } from './../../../../actions/index';
 import testData from "../../../../fixtures/not_full_reports/report/client/QA-4642.fixture";
 import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
 import { _NavigationSection } from '../../../../actions/base';
 
 describe("Verify the Client Guidelines Discussion on the page", () => {
-    it("Test body", { tags: [ Tag.report, Tag.client, Tag.check_export ] }, () => {
+    it("Test body", { tags: [ "@report", "@client", "@check_export" ] }, () => {
         cy.stepInfo("1. Proceed to the Report > Client page");
         createReport(testData.reportCreationData);
         _NavigationSection.navigateToClientPage().verifyProgressBarNotExist();
 
         cy.stepInfo("2. Click on the Edit button for Intended User and Identification of the Client sections");
-        Report._Client.Page.formEditBtn(0).click();
-        Report._Client.Page.formEditBtn(0).click();
+        Report._Client.Page.formEditBtn().click();
+        Report._Client.Page.formEditBtn().click();
 
         cy.stepInfo(`3. Enter the “=“ and verify the "Linked" chips dropdown for both sections: options 'Gross Building Area', 'Building Name', 'Property Type', 
             'Current Residential Unit Count', 'As Complete Residential Unit Count', 'Current Commercial Unit Count', 'As Complete Commercial Unit Count', 'Street Address', 
             'Street Name', 'Site Area', 'Year Built', 'Block', 'Lot', 'Concluded Cap Rate', 'Zones', 'CurrentCondition', 'As Stabilized Condition'`);
-        testData.linkedChipsDropdownOptions.forEach(chip => {
+        testData.chips.forEach(chip => {
             Report._Client.enterIntendedUserTextBox(`=${chip.typeSuggestValue}`)
                 .clickNarrativeSuggestions(chip.suggestionName)
                 .verifyIntendedUserTextBox(chip.verifySuggest);
@@ -40,7 +39,7 @@ describe("Verify the Client Guidelines Discussion on the page", () => {
             cy.stepInfo("5. Verify the linked chips on export for both sections");
             cy.visit(<string>file);
 
-            testData.linkedChipsDropdownOptions.forEach(item => {
+            testData.chips.forEach(item => {
                 cy.contains("Identification of the Client").next().scrollIntoView().should("include.text", item.verifyExport);
                 cy.contains("Intended Use & User").next().scrollIntoView().should("include.text", item.verifyExport);
             });
