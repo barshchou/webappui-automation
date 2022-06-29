d99006d92e6bfd40da7208105afc41187849314b
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-06-29 13:18:48 +0300

    expensesCardsNames edited

--- cypress/enums/expense/expenseForecast.enum.ts
+++ cypress/enums/expense/expenseForecast.enum.ts
@@ -3,17 +3,15 @@ const expensesCardsNames = {
     expenseCardsIDArray: [ "insurance", "electricity", "fuel", "waterSewer", "repairsMaintenance", "payrollBenefits",
         "generalAdministrative", "legalProfessionalFees", "miscellaneous", "managementFees", "replacementReserves" ],
 
-    realEstateTaxes: "realEstateTaxes",
-    insurace: "insurance",
+    insurance: "insurance",
     electricity: "electricity",
     fuel: "fuel",
     waterAndSewer: "waterAndSewer",
-    utilities: "utilities",
     repairAndMaintenance: "repairsAndMaintenance",
     payrollBenefits: "payrollAndBenefits",
     generalAndAdministrative: "generalAndAdministrative",
     legalAndProfessional: "legalAndProfessionalFees",
-    miscelaneous: "miscellaneous",
+    miscellaneous: "miscellaneous",
     managementFees: "management",
     replacementsAndReserves: "replacementReserves"
 };
