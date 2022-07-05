653c3b5830f11b9860728bd255cb9a08e9feaa21
Ernst Derevanko <ernst.dzeravianka@boweryvaluation.com>
2022-07-05 12:35:02 +0400

    [web-5827] refactored test data

--- cypress/fixtures/not_full_reports/sales/adjust_comps/QA-5312.fixture.ts
+++ cypress/fixtures/not_full_reports/sales/adjust_comps/QA-5312.fixture.ts
@@ -1,4 +1,5 @@
 import Enums from "../../../../enums/enums";
+import { BoweryReports } from "../../../../types/boweryReports.type";
 import ReportDataCreator from "../../../data_creator/reportData.creator";
 
 const _reportCreationData = ReportDataCreator.getReportData("5312", {
@@ -7,7 +8,12 @@ const _reportCreationData = ReportDataCreator.getReportData("5312", {
 
 const _numberOfSalesComps = [ 0, 1, 2 ];
 
+const adjustmentName: BoweryReports.SalesAdjustmentGrid.AdjustmentName = "market-adjustment";
+const rowName: BoweryReports.SalesAdjustmentGrid.RowsMarketAdjustment = "Property Description";
+
 export default {
     reportCreationData: _reportCreationData,
-    numberOfSalesComps:_numberOfSalesComps
+    numberOfSalesComps:_numberOfSalesComps,
+    adjustmentName,
+    rowName
 };
--- cypress/integration/not_full_reports/sales/adjust_comps/QA-5312.spec.ts
+++ cypress/integration/not_full_reports/sales/adjust_comps/QA-5312.spec.ts
@@ -20,11 +20,11 @@ describe("Total Utility Adjustments in Sales Adjustment Grid is calculated with 
         cy.stepInfo(`2. Go to Adjust Comps page`);
         _NavigationSection.navigateToAdjustComps();
         cy.stepInfo(`3. In the Sales adjustment grid click on the dropdown icon next to the Market Adjustment`);
-        Sales._AdjustComps.Page.getAdjustmentArrow("market-adjustment").click();
+        Sales._AdjustComps.Page.getAdjustmentArrow(testData.adjustmentName).click();
         testData.numberOfSalesComps.forEach((index) => {
             cy.stepInfo(`4. Click on the “View“ of any address`);
             Sales._AdjustComps.Page
-            .getAdjustmentRow("market-adjustment", "Property Description").eq(index).click;
+            .getAdjustmentRow(testData.adjustmentName, testData.rowName).eq(index).click();
             cy.stepInfo(`5. Verify that modal with all the information about the comp is opened`);
             Sales._AdjustComps.Page.ModalSalesCompInfo.should("be.visible");
             Sales._AdjustComps.Page.CloseIconShadowDom.click();
