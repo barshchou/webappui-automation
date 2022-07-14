749c57988668313104b9a0b8e388c241b9e4df34
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-07-14 12:30:35 +0300

    import deleted

--- cypress/integration/not_full_reports/income/expense_forecast/QA-5013.spec.ts
+++ cypress/integration/not_full_reports/income/expense_forecast/QA-5013.spec.ts
@@ -2,7 +2,6 @@ import testData from "../../../../fixtures/not_full_reports/income/expense_forec
 import { _NavigationSection } from "../../../../actions/base";
 import { Income, Property } from "../../../../actions";
 import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
-import expensesCardsNames from " ../../../cypress/enums/expense/expenseForecast.enum";
 
 describe(`[QA-5013] [Income>Expense forecast] Selected expense card is displayed in certain places`,
     { tags: [ "@income", "@expense_forecast" ] }, () => {
