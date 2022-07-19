54b7b21fb3774867d0a5eac4481c444bfe34a146
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-07-19 11:43:06 +0300

    test 5772 added

--- cypress/integration/not_full_reports/income/expense_forecast/QA-5772.spec.ts
+++ cypress/integration/not_full_reports/income/expense_forecast/QA-5772.spec.ts
@@ -1,17 +1,17 @@
-import testData from "../../../../fixtures/not_full_reports/income/expense_forecast/QA-5771.fixture";
+import testData from "../../../../fixtures/not_full_reports/income/expense_forecast/QA-5772.fixture";
 import { createReport, deleteReport } from "../../../../actions/base/baseTest.actions";
 import { Property, Income } from "../../../../actions";
 import { _NavigationSection } from "../../../../actions/base";
 import tableExpenseHistoryCellNames from "../../../../enums/expense/expenseHistoryTableRows.enum";
 
-describe("[QA-5771] Historical expense Payroll & Benefits Per SF is correctly calculated and displayed",
+describe("[QA-5772] Historical expense General & Administrative Per SF is correctly calculated and displayed",
     { tags: [ "@snapshot_tests", "@income", "@expense_forecast" ] }, () => {
 
         before("Login, create report", () => {
             createReport(testData.reportCreationData);
         });
 
-        it("[QA-5771]", () => {
+        it("[QA-5772]", () => {
 
             cy.stepInfo("1. Pre-condition: Fill in Residential Units and GBA on Property > Summary form");
             _NavigationSection.navigateToPropertySummary();
@@ -22,46 +22,46 @@ describe("[QA-5771] Historical expense Payroll & Benefits Per SF is correctly ca
             _NavigationSection.Actions.navigateToExpenseHistory();
 
             cy.stepInfo(`3. Add columns for all types of Expense Period: Actual, Actual T12, Annualized Historical and Projection + 
-                     Fill in Payroll & Benefits field for all added columns and save changes`);
+                         Fill in General & Administrative field for all added columns and save changes`);
             testData.periods.forEach((period) => {
                 Income._ExpenseHistory.Actions.selectExpensePeriod(period.expensePeriodType)
                     .enterExpenseYear(period.year)
                     .clickAddExpenseYearButton()
-                    .enterIssueByColIndex(period.payrollAndBenefits, tableExpenseHistoryCellNames.payrollAndBenefits);
+                    .enterIssueByColIndex(period.generalAndAdministrative, tableExpenseHistoryCellNames.generalAndAdministrative);
             });
             testData.periodsMonth.forEach((period) => {
                 Income._ExpenseHistory.Actions.selectExpensePeriod(period.expensePeriodType)
                     .enterExpenseMonth(period.month)
                     .enterExpenseYear(period.year)
                     .clickAddExpenseYearButton()
-                    .enterIssueByColIndex(period.payrollAndBenefits, tableExpenseHistoryCellNames.payrollAndBenefits);
+                    .enterIssueByColIndex(period.generalAndAdministrative, tableExpenseHistoryCellNames.generalAndAdministrative);
             });
             Income._ExpenseHistory.Actions.verifyAverageTable();
 
-            cy.stepInfo("4. Go to Expense Forecast and make sure that Per SF radiobutton is selected for Payroll & Benefits card");
+            cy.stepInfo("4. Go to Expense Forecast and make sure that Per SF radiobutton is selected for General & Administrative card");
             _NavigationSection.Actions.navigateToExpenseForecast();
-            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.actualPayrollAndBenefitsItem)
-                .verifyForecastItemBasis(testData.actualPayrollAndBenefitsItem);
+            Income._ExpenseForecastActions.chooseForecastItemBasis(testData.actualGeneralAndAdministrativeItem)
+                .verifyForecastItemBasis(testData.actualGeneralAndAdministrativeItem);
 
-            cy.stepInfo(`5.1 Check historical expenses values for Payroll & Benefits card. They should be calculated 
-                     for each expense type as: [Expense Period type]Payroll & Benefits / GBA`);
-            Income._ExpenseForecastActions.Actions.verifyForecastItemByExpensePeriodType(testData.actualPayrollAndBenefitsItem, 
+            cy.stepInfo(`5.1 Check historical expenses values for General & Administrative card. They should be calculated 
+                         for each expense type as: [Expense Period type]General & Administrative / GBA`);
+            Income._ExpenseForecastActions.Actions.verifyForecastItemByExpensePeriodType(testData.actualGeneralAndAdministrativeItem,
                 testData.buildingDescription, "Actual")
-                .verifyForecastItemByExpensePeriodType(testData.t12PayrollAndBenefitsItem, 
+                .verifyForecastItemByExpensePeriodType(testData.t12GeneralAndAdministrativeItem,
                     testData.buildingDescription, "Actual T12")
-                .verifyForecastItemByExpensePeriodType(testData.historicalPayrollAndBenefitsItem, 
+                .verifyForecastItemByExpensePeriodType(testData.historicalGeneralAndAdministrativeItem,
                     testData.buildingDescription, "Annualized Historical")
-                .verifyForecastItemByExpensePeriodType(testData.ownerProjectionPayrollAndBenefitsItem, 
+                .verifyForecastItemByExpensePeriodType(testData.ownerProjectionGeneralAndAdministrativeItem,
                     testData.buildingDescription, "Owner's Projection")
                 .hideHeader()
                 .clickSaveButton()
                 .verifyProgressBarNotExist();
 
-            cy.stepInfo("5.2 Check historical expenses values for Payroll & Benefits card. They should be correctly displayed on slidebars");
+            cy.stepInfo("5.2 Check historical expenses values for General & Administrative card. They should be correctly displayed on slidebars");
             Income._ExpenseForecastActions.Actions.matchElementSnapshot(
                 Income._ExpenseForecastActions.Page.forecastItemCard(
                     Income._ExpenseForecastActions.getItemNameForAverage(
-                        testData.actualPayrollAndBenefitsItem.name)), testData.payrollAndBenefitsPerSfCardSnapshotName, { padding: [ 10, 100 ] });
+                        testData.actualGeneralAndAdministrativeItem.name)), testData.generalAndAdministrativePerSfCardSnapshotName, { padding: [ 10, 100 ] });
 
             deleteReport(testData.reportCreationData.reportNumber);
         });
