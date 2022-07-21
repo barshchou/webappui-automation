b295f33678b180682790b60d305bf9224ae5a9a9
Viachaslau.Barshchou <Viachaslau.Barshchou@boweryvaluation.com>
2022-07-21 18:52:51 +0300

    [QA-6336] chunk of fixes

--- cypress/pages/final/insurableReplacementCost.page.ts
+++ cypress/pages/final/insurableReplacementCost.page.ts
@@ -1,11 +1,11 @@
 import BasePage from "../base/base.page";
 
 class InsurableReplacementCostPage extends BasePage {
-    get subjectStateDropdown() {return cy.get("[data-qa^=subjectState] [data-qa=select-value]");}
+    get subjectStateDropdown() { return cy.get("[data-qa^=subjectState] [data-qa=select-value]"); }
 
-    get subjectLocaleDropdown() {return cy.get("[data-qa^=subjectLocale] [data-qa=select-value]");}
+    get subjectLocaleDropdown() { return cy.get("[data-qa^=subjectLocale] [data-qa=select-value]"); }
 
-    get localMultiplier() {return cy.get("[name=localMultiplier]");}
+    get localMultiplier() { return cy.get("[name=localMultiplier]"); }
 }
 
 export default new InsurableReplacementCostPage();
