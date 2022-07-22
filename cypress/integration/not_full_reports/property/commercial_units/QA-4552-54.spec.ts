edf14a514a302013ed03f4052b3aad86619fd97e
Viachaslau.Barshchou <Viachaslau.Barshchou@boweryvaluation.com>
2022-07-22 10:50:31 +0300

    [QA-6336] chunk of fixes

--- cypress/integration/not_full_reports/property/commercial_units/QA-4552-54.spec.ts
+++ cypress/integration/not_full_reports/property/commercial_units/QA-4552-54.spec.ts
@@ -7,142 +7,151 @@ import testData from "../../../../fixtures/not_full_reports/property/commercial_
 describe(`[QA-4552][QA-4553][QA-4554] Verify the "Linked" chips dropdown in the new narrative component`, 
     { tags: [ "@property", "@commercial_units", "@check_export" ] }, () => {
 
-    const url = `${Cypress.config().baseUrl}`;
+        const url = `${Cypress.config().baseUrl}`;
 
-    it("[QA-4552] As Is", () => {
-        cy.stepInfo(`Preconditions: The mixed report is created and several commercial units are added.`);
-        createReport(testData.reportCreationDataAsIs);
+        it("[QA-4552] As Is", () => {
+            cy.stepInfo(`Preconditions: The mixed report is created and several commercial units are added.`);
+            createReport(testData.reportCreationDataAsIs);
 
-        _NavigationSection.navigateToPropertySummary();
-        Property._Summary.enterNumberOfCommercialUnits(testData.numberOfCommercialUnits)
-            .enterNumberOfResUnits(testData.numberOfResidentialUnits)
-            .enterGrossBuildingArea(testData.grossBuildingArea)
-            .enterSiteArea(testData.siteArea)
-            .enterBuildingName(testData.buildingName);
+            _NavigationSection.navigateToPropertySummary();
+            Property._Summary.enterNumberOfCommercialUnits(testData.numberOfCommercialUnits)
+                .enterNumberOfResUnits(testData.numberOfResidentialUnits)
+                .enterGrossBuildingArea(testData.grossBuildingArea)
+                .enterSiteArea(testData.siteArea)
+                .enterBuildingName(testData.buildingName);
 
-        cy.stepInfo("1. Proceed to the Property -> Commercial Units");
-        _NavigationSection.navigateToCommercialUnits();
+            cy.stepInfo("1. Proceed to the Property -> Commercial Units");
+            _NavigationSection.navigateToCommercialUnits();
 
-        cy.stepInfo("2. Click on the Edit button in the Commercial Unit SF Discussion section.");
-        Property._CommercialUnits.clickEditDiscussionButton();
+            cy.stepInfo("2. Click on the Edit button in the Commercial Unit SF Discussion section.");
+            Property._CommercialUnits.clickEditDiscussionButton();
 
-        cy.stepInfo("3. Enter the “=“ and select the an option. Verify each option.");
-        testData.asIsChips.forEach((chip) => {
-            Property._CommercialUnits.editDiscussionTextArea(`=${chip.typeSuggestValue}`, false)
-            .clickNarrativeSuggestions(chip.suggestionName)
-            .verifyCommentaryContainsText(chip.verifySuggest);
-        });
-        Property._CommercialUnits.clickSaveDiscussionButton()
-            .verifyProgressBarNotExist();
-
-        _NavigationSection.openReviewAndExport();
-        ReviewExport.generateDocxReport().waitForReportGenerated()
-            .downloadAndConvertDocxReport(testData.reportCreationDataAsIs.reportNumber);
-        deleteReport(testData.reportCreationDataAsIs.reportNumber);
-    });
-
-    it("[QA-4552] Check export", () => {
-        Cypress.config().baseUrl = null;
-        cy.task("getFilePath", { _reportName: testData.reportCreationDataAsIs.reportNumber, _docx_html: "html" }).then(file => {
-            cy.log(<string>file);
-            cy.stepInfo("4. Verify the linked chips on export");
-            cy.visit(<string>file);
-            testData.asIsChips.forEach(chip => {
-                let expectedText = typeof chip.verifyExport ===  "number" ? `${numberWithCommas(chip.verifyExport)}`: chip.verifyExport;
-                cy.contains("Commercial Space").next().scrollIntoView().should("include.text", expectedText);
+            cy.stepInfo("3. Enter the “=“ and select the an option. Verify each option.");
+            testData.asIsChips.forEach((chip) => {
+                Property._CommercialUnits.editDiscussionTextArea(`=${chip.typeSuggestValue}`, false)
+                    .clickNarrativeSuggestions(chip.suggestionName)
+                    .verifyCommentaryContainsText(chip.verifySuggest);
             });
-        }); 
-    });
+            Property._CommercialUnits.clickSaveDiscussionButton()
+                .verifyProgressBarNotExist();
 
-    it("[QA-4553] As Stabilized", () => {
-        Cypress.config().baseUrl = url;
-        cy.stepInfo(`Preconditions: The mixed report is created and several commercial units are added.`);
-        createReport(testData.reportCreationDataAsIs);
-
-        _NavigationSection.navigateToPropertySummary();
-        Property._Summary.enterNumberOfCommercialUnits(testData.numberOfCommercialUnits)
-            .enterNumberOfResUnits(testData.numberOfResidentialUnits)
-            .enterGrossBuildingArea(testData.grossBuildingArea)
-            .enterSiteArea(testData.siteArea)
-            .enterBuildingName(testData.buildingName);
-
-        cy.stepInfo("1. Proceed to the Property -> Commercial Units");
-        _NavigationSection.navigateToCommercialUnits();
-
-        cy.stepInfo("2. Click on the Edit button in the Commercial Unit SF Discussion section.");
-        Property._CommercialUnits.clickEditDiscussionButton();
-
-        cy.stepInfo("3. Enter the “=“ and select the an option. Verify each option.");
-        testData.asIsChips.forEach((chip) => {
-            Property._CommercialUnits.editDiscussionTextArea(`=${chip.typeSuggestValue}`, false)
-            .clickNarrativeSuggestions(chip.suggestionName)
-            .verifyCommentaryContainsText(chip.verifySuggest);
+            _NavigationSection.openReviewAndExport();
+            ReviewExport.generateDocxReport().waitForReportGenerated()
+                .downloadAndConvertDocxReport(testData.reportCreationDataAsIs.reportNumber);
+            deleteReport(testData.reportCreationDataAsIs.reportNumber);
         });
-        Property._CommercialUnits.clickSaveDiscussionButton()
-            .verifyProgressBarNotExist();
 
-        _NavigationSection.openReviewAndExport();
-        ReviewExport.generateDocxReport().waitForReportGenerated()
-            .downloadAndConvertDocxReport(testData.reportCreationDataAsIs.reportNumber);
-        deleteReport(testData.reportCreationDataAsIs.reportNumber);
-    });
-
-    it("[QA-4553] Check export", () => {
-        Cypress.config().baseUrl = null;
-        cy.task("getFilePath", { _reportName: testData.reportCreationDataAsIs.reportNumber, _docx_html: "html" }).then(file => {
-            cy.log(<string>file);
-            cy.stepInfo("4. Verify the linked chips on export");
-            cy.visit(<string>file);
-            testData.asIsChips.forEach(chip => {
-                let expectedText = typeof chip.verifyExport ===  "number" ? `${numberWithCommas(chip.verifyExport)}`: chip.verifyExport;
-                cy.contains("Commercial Space").next().scrollIntoView().should("include.text", expectedText);
-            });
-        }); 
-    });
-
-    it("[QA-4554] As Completed", () => {
-        Cypress.config().baseUrl = url;
-        cy.stepInfo(`Preconditions: The mixed report is created and several commercial units are added.`);
-        createReport(testData.reportCreationDataAsIs);
-
-        _NavigationSection.navigateToPropertySummary();
-        Property._Summary.enterNumberOfCommercialUnits(testData.numberOfCommercialUnits)
-            .enterNumberOfResUnits(testData.numberOfResidentialUnits)
-            .enterGrossBuildingArea(testData.grossBuildingArea)
-            .enterSiteArea(testData.siteArea)
-            .enterBuildingName(testData.buildingName);
-
-        cy.stepInfo("1. Proceed to the Property -> Commercial Units");
-        _NavigationSection.navigateToCommercialUnits();
-
-        cy.stepInfo("2. Click on the Edit button in the Commercial Unit SF Discussion section.");
-        Property._CommercialUnits.clickEditDiscussionButton();
-
-        cy.stepInfo("3. Enter the “=“ and select the an option. Verify each option.");
-        testData.asIsChips.forEach((chip) => {
-            Property._CommercialUnits.editDiscussionTextArea(`=${chip.typeSuggestValue}`, false)
-            .clickNarrativeSuggestions(chip.suggestionName)
-            .verifyCommentaryContainsText(chip.verifySuggest);
+        it("[QA-4552] Check export", () => {
+            Cypress.config().baseUrl = null;
+            cy.task("getFilePath", { reportName: testData.reportCreationDataAsIs.reportNumber, _docxHtml: "html" })
+                .then(file => {
+                    cy.log(<string>file);
+                    cy.stepInfo("4. Verify the linked chips on export");
+                    cy.visit(<string>file);
+                    testData.asIsChips.forEach(chip => {
+                        let expectedText = typeof chip.verifyExport ===  "number" 
+                            ? `${numberWithCommas(chip.verifyExport)}`
+                            : chip.verifyExport;
+                        cy.contains("Commercial Space").next().scrollIntoView().should("include.text", expectedText);
+                    });
+                }); 
         });
-        Property._CommercialUnits.clickSaveDiscussionButton()
-            .verifyProgressBarNotExist();
 
-        _NavigationSection.openReviewAndExport();
-        ReviewExport.generateDocxReport().waitForReportGenerated()
-            .downloadAndConvertDocxReport(testData.reportCreationDataAsIs.reportNumber);
-        deleteReport(testData.reportCreationDataAsIs.reportNumber);
-    });
+        it("[QA-4553] As Stabilized", () => {
+            Cypress.config().baseUrl = url;
+            cy.stepInfo(`Preconditions: The mixed report is created and several commercial units are added.`);
+            createReport(testData.reportCreationDataAsIs);
 
-    it("[QA-4554] Check export", () => {
-        Cypress.config().baseUrl = null;
-        cy.task("getFilePath", { _reportName: testData.reportCreationDataAsIs.reportNumber, _docx_html: "html" }).then(file => {
-            cy.log(<string>file);
-            cy.stepInfo("4. Verify the linked chips on export");
-            cy.visit(<string>file);
-            testData.asIsChips.forEach(chip => {
-                let expectedText = typeof chip.verifyExport ===  "number" ? `${numberWithCommas(chip.verifyExport)}`: chip.verifyExport;
-                cy.contains("Commercial Space").next().scrollIntoView().should("include.text", expectedText);
+            _NavigationSection.navigateToPropertySummary();
+            Property._Summary.enterNumberOfCommercialUnits(testData.numberOfCommercialUnits)
+                .enterNumberOfResUnits(testData.numberOfResidentialUnits)
+                .enterGrossBuildingArea(testData.grossBuildingArea)
+                .enterSiteArea(testData.siteArea)
+                .enterBuildingName(testData.buildingName);
+
+            cy.stepInfo("1. Proceed to the Property -> Commercial Units");
+            _NavigationSection.navigateToCommercialUnits();
+
+            cy.stepInfo("2. Click on the Edit button in the Commercial Unit SF Discussion section.");
+            Property._CommercialUnits.clickEditDiscussionButton();
+
+            cy.stepInfo("3. Enter the “=“ and select the an option. Verify each option.");
+            testData.asIsChips.forEach((chip) => {
+                Property._CommercialUnits.editDiscussionTextArea(`=${chip.typeSuggestValue}`, false)
+                    .clickNarrativeSuggestions(chip.suggestionName)
+                    .verifyCommentaryContainsText(chip.verifySuggest);
             });
-        }); 
+            Property._CommercialUnits.clickSaveDiscussionButton()
+                .verifyProgressBarNotExist();
+
+            _NavigationSection.openReviewAndExport();
+            ReviewExport.generateDocxReport().waitForReportGenerated()
+                .downloadAndConvertDocxReport(testData.reportCreationDataAsIs.reportNumber);
+            deleteReport(testData.reportCreationDataAsIs.reportNumber);
+        });
+
+        it("[QA-4553] Check export", () => {
+            Cypress.config().baseUrl = null;
+            cy.task("getFilePath", { reportName: testData.reportCreationDataAsIs.reportNumber, docxHtml: "html" })
+                .then(file => {
+                    cy.log(<string>file);
+                    cy.stepInfo("4. Verify the linked chips on export");
+                    cy.visit(<string>file);
+                    testData.asIsChips.forEach(chip => {
+                        let expectedText = typeof chip.verifyExport ===  "number" 
+                            ? `${numberWithCommas(chip.verifyExport)}`
+                            : chip.verifyExport;
+                        cy.contains("Commercial Space").next().scrollIntoView().should("include.text", expectedText);
+                    });
+                }); 
+        });
+
+        it("[QA-4554] As Completed", () => {
+            Cypress.config().baseUrl = url;
+            cy.stepInfo(`Preconditions: The mixed report is created and several commercial units are added.`);
+            createReport(testData.reportCreationDataAsIs);
+
+            _NavigationSection.navigateToPropertySummary();
+            Property._Summary.enterNumberOfCommercialUnits(testData.numberOfCommercialUnits)
+                .enterNumberOfResUnits(testData.numberOfResidentialUnits)
+                .enterGrossBuildingArea(testData.grossBuildingArea)
+                .enterSiteArea(testData.siteArea)
+                .enterBuildingName(testData.buildingName);
+
+            cy.stepInfo("1. Proceed to the Property -> Commercial Units");
+            _NavigationSection.navigateToCommercialUnits();
+
+            cy.stepInfo("2. Click on the Edit button in the Commercial Unit SF Discussion section.");
+            Property._CommercialUnits.clickEditDiscussionButton();
+
+            cy.stepInfo("3. Enter the “=“ and select the an option. Verify each option.");
+            testData.asIsChips.forEach((chip) => {
+                Property._CommercialUnits.editDiscussionTextArea(`=${chip.typeSuggestValue}`, false)
+                    .clickNarrativeSuggestions(chip.suggestionName)
+                    .verifyCommentaryContainsText(chip.verifySuggest);
+            });
+            Property._CommercialUnits.clickSaveDiscussionButton()
+                .verifyProgressBarNotExist();
+
+            _NavigationSection.openReviewAndExport();
+            ReviewExport.generateDocxReport().waitForReportGenerated()
+                .downloadAndConvertDocxReport(testData.reportCreationDataAsIs.reportNumber);
+            deleteReport(testData.reportCreationDataAsIs.reportNumber);
+        });
+
+        it("[QA-4554] Check export", () => {
+            Cypress.config().baseUrl = null;
+            cy.task("getFilePath", { reportName: testData.reportCreationDataAsIs.reportNumber, docxHtml: "html" })
+                .then(file => {
+                    cy.log(<string>file);
+                    cy.stepInfo("4. Verify the linked chips on export");
+                    cy.visit(<string>file);
+                    testData.asIsChips.forEach(chip => {
+                        let expectedText = typeof chip.verifyExport ===  "number" 
+                            ? `${numberWithCommas(chip.verifyExport)}`
+                            : chip.verifyExport;
+                        cy.contains("Commercial Space").next().scrollIntoView().should("include.text", expectedText);
+                    });
+                }); 
+        });
     });
-});
--- cypress/integration/not_full_reports/property/market/QA-4430_42.spec.ts
+++ cypress/integration/not_full_reports/property/market/QA-4430_42.spec.ts
@@ -7,50 +7,54 @@ import { _NavigationSection } from "../../../../actions/base";
 describe("Verify the functionality of the Ceiling Height radio button", 
     { tags:[ "@property", "@market", "@check_export" ] }, () => {
 
-    it("Test body", () => {
-        createReport(testData.reportCreationData);
+        it("Test body", () => {
+            createReport(testData.reportCreationData);
 
-        cy.stepInfo("1. Navigate to Property -> Market");
-        Base._NavigationSection.navigateToPropertyMarket();
+            cy.stepInfo("1. Navigate to Property -> Market");
+            Base._NavigationSection.navigateToPropertyMarket();
 
-        cy.stepInfo("2. Verify exposure time description with default exposure time is displayed");
-        Property._Market.verifyExposureTimeMin(testData.minExposureTimeDefault)
-            .verifyExposureTimeMax(testData.maxExposureTimeDefault)
-            .verifyExposureTimeDescription(testData.exposureTimeDescriptionDefault);
+            cy.stepInfo("2. Verify exposure time description with default exposure time is displayed");
+            Property._Market.verifyExposureTimeMin(testData.minExposureTimeDefault)
+                .verifyExposureTimeMax(testData.maxExposureTimeDefault)
+                .verifyExposureTimeDescription(testData.exposureTimeDescriptionDefault);
         
-        cy.stepInfo("3. [QA-4430] Verify that the [Exposure Time Min] and [Exposure Time Max] are dynamic chips");
-        Property._Market.updateExposureTimeMin(testData.customMinExposureTime)
-            .updateExposureTimeMax(testData.customMaxExposureTime)
-            .verifyExposureTimeDescription(testData.exposureTimeDescriptionCustom);
+            cy.stepInfo("3. [QA-4430] Verify that the [Exposure Time Min] and [Exposure Time Max] are dynamic chips");
+            Property._Market.updateExposureTimeMin(testData.customMinExposureTime)
+                .updateExposureTimeMax(testData.customMaxExposureTime)
+                .verifyExposureTimeDescription(testData.exposureTimeDescriptionCustom);
 
-        cy.stepInfo("4. [QA-4442] Verify the 'Marketing Time Description' generated");
-        Property._Market.checkIncludeMarketingTimeDescription()
-            .updateMarketingTimeMin(testData.customMinMarketingTime)
-            .updateMarketingTimeMax(testData.customMaxMarketingTime)
-            .verifyMarketTimeDescription(testData.marketingTimeDescriptionCustom)
-            .clickSaveButton();
+            cy.stepInfo("4. [QA-4442] Verify the 'Marketing Time Description' generated");
+            Property._Market.checkIncludeMarketingTimeDescription()
+                .updateMarketingTimeMin(testData.customMinMarketingTime)
+                .updateMarketingTimeMax(testData.customMaxMarketingTime)
+                .verifyMarketTimeDescription(testData.marketingTimeDescriptionCustom)
+                .clickSaveButton();
 
-        cy.stepInfo("5. [QA-4430] [QA-4442] Prepare report for export validation");
-         _NavigationSection.openReviewAndExport();
-        ReviewExport.generateDocxReport()
-            .waitForReportGenerated()
-            .downloadAndConvertDocxReport(testData.reportCreationData.reportNumber);
-        deleteReport(testData.reportCreationData.reportNumber);
+            cy.stepInfo("5. [QA-4430] [QA-4442] Prepare report for export validation");
+            _NavigationSection.openReviewAndExport();
+            ReviewExport.generateDocxReport()
+                .waitForReportGenerated()
+                .downloadAndConvertDocxReport(testData.reportCreationData.reportNumber);
+            deleteReport(testData.reportCreationData.reportNumber);
 
-    });
+        });
 
-    it("Check exported document other utilities values and commentaries", () => {
-        Cypress.config().baseUrl = null;
-        cy.task("getFilePath", { _reportName: testData.reportCreationData.reportNumber, _docx_html: "html" })
-        .then(file => {
-            cy.log(<string>file);
-            cy.visit(<string>file);
+        it("Check exported document other utilities values and commentaries", () => {
+            Cypress.config().baseUrl = null;
+            cy.task("getFilePath", { _reportName: testData.reportCreationData.reportNumber, _docx_html: "html" })
+                .then(file => {
+                    cy.log(<string>file);
+                    cy.visit(<string>file);
 
-            cy.stepInfo(`6. [QA-4430] Verify that the commentary exports in the Introduction as a sentence in the Introduction > Exposure Time section `);
-            cy.xpath("//h2[text() = 'Exposure Time']/following-sibling::p").eq(0).should("have.text", testData.exposureTimeDescriptionCustom);
+                    cy.stepInfo(`6. [QA-4430] Verify that the commentary exports in the Introduction 
+                    as a sentence in the Introduction > Exposure Time section `);
+                    cy.xpath("//h2[text() = 'Exposure Time']/following-sibling::p").eq(0)
+                        .should("have.text", testData.exposureTimeDescriptionCustom);
 
-            cy.stepInfo(`7. [QA-4442] Verify that the commentary exports in the Introduction as a sentence in the Introduction > Exposure Time section `);
-            cy.xpath("//h2[text() = 'Marketing Time']/following-sibling::p").eq(0).should("have.text", testData.marketingTimeDescriptionCustom);
+                    cy.stepInfo(`7. [QA-4442] Verify that the commentary exports in the Introduction 
+                    as a sentence in the Introduction > Exposure Time section `);
+                    cy.xpath("//h2[text() = 'Marketing Time']/following-sibling::p").eq(0)
+                        .should("have.text", testData.marketingTimeDescriptionCustom);
+                });
         });
     });
-});
