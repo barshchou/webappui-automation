e778f943b40a1a06bf01f23e26e9edd4a0e509f2
Viachaslau.Barshchou <Viachaslau.Barshchou@boweryvaluation.com>
2022-07-22 11:06:03 +0300

    [QA-6336] chunk of fixes

--- cypress/integration/not_full_reports/report/client/QA-4630.spec.ts
+++ cypress/integration/not_full_reports/report/client/QA-4630.spec.ts
@@ -5,27 +5,29 @@ import testData from '../../../../fixtures/not_full_reports/report/client/QA-463
 
 describe("Verify the functionality of the NYCB Application No. (optional) field",
     { tags:[ "@report", "@client" ] }, () => {
-    before("Login, create report", () => {
-        createReport(testData.reportCreationData);
-    });
+        before("Login, create report", () => {
+            createReport(testData.reportCreationData);
+        });
 
-    it("Test body", () => {
-        cy.stepInfo("1. Proceed to the Report > Client page.");
-        _NavigationSection.navigateToClientPage();
-        Report._Client.verifyProgressBarNotExist()
-            .Page.formEditBtn().click();
-        Report._Client.Page.formEditBtn().click();
+        it("Test body", () => {
+            cy.stepInfo(`1. Proceed to the Report > Client page.`);
+            _NavigationSection.navigateToClientPage();
+            Report._Client.verifyProgressBarNotExist()
+                .Page.formEditBtn().click();
+            Report._Client.Page.formEditBtn().click();
 
-        cy.stepInfo("2. Try to enter any num. value / non-integer / non-num. / long value in the Client File Number field (NO validation).");
-        Report._Client.enterNycbApplicationNumber(testData.clientFileNumber);
+            cy.stepInfo(`2. Try to enter any num. value / non-integer / non-num. / long value 
+            in the Client File Number field (NO validation).`);
+            Report._Client.enterNycbApplicationNumber(testData.clientFileNumber);
 
-        cy.stepInfo("3. Try to copy-paste any value into the Client File Number field, save it.");
-        Report._Client.Page.nycbApplicationNumber.invoke("val", testData.clientFileNumber);
+            cy.stepInfo(`3. Try to copy-paste any value into the Client File Number field, save it.`);
+            Report._Client.Page.nycbApplicationNumber.invoke("val", testData.clientFileNumber);
 
-        cy.stepInfo("4. Proceed to the Preview & Edit > Cover page and verify that the Client from the previous step is displayed in the APPRAISAL REPORT section.");
-        _NavigationSection.navigateToCoverPage();
-        PreviewEdit._CoverPage.verifyApplicationNumber(testData.clientFileNumber);
+            cy.stepInfo(`4. Proceed to the Preview & Edit > Cover page and verify that the Client 
+            from the previous step is displayed in the APPRAISAL REPORT section.`);
+            _NavigationSection.navigateToCoverPage();
+            PreviewEdit._CoverPage.verifyApplicationNumber(testData.clientFileNumber);
       
-        deleteReport(testData.reportCreationData.reportNumber);
+            deleteReport(testData.reportCreationData.reportNumber);
+        });
     });
-});
