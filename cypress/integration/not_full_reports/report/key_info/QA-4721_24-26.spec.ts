ecef1b568feb9c5503ac56f62c8ef34d7647e717
Viachaslau.Barshchou <Viachaslau.Barshchou@boweryvaluation.com>
2022-07-22 11:59:53 +0300

    [QA-6336] chunk of fixes

--- cypress/integration/not_full_reports/report/key_info/QA-4721_24-26.spec.ts
+++ cypress/integration/not_full_reports/report/key_info/QA-4721_24-26.spec.ts
@@ -6,112 +6,115 @@ import testData from '../../../../fixtures/not_full_reports/report/key_info/QA-4
 
 describe(`Verify the Save and Save & Continue button functionality on the Report > Key Info page:`,
     { tags:[ "@report", "@key_info" ] }, () => {
-    beforeEach("Login, create report", () => {
-        createReport(testData.reportCreationData);
-    });
+        beforeEach("Login, create report", () => {
+            createReport(testData.reportCreationData);
+        });
 
-    it("[QA-4721]", () => {
-        cy.stepInfo("1. Proceed to the Report > Key Info page");
-        _NavigationSection.navigateToReportInformation();
+        it("[QA-4721]", () => {
+            cy.stepInfo(`1. Proceed to the Report > Key Info page`);
+            _NavigationSection.navigateToReportInformation();
 
-        cy.stepInfo("2. Fill in the editable fields with values and click on the Save button then reload page");
-        Report._KeyInfo.enterPropertyRightsAppraisedComment(testData.enterValue, true, false, false);
-        Report._KeyInfo.clickNarrativeSuggestions(testData.listValue)
-            .verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue);
-        Report._KeyInfo.enterDefinitionMarketValue(testData.enterValue, true, false, false);
-        Report._KeyInfo.clickNarrativeSuggestions(testData.listValue, 1)
-            .verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue)
-            .renterTextBoxPropertyRightsAppraised(testData.enterValue)
-            .clickNarrativeSuggestions(testData.listValue)
-            .verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue)
-            .clickSaveButton();
-        cy.reload();
+            cy.stepInfo(`2. Fill in the editable fields with values and click on the Save button then reload page`);
+            Report._KeyInfo.enterPropertyRightsAppraisedComment(testData.enterValue, true, false, false);
+            Report._KeyInfo.clickNarrativeSuggestions(testData.listValue)
+                .verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue);
+            Report._KeyInfo.enterDefinitionMarketValue(testData.enterValue, true, false, false);
+            Report._KeyInfo.clickNarrativeSuggestions(testData.listValue, 1)
+                .verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue)
+                .renterTextBoxPropertyRightsAppraised(testData.enterValue)
+                .clickNarrativeSuggestions(testData.listValue)
+                .verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue)
+                .clickSaveButton();
+            cy.reload();
        
 
-        cy.stepInfo("3. Verify that the changes are saved");
-        Report._KeyInfo.verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue);
-        Report._KeyInfo.verifyTextBoxDefinitionOfMarketValue(testData.verifyTaxValue);
+            cy.stepInfo(`3. Verify that the changes are saved`);
+            Report._KeyInfo.verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue);
+            Report._KeyInfo.verifyTextBoxDefinitionOfMarketValue(testData.verifyTaxValue);
 
-        deleteReport(testData.reportCreationData.reportNumber);
-    });
+            deleteReport(testData.reportCreationData.reportNumber);
+        });
 
-    it("[QA-4724]", () => {
-        cy.stepInfo("1. Proceed to the Report > Key Info page");
-        _NavigationSection.navigateToReportInformation();
+        it("[QA-4724]", () => {
+            cy.stepInfo(`1. Proceed to the Report > Key Info page`);
+            _NavigationSection.navigateToReportInformation();
 
-        cy.stepInfo("2. Fill in the editable fields with values and click on the Save & Continue button");
-        Report._KeyInfo.enterPropertyRightsAppraisedComment(testData.enterValue, true, false, false);
-        Report._KeyInfo.clickNarrativeSuggestions(testData.listValue)
-            .verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue);
-        Report._KeyInfo.enterDefinitionMarketValue(testData.enterValue, true, false, false);
-        Report._KeyInfo.clickNarrativeSuggestions(testData.listValue, 1)
-            .verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue)
-            .renterTextBoxPropertyRightsAppraised(testData.enterValue)
-            .clickNarrativeSuggestions(testData.listValue)
-            .verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue)
-            .clickSaveContinueButton();
+            cy.stepInfo(`2. Fill in the editable fields with values and click on the Save & Continue button`);
+            Report._KeyInfo.enterPropertyRightsAppraisedComment(testData.enterValue, true, false, false);
+            Report._KeyInfo.clickNarrativeSuggestions(testData.listValue)
+                .verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue);
+            Report._KeyInfo.enterDefinitionMarketValue(testData.enterValue, true, false, false);
+            Report._KeyInfo.clickNarrativeSuggestions(testData.listValue, 1)
+                .verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue)
+                .renterTextBoxPropertyRightsAppraised(testData.enterValue)
+                .clickNarrativeSuggestions(testData.listValue)
+                .verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue)
+                .clickSaveContinueButton();
 
-        cy.stepInfo("3. Verify that the changes are saved and the user is redirected to the next page (Report > Appraiser)");
-        Report._Appraiser.verifyPageOpened();
-        cy.go("back");
-        Report._KeyInfo.clickYesButton();
-        Report._KeyInfo.verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue)
-            .verifyTextBoxDefinitionOfMarketValue(testData.verifyTaxValue);
+            cy.stepInfo(`3. Verify that the changes are saved and the user is redirected 
+            to the next page (Report > Appraiser)`);
+            Report._Appraiser.verifyPageOpened();
+            cy.go("back");
+            Report._KeyInfo.clickYesButton();
+            Report._KeyInfo.verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue)
+                .verifyTextBoxDefinitionOfMarketValue(testData.verifyTaxValue);
 
-        deleteReport(testData.reportCreationData.reportNumber);
-    });
+            deleteReport(testData.reportCreationData.reportNumber);
+        });
 
-    it("[QA-4725]", () => {
-        cy.stepInfo("1. Proceed to the Report > Key Info page");
-        _NavigationSection.navigateToReportInformation();
+        it("[QA-4725]", () => {
+            cy.stepInfo(`1. Proceed to the Report > Key Info page`);
+            _NavigationSection.navigateToReportInformation();
 
-        cy.stepInfo("2. Fill in the editable fields with values and do NOT click on the Save button");
-        Report._KeyInfo.enterPropertyRightsAppraisedComment(testData.enterValue, true, false, false);
-        Report._KeyInfo.clickNarrativeSuggestions(testData.listValue)
-            .verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue);
-        Report._KeyInfo.enterDefinitionMarketValue(testData.enterValue, true, false, false);
-        Report._KeyInfo.clickNarrativeSuggestions(testData.listValue, 1)
-            .verifyTextBoxDefinitionOfMarketValue(testData.verifyTaxValue);
+            cy.stepInfo(`2. Fill in the editable fields with values and do NOT click on the Save button`);
+            Report._KeyInfo.enterPropertyRightsAppraisedComment(testData.enterValue, true, false, false);
+            Report._KeyInfo.clickNarrativeSuggestions(testData.listValue)
+                .verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue);
+            Report._KeyInfo.enterDefinitionMarketValue(testData.enterValue, true, false, false);
+            Report._KeyInfo.clickNarrativeSuggestions(testData.listValue, 1)
+                .verifyTextBoxDefinitionOfMarketValue(testData.verifyTaxValue);
 
-        cy.stepInfo("3. Try to proceed on any other page and verify that the Unsaved changes modal is displayed");
-        _NavigationSection.clickPreviewEditButton()
-            .clickLetterOfTransmittal()
-            .verifyUnsavedChangesModal()
-            .clickYesButton()
-            .navigateToReportInformation();
-        Report._KeyInfo.verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue)
-            .verifyTextBoxDefinitionOfMarketValue(testData.verifyTaxValue);
+            cy.stepInfo(`3. Try to proceed on any other page and verify that the Unsaved changes modal is displayed`);
+            _NavigationSection.clickPreviewEditButton()
+                .clickLetterOfTransmittal()
+                .verifyUnsavedChangesModal()
+                .clickYesButton()
+                .navigateToReportInformation();
+            Report._KeyInfo.verifyTextBoxPropertyRightsAppraised(testData.verifyTaxValue)
+                .verifyTextBoxDefinitionOfMarketValue(testData.verifyTaxValue);
 
-        cy.stepInfo("4. Try to proceed on any other page from the Key Info page and verify that the Unsaved changes modal is displayed");
-        _NavigationSection.navigateToReportInformation();
-        Report._KeyInfo.enterPropertyRightsAppraisedComment(testData.enterSecondValue, false, false, false);
-        Report._KeyInfo.clickNarrativeSuggestions(testData.secondListValue)
-            .verifyTextBoxPropertyRightsAppraised(testData.verifySecondTaxValue);
-        Report._KeyInfo.enterDefinitionMarketValue(testData.enterSecondValue, false, false, false);
-        Report._KeyInfo.clickNarrativeSuggestions(testData.secondListValue, 1)
-            .verifyTextBoxDefinitionOfMarketValue(testData.verifySecondTaxValue);
+            cy.stepInfo(`4. Try to proceed on any other page from the Key Info page and 
+            verify that the Unsaved changes modal is displayed`);
+            _NavigationSection.navigateToReportInformation();
+            Report._KeyInfo.enterPropertyRightsAppraisedComment(testData.enterSecondValue, false, false, false);
+            Report._KeyInfo.clickNarrativeSuggestions(testData.secondListValue)
+                .verifyTextBoxPropertyRightsAppraised(testData.verifySecondTaxValue);
+            Report._KeyInfo.enterDefinitionMarketValue(testData.enterSecondValue, false, false, false);
+            Report._KeyInfo.clickNarrativeSuggestions(testData.secondListValue, 1)
+                .verifyTextBoxDefinitionOfMarketValue(testData.verifySecondTaxValue);
 
-        cy.stepInfo("5 Verify that the changes are NOT saved on the Key Info page");
-        _NavigationSection.clickPreviewEditButton()
-            .clickLetterOfTransmittal()
-            .verifyUnsavedChangesModal()
-            .clickNoButton();
-        cy.go("back");
-        Report._KeyInfo.verifyTextBoxPropertyRightsAppraised(testData.verifySecondTaxValue, "not.include.text")
-            .verifyTextBoxDefinitionOfMarketValue(testData.verifySecondTaxValue, "not.include.text");
+            cy.stepInfo(`5 Verify that the changes are NOT saved on the Key Info page`);
+            _NavigationSection.clickPreviewEditButton()
+                .clickLetterOfTransmittal()
+                .verifyUnsavedChangesModal()
+                .clickNoButton();
+            cy.go("back");
+            Report._KeyInfo.verifyTextBoxPropertyRightsAppraised(testData.verifySecondTaxValue, "not.include.text")
+                .verifyTextBoxDefinitionOfMarketValue(testData.verifySecondTaxValue, "not.include.text");
 
-        deleteReport(testData.reportCreationData.reportNumber);
-    });
+            deleteReport(testData.reportCreationData.reportNumber);
+        });
 
-    it("[QA-4726]", () => {
-        cy.stepInfo("1. Proceed to the Report > Key Info page");
-        _NavigationSection.navigateToReportInformation();
+        it("[QA-4726]", () => {
+            cy.stepInfo(`1. Proceed to the Report > Key Info page`);
+            _NavigationSection.navigateToReportInformation();
        
-        cy.stepInfo("2. Click on the Back button and verify the user is redirected to another page (Settings & Export > Review and Export).");
-        Report._KeyInfo.clickBackButton()
-            .clickYesButton();
-        ReviewExport.verifyPageIsOpened();
+            cy.stepInfo(`2. Click on the Back button and verify the user is redirected 
+            to another page (Settings & Export > Review and Export).`);
+            Report._KeyInfo.clickBackButton()
+                .clickYesButton();
+            ReviewExport.verifyPageIsOpened();
 
-        deleteReport(testData.reportCreationData.reportNumber);
+            deleteReport(testData.reportCreationData.reportNumber);
+        });
     });
-});
--- cypress/integration/not_full_reports/sales/adjust_comps/QA-4109.spec.ts
+++ cypress/integration/not_full_reports/sales/adjust_comps/QA-4109.spec.ts
@@ -7,42 +7,43 @@ import { ReviewExport } from "../../../../actions";
 describe("Adjusted Price per Residential Unit in Sales Adjustment Grid is calculated with correct formula", 
     { tags: [ "@adjust_comps", "@sales", "@check_export" ] }, () => {
 
-    it("Test body", () => {
-        createReport(testData.reportCreationData);
+        it("Test body", () => {
+            createReport(testData.reportCreationData);
 
-        NavigationSection.navigateToFindComps();
-        Sales.FindComps.selectCompFromMap();
+            NavigationSection.navigateToFindComps();
+            Sales.FindComps.selectCompFromMap();
         
-        NavigationSection.navigateToAdjustComps();
-        Sales.AdjustComps.checkCalculationUnitsRadio(testData.calculationUnits)
-            .enterMarketAdjustmentsGroup(Object.keys(testData.comparableAdjustment), Object.values(testData.comparableAdjustment))            
-            .verifyTrendedPricePerBasis(Object.values(testData.comparableAdjustment), testData.basis);
+            NavigationSection.navigateToAdjustComps();
+            Sales.AdjustComps.checkCalculationUnitsRadio(testData.calculationUnits)
+                .enterMarketAdjustmentsGroup(Object.keys(testData.comparableAdjustment), 
+                    Object.values(testData.comparableAdjustment))
+                .verifyTrendedPricePerBasis(Object.values(testData.comparableAdjustment), testData.basis);
 
-        cy.stepInfo(`[QA-4109] -> 'Cumulative Price Per SF' is displayed in bold`);
-        Sales.AdjustComps.Actions.checkCumulativePriceName("SF");
+            cy.stepInfo(`[QA-4109] -> 'Cumulative Price Per SF' is displayed in bold`);
+            Sales.AdjustComps.Actions.checkCumulativePriceName("SF");
 
-        cy.stepInfo(`[QA-4109] -> Generate and download this report `);
-        NavigationSection.Actions.openReviewAndExport();
-        ReviewExport.Actions.generateDocxReport().downloadAndConvertDocxReport(testData.reportCreationData.reportNumber);
+            cy.stepInfo(`[QA-4109] -> Generate and download this report `);
+            NavigationSection.Actions.openReviewAndExport();
+            ReviewExport.Actions.generateDocxReport()
+                .downloadAndConvertDocxReport(testData.reportCreationData.reportNumber);
         
-        deleteReport(testData.reportCreationData.reportNumber);
-    });
+            deleteReport(testData.reportCreationData.reportNumber);
+        });
 
-    it("Check export", () => {
-        cy.stepInfo(`
-        [QA-4109] → open Sales Adjustment Grid 
-        → verify the 'Cumulative Price Per SF:' label and the same calculations`);
-
-        Cypress.config().baseUrl = null;
-        cy.task("getFilePath", { _reportName: testData.reportCreationData.reportNumber, _docx_html: "html" }).then(file => {
-            cy.log(<string>file);
-            cy.visit(<string>file);
-            cy.readFile(`./cypress/spec_data/${Cypress.spec.name}.txt`).then(text => {
-                cy.contains("Cumulative Price Per SF")
-                .parent().parent().parent()
-                .scrollIntoView().find("td").last()
-                .should("have.text", text);
-              });
-        }); 
+        it("Check export", () => {
+            Cypress.config().baseUrl = null;
+            cy.task("getFilePath", { reportName: testData.reportCreationData.reportNumber, docxHtml: "html" })
+                .then(file => {
+                    cy.log(<string>file);
+                    cy.visit(<string>file);
+                    cy.stepInfo(`[QA-4109] → open Sales Adjustment Grid → 
+                    verify the 'Cumulative Price Per SF:' label and the same calculations`);
+                    cy.readFile(`./cypress/spec_data/${Cypress.spec.name}.txt`).then(text => {
+                        cy.contains("Cumulative Price Per SF")
+                            .parent().parent().parent()
+                            .scrollIntoView().find("td").last()
+                            .should("have.text", text);
+                    });
+                }); 
+        });
     });
-});
