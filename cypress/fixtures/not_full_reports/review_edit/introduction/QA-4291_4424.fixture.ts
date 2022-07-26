fde21131e9e9e2f7b29d97cede53f4fcb52b0652
Nikita Radzkov <nikita.radzkou@gmail.com>
2022-07-26 19:56:10 +0300

    [QA-6364] Done QA-4424

---
+++ cypress/fixtures/not_full_reports/review_edit/introduction/QA-4291_4424.fixture.ts
@@ -0,0 +1,7 @@
+import ReportDataCreator from "../../../data_creator/reportData.creator";
+
+export default {
+    reportCreationData: ReportDataCreator.getReportData("4291_4424"),
+    typeValue: "Test values",
+    backLinkName: "Identification of the Client"
+};
---
+++ cypress/integration/not_full_reports/preview_edit/introduction/QA-4291_4424.spec.ts
@@ -0,0 +1,45 @@
+import { _NavigationSection } from '../../../../actions/base/index';
+import { createReport } from '../../../../actions/base/baseTest.actions';
+import testData from "../../../../fixtures/not_full_reports/review_edit/introduction/QA-4291_4424.fixture";
+import { PreviewEdit, Report } from '../../../../actions';
+import mapKeysUtils from '../../../../utils/mapKeys.utils';
+
+describe('[QA-4291] Check the Introduction page', 
+    { tags:[ "@preview_edit", "@introduction" ] }, () => {
+        
+        before("Login, create report", () => {
+            cy.stepInfo(`1. Create a report`);
+            createReport(testData.reportCreationData);
+        });
+
+        it("Test body", () => {
+            cy.stepInfo("2. Navigate to Report > Client and Edit Identification of the Client");
+            _NavigationSection.navigateToClientPage()
+                .Page.formEditBtn(1).click();
+
+            Report._Client.enterIdentificationOfTheClientTextBox(testData.typeValue)
+                .Page.formSaveBtn().click();
+            Report._Client.Page.formEditBtn().click();
+            Report._Client.enterIntendedUserTextBox(testData.typeValue)
+                .Page.formCancelButton().click();
+           
+
+            cy.stepInfo("3. Proceed to the Introduction page and verify that page exist");
+            _NavigationSection.navigateToIntroduction()
+                .verifyProgressBarNotExist()
+                .Page.introduction.should("exist");
+
+            cy.stepInfo("4. Verify 'Identification of the Client' exist and edited");
+            PreviewEdit._Introduction.Page.SwitchEditBtn.click();
+            
+            PreviewEdit._Introduction.Page.ChipModified.should("be.visible");
+            PreviewEdit._Introduction.Page.getBackLink(testData.backLinkName)
+                .should("be.visible")
+                .invoke("attr", "href")
+                .then(href => {
+                    cy._mapGet(mapKeysUtils.reportId).then(val => {
+                        expect(href).includes(val);
+                    });
+                });
+        });
+    });
