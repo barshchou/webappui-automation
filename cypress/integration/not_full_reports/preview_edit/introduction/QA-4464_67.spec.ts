bfb9162604f319b2c40d6a0da8e0f82ebaba6d9d
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-08-01 17:27:52 +0300

    [QA-6376] Done QA-4467

--- cypress/integration/not_full_reports/preview_edit/introduction/QA-4464_67.spec.ts
+++ cypress/integration/not_full_reports/preview_edit/introduction/QA-4464_67.spec.ts
@@ -1,4 +1,4 @@
-import testData from "../../../../fixtures/not_full_reports/review_edit/introduction/QA-4464.fixture";
+import testData from "../../../../fixtures/not_full_reports/review_edit/introduction/QA-4464_67.fixture";
 import { PreviewEdit, Final } from "../../../../actions";
 import { _NavigationSection } from "../../../../actions/base";
 import { createReport } from "../../../../actions/base/baseTest.actions";
@@ -18,10 +18,10 @@ describe('Verify the "Property Rights Appraised" commentary on the Introduction 
             PreviewEdit._Introduction.Page.SwitchEditBtn.click();
         });
 
-        it("Test body", () => {
+        it("[QA-4464]", () => {
             cy.stepInfo(`3. Scope text and a bulleted list of the scope items as 
-                set on the Final > Scope page and verify from link page`);
-            PreviewEdit._Introduction.Page.scopeOfTheAppraisalListItem.then($li => {
+                        set on the Final > Scope page and verify from link page`);
+            PreviewEdit._Introduction.Page.getUlTextListItem(testData.scopeOfTheAppraisal).then($li => {
                 // Add text from li in array and verify with another text array
                 const liText = $li.toArray().map(el => el.innerText);
 
@@ -29,6 +29,23 @@ describe('Verify the "Property Rights Appraised" commentary on the Introduction 
                     .verifyProgressBarNotExist();
 
                 Final._Scope.Page.itemsTextAreas.then($textarea => {
+                    const textareaText = $textarea.toArray().map(el => el.innerHTML);
+                    expect(textareaText).to.deep.eq(liText);
+                });
+            });
+        });
+
+        it("[QA-4467]", () => {
+            cy.stepInfo(`3.Technical Expertise list copies the technical expertise items as set on the 
+                        Final > Assumptions & Conditions page and verify from link page`);
+            PreviewEdit._Introduction.Page.getUlTextListItem(testData.generalAssumptionsDiscussion).then($li => {
+                // Add text from li in array and verify with another text array
+                const liText = $li.toArray().map(el => el.innerText);
+
+                _NavigationSection.navigateToAssumptionsConditions()
+                    .verifyProgressBarNotExist();
+
+                Final._AssumptionsConditions.Page.itemsGeneralAssumptionsTextarea.then($textarea => {
                     const textareaText = $textarea.toArray().map(el => el.innerHTML);
                     expect(textareaText).to.deep.eq(liText);
                 });
