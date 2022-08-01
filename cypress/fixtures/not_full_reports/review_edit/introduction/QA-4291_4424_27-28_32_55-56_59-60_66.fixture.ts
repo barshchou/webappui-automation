81abb7b0383b72baad85ec5c2b26492ab20c5da6
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-08-01 11:20:37 +0300

    [QA-6375] Done QA-4466 and refactor another tests

--- cypress/fixtures/not_full_reports/review_edit/introduction/QA-4291_4424_27-28_32_55-56_59-60_66.fixture.ts
+++ cypress/fixtures/not_full_reports/review_edit/introduction/QA-4291_4424_27-28_32_55-56_59-60_66.fixture.ts
@@ -1,5 +1,17 @@
 import ReportDataCreator from "../../../data_creator/reportData.creator";
 
+const _backLinkNames = {
+    identificationOfClient: "Identification of the Client", 
+    intendedUser: "Intended Use & User", 
+    definitionOfMarketValue: "Definition of Market Value",
+    generalAssumptionsDiscussion: "General Assumptions Discussion",
+    recentSalesHistoryDiscussion: "Recent Sales History Discussion",
+    propertyContractHistoryDiscussion: "Property Contract History Discussion",
+    purposeAndDateOfValue: "Purpose & Date of Value",
+    exposureTime: "Exposure Time",
+    marketingTime: "Marketing Time"
+};
+
 export default {
     reportCreationData: ReportDataCreator.getReportData("4291_4424_27-28_32_55-56_59-60"),
     typeValue: "Test values",
@@ -13,13 +25,43 @@ export default {
         exposureTimeDescription: "Exposure Time Description",
         marketingTimeDescription: "Marketing Time Description"
     },
-    backLinkNames: [ 
-        "Identification of the Client", 
-        "Intended Use & User", 
-        "Definition of Market Value",
-        "General Assumptions Discussion",
-        "Property Contract History Discussion",
-        "Purpose & Date of Value",
-        "Exposure Time" 
-    ],
+    backLinkNames: _backLinkNames,
+    marketGoToBackLink: {
+        pageName: "Property Market",
+        whereTo: _backLinkNames.marketingTime,  
+    },
+    backLinkAndPageNames: [
+        {
+            pageName: "Client",
+            whereTo: _backLinkNames.identificationOfClient,  
+        },
+        {
+            pageName: "Client",
+            whereTo: _backLinkNames.intendedUser,  
+        },
+        {
+            pageName: "Key Info",
+            whereTo: _backLinkNames.definitionOfMarketValue,  
+        },
+        {
+            pageName: "Assumptions/Conditions",
+            whereTo: _backLinkNames.generalAssumptionsDiscussion,  
+        },
+        {
+            pageName: "Property History",
+            whereTo: _backLinkNames.recentSalesHistoryDiscussion,  
+        },
+        {
+            pageName: "Property History",
+            whereTo: _backLinkNames.propertyContractHistoryDiscussion,  
+        },
+        {
+            pageName: "Cap Rate Conclusion",
+            whereTo: _backLinkNames.purposeAndDateOfValue,  
+        },
+        {
+            pageName: "Property Market",
+            whereTo: _backLinkNames.exposureTime,  
+        },
+    ]
 };
--- cypress/integration/not_full_reports/preview_edit/introduction/QA-4291_4424_27-28_32_55-56_59-60_66.spec.ts
+++ cypress/integration/not_full_reports/preview_edit/introduction/QA-4291_4424_27-28_32_55-56_59-60_66.spec.ts
@@ -6,32 +6,122 @@ import { PreviewEdit, Report, Property } from '../../../../actions';
 import mapKeysUtils from '../../../../utils/mapKeys.utils';
 
 describe('[QA-4291_4424_27-28_32_55-56_59] Check the Introduction page',
-    { tags: [ "@preview_edit", "@introduction" ] }, () => {
+    { tags: ["@preview_edit", "@introduction"] }, () => {
 
-        before("Login, create report", () => {
+        beforeEach("Login, create report", () => {
             cy.stepInfo(`1. Create a report`);
             createReport(testData.reportCreationData);
         });
 
-        it("Test body", () => {
+        it("[QA-4291]", () => {
+            cy.stepInfo("2. Proceed to the Introduction page and verify that page exist");
+            _NavigationSection.navigateToIntroduction()
+                .verifyProgressBarNotExist()
+                .Page.introduction.should("exist");
+
+            cy.stepInfo("3. Verify 'Edit' button exist");
+            PreviewEdit._Introduction.Page.SwitchEditBtn.click();
+        });
+
+        it("[QA-4424]", () => {
             cy.stepInfo("2. Navigate to Report > Client and Edit Identification of the Client and Intended User Text");
             _NavigationSection.navigateToClientPage()
                 .clickFormEditBtn(1)
                 .enterFormCommentTextBox(testData.textBoxNames.identificationOfClient, testData.typeValue)
-                .clickFormSaveBtn()
+                .clickFormSaveBtn();
+            // Need wait for changes to take place.
+            cy.wait(1000);
+
+            cy.stepInfo("3. Proceed to the Introduction page and verify that page exist");
+            _NavigationSection.navigateToIntroduction()
+                .verifyProgressBarNotExist()
+                .Page.introduction.should("exist");
+
+            cy.stepInfo("4. Verify 'Edit' button exist");
+            PreviewEdit._Introduction.Page.SwitchEditBtn.click();
+
+            cy.stepInfo("5. Verify Modified textBox");
+            PreviewEdit._Introduction.Page
+                .getChipModified(testData.backLinkNames.identificationOfClient).should("be.visible");
+            PreviewEdit._Introduction.Page
+                .formCommentTextBox(testData.backLinkNames.identificationOfClient)
+                .should("include.text", testData.typeValue);
+            PreviewEdit._Introduction.Page.getBackLink(testData.backLinkNames.identificationOfClient)
+                .should("be.visible")
+                .invoke("attr", "href")
+                .then(href => {
+                    cy._mapGet(mapKeysUtils.reportId).then(val => {
+                        expect(href).includes(val);
+                    });
+                });
+        });
+
+        it("[QA-4427]", () => {
+            cy.stepInfo("2. Navigate to Report > Client and Edit Intended User");
+            _NavigationSection.navigateToClientPage()
                 .clickFormEditBtn()
                 .enterFormCommentTextBox(testData.textBoxNames.intendedUser, testData.typeValue)
                 .clickFormSaveBtn();
             // Need wait for changes to take place.
             cy.wait(1000);
 
-            cy.stepInfo("3. Navigate to Report > Key Info and Edit Definition of Market Value");
+            cy.stepInfo("3. Proceed to the Introduction page and verify that page exist");
+            _NavigationSection.navigateToIntroduction()
+                .verifyProgressBarNotExist()
+                .Page.introduction.should("exist");
+
+            cy.stepInfo("4. Verify 'Edit' button exist");
+            PreviewEdit._Introduction.Page.SwitchEditBtn.click();
+
+            cy.stepInfo("5. Verify Modified textBox");
+            PreviewEdit._Introduction.Page
+                .getChipModified(testData.backLinkNames.intendedUser).should("be.visible");
+            PreviewEdit._Introduction.Page
+                .formCommentTextBox(testData.backLinkNames.intendedUser)
+                .should("include.text", testData.typeValue);
+            PreviewEdit._Introduction.Page.getBackLink(testData.backLinkNames.intendedUser)
+                .should("be.visible")
+                .invoke("attr", "href")
+                .then(href => {
+                    cy._mapGet(mapKeysUtils.reportId).then(val => {
+                        expect(href).includes(val);
+                    });
+                });
+        });
+
+        it("[QA-4428]", () => {
+            cy.stepInfo("2. Navigate to Report > Key Info and Edit Definition of Market Value");
             _NavigationSection.navigateToReportInformation();
 
             Report._KeyInfo.enterDefinitionMarketValue(testData.typeValue);
             cy.wait(1000);
 
-            cy.stepInfo(`4. Navigate to Final > Assumptions and Conditions page and 
+            cy.stepInfo("3. Proceed to the Introduction page and verify that page exist");
+            _NavigationSection.navigateToIntroduction()
+                .verifyProgressBarNotExist()
+                .Page.introduction.should("exist");
+
+            cy.stepInfo("4. Verify 'Edit' button exist");
+            PreviewEdit._Introduction.Page.SwitchEditBtn.click();
+
+            cy.stepInfo("5. Verify Modified textBox");
+            PreviewEdit._Introduction.Page
+                .getChipModified(testData.backLinkNames.definitionOfMarketValue).should("be.visible");
+            PreviewEdit._Introduction.Page
+                .formCommentTextBox(testData.backLinkNames.definitionOfMarketValue)
+                .should("include.text", testData.typeValue);
+            PreviewEdit._Introduction.Page.getBackLink(testData.backLinkNames.definitionOfMarketValue)
+                .should("be.visible")
+                .invoke("attr", "href")
+                .then(href => {
+                    cy._mapGet(mapKeysUtils.reportId).then(val => {
+                        expect(href).includes(val);
+                    });
+                });
+        });
+
+        it("[QA-4432]", () => {
+            cy.stepInfo(`2. Navigate to Final > Assumptions and Conditions page and 
                         Edit General Assumptions Discussion`);
             _NavigationSection.navigateToAssumptionsConditions()
                 .clickFormEditBtn()
@@ -39,36 +129,161 @@ describe('[QA-4291_4424_27-28_32_55-56_59] Check the Introduction page',
                 .clickFormSaveBtn();
             cy.wait(1000);
 
-            cy.stepInfo("5. Navigate to Property > Property History and Edit Recent Sales History Discussion");
+            cy.stepInfo("3. Proceed to the Introduction page and verify that page exist");
+            _NavigationSection.navigateToIntroduction()
+                .verifyProgressBarNotExist()
+                .Page.introduction.should("exist");
+
+            cy.stepInfo("4. Verify 'Edit' button exist");
+            PreviewEdit._Introduction.Page.SwitchEditBtn.click();
+
+            cy.stepInfo("5. Verify Modified textBox");
+            PreviewEdit._Introduction.Page
+                .getChipModified(testData.backLinkNames.generalAssumptionsDiscussion).should("be.visible");
+            PreviewEdit._Introduction.Page
+                .formCommentTextBox(testData.backLinkNames.generalAssumptionsDiscussion)
+                .should("include.text", testData.typeValue);
+            PreviewEdit._Introduction.Page.getBackLink(testData.backLinkNames.generalAssumptionsDiscussion)
+                .should("be.visible")
+                .invoke("attr", "href")
+                .then(href => {
+                    cy._mapGet(mapKeysUtils.reportId).then(val => {
+                        expect(href).includes(val);
+                    });
+                });
+        });
+
+        it("[QA-4455]", () => {
+            cy.stepInfo("2. Navigate to Property > Property History and Edit Recent Sales History Discussion");
             _NavigationSection.navigateToPropertyHistory()
                 .clickFormEditBtn()
                 .enterFormCommentTextBox(testData.textBoxNames.recentSalesHistoryDiscussion, testData.typeValue)
                 .clickFormSaveBtn();
             cy.wait(1000);
 
-            cy.stepInfo("6. Navigate to Property > Property History and Edit Property Contract History Discussion");
+            cy.stepInfo("3. Proceed to the Introduction page and verify that page exist");
+            _NavigationSection.navigateToIntroduction()
+                .verifyProgressBarNotExist()
+                .Page.introduction.should("exist");
+
+            cy.stepInfo("4. Verify 'Edit' button exist");
+            PreviewEdit._Introduction.Page.SwitchEditBtn.click();
+
+            cy.stepInfo("5. Verify Modified textBox");
+            PreviewEdit._Introduction.Page
+                .getChipModified(testData.backLinkNames.recentSalesHistoryDiscussion).should("be.visible");
+            PreviewEdit._Introduction.Page
+                .formCommentTextBox(testData.backLinkNames.recentSalesHistoryDiscussion)
+                .should("include.text", testData.typeValue);
+            PreviewEdit._Introduction.Page.getBackLink(testData.backLinkNames.recentSalesHistoryDiscussion)
+                .should("be.visible")
+                .invoke("attr", "href")
+                .then(href => {
+                    cy._mapGet(mapKeysUtils.reportId).then(val => {
+                        expect(href).includes(val);
+                    });
+                });
+        });
+
+        it("[QA-4456]", () => {
+            cy.stepInfo("2. Navigate to Property > Property History and Edit Property Contract History Discussion");
             _NavigationSection.navigateToPropertyHistory()
                 .clickFormEditBtn(1)
                 .enterFormCommentTextBox(testData.textBoxNames.propertyContractHistoryDiscussion, testData.typeValue)
                 .clickFormSaveBtn();
             cy.wait(1000);
 
-            cy.stepInfo("7. Navigate to Income > Cap Rate Conclusion and Edit Purpose & Date of Value Discussion");
+            cy.stepInfo("3. Proceed to the Introduction page and verify that page exist");
+            _NavigationSection.navigateToIntroduction()
+                .verifyProgressBarNotExist()
+                .Page.introduction.should("exist");
+
+            cy.stepInfo("4. Verify 'Edit' button exist");
+            PreviewEdit._Introduction.Page.SwitchEditBtn.click();
+
+            cy.stepInfo("5. Verify Modified textBox");
+            PreviewEdit._Introduction.Page
+                .getChipModified(testData.backLinkNames.propertyContractHistoryDiscussion).should("be.visible");
+            PreviewEdit._Introduction.Page
+                .formCommentTextBox(testData.backLinkNames.propertyContractHistoryDiscussion)
+                .should("include.text", testData.typeValue);
+            PreviewEdit._Introduction.Page.getBackLink(testData.backLinkNames.propertyContractHistoryDiscussion)
+                .should("be.visible")
+                .invoke("attr", "href")
+                .then(href => {
+                    cy._mapGet(mapKeysUtils.reportId).then(val => {
+                        expect(href).includes(val);
+                    });
+                });
+        });
+
+        it("[QA-4459]", () => {
+            cy.stepInfo("2. Navigate to Income > Cap Rate Conclusion and Edit Purpose & Date of Value Discussion");
             _NavigationSection.navigateToCapRateConclusion()
                 .clickFormEditBtn()
                 .enterFormCommentTextBox(testData.textBoxNames.purposeDateOfValueDiscussion, testData.typeValue)
                 .clickFormSaveBtn();
             cy.wait(1000);
 
-            cy.stepInfo("8. Navigate to Property > Market and Edit Exposure Time Description");
+            cy.stepInfo("3. Proceed to the Introduction page and verify that page exist");
+            _NavigationSection.navigateToIntroduction()
+                .verifyProgressBarNotExist()
+                .Page.introduction.should("exist");
+
+            cy.stepInfo("4. Verify 'Edit' button exist");
+            PreviewEdit._Introduction.Page.SwitchEditBtn.click();
+
+            cy.stepInfo("5. Verify Modified textBox");
+            PreviewEdit._Introduction.Page
+                .getChipModified(testData.backLinkNames.purposeAndDateOfValue).should("be.visible");
+            PreviewEdit._Introduction.Page
+                .formCommentTextBox(testData.backLinkNames.purposeAndDateOfValue)
+                .should("include.text", testData.typeValue);
+            PreviewEdit._Introduction.Page.getBackLink(testData.backLinkNames.purposeAndDateOfValue)
+                .should("be.visible")
+                .invoke("attr", "href")
+                .then(href => {
+                    cy._mapGet(mapKeysUtils.reportId).then(val => {
+                        expect(href).includes(val);
+                    });
+                });
+        });
+
+        it("[QA-4460]", () => {
+            cy.stepInfo("2. Navigate to Property > Market and Edit Exposure Time Description");
             _NavigationSection.navigateToPropertyMarket()
                 .clickFormEditBtn()
                 .enterFormCommentTextBox(testData.textBoxNames.exposureTimeDescription, testData.typeValue)
                 .clickFormSaveBtn();
             cy.wait(1000);
 
-            cy.stepInfo(`9. Navigate to Property > Market, check Include Marketing Time in the report
-                        and Edit Marketing Time Description`);
+            cy.stepInfo("3. Proceed to the Introduction page and verify that page exist");
+            _NavigationSection.navigateToIntroduction()
+                .verifyProgressBarNotExist()
+                .Page.introduction.should("exist");
+
+            cy.stepInfo("4. Verify 'Edit' button exist");
+            PreviewEdit._Introduction.Page.SwitchEditBtn.click();
+
+            cy.stepInfo("5. Verify Modified textBox");
+            PreviewEdit._Introduction.Page
+                .getChipModified(testData.backLinkNames.exposureTime).should("be.visible");
+            PreviewEdit._Introduction.Page
+                .formCommentTextBox(testData.backLinkNames.exposureTime)
+                .should("include.text", testData.typeValue);
+            PreviewEdit._Introduction.Page.getBackLink(testData.backLinkNames.exposureTime)
+                .should("be.visible")
+                .invoke("attr", "href")
+                .then(href => {
+                    cy._mapGet(mapKeysUtils.reportId).then(val => {
+                        expect(href).includes(val);
+                    });
+                });
+        });
+
+        it("[QA-4466]", () => {
+            cy.stepInfo(`2. Navigate to Property > Market, check Include Marketing Time in the report
+            and Edit Marketing Time Description`);
             _NavigationSection.navigateToPropertyMarket();
 
             Property._Market.checkIncludeMarketingTimeDescription()
@@ -77,26 +292,46 @@ describe('[QA-4291_4424_27-28_32_55-56_59] Check the Introduction page',
                 .clickFormSaveBtn();
             cy.wait(1000);
 
-
-            cy.stepInfo("10. Proceed to the Introduction page and verify that page exist");
+            cy.stepInfo("3. Proceed to the Introduction page and verify that page exist");
             _NavigationSection.navigateToIntroduction()
                 .verifyProgressBarNotExist()
                 .Page.introduction.should("exist");
 
-            cy.stepInfo("11. Verify 'Identification of the Client' exist and edited");
+            cy.stepInfo("4. Verify 'Edit' button exist");
             PreviewEdit._Introduction.Page.SwitchEditBtn.click();
 
-            testData.backLinkNames.forEach(name => {
-                PreviewEdit._Introduction.Page.getChipModified(name).should("be.visible");
-                PreviewEdit._Introduction.Page.formCommentTextBox(name).should("include.text", testData.typeValue);
-                PreviewEdit._Introduction.Page.getBackLink(name)
-                    .should("be.visible")
-                    .invoke("attr", "href")
-                    .then(href => {
-                        cy._mapGet(mapKeysUtils.reportId).then(val => {
-                            expect(href).includes(val);
-                        });
+            cy.stepInfo("5. Verify Modified textBox");
+            PreviewEdit._Introduction.Page
+                .getChipModified(testData.backLinkNames.marketingTime).should("be.visible");
+            PreviewEdit._Introduction.Page
+                .formCommentTextBox(testData.backLinkNames.marketingTime)
+                .should("include.text", testData.typeValue);
+            PreviewEdit._Introduction.Page.getBackLink(testData.backLinkNames.marketingTime)
+                .should("be.visible")
+                .invoke("attr", "href")
+                .then(href => {
+                    cy._mapGet(mapKeysUtils.reportId).then(val => {
+                        expect(href).includes(val);
                     });
+                });
+            
+            cy.stepInfo("6. Go to back page and verify that opened page");
+            PreviewEdit._Introduction.goToBackLink(
+                testData.marketGoToBackLink.whereTo, testData.marketGoToBackLink.pageName);
+        });
+
+        it("Go to Back link", () => {
+            testData.backLinkAndPageNames.forEach((elem, index) => {
+                cy.stepInfo("2. Proceed to the Introduction page and verify that page exist");
+                _NavigationSection.navigateToIntroduction()
+                    .verifyProgressBarNotExist()
+                    .Page.introduction.should("exist");
+
+                cy.stepInfo("3. Verify 'Edit' button exist");
+                PreviewEdit._Introduction.Page.SwitchEditBtn.click();
+
+                cy.stepInfo(`4.${index} Go to back page and verify that opened page`);
+                PreviewEdit._Introduction.goToBackLink(elem.whereTo, elem.pageName);
             });
         });
     });
