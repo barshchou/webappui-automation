7bad0595ed6429c11d4aeffc931b78b7061a4cc6
Ernst-D <ernst.dzerevanko@gmail.com>
2022-08-02 01:38:09 +0400

    [QA-5473] refactored QA-5394 spec

--- cypress/integration/comp_plex/sales/find_comps/QA-5394.spec.ts
+++ cypress/integration/comp_plex/sales/find_comps/QA-5394.spec.ts
@@ -1,5 +1,5 @@
 import { Sales } from "../../../../actions";
-import { salesInterceptions } from "../../../../actions/base/baseTest.actions";
+import { navigateToCompplex } from "../../../../actions/base/baseTest.actions";
 import { Alias } from "../../../../utils/alias.utils";
 import testData from "../../../../fixtures/comp_plex/sales/find_comps/QA-5394.fixture";
 
@@ -8,27 +8,24 @@ describe(`[QA-5394] -> [Sales > Find Comps > Create Comp >
     Enter Property Information form] Verify the Site Area* field`, { tags:[ "@comp_plex_standalone" ] },  () => {
 
     beforeEach(() => {
-        salesInterceptions();
-        cy.visit("/index.html");
+        navigateToCompplex();
     });
 
     it("[QA-5394] -> Verify the Site Area* field", () => {
-        cy.wait(`@${Alias.gql.SearchSalesTransactions}`);
-        cy.log("test");
-        cy.stepInfo(`1. Something`);
+        cy.stepInfo(`1. Creating a new comp by clicking "Create comp" btn`);
         cy.get('[id="createComp"]').should("be.visible").click();
-
-        Sales._FindComps.Page.newCompContinueButton.click();
-        
-        cy.log(<any>Object.values(testData.spec5394));
-        
         Sales._FindComps.Page.siteAreaNewComp;
         let _alias = Alias.pageElements.compPlex.siteAreaNewComp;
+
+        cy.stepInfo(`2. Verifying Site Area* field with SF option`);
         Object.values(testData.spec5394).forEach(n => {
             Sales._FindComps.Actions.enterNumericInputNewComp(_alias, n).clearNumericInputNewComp(_alias);
         });
-        cy.get('[title="SF"]', { includeShadowDom:true }).click();
-        cy.get('[title="Acre"]', { includeShadowDom:true }).click();
+        cy.stepInfo(`3. Switching Site Area* field to Acre option`);
+        Sales._FindComps.Actions
+            .selectDropdownOptionNewComp( cy.get('[title="SF"]', { includeShadowDom:true }), "Acre");
+        
+        cy.stepInfo(`4. Verifying Site Area* field with Acre option`);
         Object.values(testData.spec5394).forEach(n => {
             Sales._FindComps.Actions.enterNumericInputNewComp(_alias, n).clearNumericInputNewComp(_alias);
         });
