95a2e60eb64cfed44050a3b9619c94ad03c93dff
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-08-02 17:40:42 +0300

    includeShadowDom deleted

--- cypress/pages/sales/comp_plex/salesInfo.page.ts
+++ cypress/pages/sales/comp_plex/salesInfo.page.ts
@@ -1,32 +1,31 @@
 export default class SaleInformationForm {
     get LinkTypeInput() {
-        return cy.get('[data-qa="Link Type"]', { includeShadowDom: true });
+        return cy.get('[data-qa="Link Type"]');
     }
 
     get LinkTypeList() {
-        return cy.get('[class="rc-virtual-list"]', { includeShadowDom: true });
+        return cy.get('[class="rc-virtual-list"]');
     }
 
     getLinkTypeListOption(optionName = "Bowery Subject") {
         return cy.get(`[data-qa="Link Type"] [title="${optionName}"]`);
     }
 
-    get SaleDateCalendarNewComp() { return cy.get('[data-qa="Sale Date"]', { includeShadowDom: true }); }
+    get SaleDateCalendarNewComp() { return cy.get('[data-qa="Sale Date"]'); }
 
     get SaleDateToday() {
-        return cy.get('[class="ant-picker-today-btn"]', { includeShadowDom: true });
+        return cy.get('[class="ant-picker-today-btn"]');
     }
 
     get DropdownDatePicker() {
-        return cy.get('[class="ant-picker-dropdown ant-picker-dropdown-placement-bottomLeft "]',
-            { includeShadowDom: true });
+        return cy.get('[class="ant-picker-dropdown ant-picker-dropdown-placement-bottomLeft "]');
     }
 
-    get BuyerGranteeNewComp() { return cy.get('[data-qa="Buyer (Grantee)"]', { includeShadowDom: true }); }
+    get BuyerGranteeNewComp() { return cy.get('[data-qa="Buyer (Grantee)"]'); }
 
-    get SellerGrantor() { return cy.get('[data-qa="Seller (Grantor)"]', { includeShadowDom: true }); }
+    get SellerGrantor() { return cy.get('[data-qa="Seller (Grantor)"]'); }
 
     get SaleStatusDropdown() {
-        return cy.get('[data-qa="Sale Status"]', { includeShadowDom: true });
+        return cy.get('[data-qa="Sale Status"]');
     }
 }
