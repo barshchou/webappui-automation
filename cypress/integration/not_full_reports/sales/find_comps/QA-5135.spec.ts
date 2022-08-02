dffade44da00023e7ee05603c9201061edb7023b
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-08-02 13:27:22 +0300

    5135 fixed

--- cypress/integration/not_full_reports/sales/find_comps/QA-5135.spec.ts
+++ cypress/integration/not_full_reports/sales/find_comps/QA-5135.spec.ts
@@ -12,6 +12,8 @@ describe(`[QA-5135] -> [Sales > Find Comps] Check the comps order when "custom" 
         it("Test body", () => {
             cy.stepInfo(`1. [QA-5135] -> User navigates to SalesComps Search page `);
             _NavigationSection.navigateToFindComps();
+            Sales._FindComps.Actions.selectedCompsSetSort("Custom")
+                .Page.sortSalesCompsSelectValue.should('contain', "Custom");
         
             cy.stepInfo(`2. [QA-5135] -> User selects n-first comps from map`);
             testData.compsToAdd.forEach(() => {
@@ -23,7 +25,7 @@ describe(`[QA-5135] -> [Sales > Find Comps] Check the comps order when "custom" 
         
             cy.stepInfo(`4. [QA-5135] -> Order doesn't change upon selection of “custom“ choice, 
                     until the appraiser manually moves comps around`);
-            Sales._FindComps.Actions.selectedCompsSetSort("Custom").checkSalesCompAddedToList()
+            Sales._FindComps.Actions.checkSalesCompAddedToList()
                 .moveComparableByDnD(Sales._FindComps.Page.selectorDraggableElement, 0, "down", 2)
                 .checkSalesCompAddedToList({ reverse: true });
 
@@ -31,7 +33,7 @@ describe(`[QA-5135] -> [Sales > Find Comps] Check the comps order when "custom" 
             re-ordered comps and after that selected “Date Sold” sorting -> comps get 
             reordered by sale date on the fly.`);
 
-            Sales._FindComps.Actions.selectedCompsSetSort("Date Sold").checkSalesCompAddedToList()
+            Sales._FindComps.Actions.selectedCompsSetSort("Date Sold")
                 .checkSalesCompSortedByDateSold();
         });
     });
