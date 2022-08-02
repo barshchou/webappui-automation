e6b13b0e5a17ea12c66b947169d878712af9f476
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-08-02 09:19:02 +0300

    [QA-6407] Created QA-4281

---
+++ cypress/integration/not_full_reports/final/swotAnalysis/QA-4281.spec.ts
@@ -0,0 +1,24 @@
+import testData from "../../../../fixtures/not_full_reports/final/swotAnalysis/QA-4281.fixture";
+import { createReport } from "../../../../actions/base/baseTest.actions";
+import { _NavigationSection } from "../../../../actions/base";
+
+
+describe("Verify the text in the Opportunities section on the SWOT Analysis page", 
+    { tags:[ "@final", "@swot_analysis", ] }, () => {
+        beforeEach("Login, create report", () => {
+            createReport(testData.reportCreationData);
+        });
+
+        it("[QA-4281]", () => {
+            cy.stepInfo("1. Navigate to Final > Swot Analysis page and verify the text in the Opportunities section");
+            _NavigationSection.navigateToFindComps();
+
+            cy.stepInfo(`2. The text in the Opportunities section is the following:
+                        - All units are rent stabilized, and there is potential upside upon tenant turnover.
+                        - There has been increased demand for similar assets as investors from prime New York 
+                        Metro submarkets seek higher returns increasing pricing for similar assets.
+            `);
+            
+
+        });
+    });
