e8db01f7fbfab732cf4269e092ec9e81c484a2ac
Viachaslau.Barshchou <Viachaslau.Barshchou@boweryvaluation.com>
2022-08-04 10:52:50 +0300

    [QA-6326] fixed related tests

--- cypress/integration/not_full_reports/sales/value_conclusion/QA-4280.spec.ts
+++ cypress/integration/not_full_reports/sales/value_conclusion/QA-4280.spec.ts
@@ -32,7 +32,8 @@ describe(`Prospective Market Value As Stabilized -> Less Residential Rent Loss
         }
         Income.CapRateConclusion.checkRentLossCheckboxByRow()
             .clickAddButton()
-            .enterAsStabResRentLossTimePeriodByRow(testData.data.rentLossTimePeriod);
+            .enterAsStabResRentLossTimePeriodByRow(testData.data.rentLossTimePeriod, 
+                testData.valueConclusionKeyAsStabilized);
         NavigationSection.navigateToSalesValueConclusion()
             .verifyProgressBarNotExist();
         Sales.ValueConclusion.verifyAsStabResRentLossTimePeriodByRow(testData.data.rentLossTimePeriod);
