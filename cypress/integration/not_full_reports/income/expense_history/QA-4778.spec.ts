7d427061e8410efa5c9674c5893753d7c18a4c23
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-08-05 14:19:09 +0300

    [QA-6416] Added stepInfos

--- cypress/integration/not_full_reports/income/expense_history/QA-4778.spec.ts
+++ cypress/integration/not_full_reports/income/expense_history/QA-4778.spec.ts
@@ -11,22 +11,29 @@ describe("Verify the calculation on the Expense History page", { tags: [ "@incom
     });
 
     it("Test body", () => {
+        cy.stepInfo("Navigate to Income -> Expense History page");
         _NavigationSection.navigateToExpenseHistory();
+
         testData.periods.forEach(period => {
+            cy.stepInfo(`Add '${period.expensePeriodType}' expense period`);
             Income._ExpenseHistory.selectExpensePeriod(period.expensePeriodType)
                 .enterExpenseYear(period.year);
             if (period.month) {
                 Income._ExpenseHistory.enterExpenseMonth(period.month);
             }
             Income._ExpenseHistory.clickAddExpenseYearButton();
+
+            cy.stepInfo("Fill table with data");
             ExpenseHistoryCells.operatingExpensesCellsNamesArray.forEach(cell => {
                 Income._ExpenseHistory.enterIssueByColIndex(period[cell], cell);
             });
+
+            cy.stepInfo(`Verify calculations for Total OP Expenses, TOE excl RET and NET OP Income 
+            for ${period.expensePeriodType} expense period`);
             Income._ExpenseHistory.verifyTotalOpExpensesByColIndex()
                 .verifyTOEExcludingRETByIndex(period.realEstateTaxes, )
                 .verifyNetOpIncomeByIndex(period.grossRevenue)
                 .clickRemoveExpensePeriodButtonBuColIndex();
         });
-        // TODO: Add stepInfos for test
     });
 });
--- cypress/integration/not_full_reports/income/expense_history/QA-4846.spec.ts
+++ cypress/integration/not_full_reports/income/expense_history/QA-4846.spec.ts
@@ -13,41 +13,54 @@ describe("Verify that combined utilities expenses is enabled", { tags: [ "@incom
     it("Test body", () => {
         const combinedElectricityFuel = testData.electricity + testData.fuel;
         const combinedAll = testData.electricity + testData.fuel + testData.waterAndSewer;
+        cy.stepInfo("1. Navigate to Expense History, add any Expense Period");
         _NavigationSection.navigateToExpenseHistory();
         Income._ExpenseHistory.selectExpensePeriod(testData.expensePeriodType)
             .enterExpenseYear(testData.year)
-            .clickAddExpenseYearButton()
-            .enterIssueByColIndex(testData.electricity, Enums.EXPENSE_HISTORY_TABLE_ROWS.electricity)
+            .clickAddExpenseYearButton();
+
+        cy.stepInfo("2. Broken out is selected by default, fill electricity, fuel and waterSewer values");
+        Income._ExpenseHistory.enterIssueByColIndex(testData.electricity, Enums.EXPENSE_HISTORY_TABLE_ROWS.electricity)
             .enterIssueByColIndex(testData.fuel, Enums.EXPENSE_HISTORY_TABLE_ROWS.fuel)
-            .enterIssueByColIndex(testData.waterAndSewer, Enums.EXPENSE_HISTORY_TABLE_ROWS.waterAndSewer)
-            .checkUtilitiesExpensesOption(Enums.UTILITY_EXPENSES.combinedElectricityAndFuel)
+            .enterIssueByColIndex(testData.waterAndSewer, Enums.EXPENSE_HISTORY_TABLE_ROWS.waterAndSewer);
+
+        cy.stepInfo("3. Change from Broken out to Combined electricity and fuel, verify changed raws and cells values");
+        Income._ExpenseHistory.checkUtilitiesExpensesOption(Enums.UTILITY_EXPENSES.combinedElectricityAndFuel)
             .verifyTableStateByUtilityExpensesRadio(Enums.UTILITY_EXPENSES.combinedElectricityAndFuel)
             .verifyIssueTextByColIndex(combinedElectricityFuel, Enums.EXPENSE_HISTORY_TABLE_ROWS.utilities)
-            .verifyIssueTextByColIndex(testData.waterAndSewer, Enums.EXPENSE_HISTORY_TABLE_ROWS.waterAndSewer)
-            .checkUtilitiesExpensesOption(Enums.UTILITY_EXPENSES.combinedAll)
+            .verifyIssueTextByColIndex(testData.waterAndSewer, Enums.EXPENSE_HISTORY_TABLE_ROWS.waterAndSewer);
+
+        cy.stepInfo("4. Change from Combined electricity and fuel to combined all, verify cells values and raws");
+        Income._ExpenseHistory.checkUtilitiesExpensesOption(Enums.UTILITY_EXPENSES.combinedAll)
             .verifyTableStateByUtilityExpensesRadio(Enums.UTILITY_EXPENSES.combinedAll)
-            .verifyIssueTextByColIndex(combinedAll, Enums.EXPENSE_HISTORY_TABLE_ROWS.utilities)
-            .checkUtilitiesExpensesOption(Enums.UTILITY_EXPENSES.combinedElectricityAndFuel)
+            .verifyIssueTextByColIndex(combinedAll, Enums.EXPENSE_HISTORY_TABLE_ROWS.utilities);
+
+        cy.stepInfo("5. Change from Combined all to Combined electricity and fuel, verify cells values");
+        Income._ExpenseHistory.checkUtilitiesExpensesOption(Enums.UTILITY_EXPENSES.combinedElectricityAndFuel)
             .verifyTableStateByUtilityExpensesRadio(Enums.UTILITY_EXPENSES.combinedElectricityAndFuel)
             .verifyIssueTextByColIndex(combinedAll, Enums.EXPENSE_HISTORY_TABLE_ROWS.utilities)
             .verifyIssueTextByColIndex(0, Enums.EXPENSE_HISTORY_TABLE_ROWS.waterAndSewer)
-            .enterIssueByColIndex(testData.waterAndSewer, Enums.EXPENSE_HISTORY_TABLE_ROWS.waterAndSewer)
-            .checkUtilitiesExpensesOption(Enums.UTILITY_EXPENSES.brokenOut)
+            .enterIssueByColIndex(testData.waterAndSewer, Enums.EXPENSE_HISTORY_TABLE_ROWS.waterAndSewer);
+
+        cy.stepInfo("6. Change from Combined electricity and fuel to Broken out, verify cells values");
+        Income._ExpenseHistory.checkUtilitiesExpensesOption(Enums.UTILITY_EXPENSES.brokenOut)
             .verifyTableStateByUtilityExpensesRadio(Enums.UTILITY_EXPENSES.brokenOut)
             .verifyIssueTextByColIndex(combinedAll, Enums.EXPENSE_HISTORY_TABLE_ROWS.electricity)
             .verifyIssueTextByColIndex(0, Enums.EXPENSE_HISTORY_TABLE_ROWS.fuel)
             .verifyIssueTextByColIndex(testData.waterAndSewer, Enums.EXPENSE_HISTORY_TABLE_ROWS.waterAndSewer)
             .enterIssueByColIndex(testData.electricity, Enums.EXPENSE_HISTORY_TABLE_ROWS.electricity)
-            .enterIssueByColIndex(testData.fuel, Enums.EXPENSE_HISTORY_TABLE_ROWS.fuel)
-            .checkUtilitiesExpensesOption(Enums.UTILITY_EXPENSES.combinedAll)
+            .enterIssueByColIndex(testData.fuel, Enums.EXPENSE_HISTORY_TABLE_ROWS.fuel);
+
+        cy.stepInfo("7. Change from Broken out to Combined All, verify cells values");
+        Income._ExpenseHistory.checkUtilitiesExpensesOption(Enums.UTILITY_EXPENSES.combinedAll)
             .verifyTableStateByUtilityExpensesRadio(Enums.UTILITY_EXPENSES.combinedAll)
-            .verifyIssueTextByColIndex(combinedAll, Enums.EXPENSE_HISTORY_TABLE_ROWS.utilities)
-            .checkUtilitiesExpensesOption(Enums.UTILITY_EXPENSES.brokenOut)
+            .verifyIssueTextByColIndex(combinedAll, Enums.EXPENSE_HISTORY_TABLE_ROWS.utilities);
+
+        cy.stepInfo("8. Change from Combined All to Broken out, verify cells values");
+        Income._ExpenseHistory.checkUtilitiesExpensesOption(Enums.UTILITY_EXPENSES.brokenOut)
             .verifyTableStateByUtilityExpensesRadio(Enums.UTILITY_EXPENSES.brokenOut)
             .verifyIssueTextByColIndex(combinedAll, Enums.EXPENSE_HISTORY_TABLE_ROWS.electricity)
             .verifyIssueTextByColIndex(0, Enums.EXPENSE_HISTORY_TABLE_ROWS.fuel)
             .verifyIssueTextByColIndex(0, Enums.EXPENSE_HISTORY_TABLE_ROWS.waterAndSewer);
-        // TODO: Add stepInfos for test
-
     });
 });
