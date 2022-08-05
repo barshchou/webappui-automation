49d3175b2e4d59251d6ede1a21e74587b78d64c6
Mikita Radzkou <Mikita.Radzkou@itechart-group.com>
2022-08-05 11:38:03 +0300

    [QA-6461] Done QA-4968

--- cypress/integration/not_full_reports/sf-integration/report_settings_modal/QA-4965-68.spec.ts
+++ cypress/integration/not_full_reports/sf-integration/report_settings_modal/QA-4965-68.spec.ts
@@ -1,4 +1,4 @@
-import testData from "../../../../fixtures/not_full_reports/sf_integration/report_settings_modal/QA-4965-67.fixture";
+import testData from "../../../../fixtures/not_full_reports/sf_integration/report_settings_modal/QA-4965-68.fixture";
 import { loginAction } from "../../../../actions/base/baseTest.actions";
 import { _HomePage } from "../../../../actions/base";
 
@@ -50,5 +50,19 @@ describe("Verify Freddie Mac pre-fill with the Agency Client Type from SF",
             
             cy.stepInfo("3. Verify the “Freddie Mac” section has the radio button enabled but deselected.");
             _HomePage.Page.templateTypesRadios.should("not.be.checked");
+        });
+
+        it("[QA-4968]", () => {
+            cy.stepInfo("2. Proceed to the WebApp and paste the number or enter manually in the Bowery Job # field");
+            _HomePage.clickNewReportButton()
+                .enterAddressToSearch(testData.address)
+                .clickSubmitButton()
+                .clickToSearchResultRow()
+                .clickSubmitButton()
+                .enterReportNumber(testData.sfJobs.withFreddieMac);
+            
+            cy.stepInfo(`3. Verify the Income Type section has the radio button automatically enabled for 
+                        ‘The subject Property has only residential income.’.`);
+            _HomePage.Page.incomeTypesRadios.eq(0).should("not.be.checked");
         });
     });
