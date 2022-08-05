a25cf4c9c0d5021523c5eb57541141e15eef79b0
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-08-05 11:32:24 +0300

    5162 completed

--- cypress/fixtures/not_full_reports/sales/find_comps/QA-5162.fixture.ts
+++ cypress/fixtures/not_full_reports/sales/find_comps/QA-5162.fixture.ts
@@ -1,6 +1,5 @@
 import ReportDataCreator from "../../../data_creator/reportData.creator";
 import Enums from "../../../../enums/enums";
-import { BoweryReports } from "../../../../types/boweryReports.type";
 import { BoweryAutomation } from "../../../../types/boweryAutomation.type";
 
 const reportCreationData: BoweryAutomation.ReportCreationData = ReportDataCreator.getReportData("5162", {
@@ -12,42 +11,57 @@ const sortSalesCompsDateSold = Enums.SORT_VALUES.dateSold;
 const sortSalesCompsCustom = Enums.SORT_VALUES.custom;
 const salePeriodValue = Enums.SALE_PERIOD_VALUES.lastThreeMonths;
 
+const comparableFixture1 = {
+    address: "116 Cooper Street"
+};
+const comparableFixture2 = {
+    address: "1715 Lexington Avenue"
+};
+const comparableFixture3 = {
+    address: "1074 Fulton Street"
+};
+const comparableFixture4 = {
+    address: "31-83 34 Street"
+};
+const comparableFixture5 = {
+    address: "96 East Broadway"
+};
+const comparableFixture6 = {
+    address: "95 East Broadway"
+};
+const comparableFixture7 = {
+    address: "168 North 10 Street"
+};
+const comparableFixture8 = {
+    address: "151 Freeman Street"
+};
+const comparableFixture9 = {
+    address: "125 North 1 Street"
+};
+
 const arrayOfCompsForAdditionFromMap1 = [
-    {
-        address: "116 Cooper Street"
-    },
-    {
-        address: "1715 Lexington Avenue"
-    },
-    {
-        address: "1074 Fulton Street"
-    },
-    {
-        address: "31-83 34 Street"
-    },
-    {
-        address: "96 East Broadway"
-    },
-    {
-        address: "95 East Broadway"
-    }
+    comparableFixture1,
+    comparableFixture2,
+    comparableFixture3,
+    comparableFixture4,
+    comparableFixture5,
+    comparableFixture6,
 ];
 const arrayOfCompsForAdditionFromMap2 = [
-    {
-        address: "168 North 10 Street"
-    },
-    {
-        address: "151 Freeman Street"
-    },
-    {
-        address: "125 North 1 Street"
-    }
+    comparableFixture7,
+    comparableFixture8,
+    comparableFixture9,
 ];
-
+const arrayOfCompsForRemovingFromMap = [
+    comparableFixture5,
+    comparableFixture6,
+    comparableFixture9
+];
 
 export default {
     arrayOfCompsForAdditionFromMap1,
     arrayOfCompsForAdditionFromMap2,
+    arrayOfCompsForRemovingFromMap,
     reportCreationData,
     sortSalesCompsCustom,
     sortSalesCompsDateSold,
--- cypress/integration/not_full_reports/sales/find_comps/QA-5162.spec.ts
+++ cypress/integration/not_full_reports/sales/find_comps/QA-5162.spec.ts
@@ -4,50 +4,62 @@ import testData from "../../../../fixtures/not_full_reports/sales/find_comps/QA-
 import { createReport } from "../../../../actions/base/baseTest.actions";
 import { salesInterceptions } from "../../../../actions/base/baseTest.actions";
 import { conditionalDescribe } from "../../../checkIsProd.utils";
-/*
- * import mapKeysUtils from "../../../../utils/mapKeys.utils";
- * import Homepage from "../../../../actions/base/homepage.actions";
- */
 
-conditionalDescribe(`[QA-5162] [QA-51] [Sales > Find Comps] "Date Sold" sorting is selected by default 
-                     for sales comps + sorting is applied correctly`,
-{ tags: [ "@sales", "@find_comps", "@comp_plex" ] }, () => {
-    before("Login, create report", () => {
-        createReport(testData.reportCreationData);
-        cy.saveLocalStorage();
-    });
-    beforeEach(() => {
-        cy.restoreLocalStorage();
-        salesInterceptions();
-    });
 
-    it(`[QA-5162] [Sales > Find Comps] "Date Sold" sorting is updated when user adds/removes comps, 
-    changes sorting`, () => {
-        cy.stepInfo(`1.Navigate to Sales > Find Comps page `);
-        _NavigationSection.navigateToFindComps();
-
-        cy.stepInfo(`2. Verify "Date Sold" sorting is selected by default for sales comps`);
-        Sales._FindComps.Page.sortSalesCompsSelectValue.should('contain', testData.sortSalesCompsDateSold);
-
-        cy.stepInfo(`3. Add comps to Selected category??? + Verify that "Date Sold" option in 
-                     Sort dropdown is correctly applied after each comp addition`);
-        Sales._FindComps.resetAllFilters()
-            .selectFilterSalePeriodValue(testData.salePeriodValue);
-        testData.arrayOfCompsForAdditionFromMap1.forEach(comp => {
-            Sales._FindComps.selectCompFromMapByAddress(comp.address);
-            Sales._FindComps.checkSalesCompSortedByDateSold();
-        });    
-        Sales._FindComps.resetAllFilters();
-        testData.arrayOfCompsForAdditionFromMap2.forEach(comp => {
-            Sales._FindComps.selectCompFromMapByAddress(comp.address);
-            Sales._FindComps.checkSalesCompSortedByDateSold();
+conditionalDescribe(`[QA-5162] [Sales > Find Comps] "Date Sold" sorting is updated when user adds/removes comps`,
+    { tags: [ "@sales", "@find_comps", "@comp_plex" ] }, () => {
+        before("Login, create report", () => {
+            createReport(testData.reportCreationData);
+            salesInterceptions();
         });
-        Sales._FindComps.checkSalesCompSortedByDateSold();
 
-        cy.stepInfo(`3. Remove a few comps + Verify that "Date Sold" option in 
+        it(`Test body`, () => {
+            cy.stepInfo(`1.Navigate to Sales > Find Comps page `);
+            _NavigationSection.navigateToFindComps();
+
+            cy.stepInfo(`2. Verify "Date Sold" sorting is selected`);
+            Sales._FindComps.Page.sortSalesCompsSelectValue.should('contain', testData.sortSalesCompsDateSold);
+
+            cy.stepInfo(`3. Add comps to Selected Comps table + Verify that "Date Sold" option in 
+                     Sort dropdown is correctly applied after each comp addition`);
+            Sales._FindComps.resetAllFilters()
+                .selectFilterSalePeriodValue(testData.salePeriodValue);
+            testData.arrayOfCompsForAdditionFromMap1.forEach(comp => {
+                Sales._FindComps.selectCompFromMapByAddress(comp.address)
+                    .verifyAddedCompAddress(comp.address)
+                    .checkSalesCompSortedByDateSold();
+            });    
+            Sales._FindComps.resetAllFilters();
+            testData.arrayOfCompsForAdditionFromMap2.forEach(comp => {
+                Sales._FindComps.selectCompFromMapByAddress(comp.address) 
+                    .verifyAddedCompAddress(comp.address)
+                    .checkSalesCompSortedByDateSold();
+            });
+            Sales._FindComps.checkSalesCompSortedByDateSold();
+
+            cy.stepInfo(`4. Remove a few comps + Verify that "Date Sold" option in 
                      Sort dropdown is correctly applied after each comp removing`);
+            testData.arrayOfCompsForRemovingFromMap.forEach(comp => {
+                Sales._FindComps.removeCompByAddress(comp.address) 
+                    .verifyCompIsInRemovedSection(comp.address)
+                    .checkSalesCompSortedByDateSold();
+            });
 
+            cy.stepInfo(`5. Add from deleted section + Verify that "Date Sold" option in 
+                     Sort dropdown is correctly applied after each comp adding`);
+            testData.arrayOfCompsForRemovingFromMap.forEach(comp => {
+                Sales._FindComps.addDeletedCompByAddress(comp.address) 
+                    .verifyAddedCompAddress(comp.address)
+                    .checkSalesCompSortedByDateSold();
+            });
 
-
+            cy.stepInfo(`6. Selects Custom sorting, rearranges comps and selects Date Sold sorting again + Verify that 
+                         "Date Sold" option in Sort dropdown is correctly applied after each comp removing`);
+            Sales._FindComps.selectedCompsSetSort(testData.sortSalesCompsCustom)
+                .Page.sortSalesCompsSelectValue.should('contain', testData.sortSalesCompsCustom);
+            Sales._FindComps.moveComparableByDnD(Sales._FindComps.Page.selectorDraggableElement, 2, "down", 4)
+                .moveComparableByDnD(Sales._FindComps.Page.selectorDraggableElement, 4, "down", 5)  
+                .selectedCompsSetSort(testData.sortSalesCompsDateSold)
+                .checkSalesCompSortedByDateSold();
+        });
     });
-});
