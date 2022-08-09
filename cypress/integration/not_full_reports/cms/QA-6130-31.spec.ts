fe587544c6083ecc13662ee3dec2fd63a526c5e3
Viachaslau.Barshchou <Viachaslau.Barshchou@boweryvaluation.com>
2022-08-09 11:37:47 +0300

    [QA-6504] action name fix

--- cypress/integration/not_full_reports/cms/QA-6130-31.spec.ts
+++ cypress/integration/not_full_reports/cms/QA-6130-31.spec.ts
@@ -3,7 +3,7 @@ import { _NavigationSection } from '../../../actions/base';
 import testData from "../../../fixtures/not_full_reports/cms/QA-6130-31.fixture";
 import launchDarklyApi from '../../../api/launchDarkly.api';
 import { conditionalDescribe } from "../../checkIsProd.utils";
-import { _CmsBasePage } from '../../../actions/cms';
+import { _CmsBaseActions } from '../../../actions/cms';
 
 conditionalDescribe("Verify navigation to CMS page and common elements on the page", 
     { tags:[ "@cms", "@feature_flag" ] }, () => {
@@ -26,7 +26,7 @@ conditionalDescribe("Verify navigation to CMS page and common elements on the pa
                         - Globe icon on the far left
                         - 'Global' sub-header in the nav
                         - The bottom panel is removed except the profile photo in the sidebar`);
-            _CmsBasePage.verifyPageIsOpened()
+            _CmsBaseActions.verifyPageIsOpened()
                 .verifyCmsSubHeaderDisplayed()
                 .verifyGlobalIconDisplayed();
             _NavigationSection.verifyGlobalIconTooltip();
