eaf8eb1c034b9f455ca52c5d6c72a0edca97f1df
NikitaRadzkov <Mikita.Radzkou@itechart-group.com>
2022-08-12 16:57:25 +0300

    [QA-6376] Changed name in QA-4466

--- cypress/fixtures/not_full_reports/review_edit/introduction/QA-4466.fixture.ts
+++ cypress/fixtures/not_full_reports/review_edit/introduction/QA-4466.fixture.ts
@@ -5,7 +5,7 @@ import ReportDataCreator from "../../../data_creator/reportData.creator";
 
 
 export default {
-    reportCreationData: ReportDataCreator.getReportData("4291_4424_27-28_32_55-56_59-60_66"),
+    reportCreationData: ReportDataCreator.getReportData("4466"),
     typeValue: "Test values",
     textBoxName: Enums.PAGES_TEXTBOX_NAMES.marketingTimeDescription,
     backLinkName: Enums.INTRODUCTION_TEXTBOX_NAMES.marketingTime,
