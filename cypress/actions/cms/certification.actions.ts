1767ce8d667023aae212a0855b9a61541c9bec47
Viachaslau.Barshchou <Viachaslau.Barshchou@boweryvaluation.com>
2022-08-15 11:52:07 +0300

    [QA-6403] QA-5382 update

--- cypress/actions/cms/certification.actions.ts
+++ cypress/actions/cms/certification.actions.ts
@@ -14,6 +14,49 @@ class CertificationActions extends BaseActionsExt<typeof certificationPage> {
             .should('deep.equal', expectedText);
         return this;
     }
+
+    updateSectionDiscussion(sectionName: ContentManagementSystem.CertificationSections, 
+        text: string, clear = false): CertificationActions {
+        this.clickSectionForEdit(sectionName)
+            .editSectionDiscussionText(sectionName, text, clear);
+
+        this.Page.Header.realClick();
+
+        // Get some time to not overlap comment saving with global save
+        cy.wait(1000);
+        this.saveCmsSettings();
+        return this;
+    }
+
+    clickSectionForEdit(sectionName: ContentManagementSystem.CertificationSections): 
+    CertificationActions {
+        certificationPage.certificationDiscussionSection(sectionName)
+            .scrollIntoView().realClick();
+        return this;
+    }
+
+    editSectionDiscussionText(sectionName: ContentManagementSystem.CertificationSections, 
+        text: string, clear = false): CertificationActions {
+        if (clear) { 
+            this.clickSectionForEdit(sectionName);
+            certificationPage.certificationDiscussionSection(sectionName).clear(); 
+        }
+        certificationPage.certificationDiscussionSection(sectionName)
+            .type(text);
+        this.verifyCertificationBulletsText(sectionName, text);
+        return this;
+    }
+
+    revertSectionToOriginal(sectionName: ContentManagementSystem.CertificationSections): 
+    CertificationActions {
+        this.clickSectionForEdit(sectionName);
+        certificationPage.certificationDiscussionSection(sectionName).type(`{ESC}`);
+        certificationPage.certificationDiscussionSection(sectionName).focus();
+        this.Page.formRevertToOriginalBtn().click();
+        this.Page.formYesRevertBtn.click();
+        this.saveCmsSettings();
+        return this;
+    }
 }
 
 export default new CertificationActions(certificationPage);
