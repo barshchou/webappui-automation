88c28d4c7ed79b983a49b5bc164f512f9cd18e9f
NikitaRadzkov <Mikita.Radzkou@itechart-group.com>
2022-08-16 12:50:07 +0300

    [QA-6456] Added scrollIntoView in QA-4035_38

--- cypress/integration/sf_integration/report/client/QA-4035_38.spec.ts
+++ cypress/integration/sf_integration/report/client/QA-4035_38.spec.ts
@@ -34,7 +34,7 @@ describe(`Verify that the Client field is pre-filled on the WebApp with the date
             cy.stepInfo("5. Verify export name");
             cy.contains("Requested By").next().should("have.text", testData.clientName);
             cy.contains("In accordance with your request").prev()
-                .prev().prev().should("include.text", testData.clientName);
+                .prev().prev().scrollIntoView().should("include.text", testData.clientName);
         });
     });
 
