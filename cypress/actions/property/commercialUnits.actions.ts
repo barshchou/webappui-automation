292d1de6fbfa4d7b5349e4233e76721b90d46904
Viachaslau.Barshchou <Viachaslau.Barshchou@boweryvaluation.com>
2022-08-17 15:37:45 +0300

    [QA-6562] QA-6194 done

--- cypress/actions/property/commercialUnits.actions.ts
+++ cypress/actions/property/commercialUnits.actions.ts
@@ -99,8 +99,13 @@ class CommercialUnitsActions extends BaseActionsExt<typeof commercialUnitsPage> 
         if (isHasDecimalPartMoreNumberOfDigits(squareFeet)) {
             squareFeetToBe = cutDecimalPartToNumberOfDigits(squareFeet);
         }
-        commercialUnitsPage.commercialUnitsSFInputs.eq(index).clear().type(`${squareFeet}`)
-            .should("have.value", squareFeetToBe);
+
+        commercialUnitsPage.commercialUnitsSFInputs.eq(index).clear();
+        ("" + squareFeet).split("").forEach(char => {
+            commercialUnitsPage.commercialUnitsSFInputs.eq(index).type(`${char}`);
+        });
+        
+        commercialUnitsPage.commercialUnitsSFInputs.eq(index).should("have.value", squareFeetToBe);
         return this;
     }
 
