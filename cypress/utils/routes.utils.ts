101eec77bbc6179fc214834d5fb5a8a36f79fdcc
Ernst-D <80358262+Ernst-D@users.noreply.github.com>
2022-08-17 11:48:30 +0400

    Regression - Fix "`cy.click()` failed because this element is detached from the DOM." (#363)

---
+++ cypress/utils/routes.utils.ts
@@ -0,0 +1,9 @@
+const routes =  {
+    commercialInPlaceRentRoll: "/commercial-rent-roll",
+    residentialInPlaceRentRoll: "/residential-rent-roll",
+    propertySummary: "/property-summary",
+    propertyCommercialUnits: "/commercial-units",
+    commercialStabilizedRentRoll: "/commercial-projected-rent-roll"
+} as const;
+
+export default routes;
