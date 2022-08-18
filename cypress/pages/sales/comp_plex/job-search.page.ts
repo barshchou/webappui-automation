d58f562e1b7324dcb6eea3cb03b7e7f0c2627910
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-08-19 00:32:37 +0300

    locators added

--- cypress/pages/sales/comp_plex/job-search.page.ts
+++ cypress/pages/sales/comp_plex/job-search.page.ts
@@ -11,6 +11,10 @@ export default class JobSearchPage {
         return cy.get('[data-qa="filter-completedInPeriod"]');
     }
 
+    get jobSearchFilterInputCompletedIn() {
+        return this.jobSearchFilterCompletedIn.find('input');
+    }
+
     get jobSearchFilterPricePerUnit() {
         return cy.get('[data-qa="filter-pricePerUnit"]');
     }
@@ -19,28 +23,72 @@ export default class JobSearchPage {
         return cy.get('[data-qa="pricePerUnit-min-input"]');
     }
 
+    get jobSearchFilterInputPricePerUnitMin() {
+        return this.jobSearchFilterCommercialUnits.find('input');
+    }
+
     get jobSearchFilterPricePerUnitMax() {
         return cy.get('[data-qa="pricePerUnit-max-input"]');
+    }
+
+    get jobSearchFilterInputPricePerUnitMax() {
+        return this.jobSearchFilterPricePerUnitMax.find('input');
     }
 
     get jobSearchFilterPricePerSF() {
         return cy.get('[data-qa="filter-pricePerSf"]');
     }
 
+    get jobSearchFilterInputMinPricePerSF() {
+        return cy.get('[data-qa="pricePerSF-min-input"]').find('input');
+    }
+
+    get jobSearchFilterInputMaxPricePerSF() {
+        return cy.get('[data-qa="pricePerSF-max-input"]').find('input');
+    }
+
     get jobSearchFilterPropertyType() {
         return cy.get('[data-qa="filter-propertyTypes"]');
+    }
+
+    get jobSearchFilterInputPropertyType() {
+        return this.jobSearchFilterPropertyType.find('input');
     }
 
     get jobSearchFilterResidentialUnits() {
         return cy.get('[data-qa="filter-residentialUnits"]');
     }
 
+    get jobSearchFilterInputResidentialUnitsMin() {
+        return cy.get('[data-qa="residentialUnits-min-input"]').find('input');
+    }
+
+    get jobSearchFilterInputResidentialUnitsMax() {
+        return cy.get('[data-qa="residentialUnits-min-input"]').find('input');
+    }
+
     get jobSearchFilterCommercialUnits() {
         return cy.get('[data-qa="filter-commercialUnits"]');
     }
 
+    get jobSearchFilterInputCommercialUnitsMin() {
+        return cy.get('[data-qa="commercialUnits-min-input"]').find('input');
+    }
+
+    get jobSearchFilterInputCommercialUnitsMax() {
+        return cy.get('[data-qa="commercialUnits-max-input"]').find('input');
+    }
+
     get jobSearchFilterCapRate() {
         return cy.get('[data-qa="filter-capRate"]');
+    }
+
+    get jobSearchFilterInputCapRateMin() {
+        return cy.get('[data-qa="capRate-min-input"]').find('input');
+    }
+
+    get jobSearchFilterInputCapRateMax() {
+        return cy.get('[data-qa="capRate-max-input"]').find('input');
     }
 
     get jobSearchOnAppJobCheckbox() {
@@ -57,5 +105,13 @@ export default class JobSearchPage {
 
     jobSearchGetFilterInput(isMinOrMax:string, filterName: string) {
         return cy.get(`[data-qa="${filterName}-${isMinOrMax}-input"]`);
+    }
+
+    get reportIdInput() {
+        return cy.get('input[placeholder="Find a specific Report ID"]');
+    }
+
+    get reportIdClearButton() {
+        return cy.get('[data-testid="ClearIcon"]');
     }
 }
