3f14bace38a136217a5d723282da2a7d7d935ef9
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-08-19 17:37:47 +0300

    [QA--6613] Done QA-4463

---
+++ cypress/fixtures/not_full_reports/sf_integration/report/appraiser/QA-4463.fixture.ts
@@ -0,0 +1,7 @@
+import ReportDataCreator from "../../../../data_creator/reportData.creator";
+import Enums from "../../../../../enums/enums";
+
+export default {
+    reportData: ReportDataCreator.getReportData(Enums.APPRAISER_SF.appraiserLeadReviewerReviewer2, {}, true),
+    reviewer2: "James Dunne",
+};
