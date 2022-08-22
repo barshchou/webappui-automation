8712357e8dea7d71c28680d27110854cadb9df34
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-08-22 10:12:02 +0300

    [QA-6615] Added new SF Job to enum

--- cypress/enums/salesforceJobs/appraiserSF.enum.ts
+++ cypress/enums/salesforceJobs/appraiserSF.enum.ts
@@ -2,7 +2,8 @@ const appraisersSF = {
     appraiserLeadReviewer: "1764459005",
     appraiserLeadReviewerReviewer2: "2008055387",
     noMatches: "2200019374",
-    appraiserLead: "2100013420"
+    appraiserLead: "2100013420",
+    appraiser: "2100014529"
 } as const;
 
 export default Object.freeze(appraisersSF);
