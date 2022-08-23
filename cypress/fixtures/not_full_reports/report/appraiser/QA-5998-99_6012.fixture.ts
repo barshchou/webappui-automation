4b2b072cd93746141062792e804ac6c0a72bb40b
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-08-23 09:26:44 +0300

    [QA-6621] QA-6012

--- cypress/fixtures/not_full_reports/report/appraiser/QA-5998-99_6012.fixture.ts
+++ cypress/fixtures/not_full_reports/report/appraiser/QA-5998-99_6012.fixture.ts
@@ -2,12 +2,13 @@ import ReportDataCreator from "../../../data_creator/reportData.creator";
 
 
 const reportCreationFixture = () => {
-    return ReportDataCreator.getReportData("5998-99");
+    return ReportDataCreator.getReportData("5998-99_6012");
 };
 
 
 export default {
     reportCreationData: reportCreationFixture(),
     appraiserName: "Matt Roman",
-    inspectorName: "Robocop Inspector"
+    inspectorName: "Robocop Inspector",
+    validationText: "This appraiser / inspector isn't in the system. Please select 'External Inspector' "
 };
--- cypress/integration/not_full_reports/report/appraiser/QA-5998-99_6012.spec.ts
+++ cypress/integration/not_full_reports/report/appraiser/QA-5998-99_6012.spec.ts
@@ -1,7 +1,7 @@
 import { _NavigationSection } from '../../../../actions/base';
 import { createReport } from '../../../../actions/base/baseTest.actions';
 import { Report } from '../../../../actions';
-import testData from "../../../../fixtures/not_full_reports/report/appraiser/QA-5998-99.fixture";
+import testData from "../../../../fixtures/not_full_reports/report/appraiser/QA-5998-99_6012.fixture";
 import { conditionalDescribe } from "../../../checkIsProd.utils";
 
 conditionalDescribe("Add Appraiser / Inspector button functionality", 
@@ -12,7 +12,7 @@ conditionalDescribe("Add Appraiser / Inspector button functionality",
             createReport(testData.reportCreationData);
         });
 
-        it("[QA-5998]", () => {
+        it("[QA-5998-99_6012]", () => {
             cy.stepInfo("1. Navigate to Report -> Appraiser page");
             _NavigationSection.navigateToReportAppraiser();
 
@@ -24,6 +24,13 @@ conditionalDescribe("Add Appraiser / Inspector button functionality",
                         Appraisers table`);
             Report._Appraiser.verifySignCheckbox(testData.appraiserName, true)
                 .verifySignCheckbox(testData.inspectorName, false);
+
+            cy.stepInfo("4. Try to add wrong Bowery Appraiser / Inspector");
+            Report._Appraiser.Page.btnAddAppraiserInspector.click();
+            Report._Appraiser.Page.searchAppraiserTextField.clear()
+                .type(testData.inspectorName).should('have.value', testData.inspectorName);
+            cy.xpath("//*[@data-qa='addAppraiserInspectorModal']/following::*[@role='presentation'][1]")
+                .should("have.text", testData.validationText);
         });
     
     });
