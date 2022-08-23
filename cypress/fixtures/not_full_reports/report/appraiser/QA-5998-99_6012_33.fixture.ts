9a058b43af15c6a206d25fcc90990f795460ea22
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-08-23 15:21:15 +0300

    [QA-6622] Done QA-6033

---
+++ cypress/fixtures/not_full_reports/report/appraiser/QA-5998-99_6012_33.fixture.ts
@@ -0,0 +1,29 @@
+import { getTodayDateString } from "../../../../../utils/date.utils";
+import Enums from "../../../../enums/enums";
+import ReportDataCreator from "../../../data_creator/reportData.creator";
+
+const reportCreationFixture = () => {
+    return ReportDataCreator.getReportData("5998-99_6012", {
+        templateValue: Enums.TEMPLATE_TYPE.notFreddieMac
+    });
+};
+
+const date = getTodayDateString("/", false);
+
+const appraiserName = "Matt Roman";
+
+const inspectorName = "Robocop Inspector";
+
+const certificationInspectionComment = `${inspectorName} has made a personal inspection of the property` + 
+` that is the subject of this report on ${date}. Brandon Gollotti, Robocop Automation and ${appraiserName}` + 
+" have not made a personal inspection of the property that is the subject of this report.";
+
+const validationText = "This appraiser / inspector isn't in the system. Please select 'External Inspector' ";
+
+export default {
+    reportCreationData: reportCreationFixture(),
+    appraiserName,
+    inspectorName,
+    validationText,
+    certificationInspectionComment
+};
---
+++ cypress/integration/not_full_reports/report/appraiser/QA-5998-99_6012_33.export.spec.ts
@@ -0,0 +1,54 @@
+import { _NavigationSection } from '../../../../actions/base';
+import { createReport } from '../../../../actions/base/baseTest.actions';
+import { Report, ReviewExport } from '../../../../actions';
+import testData from "../../../../fixtures/not_full_reports/report/appraiser/QA-5998-99_6012_33.fixture";
+
+describe("Add Appraiser / Inspector button functionality", 
+    { tags:[ "@report", "@appraiser", "@check_export" ] }, () => {
+
+        it("[QA-5998-99_6012]", () => {
+            cy.stepInfo("Precondition: Create report");
+            createReport(testData.reportCreationData);
+
+            cy.stepInfo("1. Navigate to Report -> Appraiser page");
+            _NavigationSection.navigateToReportAppraiser();
+
+            cy.stepInfo("2. Try to add wrong Bowery Appraiser / Inspector");
+            Report._Appraiser.Page.btnAddAppraiserInspector.click();
+            Report._Appraiser.Page.searchAppraiserTextField.clear()
+                .type(testData.inspectorName).should('have.value', testData.inspectorName);
+            Report._Appraiser.Page.hintText.should("have.text", testData.validationText);
+            Report._Appraiser.Page.searchAppraiserTextField.clear().blur();
+            Report._Appraiser.Page.formCancelButton().click();
+
+            cy.stepInfo("3. Add Appraiser and External Inspector");
+            Report._Appraiser.searchAndAddAppraiser(testData.appraiserName)
+                .searchAndAddExternalInspector(testData.inspectorName);
+            
+            cy.stepInfo(`4. Verify that Bowery Appraiser/Inspector and External Inspector were added to 
+                        Appraisers table`);
+            Report._Appraiser.verifySignCheckbox(testData.appraiserName, true)
+                .verifySignCheckbox(testData.inspectorName, false);
+
+            cy.stepInfo("5. Verify generated Certification Inspection comment");
+            Report._Appraiser.Page.certificationInspectionText
+                .should("have.text", testData.certificationInspectionComment);
+
+            cy.stepInfo("6. Export report");
+            _NavigationSection.openReviewAndExport();
+            ReviewExport.generateDocxReport().waitForReportGenerated()
+                .downloadAndConvertDocxReport(testData.reportCreationData.reportNumber);
+        });
+
+        it('Check export', () => {
+            cy.task("getFilePath", { _reportName: testData.reportCreationData.reportNumber, _docxHtml: "html" })
+                .then(file => {
+                    cy.log(<string>file);
+                    cy.visit(<string>file);
+                    cy.stepInfo("6. Verify commentary text in exported report");
+                    cy.xpath("//h1[contains(text(), 'Certification')]").next().next()
+                        .should("include.text", testData.certificationInspectionComment);
+                });
+        });
+    
+    });
