ad271c707840f20478cce1869bfb524b27c38387
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-08-25 15:26:10 +0300

    [QA-6644] Done QA-4668

--- cypress/fixtures/not_full_reports/property/amenities/QA-4662_64-65_67-68.fixture.ts
+++ cypress/fixtures/not_full_reports/property/amenities/QA-4662_64-65_67-68.fixture.ts
@@ -8,17 +8,22 @@ const buildingCheckboxes = allCheckboxes.filter(unit => unit.startsWith("buildin
 const unitCheckboxes = allCheckboxes.filter(unit => unit.startsWith("unit"));
 
 const generatedCommentName = {
-    storageIncomeDiscussion: "storageIncomeDiscussion"
+    storageIncomeDiscussion: "storageIncomeDiscussion",
+    parkingIncomeDiscussion: "parkingIncomeDiscussion"
 };
 
 export default {
-    reportCreationData: ReportDataCreator.getReportData("4626-64-65_67"),
+    reportCreationData: ReportDataCreator.getReportData("4626-64-65_67-68"),
     allCheckboxes,
     buildingCheckboxes,
     unitCheckboxes,
     imagePath: "not_full_reports/Laundry_Room.png",
     storageValue: 1000,
     storageValueMore: 1001,
+    parkingValueMore: 2501,
+    parkingValue: 5,
     testValue: "some value",
-    generatedCommentName
+    generatedCommentName,
+    laundryRoom: "laundryRoom",
+    parking: "parking"
 };
--- cypress/integration/not_full_reports/property/amenities/QA-4662_64-65_67-68.spec.ts
+++ cypress/integration/not_full_reports/property/amenities/QA-4662_64-65_67-68.spec.ts
@@ -2,7 +2,7 @@ import { Income, Property } from "../../../../actions";
 import { _NavigationSection } from "../../../../actions/base";
 import { createReport } from "../../../../actions/base/baseTest.actions";
 import Enums from "../../../../enums/enums";
-import testData from '../../../../fixtures/not_full_reports/property/amenities/QA-4662_64-65_67.fixture';
+import testData from '../../../../fixtures/not_full_reports/property/amenities/QA-4662_64-65_67-68.fixture';
 
 describe("Verify the display of the Amenities page", { tags:[ "@property", "@amenities" ] }, () => {
     before("Login, create report", () => {
@@ -43,27 +43,26 @@ describe("Verify the display of the Amenities page", { tags:[ "@property", "@ame
         Property._Amenities.checkCheckboxByName(Enums.AMENITIES_CHECKBOXES.hasLaundryRoom);
 
         cy.stepInfo("3. Upload photo");
-        Property._Amenities.uploadLaundryRoomImage(testData.imagePath);
+        Property._Amenities.uploadImageByName(testData.laundryRoom, testData.imagePath);
 
         cy.stepInfo("4. Verify functionality of the Rotate button on the uploaded photo");
-        Property._Amenities.rotateLaundryRoomImage();
+        Property._Amenities.rotateImageByName(testData.laundryRoom);
 
         cy.stepInfo("5. Verify functionality of the Delete button on the uploaded photo");
-        Property._Amenities.deleteLaundryRoomImage()
+        Property._Amenities.removeImageByName(testData.laundryRoom)
             .checkCheckboxByName(Enums.AMENITIES_CHECKBOXES.hasLaundryRoom, false);
     });
 
     it("[QA-4667]", () => {
         cy.stepInfo("2. Check Storage Units checkbox");
-        Property._Amenities.checkCheckboxByName(Enums.AMENITIES_CHECKBOXES.hasStorageUnits)
-            .verifyProgressBarNotExist();
+        Property._Amenities.checkCheckboxByName(Enums.AMENITIES_CHECKBOXES.hasStorageUnits);
 
         cy.stepInfo("3. Verify not valid of Storage Units field");
-        Property._Amenities.enterStoreInput(testData.storageValue)
-            .enterStoreInput(testData.testValue);
+        Property._Amenities.enterAmenitiesInput("storageUnitCount", 1000, testData.storageValueMore)
+            .enterAmenitiesInput("storageUnitCount", 1000, testData.testValue);
 
         cy.stepInfo("4. Verify valid value entered in the Number of Storage Units field");
-        Property._Amenities.enterStoreInput(testData.storageValue)
+        Property._Amenities.enterAmenitiesInput("storageUnitCount", 1000, testData.storageValue)
             .verifyProgressBarNotExist();
 
         cy.stepInfo("5. Proceed to the Property > Amenities page and verify generated comment");
@@ -71,9 +70,36 @@ describe("Verify the display of the Amenities page", { tags:[ "@property", "@ame
         Income._MiscellaneousManager.Storage.Page.generatedCommentaryText(
             testData.generatedCommentName.storageIncomeDiscussion)
             .should("include.text", `${testData.storageValue} storage units`);
+    });
 
-        cy.stepInfo("6. Proceed to the Property > Amenities page and uncheck Storage Units");
-        _NavigationSection.navigateToPropertyAmenities();
-        Property._Amenities.checkCheckboxByName(Enums.AMENITIES_CHECKBOXES.hasStorageUnits, false);
+    it("[QA-4668]", () => {
+        cy.stepInfo("2. Check Storage Units checkbox");
+        Property._Amenities.checkCheckboxByName(Enums.AMENITIES_CHECKBOXES.hasParking);
+
+        cy.stepInfo("3. Upload photo");
+        Property._Amenities.uploadImageByName(testData.parking, testData.imagePath);
+
+        cy.stepInfo("4. Verify functionality of the Rotate button on the uploaded photo");
+        Property._Amenities.rotateImageByName(testData.parking);
+
+        cy.stepInfo("5. Verify functionality of the Delete button on the uploaded photo");
+        Property._Amenities.removeImageByName(testData.parking);
+        
+        cy.stepInfo("6. Verify not valid of Parking Spaces field");
+        Property._Amenities.enterAmenitiesInput("parkingSpaceCount", 2500, testData.parkingValueMore)
+            .enterAmenitiesInput("parkingSpaceCount", 2500, testData.testValue);
+
+        cy.stepInfo("7. Verify valid value entered in the Number of Storage Units field");
+        Property._Amenities.enterAmenitiesInput("parkingSpaceCount", 2500, testData.parkingValue)
+            .verifyProgressBarNotExist();
+
+        cy.stepInfo("8. Proceed to the Property > Parking page and verify that the value entered");
+        _NavigationSection.navigateToParking();
+        Income._MiscellaneousManager.Parking.Page.parkingTable.children().then(elems => {
+            expect(elems.length).to.eq( testData.parkingValue);
+        });
+        Income._MiscellaneousManager.Parking.Page.generatedCommentaryText(
+            testData.generatedCommentName.parkingIncomeDiscussion)
+            .should("include.text", `${testData.parkingValue} parking spaces`);
     });
 });
