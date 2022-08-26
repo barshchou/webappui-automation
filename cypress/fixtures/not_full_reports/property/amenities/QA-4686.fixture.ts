f23a86c3733441503140e4851491e497dd63e639
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-08-26 13:34:41 +0300

    [QA-6654] Done QA-4686

---
+++ cypress/fixtures/not_full_reports/property/amenities/QA-4686.fixture.ts
@@ -0,0 +1,16 @@
+import Enums from "../../../../enums/enums";
+import ReportDataCreator from "../../../data_creator/reportData.creator";
+
+const inputs = [
+    {
+        specName: "[QA-4686]",
+        checkboxName: Enums.AMENITIES_CHECKBOXES.hasWasherDryer,
+        uploadName: Enums.AMENITIES_INPUTS.washerDryerUnits
+    },
+];
+
+export default {
+    reportCreationData: ReportDataCreator.getReportData("4686"),
+    enterValue: "Lorem Ipsum is simply dummy text he leap into. 1234567890:?;№!@#$%^&*(){}<>",
+    inputs
+};
