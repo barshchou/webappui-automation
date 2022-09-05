831b21c34d2d35fee16b77b7af61ae9e74fe7bdd
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-05 17:20:15 +0300

    [QA-6707] Added new enum Job

--- cypress/enums/salesforceJobs/marketResearchTypes.enum.ts
+++ cypress/enums/salesforceJobs/marketResearchTypes.enum.ts
@@ -1,7 +1,8 @@
 const marketResearchTypes = {
+    withNeighborhood: "2100014529",
     withNeighborhoodAndArea: "2200016060",
     withoutMultipleNeighborhoodAndArea: "1764459005",
-    withoutEmptyNeighborhoodAndArea: "2010277287"
+    withoutEmptyNeighborhoodAndArea: "2010277287",
 } as const;
 
 export default Object.freeze(marketResearchTypes);
