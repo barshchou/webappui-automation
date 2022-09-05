1771c2e3119a68e3d10c34baab0f6fbe4327f828
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-05 16:36:25 +0300

    [QA-6705] Added SF Jobs enum and Done QA-4821

---
+++ cypress/enums/salesforceJobs/marketResearchTypes.enum.ts
@@ -0,0 +1,5 @@
+const marketResearchTypes = {
+    withNeighborhoodAndArea: "2200016060"
+} as const;
+
+export default Object.freeze(marketResearchTypes);
--- cypress/fixtures/not_full_reports/sf_integration/property/market/QA-4821-22.fixture.ts
+++ cypress/fixtures/not_full_reports/sf_integration/property/market/QA-4821-22.fixture.ts
@@ -2,7 +2,7 @@ import ReportDataCreator from "../../../../data_creator/reportData.creator";
 import Enums from "../../../../../enums/enums";
 
 export default {
-    reportData: ReportDataCreator.getReportData("2200016060", {}, true),
-    neighborhood: "James Dunne",
+    reportData: ReportDataCreator.getReportData(Enums.MARKET_RESEARCH_TYPES.withNeighborhoodAndArea, {}, true),
+    neighborhood: "Gravesend",
     area: "Kings County"
 };
