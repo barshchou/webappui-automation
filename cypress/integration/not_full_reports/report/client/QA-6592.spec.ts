9d669f18bc5ccea7e53a2472791e2fe57b1d9476
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-07 19:31:08 +0300

    [QA-6838] WIP QA-6592

---
+++ cypress/integration/not_full_reports/report/client/QA-6592.spec.ts
@@ -0,0 +1,29 @@
+import { Report } from '../../../../actions';
+import testData from "../../../../fixtures/not_full_reports/report/client/QA-6592.fixture";
+import { createReport } from "../../../../actions/base/baseTest.actions";
+import { _NavigationSection } from "../../../../actions/base";
+
+describe("Verify the display of the Clients column", 
+    { tags: [ "@report", "@client" ] }, () => {
+        beforeEach("Login, create report", () => {
+            createReport(testData.reportCreationData);
+
+            cy.stepInfo("1. Navigate to the Report > Client page");
+            _NavigationSection.navigateToClientPage();
+        });
+
+        it("[QA-6592]", () => {
+            cy.stepInfo("2. Fill the 'Client' field and click 'Add additional client");
+            Report._Client.enterClientName(testData.clientName)
+                .clickAddAdditionalClientBtn();
+
+            cy.stepInfo("3. Fill the Client 2 field with any valid data");
+            Report._Client.enterClientName(testData.clientName, 1);
+
+            cy.stepInfo(`4. Navigate to the Report Dashboard page and verify in 'Clients' column the Client 
+                        organizations are displayed truncated and separated by a comma 
+                        (e.g. “<client1orgname>, <cli…”).`);
+            //TODO: QA-6592 Need add logic
+        });
+
+    });
