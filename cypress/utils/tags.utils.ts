95869d5bc6649997192b4f4a61ee26f1eb238468
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-09 18:08:04 +0300

    [QA-6844] Added new tag

--- cypress/utils/tags.utils.ts
+++ cypress/utils/tags.utils.ts
@@ -55,5 +55,6 @@ export enum Tag {
     unit_inspection = "@unit_inspection",
     source_information = "@source_information",
     expense_history = "@expense_history",
-    cms = "@cms"
+    cms = "@cms",
+    cover_page= "@cover_page"
 }
