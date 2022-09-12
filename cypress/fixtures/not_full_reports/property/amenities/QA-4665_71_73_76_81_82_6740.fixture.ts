0af1e5d48f4b65e7c28634cd4863d1c6b8d555ae
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-12 18:09:41 +0300

    [QA-6876] Done QA-6740

--- cypress/fixtures/not_full_reports/property/amenities/QA-4665_71_73_76_81_82_6740.fixture.ts
+++ cypress/fixtures/not_full_reports/property/amenities/QA-4665_71_73_76_81_82_6740.fixture.ts
@@ -31,11 +31,20 @@ const uploadValues = [
         specName: "[QA-4682]",
         checkboxName: Enums.AMENITIES_CHECKBOXES.hasCommonLoungeSpace,
         uploadName: Enums.AMENITIES_UPLOADS.commonLoungeSpace
+    },
+    {
+        specName: "[QA-6740]",
+        checkboxName: Enums.AMENITIES_CHECKBOXES.hasCourtyard,
+        uploadName: Enums.AMENITIES_UPLOADS.courtyard
     }
 ];
 
 export default {
-    reportCreationData: ReportDataCreator.getReportData("4665_71_73_76_81_82"),
+    reportCreationData: ReportDataCreator.getReportData("4665_71_73_76_81_82_6740", {
+        templateValue: Enums.TEMPLATE_TYPE.freddieMac,
+        incomeValue: Enums.INCOME_TYPE.both,
+        conclusionValue: Enums.VALUE_CONCLUSION_TYPE.AS_COMPLETE,
+    }),
     imagePath: "test_files/Laundry_Room.png",
     uploadValues
 };
