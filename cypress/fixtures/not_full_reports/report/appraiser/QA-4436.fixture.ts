8d1ef721ecbf398aaa0a2cc894d91711b06a32a0
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-14 15:08:07 +0300

    [QA-6895] Done QA-4436

--- cypress/fixtures/not_full_reports/report/appraiser/QA-4436.fixture.ts
+++
@@ -1,5 +0,0 @@
-import ReportDataCreator from "../../../data_creator/reportData.creator";
-
-export default {
-    reportCreationData: ReportDataCreator.getReportData("4436"),
-};
--- cypress/integration/not_full_reports/sf_integration/report/appraiser/QA-4436.spec.ts
+++ cypress/integration/not_full_reports/sf_integration/report/appraiser/QA-4436.spec.ts
@@ -1,39 +1,27 @@
 import { _NavigationSection } from '../../../../../actions/base';
 import { createReport } from '../../../../../actions/base/baseTest.actions';
 import { Report } from '../../../../../actions';
-import testData from "../../../../../fixtures/not_full_reports/report/appraiser/QA-6126-27.fixture";
-import Enums from '../../../../../enums/enums';
-import { normalizeText } from '../../../../../../utils/string.utils';
+import testData from "../../../../../fixtures/not_full_reports/sf_integration/report/appraiser/QA-4436.fixture";
+import launchDarklyApi from '../../../../../api/launchDarkly.api';
 
 describe("Launch Darkly for Inspector (feature in OFF)",
     { tags:[ "@report", "@appraiser", "@salesforce", "@feature_flag" ] }, () => {
 
         beforeEach("Login, create report", () => {
+            launchDarklyApi.setFeatureFlagForUser(testData.featureFlag, testData.offFeatureFlag);
             createReport(testData.reportCreationData);
         });
 
         it("[QA-4436]", () => {
-            
             cy.stepInfo("1. Navigate to Report -> Appraiser page");
             _NavigationSection.navigateToReportAppraiser();
 
-            cy.stepInfo("3. Add External Inspector");
-            Report._Appraiser.searchAndAddExternalInspector(testData.inspectorName);
-            
-            cy.stepInfo(`4. Verify that Bowery Appraiser/Inspector and External Inspector were added to 
-                        Appraisers table`);
-            Report._Appraiser.verifySignCheckbox(testData.inspectorName, false);
+            cy.stepInfo(`2. Verify that when the feature “Prefill inspector from salesforce“ is OFF, 
+                        the Inspector from SF does NOT appear in the Appraisers table`);
+            Report._Appraiser.Page.appraiserSignCheckbox(testData.appraiserName).should("not.exist");
+        });
 
-            cy.stepInfo("5. Verify generated Certification Inspection comment");
-            Report._Appraiser.Page.formCommentTextBox(Enums.PAGES_TEXTBOX_NAMES.certificationInspection).invoke('text')
-                .then(text => {
-                    expect(normalizeText(text)).to.be.eq(testData.certificationInspectionComment);
-                });
-
-            cy.stepInfo("6. Verify generated Certification Assistance comment");
-            Report._Appraiser.Page.formCommentTextBox(Enums.PAGES_TEXTBOX_NAMES.certificationAssistance).invoke("text")
-                .then(text => {
-                    expect(normalizeText(text)).to.eq(testData.certificationAssistanceComment);
-                });
+        afterEach("Remove user form feature flag", () => {
+            launchDarklyApi.removeUserTarget(testData.featureFlag);
         });
     });
