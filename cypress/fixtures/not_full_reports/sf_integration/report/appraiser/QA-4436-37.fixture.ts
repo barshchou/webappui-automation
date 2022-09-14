0d0e4069480402964dc903a82b909b8bd5d9aa06
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-14 17:37:11 +0300

    [QA-6896] Done QA-4437

--- cypress/fixtures/not_full_reports/sf_integration/report/appraiser/QA-4436-37.fixture.ts
+++ cypress/fixtures/not_full_reports/sf_integration/report/appraiser/QA-4436-37.fixture.ts
@@ -1,10 +1,22 @@
 import Enums from "../../../../../enums/enums";
 import ReportDataCreator from "../../../../data_creator/reportData.creator";
 
+const reportData = [
+    {
+        specName: "[QA-4436]",
+        stateFeatureFlag: 1,
+        mather: "not.exist"
+    },
+    {
+        specName: "[QA-4437]",
+        stateFeatureFlag: 0,
+        mather: "exist"
+    }
+];
+
 export default {
     reportCreationData: ReportDataCreator.getReportData(Enums.APPRAISER_SF.inspector, {}, true),
     featureFlag:  Enums.FEATURE_FLAG_KEYS.prefillInspectorFromSalesforce,
-    offFeatureFlag: 1,
-    onFeatureFlag: 0,
+    reportData,
     appraiserName: "Pat Ippolito"
 };
---
+++ cypress/integration/not_full_reports/sf_integration/report/appraiser/QA-4436-37.spec.ts
@@ -0,0 +1,30 @@
+import { _NavigationSection } from '../../../../../actions/base';
+import { createReport } from '../../../../../actions/base/baseTest.actions';
+import { Report } from '../../../../../actions';
+import testData from "../../../../../fixtures/not_full_reports/sf_integration/report/appraiser/QA-4436-37.fixture";
+import launchDarklyApi from '../../../../../api/launchDarkly.api';
+
+describe("Launch Darkly for Inspector (feature in ON/OFF)",
+    { tags:[ "@report", "@appraiser", "@salesforce", "@feature_flag" ] }, () => {
+
+        testData.reportData.forEach(data => {
+            it(`${data.specName}`, () => {
+                cy.stepInfo("Preconditions: Set feature flag. Login, create report");
+                launchDarklyApi.setFeatureFlagForUser(testData.featureFlag, data.stateFeatureFlag);
+                createReport(testData.reportCreationData);
+    
+                cy.stepInfo("1. Navigate to Report -> Appraiser page");
+                _NavigationSection.navigateToReportAppraiser();
+    
+                cy.stepInfo(`2. Verify that when the feature “Prefill inspector from salesforce“ is ON/OFF, 
+                            the Inspector from SF appear/doesn't appear in the Appraisers table`);
+                cy.contains(testData.appraiserName).should(data.mather);
+                if (data.specName === "[QA-4437]") {
+                    Report._Appraiser.verifyPersonallyInspectedCheckbox(testData.appraiserName, true);
+                }
+    
+                cy.stepInfo("3. Remove user form feature flag");
+                launchDarklyApi.removeUserTarget(testData.featureFlag);
+            });
+        });
+    });
