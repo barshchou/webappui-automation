2c5fc547f914a7e16075e79eee0dd45654f01b06
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-09-20 15:26:27 +0300

    [QA-6605] Deleted bug TODO

--- cypress/integration/not_full_reports/property/site_description/QA-4481.export.spec.ts
+++ cypress/integration/not_full_reports/property/site_description/QA-4481.export.spec.ts
@@ -3,8 +3,6 @@ import { createReport } from "../../../../actions/base/baseTest.actions";
 import { _NavigationSection } from "../../../../actions/base";
 import { Property, ReviewExport } from "../../../../actions";
 
-
-// TODO: [QA-6605] Error pulling 'neighborhood' data into a comment 'Location Within Market'
 describe(`Check that generated text pulls in the first submarket`,
     { tags: [ "@property", "@site_description", "@check_export" ] }, () => {
         
