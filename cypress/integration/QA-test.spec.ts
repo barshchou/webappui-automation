5aac847153fed1c52ee0f2e2090408036c3c3877
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-09-20 12:21:09 +0300

    [QA-6347] Added test tag

--- cypress/integration/QA-test.spec.ts
+++ cypress/integration/QA-test.spec.ts
@@ -1,7 +1,7 @@
 import { createReport } from "../actions/base/baseTest.actions";
 import ReportDataCreator from "../fixtures/data_creator/reportData.creator";
 
-describe("Test", () => {
+describe("Test", { tags: [ "@test" ] }, () => {
     it("Test", () => {
         createReport(ReportDataCreator.getReportData("test"));
     });
