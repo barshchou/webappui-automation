183cefc9b1c0c06d0a7fb22bfb5fe171b4a96c24
Uladzislau.Samykou <vlad.samykou@boweryvaluation.com>
2022-09-20 08:48:13 +0300

    Revert "[QA-6347] Added test spec"
    This reverts commit 579442f3a03af56035a3046411e4de2ebdba5bf6.

--- cypress/integration/QA-test.spec.ts
+++
@@ -1,8 +0,0 @@
-import { createReport } from "../actions/base/baseTest.actions";
-import ReportDataCreator from "../fixtures/data_creator/reportData.creator";
-
-describe("Test", () => {
-    it("Test", () => {
-        createReport(ReportDataCreator.getReportData("test"));
-    });
-});
