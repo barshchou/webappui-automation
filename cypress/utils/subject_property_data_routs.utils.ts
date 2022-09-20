dc8183d53da29dd449c719bb5aa189568b71d19d
Ramuald Vishneuski <r.vishneuski@gmail.com>
2022-09-20 16:22:28 +0300

    4721 navigation edited

--- cypress/utils/subject_property_data_routs.utils.ts
+++ cypress/utils/subject_property_data_routs.utils.ts
@@ -2,6 +2,7 @@ import routesUtils from "./routes.utils";
 import Enums from "../enums/enums";
 
 const routes = {
+    subjectProperty: `${routesUtils.subjectPropertyData}`,
     siteDetails: `${routesUtils.subjectPropertyData}#${Enums.SUBJECT_PROPERTY_DATA_SECTIONS.siteDetails}`,
     propertyDescription: `${routesUtils.subjectPropertyData}#${Enums
         .SUBJECT_PROPERTY_DATA_SECTIONS.propertyDescription}`,
