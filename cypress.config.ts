d022cfa7529f8ddd79496e3529458627523b0c8a
Ernst-D <ernst.dzerevanko@gmail.com>
2022-09-21 23:51:55 +0400

    [QA-6962] fixed `retrieveDataFromDb` task

--- cypress.config.ts
+++ cypress.config.ts
@@ -101,8 +101,8 @@ export default defineConfig({
             });
             
             on("task", {
-                async retrieveDataFromDb({ dbUrl, dbName, compPropertyPathInDB, compPropertyValueInDB }) {
-                    return await db.retrieveDataFromDb(dbUrl, dbName, compPropertyPathInDB, compPropertyValueInDB);
+                async retrieveDataFromDb({ dbUrl, dbName, findFilter }) {
+                    return await db.retrieveDataFromDb(dbUrl, dbName, findFilter);
                 }
             });
 
