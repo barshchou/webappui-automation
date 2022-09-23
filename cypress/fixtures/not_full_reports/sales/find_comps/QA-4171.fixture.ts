d0dc215bbc488cd5c0995cabe2d3cde68af1f249
Ernst-D <ernst.dzerevanko@gmail.com>
2022-09-23 11:51:14 +0400

    [fix] lint

--- cypress/fixtures/not_full_reports/sales/find_comps/QA-4171.fixture.ts
+++ cypress/fixtures/not_full_reports/sales/find_comps/QA-4171.fixture.ts
@@ -9,9 +9,7 @@ const comparableFixture = () => {
 };
 
 const compProperty = Enums.COMP_PROPERTIES_PATHS_DB.compPropertyPathsInDB.saleStatus;
-const compStatusContract = Enums.COMP_PROPERTIES_PATHS_DB.saleStatusValuesInDB.inContract;
 const compStatusDate = Enums.COMP_PROPERTIES_PATHS_DB.saleStatusValuesInDB.date;
-const compStatusListing = Enums.COMP_PROPERTIES_PATHS_DB.saleStatusValuesInDB.listing;
 
 const filter: Filter<object> = { $or: [ { [compProperty]:compStatusDate } ] };
 
