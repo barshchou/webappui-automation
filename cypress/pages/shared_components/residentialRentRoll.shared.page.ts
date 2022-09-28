18c41ac9e62556df751faa56bb5cda9cd9a2169e
Ernst-D <ernst.dzerevanko@gmail.com>
2022-09-29 01:48:22 +0400

    [QA-7000] fixed QA-4220

--- cypress/pages/shared_components/residentialRentRoll.shared.page.ts
+++ cypress/pages/shared_components/residentialRentRoll.shared.page.ts
@@ -2,7 +2,7 @@ import BasePage from "../base/base.page";
 
 export default class ResidentialRentRollSharedPage extends BasePage {
 
-    get isInspectedInputs() { return cy.get("[data-qa^=isInspected][data-qa$=cell] input"); }
+    get isInspectedInputs() { return cy.get("[data-qa^=isInspected][data-qa$=cell]"); }
 
     get isInspectedColumnCells() { return cy.get("*[data-qa^='isInspected']"); }
 
